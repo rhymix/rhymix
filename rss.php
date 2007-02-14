ef0f13e80074c8faead9ee9c5965608981299464
zero <skklove@gmail.com>
2007-02-14 06:54:08 +0000

    git-svn-id: http://xe-core.googlecode.com/svn/trunk@21 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- rss.php
+++ rss.php
@@ -1,22 +1,31 @@
 <?php
-  /**
-   * @file   : rss.php
-   * @author : zero <zero@nzeo.com>
-   * @desc   : rss를 출력하기 위한 파일. index.php와 거의 동일하나 act를 dispRss 로 고정시키는 것만 다름
-   **/
+    /**
+     * @file   ./rss.php
+     * @author zero (zero@nzeo.com)
+     * @brief  rss를 출력하기 위한 파일. index.php와 거의 동일하나 act를 dispRss 로 고정시키는 것만 다름
+     **/
 
-  // 필요한 설정 파일들을 include
-  require_once("./config/config.inc.php");
+    /**
+     * @brief 필요한 설정 파일들을 includ
+     **/
+    require_once("./config/config.inc.php");
 
-  // Request Method와 설정값들을 세팅
-  $oContext = &Context::getInstance();
-  $oContext->init();
+    /**
+     * @brief Request Method와 설정값들을 세팅
+     **/
+    $oContext = &Context::getInstance();
+    $oContext->init();
 
-  // act값을 dispRss로 강제 설정
-  // 각 모듈마다 dispRss가 필수적으로 있어야 함
-  Context::set('act', 'dispRss');
+    /**
+     * @brief act값을 dispRss로 강제 설정
+     *
+     * 각 모듈마다 dispRss가 필수적으로 있어야 함
+     **/
+    Context::set('act', 'dispRss');
 
-  // ModuleHandler 호출하여 content 출력
-  $oModuleHandler = new ModuleHandler();
-  $oModule = $oModuleHandler->proc();
+    /**
+     * @brief ModuleHandler 호출하여 content 출력
+     **/
+    $oModuleHandler = new ModuleHandler();
+    $oModule = $oModuleHandler->proc();
 ?>
--- trackback.php
+++ trackback.php
@@ -1,22 +1,31 @@
 <?php
-  /**
-   * @file   : trackback.php
-   * @author : zero <zero@nzeo.com>
-   * @desc   : 트랙백을 받기 위한 파일. 파일이름때문에.. index.php와 거의 동일하나 act를 procReceiveTrackback로 고정
-   **/
+    /**
+     * @file   ./trackback.php
+     * @author zero (zero@nzeo.com)
+     * @brief  트랙백을 받기 위한 파일. 파일이름때문에.. index.php와 거의 동일하나 act를 procReceiveTrackback로 고정
+     **/
 
-  // 필요한 설정 파일들을 include
-  require_once("./config/config.inc.php");
+    /**
+     * @brief 필요한 설정 파일들을 include
+     **/
+    require_once("./config/config.inc.php");
 
-  // Request Method와 설정값들을 세팅
-  $oContext = &Context::getInstance();
-  $oContext->init();
+    /**
+     * @brief Request Method와 설정값들을 세팅
+     **/
+    $oContext = &Context::getInstance();
+    $oContext->init();
 
-  // act값을 procReceiveTrackback로 강제 설정
-  // 각 모듈마다 procReceiveTrackback가 필수적으로 있어야 함
-  Context::set('act', 'procReceiveTrackback');
+    /**
+     * @brief act값을 procReceiveTrackback로 강제 설정 
+     *
+     * 각 모듈마다 procReceiveTrackback가 필수적으로 있어야 함
+     **/
+    Context::set('act', 'procReceiveTrackback');
 
-  // ModuleHandler 호출하여 content 출력
-  $oModuleHandler = new ModuleHandler();
-  $oModule = $oModuleHandler->proc();
+    /**
+     * @brief ModuleHandler 호출하여 content 출력
+     **/
+    $oModuleHandler = new ModuleHandler();
+    $oModule = $oModuleHandler->proc();
 ?>
