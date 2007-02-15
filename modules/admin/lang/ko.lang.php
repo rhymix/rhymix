fedaeabed9e1310b1658584406d33570ccaade17
zero <skklove@gmail.com>
2007-02-15 05:18:56 +0000

    git-svn-id: http://xe-core.googlecode.com/svn/trunk@38 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/admin/lang/ko.lang.php
+++ modules/admin/lang/ko.lang.php
@@ -1,9 +1,9 @@
 <?php
-  /**
-   * @file   : modules/member/lang/ko.lang.php
-   * @author : zero <zero@nzeo.com>
-   * @desc   : 한국어 언어팩 (기본적인 내용만 수록)
-   **/
+    /**
+     * @file   ko.lang.php
+     * @author zero (zero@nzeo.com)
+     * @brief  한국어 언어팩 (기본적인 내용만 수록)
+     **/
 
-  $lang->msg_is_not_administrator = '관리자만 접속이 가능합니다';
+    $lang->msg_is_not_administrator = '관리자만 접속이 가능합니다';
 ?>
