f598c04a93296d43e144e82c3eb854053dc09096
zero <skklove@gmail.com>
2007-02-15 13:49:49 +0000

    git-svn-id: http://xe-core.googlecode.com/svn/trunk@96 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- layouts/sample_layout/sample_layout.layout.php
+++ layouts/sample_layout/sample_layout.layout.php
@@ -1,13 +1,12 @@
 <?php
-
     /**
-    * @class test
+    * @class sample_layout
     * @author zero (zero@nzeo.com)
     * @brief  test 레이아웃
     * @todo 미구현
     **/
 
-    class test {
+    class sample_layout extends LayoutHandler {
         function proc(&$oModule, $oModuleInfo) {
             $oModule->setHtml('레이아웃');
         }
