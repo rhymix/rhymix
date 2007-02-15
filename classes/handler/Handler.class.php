5275f04035f463a496d70d75ea518f6aa56622de
zero <skklove@gmail.com>
2007-02-15 13:34:35 +0000

    git-svn-id: http://xe-core.googlecode.com/svn/trunk@90 201d5d3c-b55e-5fd7-737f-ddc643e51545

---
+++ classes/handler/Handler.class.php
@@ -0,0 +1,17 @@
+<?php
+    /**
+     * @class  Handler
+     * @author zero (zero@nzeo.com)
+     * @brief  *Handler 클래스의 추상화 클래스
+     **/
+
+    class Handler {
+
+        /**
+         * @brief constructor
+         **/
+        function Handler() {
+        }
+
+    }
+?>
