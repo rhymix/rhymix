3275c2aefac239ff89ec85c25dd1955fed51f777
zero <skklove@gmail.com>
2007-02-15 14:20:28 +0000

    git-svn-id: http://xe-core.googlecode.com/svn/trunk@104 201d5d3c-b55e-5fd7-737f-ddc643e51545

---
+++ plugins/rainbow_link/rainbow_link.plugin.php
@@ -0,0 +1,11 @@
+<?php
+    /**
+    * @class rainbow_link
+    * @author zero (zero@nzeo.com)
+    * @brief 링크 마우스 오버시 무지개색으로 보여줌
+    * @todo 미구현
+    **/
+
+    class rainbow_link extends Plugin {
+    }
+?>
