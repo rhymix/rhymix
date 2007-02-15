672c49ab000815ecfb897abf250fe511fab3e840
zero <skklove@gmail.com>
2007-02-15 06:35:50 +0000

    git-svn-id: http://xe-core.googlecode.com/svn/trunk@48 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/module_manager/module_manager.controller.php
+++ modules/module_manager/module_manager.controller.php
@@ -1,11 +1,11 @@
 <?php
-  /**
-   * @file   : modules/module/module_manager.module.php
-   * @author : zero <zero@nzeo.com>
-   * @desc   : 기본 모듈중의 하나
-   *           Module class에서 상속을 받아서 사용
-   *           모듈과 관련된 method들이 존재
-   **/
+    /**
+     * @file   : modules/module/module_manager.module.php
+     * @author : zero <zero@nzeo.com>
+     * @desc   : 기본 모듈중의 하나
+     *           Module class에서 상속을 받아서 사용
+     *           모듈과 관련된 method들이 존재
+     **/
 
   class module_manager extends Module {
 
