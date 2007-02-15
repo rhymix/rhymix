2312b2f057b5fbc8d5053472e57d1da92749b0eb
zero <skklove@gmail.com>
2007-02-15 06:34:23 +0000

    git-svn-id: http://xe-core.googlecode.com/svn/trunk@47 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/module_manager/lang/ko.lang.php
+++ modules/module_manager/lang/ko.lang.php
@@ -1,12 +1,13 @@
 <?php
-  /**
-   * @file   : modules/module_manager/lang/ko.lang.php
-   * @author : zero <zero@nzeo.com>
-   * @desc   : 한국어 언어팩 
-   **/
-  $lang->module_name = "모듈 이름";
-  $lang->module_table_count = "테이블수";
-  $lang->module_date = "제작일";
-  $lang->module_installed_path = "설치경로";
-  $lang->module_version = "버전";
+    /**
+     * @file   ko.lang.php
+     * @author zero (zero@nzeo.com)
+     * @brief  한국어 언어팩 
+     **/
+
+    $lang->module_name = "모듈 이름";
+    $lang->module_table_count = "테이블수";
+    $lang->module_date = "제작일";
+    $lang->module_installed_path = "설치경로";
+    $lang->module_version = "버전";
 ?>
