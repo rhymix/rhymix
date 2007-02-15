3ce41631ea6aa204ec53b93eae2f7548f2145264
zero <skklove@gmail.com>
2007-02-15 09:40:52 +0000

    git-svn-id: http://xe-core.googlecode.com/svn/trunk@73 201d5d3c-b55e-5fd7-737f-ddc643e51545

---
+++ modules/log/log.controller.php
@@ -0,0 +1,16 @@
+<?php
+    /**
+     * @class  logController
+     * @author zero (zero@nzeo.com)
+     * @brief  log 모듈의 controller class
+     * @todo   미구현
+     **/
+
+    class logController extends Module {
+
+        /**
+         * @brief 초기화
+         **/
+
+    }
+?>
