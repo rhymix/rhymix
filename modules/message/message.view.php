0632a4a5da684c68dd674b982949d3d7d10697b4
zero <skklove@gmail.com>
2007-02-15 09:39:13 +0000

    git-svn-id: http://xe-core.googlecode.com/svn/trunk@72 201d5d3c-b55e-5fd7-737f-ddc643e51545

---
+++ modules/message/message.view.php
@@ -0,0 +1,23 @@
+<?php
+    /**
+     * @class  messageView
+     * @author zero (zero@nzeo.com)
+     * @brief  message모듈의 view class
+     **/
+
+    class message extends Module {
+
+        /**
+         * @brief 초기화
+         **/
+        function init() {
+        }
+
+        /**
+         * @brief 메세지 출력 
+         **/
+        function dispMessage() {
+            $this->setTemplateFile('system_message');
+        }
+    }
+?>
