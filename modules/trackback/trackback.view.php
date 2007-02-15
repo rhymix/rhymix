0af812bb4fd6f2210ccf60c497c6ed6499bc06a7
zero <skklove@gmail.com>
2007-02-15 10:00:06 +0000

    git-svn-id: http://xe-core.googlecode.com/svn/trunk@81 201d5d3c-b55e-5fd7-737f-ddc643e51545

---
+++ modules/trackback/trackback.view.php
@@ -0,0 +1,33 @@
+<?php
+    /**
+     * @class  trackbackView
+     * @author zero (zero@nzeo.com)
+     * @desc   trackback모듈의 View class
+     **/
+
+    class trackbackView extends Module {
+
+        /**
+         * @brief 초기화
+         **/
+        function init() {
+        }
+
+        /**
+         * @brief 메세지 출력
+         **/
+        function dispMessage($error, $message) {
+            // 헤더 출력
+            header("Content-Type: text/xml; charset=UTF-8");
+            header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+            header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+            header("Cache-Control: no-store, no-cache, must-revalidate");
+            header("Cache-Control: post-check=0, pre-check=0", false);
+            header("Pragma: no-cache");
+            print '<?xml version="1.0" encoding="utf-8" ?>'."\n";
+            print "<response>\n<error>{$error}</error><message>{$message}</message></response>";
+            exit();
+        }
+
+    }
+?>
