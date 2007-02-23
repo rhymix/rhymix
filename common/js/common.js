feb60a075189d269250587d1e3b545cc8ca08c87
zero <skklove@gmail.com>
2007-02-23 08:55:53 +0000

    git-svn-id: http://xe-core.googlecode.com/svn/trunk@181 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- common/js/common.js
+++ common/js/common.js
@@ -3,28 +3,28 @@
  **/
 
 // string prototype으로 trim 함수 추가
-String.prototype.trim = function() {/*{{{*/
+String.prototype.trim = function() {
   return this.replace(/(^\s*)|(\s*$)/g, "");
-}/*}}}*/
+}
 
 // 주어진 인자가 하나라도 defined되어 있지 않으면 false return
-function isDef() {/*{{{*/
+function isDef() {
   for(var i=0; i<arguments.length; ++i) {
     if(typeof(arguments[i])=='undefined') return false;
   }
   return true;
-}/*}}}*/
+}
 
 // 특정 div(or span...)의 display옵션 토글
-function toggleDisplay(obj, opt) {/*{{{*/
+function toggleDisplay(obj, opt) {
   obj = xGetElementById(obj);
   if(typeof(opt)=='undefined') opt = 'inline';
   if(obj.style.display == 'none') obj.style.display = opt;
   else obj.style.display = 'none';
-}/*}}}*/
+}
 
 // 멀티미디어 출력용 (IE에서 플래쉬/동영상 주변에 점선 생김 방지용)
-function displayMultimedia(type, src, style) {/*{{{*/
+function displayMultimedia(type, src, style) {
   var clsid = '';
   var codebase = '';
   var html = '';
@@ -34,31 +34,23 @@ function displayMultimedia(type, src, style) {/*{{{*/
         clsid = 'clsid:D27CDB6E-AE6D-11cf-96B8-444553540000'; 
         codebase = 'http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.c-ab#version=6,0,29,0'; 
         html = ""+
-          "<script type=\"text/javascript\">"+
-          "document.writeln(\""+
           "<object classid='"+clsid+"' codebase='"+codebase+"' "+style+">"+
           "<param name='movie' value='"+src+"' />"+
           "<param name='quality' value='high' />"+
           "<embed src='"+src+"' autostart='0' "+style+"></embed>"+
-          "<\/object>"+
-          "\");"+
-          "</script>";
+          "<\/object>";
       break;
     default : 
         html = ""+
-          "<script type=\"text/javascript\">"+
-          "document.writeln(\""+
-          "<embed src='"+src+"' autostart='0' "+style+"></embed>"+
-          "\");"+
-          "</script>";
+          "<embed src='"+src+"' autostart='0' "+style+"></embed>";
       break;
   }
 
   document.writeln(html);
-}/*}}}*/
+}
 
 // 화면내에서 이미지 리사이즈 및 클릭할 수 있도록 
-function resizeImageContents() {/*{{{*/
+function resizeImageContents() {
   var objs = xGetElementsByTagName('img');
   for(var i in objs) {
     var obj = objs[i];
@@ -76,10 +68,10 @@ function resizeImageContents() {/*{{{*/
 
     xAddEventListener(obj,'click', resizeImagePopup);
   }
-}/*}}}*/
+}
 xAddEventListener(window, 'load', resizeImageContents);
 
-function resizeImagePopup(evt) {/*{{{*/
+function resizeImagePopup(evt) {
   var e = new xEvent(evt);
   if(!e.target.src) return;
   var obj = e.target;
@@ -100,23 +92,23 @@ function resizeImagePopup(evt) {/*{{{*/
   }
   var popup = window.open(e.target.src,"_imagePopup","width="+width+",height="+height+",top=1,left=1,resizable="+resizable+",toolbars=no,scrollbars="+resizable);
   if(popup) popup.focus();
-}/*}}}*/
+}
 
 // 에디터에서 사용하는 내용 여닫는 코드 (고정)
-function svc_folder_open(id) {/*{{{*/
+function svc_folder_open(id) {
     var open_text_obj = xGetElementById("_folder_open_"+id);
     var close_text_obj = xGetElementById("_folder_close_"+id);
     var folder_obj = xGetElementById("_folder_"+id);
     open_text_obj.style.display = "none";
     close_text_obj.style.display = "block";
     folder_obj.style.display = "block";
-}/*}}}*/
+}
 
-function svc_folder_close(id) {/*{{{*/
+function svc_folder_close(id) {
     var open_text_obj = xGetElementById("_folder_open_"+id);
     var close_text_obj = xGetElementById("_folder_close_"+id);
     var folder_obj = xGetElementById("_folder_"+id);
     open_text_obj.style.display = "block";
     close_text_obj.style.display = "none";
     folder_obj.style.display = "none";
-}/*}}}*/
+}
