591f780345b547eb1d9fd8bae83a5c3cc4dfc77e
zero <skklove@gmail.com>
2007-02-26 02:21:40 +0000

    git-svn-id: http://xe-core.googlecode.com/svn/trunk@189 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/member/tpl.admin/js/admin.js
+++ modules/member/tpl.admin/js/admin.js
@@ -135,3 +135,41 @@ function doShowJoinFormValue(sel_obj) {
       break;
   }
 }
+
+function doEditDefaultValue(obj, cmd) {
+  var listup_obj = xGetElementById('default_value_listup');
+  var item_obj = xGetElementById('default_value_item');
+  var idx = listup_obj.selectedIndex;
+  var lng = listup_obj.options.length;
+  var val = item_obj.value;
+  switch(cmd) {
+    case 'insert' :
+        if(!val) return;
+        var opt = new Option(val, val, false, true);
+        listup_obj.options[listup_obj.length] = opt;
+        item_obj.value = '';
+        item_obj.focus();
+      break;
+    case 'up' :
+        if(lng < 2 || idx<1) return;
+
+        var value1 = listup_obj.options[idx].value;
+        var value2 = listup_obj.options[idx-1].value;
+        listup_obj.options[idx] = new Option(value2,value2,false,false);
+        listup_obj.options[idx-1] = new Option(value1,value1,false,true);
+      break;
+    case 'down' :
+        if(lng < 2 || idx == lng-1) return;
+
+        var value1 = listup_obj.options[idx].value;
+        var value2 = listup_obj.options[idx+1].value;
+        listup_obj.options[idx] = new Option(value2,value2,false,false);
+        listup_obj.options[idx+1] = new Option(value1,value1,false,true);
+      break;
+    case 'delete' :
+        listup_obj.remove(idx);
+        if(idx==0) listup_obj.selectedIndex = 0;
+        else listup_obj.selectedIndex = idx-1;
+      break;
+  }
+}
