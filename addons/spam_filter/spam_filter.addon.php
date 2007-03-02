8360d288057548cfe36096d783c44eed43e50634
zero <skklove@gmail.com>
2007-03-02 06:37:09 +0000

    git-svn-id: http://xe-core.googlecode.com/svn/trunk@211 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- addons/spam_filter/spam_filter.addon.php
+++ addons/spam_filter/spam_filter.addon.php
@@ -1,11 +1,12 @@
 <?php
     /**
-    * @class spam_filter
+    * @file spam_filter.addon.php
     * @author zero (zero@nzeo.com)
     * @brief 스팸필터링 애드온
     * @todo 미구현
+    *
+    * addOn은 ModuleObject 에서 모듈이 불러지기 전/후에 include되는 것으로 실행을 한다.
+    * 즉 별도의 interface가 필요한 것이 아니고 모듈의 일부라고 판단하여 코드를 작성하면 된다.
     **/
 
-    class spam_filter extends AddOn {
-    }
 ?>
