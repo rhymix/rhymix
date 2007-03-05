cafa478c5b0b713bdda2ef11988fcd9919df0e84
zero <skklove@gmail.com>
2007-03-05 10:10:32 +0000

    git-svn-id: http://xe-core.googlecode.com/svn/trunk@249 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/spamfilter/lang/ko.lang.php
+++ modules/spamfilter/lang/ko.lang.php
@@ -16,7 +16,7 @@
     $lang->word = "단어";
 
     // 설명문
-    $lang->about_interval = "지정된 시간내에 다시 글이 등록이 되면 스팸으로 간주가 됩니다";
+    $lang->about_interval = "지정된 시간내에 글을 등록하지 못하게 합니다";
     $lang->about_limit_count = "지정된 시간내에 제한수를 넘겨서 글 작성을 시도하면 스팸으로 인식, IP를 금지 시킵니다";
     $lang->about_denied_ip = "127.0.0.* 와 같이 * 로 정해진 패턴의 IP 대역을 모두 금지 시킬 수 있습니다";
     $lang->about_denied_word = "금지 단어로 등록되면 해당 단어가 있는 글은 등록을 금지 시킬 수 있습니다";
