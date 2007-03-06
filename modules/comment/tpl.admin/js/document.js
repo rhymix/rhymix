847c335901c1896c42a0bf7615eaf169376e4530
zero <skklove@gmail.com>
2007-03-06 02:48:55 +0000

    git-svn-id: http://xe-core.googlecode.com/svn/trunk@271 201d5d3c-b55e-5fd7-737f-ddc643e51545

---
+++ modules/comment/tpl.admin/js/document.js
@@ -0,0 +1,10 @@
+/**
+ * @file   modules/document/tpl.admin/js/board.js
+ * @author zero <zero@nzeo.com>
+ * @biref  board 모듈의 javascript
+ **/
+
+/* 검색 실행 */
+function completeSearch(fo_obj) {
+  fo_obj.submit();
+}
