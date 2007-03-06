5e9d217f353ff18b25ce6856ce4a149d58e34b06
zero <skklove@gmail.com>
2007-03-06 04:01:44 +0000

    git-svn-id: http://xe-core.googlecode.com/svn/trunk@283 201d5d3c-b55e-5fd7-737f-ddc643e51545

---
+++ modules/layout/addon.controller.php
@@ -0,0 +1,74 @@
+<?php
+    /**
+     * @class  addonController
+     * @author zero (zero@nzeo.com)
+     * @brief  addon 모듈의 Controller class
+     **/
+
+    class addonController extends addon {
+
+        /**
+         * @brief 초기화
+         **/
+        function init() {
+        }
+
+        /**
+         * @brief 애드온의 활성/비활성 체인지
+         **/
+        function procToggleActivateAddon() {
+            $oAddonModel = &getModel('addon');
+
+            // addon값을 받아옴
+            $addon = Context::get('addon');
+            if($addon) {
+                // 활성화 되어 있으면 비활성화 시킴
+                if($oAddonModel->isActivatedAddon($addon)) $this->doDeactivate($addon);
+
+                // 비활성화 되어 있으면 활성화 시킴
+                else $this->doActivate($addon);
+            }
+
+            // 모듈에서 애드온을 사용하기 위한 캐시 파일 생성
+            $buff = "";
+            $addon_list = $oAddonModel->getActivatedAddons();
+            $addon_count = count($addon_list);
+            for($i=0;$i<$addon_count;$i++) {
+                $addon = trim($addon_list[$i]);
+                if(!$addon) continue;
+
+                $buff .= sprintf(' include("./addons/%s/%s.addon.php"); ', $addon, $addon);
+            }
+
+            $buff = sprintf('<?if(!__ZB5__)exit(); %s ?>', $buff);
+
+            FileHandler::writeFile($this->cache_file, $buff);
+
+            // 페이지를 애드온 목록으로 이동
+            $this->setRedirectUrl("./?module=admin&act=dispAddonList");
+        }
+
+        /**
+         * @brief 애드온 활성화 
+         *
+         * addons라는 테이블에 애드온의 이름을 등록하는 것으로 활성화를 시키게 된다
+         **/
+        function doActivate($addon) {
+            $oDB = &DB::getInstance();
+            $args->addon = $addon;
+            return $oDB->executeQuery('addon.insertAddon', $args);
+        }
+
+        /**
+         * @brief 애드온 비활성화 
+         *
+         * addons라는 테이블에 애드온의 이름을 제거하는 것으로 비활성화를 시키게 된다
+         **/
+        function doDeactivate($addon) {
+            $oDB = &DB::getInstance();
+            $args->addon = $addon;
+            return $oDB->executeQuery('addon.deleteAddon', $args);
+        }
+
+    }
+?>
---
+++ modules/layout/tpl.admin/js/addon.js
@@ -0,0 +1,9 @@
+/**
+ * @brief 애드온의 활성/비활성 토글용 함수
+ * fo_addon이라는 id를 가지는 form에 인자로 주어진 addon값을 세팅후 실행
+ **/
+function doToggleAddon(addon) {
+  var fo_obj = xGetElementById('fo_addon');
+  fo_obj.addon.value = addon;
+  procFilter(fo_obj, toggle_activate_addon);
+}
