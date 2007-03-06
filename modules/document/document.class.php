0c844db533bb802817fe452c4f3f1c058f904216
zero <skklove@gmail.com>
2007-03-06 01:55:01 +0000

    git-svn-id: http://xe-core.googlecode.com/svn/trunk@266 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/document/document.class.php
+++ modules/document/document.class.php
@@ -10,5 +10,8 @@
         // 공지사항용 값
         var $notice_list_order = -2100000000;
 
+        // 관리자페이지에서 사용할 검색 옵션
+        var $search_option = array('title','content','title_content','user_name','user_id'); ///< 검색 옵션
+
     }
 ?>
--- modules/document/document.view.php
+++ modules/document/document.view.php
@@ -13,5 +13,43 @@
         function init() {
         }
 
+        /**
+         * @brief 목록 출력 (관리자용)
+         **/
+        function dispList() {
+            // 목록을 구하기 위한 옵션
+            $args->page = $page; ///< 페이지
+            $args->list_count = 50; ///< 한페이지에 보여줄 글 수
+            $args->page_count = 10; ///< 페이지 네비게이션에 나타날 페이지의 수
+
+            $args->search_target = Context::get('search_target'); ///< 검색 대상 (title, contents...)
+            $args->search_keyword = Context::get('search_keyword'); ///< 검색어
+
+            $args->sort_index = 'list_order'; ///< 소팅 값
+
+            // 목록 구함, document->getDocumentList 에서 걍 알아서 다 해버리는 구조이다... (아.. 이거 나쁜 버릇인데.. ㅡ.ㅜ 어쩔수 없다)
+            $oDocumentModel = &getModel('document');
+            $output = $oDocumentModel->getDocumentList($args);
+
+            // 템플릿에 쓰기 위해서 document_model::getDocumentList() 의 return object에 있는 값들을 세팅
+            Context::set('total_count', $output->total_count);
+            Context::set('total_page', $output->total_page);
+            Context::set('page', $output->page);
+            Context::set('document_list', $output->data);
+            Context::set('page_navigation', $output->page_navigation);
+
+            // 템플릿에서 사용할 검색옵션 세팅
+            $count_search_option = count($this->search_option);
+            for($i=0;$i<$count_search_option;$i++) {
+                $search_option[$this->search_option[$i]] = Context::getLang($this->search_option[$i]);
+            }
+            Context::set('search_option', $search_option);
+
+
+            // 템플릿 지정
+            $this->setTemplatePath($this->module_path.'tpl.admin');
+            $this->setTemplateFile('document_list');
+        }
+
     }
 ?>
