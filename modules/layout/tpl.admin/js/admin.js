7238803e855d7a3b2b90a667bc4a2f8e2006fae9
zero <skklove@gmail.com>
2007-03-07 07:22:42 +0000

    git-svn-id: http://xe-core.googlecode.com/svn/trunk@296 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/layout/tpl.admin/js/admin.js
+++ modules/layout/tpl.admin/js/admin.js
@@ -1,5 +1,9 @@
 /* 레이아웃 메뉴에를 클릭시 적용할 함수 */
 function doDisplayMenuInfo(menu_id, obj) {
+  // layout, menu_id, node_srl을 추출
+  var fo_obj = xGetElementById("fo_layout");
   var node_srl = obj.getAttribute("node_srl");
-  alert(node_srl);
+  var layout = fo_obj.layout.value;
+
+  // 서버에 요청하여 해당 노드의 정보를 수정할 수 있도록 한다. 
 }
