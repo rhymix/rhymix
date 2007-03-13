b313d5b5c4e70aec10a0e5dd1f11e9cc7e7099bc
zero <skklove@gmail.com>
2007-03-13 02:30:15 +0000

    git-svn-id: http://xe-core.googlecode.com/svn/trunk@385 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/message/message.view.php
+++ modules/message/message.view.php
@@ -11,7 +11,14 @@
          * @brief 초기화
          **/
         function init() {
+            // 설정 정보를 받아옴 (module model 객체를 이용)
+            $oModuleModel = &getModel('module');
+            $config = $oModuleModel->getModuleConfig('rss');
+            if(!$config->skin) $config->skin = 'default';
+            Context::set('skin',$config->skin);
+
             // 템플릿 경로를 지정
+            $template_path = sprintf('%sskins/%s', $this->module_path, $config->skin);
             $this->setTemplatePath($this->module_path.'tpl');
         }
 
@@ -27,12 +34,8 @@
          * @brief 설정 
          **/
         function dispConfig() {
-            // 설정 정보를 받아옴 (module model 객체를 이용)
-            $oModuleModel = &getModel('module');
-            $config = $oModuleModel->getModuleConfig('rss');
-            Context::set('skin',$config);
-
             // 스킨 목록을 구해옴
+            $oModuleModel = &getModel('module');
             $skin_list = $oModuleModel->getskins($this->module_path);
             Context::set('skin_list', $skin_list);
 
