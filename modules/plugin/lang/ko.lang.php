c504ed646fdcf1a03d6b66c4eb7cff4966f124b0
zero <skklove@gmail.com>
2007-03-14 12:49:12 +0000

    git-svn-id: http://xe-core.googlecode.com/svn/trunk@444 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/plugin/lang/ko.lang.php
+++ modules/plugin/lang/ko.lang.php
@@ -12,7 +12,7 @@
     $lang->plugin_history = '변경사항';
     $lang->plugin_info = '플러그인 정보';
     $lang->plugin_code = '코드';
-    $lang->about_plugin_code = '생성된 코드는 제로보드XE 내의 템플릿 내용에 추가하시면 동작하게 됩니다';
+    $lang->about_plugin_code = '선택하신 플러그인에서 요구하는 아래 항목들의 값을 넣고 [코드생성]버튼을 누르시면 제일 아래 칸에 템플릿 파일에 적용할 수 있는 코드가 출력 됩니다';
 
     $lang->msg_plugin_is_not_exists = '%s 플러그인을 찾을 수 없습니다';
     $lang->msg_plugin_object_is_null = '%s 플러그인의 객체 생성을 할 수가 없습니다';
