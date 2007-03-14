395e5f95b2bbbb28279a24210e9361671b71e97c
zero <skklove@gmail.com>
2007-03-14 09:04:09 +0000

    git-svn-id: http://xe-core.googlecode.com/svn/trunk@426 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/plugin/plugin.view.php
+++ modules/plugin/plugin.view.php
@@ -43,9 +43,30 @@
         }
 
         /**
-         * @brief 플러그인의 상세 정보(conf/info.xml)를 팝업 출력
+         * @brief 플러그인의 코드 생성기
          **/
         function dispGenerateCode() {
+            // 선택된 플러그인 정보를 구함
+            $oPluginModel = &getModel('plugin');
+            $plugin_info = $oPluginModel->getPluginInfo(Context::get('selected_plugin'));
+            Context::set('plugin_info', $plugin_info);
+
+            // mid 목록을 가져옴
+            $oModuleModel = &getModel('module');
+            $mid_list = $oModuleModel->getMidList();
+            Context::set('mid_list', $mid_list);
+
+            // 플러그인을 팝업으로 지정
+            $this->setLayoutFile('popup_layout');
+
+            // 템플릿 파일 지정
+            $this->setTemplateFile('plugin_generate_code');
+        }
+
+        /**
+         * @brief 플러그인의 생성된 코드를 출력
+         **/
+        function dispGeneratedCode() {
             // 선택된 플러그인 정보를 구함
             $oPluginModel = &getModel('plugin');
             $plugin_info = $oPluginModel->getPluginInfo(Context::get('selected_plugin'));
