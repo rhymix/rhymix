2acf1afd1a66c42934df176efdb465edd8262220
zero <skklove@gmail.com>
2007-03-15 00:50:00 +0000

    git-svn-id: http://xe-core.googlecode.com/svn/trunk@447 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/pagemaker/lang/ko.lang.php
+++ modules/pagemaker/lang/ko.lang.php
@@ -1,26 +1,7 @@
 <?php
     /**
-     * @file   modules/comment/lang/ko.lang.php
+     * @file   modules/pagemaker/lang/ko.lang.php
      * @author zero <zero@nzeo.com>
-     * @brief  댓글(comment) 모듈의 기본 언어팩
+     * @brief  페이지생성기 (pagemaker) 모듈의 기본 언어팩
      **/
-
-    $lang->module = '모듈';
-
-    $lang->cmd_delete_checked_comment = '선택항목 삭제';
-
-    $lang->msg_cart_is_null = '삭제할 글을 선택해주세요';
-    $lang->msg_checked_comment_is_deleted = '%d개의 댓글이 삭제되었습니다';
-
-    $lang->search_target_list = array(
-        'content' => '내용',
-        'user_id' => '아이디',
-        'user_name' => '이름',
-        'nick_name' => '닉네임',
-        'email_address' => '이메일주소',
-        'homepage' => '홈페이지',
-        'regdate' => '등록일',
-        'last_update' => '최근수정일 ',
-        'ipaddress' => 'IP 주소',
-    );
 ?>
