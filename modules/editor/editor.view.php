db68b3ebfea0a0f75e97af553f4bcc77a4ef5a9a
zero <skklove@gmail.com>
2007-03-16 05:47:30 +0000

    git-svn-id: http://xe-core.googlecode.com/svn/trunk@487 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/editor/editor.view.php
+++ modules/editor/editor.view.php
@@ -17,9 +17,16 @@
          * @brief 에디터를 return
          **/
         function getEditor($upload_target_srl, $allow_fileupload = false) {
+            // 업로드를 위한 변수 설정
             Context::set('upload_target_srl', $upload_target_srl);
             Context::set('allow_fileupload', $allow_fileupload);
 
+            // 에디터 컴포넌트를 구함
+            $component_list = FileHandler::readDir($this->module_path.'components');
+            debugPrint($component_list);
+
+
+            // 템플릿을 미리 컴파일해서 컴파일된 소스를 return
             $tpl_path = $this->module_path.'tpl';
             $tpl_file = 'editor.html';
 
