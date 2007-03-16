312aedcce7b53528ef6de4bec019b8c9f2f263cf
zero <skklove@gmail.com>
2007-03-16 01:48:46 +0000

    git-svn-id: http://xe-core.googlecode.com/svn/trunk@470 201d5d3c-b55e-5fd7-737f-ddc643e51545

---
+++ editor/lang/ko.lang.php
@@ -0,0 +1,60 @@
+<?php
+  /**
+   * @file   : editor/lang/ko.lang.php
+   * @author : zero <zero@nzeo.com>
+   * @desc   : 한국어 언어팩 (기본적인 내용만 수록)
+   **/
+
+  $lang->edit->fontname = '폰트';
+  $lang->edit->fontsize = '크기';
+  $lang->edit->use_paragraph = '문단기능';
+  $lang->edit->fontlist = array(
+    "굴림",
+    "돋움",
+    "바탕",
+    "궁서",
+    "times",
+    "Courier",
+    "Tahoma",
+    "Arial",
+  );
+
+  $lang->edit->image_url = '이미지 경로';
+
+  $lang->edit->multimedia_url = '멀티미디어 경로';
+  $lang->edit->multimedia_width = '가로크기';
+  $lang->edit->multimedia_height = '세로크기';
+
+  $lang->edit->submit = '확인';
+
+  $lang->edit->help_fontcolor = "글자의 색상을 지정합니다";
+  $lang->edit->help_fontbgcolor = "글자의 배경색상을 지정합니다";
+  $lang->edit->help_bold = "글자를 진하게 합니다";
+  $lang->edit->help_italic = "글자를 기울이게 합니다";
+  $lang->edit->help_underline = "밑줄을 긋습니다";
+  $lang->edit->help_strike = "취소선을 긋습니다";
+  $lang->edit->help_add_url= "link를 만듭니다";
+  $lang->edit->help_add_image = "이미지를 추가합니다";
+  $lang->edit->help_add_multimedia = "동영상/플래쉬등을 추가합니다";
+  $lang->edit->help_add_emoticon = "이모티콘을 추가합니다";
+  $lang->edit->help_add_quotation = "글박스를 만들거나 글숨김 기능을 추가합니다";
+  $lang->edit->help_add_html = "html 코드를 직접 입력합니다";
+  $lang->edit->help_add_html = "html 코드를 직접 입력합니다";
+  $lang->edit->help_align_left = "왼쪽 정렬을 합니다";
+  $lang->edit->help_align_center = "가운데 정렬을 합니다";
+  $lang->edit->help_align_right = "오른쪽 정렬을 합니다";
+  $lang->edit->help_add_indent = "들여쓰기를 합니다";
+  $lang->edit->help_remove_indent = "들여쓰기를 제거합니다";
+  $lang->edit->help_use_paragrapth = "정렬, 들여쓰기등의 문단 꾸미기 기능을 사용합니다.\n문단구분자로 P태그가 적용됩니다.\nShift+엔터를 이용하시면
+ BR태그가 적용됩니다.";
+
+  $lang->edit->upload = '첨부';
+  $lang->edit->link_selected = '다운로드 링크 추가';
+  $lang->edit->delete_selected = '선택파일 삭제';
+
+  $lang->edit->icon_align_article = '한 문단을 차지';
+  $lang->edit->icon_align_left = '글의 왼쪽으로';
+  $lang->edit->icon_align_middle = '가운데 정렬';
+  $lang->edit->icon_align_right = '글의 우측으로';
+
+?>
