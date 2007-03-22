0602985e325781118886e2d22fb94c0b166893d6
zero <skklove@gmail.com>
2007-03-22 09:43:47 +0000

    git-svn-id: http://xe-core.googlecode.com/svn/trunk@612 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/editor/components/slide_show/lang/ko.lang.php
+++ modules/editor/components/slide_show/lang/ko.lang.php
@@ -9,5 +9,6 @@
     $lang->width = "가로크기"; 
     $lang->height = "세로크기"; 
     $lang->image_list = "이미지 목록"; 
+    $lang->cmd_make_thumbnail = "썸네일 생성"; 
     $lang->about_image_list = "슬라이드쇼에 추가할 파일을 선택하세요. 선택후 드래그 또는 shift+클릭(범위선택), ctrl+클릭(개별선택) 가능합니다";
 ?>
