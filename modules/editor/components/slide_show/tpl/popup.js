4ebffe05b94a4dae8d7d1ea4728a929f58f91b65
zero <skklove@gmail.com>
2007-03-23 01:56:11 +0000

    git-svn-id: http://xe-core.googlecode.com/svn/trunk@613 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/editor/components/slide_show/tpl/popup.js
+++ modules/editor/components/slide_show/tpl/popup.js
@@ -3,28 +3,84 @@ function getSlideShow() {
     // 부모창이 있는지 체크 
     if(typeof(opener)=="undefined") return;
 
-    // 부모창의 업로드 이미지 목록을 모두 가져와서 세팅 
-    var fo = xGetElementById("fo");
-    var upload_target_srl = fo.upload_target_srl.value;
-    var parent_list_obj = opener.xGetElementById("uploaded_file_list_"+upload_target_srl);
-    var list_obj = xGetElementById("image_list");
-
-    var length = parent_list_obj.length;
-    for(var i=0;i<length;i++) {
-      var opt = new Option(parent_list_obj[i].text, parent_list_obj[i].value, false, false);
-      list_obj.options.add(opt);
-    }
-
     // 부모 위지윅 에디터에서 선택된 영역이 있으면 처리
     var node = opener.editorPrevNode;
-    if(!node || node.nodeName != "DIV") {
-        return;
+    var selected_images = "";
+    if(node && node.nodeName == "DIV") {
+        selected_node = node;
+
+        var width = xWidth(selected_node)-6;
+        var height = xHeight(selected_node)-6;
+        xGetElementById("width").value = width; 
+        xGetElementById("height").value = height; 
+
+        selected_images = xInnerHtml(selected_node);
     }
-    selected_node = node;
+
+    // 부모창의 업로드된 파일중 이미지 목록을 모두 가져와서 세팅 
+    var fo = xGetElementById("fo");
+    var upload_target_srl = fo.upload_target_srl.value;
+
+    var parent_list_obj = opener.xGetElementById("uploaded_file_list_"+upload_target_srl);
+
+    var list_obj = xGetElementById("image_list");
+
+    for(var i=0;i<parent_list_obj.length;i++) {
+        var opt = parent_list_obj.options[i];
+        var file_srl = opt.value;
+        var file_obj = opener.uploaded_files[file_srl];
+        var filename = file_obj.uploaded_filename;
+        if((/(jpg|jpeg|gif|png)$/).test(filename)) {
+            var selected = false;
+            if(selected_images.indexOf(filename)!=-1) selected = true;
+            var opt = new Option(opt.text, opt.value, false, selected);
+            list_obj.options.add(opt);
+        }
+    }
 }
 
-function insertSlideShow(obj) {
+function insertSlideShow() {
     if(typeof(opener)=="undefined") return;
+
+    var list = new Array();
+    var list_obj = xGetElementById("image_list");
+    for(var i=0;i<list_obj.length;i++) {
+        var opt = list_obj.options[i];
+        if(opt.selected) {
+            var file_srl = opt.value;
+            var file_obj = opener.uploaded_files[file_srl];
+            var filename = file_obj.uploaded_filename;
+            list[list.length] = filename;
+        }
+    }
+
+    if(!list.length) {
+        window.close();
+        return;
+    }
+
+    var width = xGetElementById("width").value;
+    var height = xGetElementById("height").value;
+    var images_list = "";
+    for(var i=0; i<list.length;i++) {
+        images_list += list[i]+"\n";
+    }
+
+
+    if(selected_node) {
+        selected_node.setAttribute("width", width);
+        selected_node.setAttribute("height", height);
+        selected_node.style.width = width+"px";
+        selected_node.style.height = height+"px";
+        xInnerHtml(selected_node, images_list);
+    } else {
+        var text = "<div editor_component=\"slide_show\" class=\"editor_component_output\" width=\""+width+"\" height=\""+height+"\" style=\"width:"+width+"px;height:"+height+"px;\" >"+images_list+"</div>";
+        opener.editorFocus(opener.editorPrevSrl);
+        var iframe_obj = opener.editorGetIFrame(opener.editorPrevSrl)
+        opener.editorReplaceHTML(iframe_obj, text);
+    }
+
+    opener.editorFocus(opener.editorPrevSrl);
 
     window.close();
 }
