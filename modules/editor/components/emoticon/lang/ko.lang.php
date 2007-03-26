a6e0e2260e5aaaf4362b2dd47aca8d55c883350c
zero <skklove@gmail.com>
2007-03-26 09:57:07 +0000

    git-svn-id: http://xe-core.googlecode.com/svn/trunk@666 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/editor/components/emoticon/lang/ko.lang.php
+++
@@ -1,9 +0,0 @@
-<?php
-    /**
-     * @file   /modules/editor/components/emoticon/lang/ko.lang.php
-     * @author zero <zero@nzeo.com>
-     * @brief  위지윅에디터(editor) 모듈 > 이모티콘(emoticon) 컴포넌트의 언어팩
-     **/
-
-    $lang->emoticon = "이모티콘";
-?>
--- modules/editor/components/html_editor/lang/ko.lang.php
+++
@@ -1,9 +0,0 @@
-<?php
-    /**
-     * @file   /modules/editor/components/html_editor/lang/ko.lang.php
-     * @author zero <zero@nzeo.com>
-     * @brief  위지윅에디터(editor) 모듈 > HTML편집기(html_editor) 컴포넌트의 언어팩
-     **/
-
-    $lang->html_editor = "HTML 편집"; 
-?>
