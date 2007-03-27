b5e08ad2d9082c69dcd8004a3fed46deaadabfbf
zero <skklove@gmail.com>
2007-03-27 07:28:22 +0000

    git-svn-id: http://xe-core.googlecode.com/svn/trunk@709 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- classes/module/ModuleHandler.class.php
+++ classes/module/ModuleHandler.class.php
@@ -166,6 +166,7 @@
             // 현재 페이지의 정보를 $zbxe_url이라는 변수로 등록 (common/tpl/common_header.tpl에서 javascript 변수로 설정함)
             if($_REQUEST['mid']) $zbxe_url = sprintf('mid=%s',$_REQUEST['mid']);
             elseif($_REQUEST['module']) $zbxe_url = sprintf('module=%s',$_REQUEST['module']);
+            elseif($_REQUEST['document_srl']) $zbxe_url = sprintf('mid=%s', $this->mid);
             else $zbxe_url = Context::getRequestUri();
             Context::set('zbxe_url', $zbxe_url);
 
