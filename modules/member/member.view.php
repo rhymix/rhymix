044e53c88c2880ca6df0957ccfe51b2afc9a85f6
zero <skklove@gmail.com>
2007-03-29 05:37:08 +0000

    git-svn-id: http://xe-core.googlecode.com/svn/trunk@762 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/member/member.view.php
+++ modules/member/member.view.php
@@ -127,7 +127,7 @@
             // 설정 정보를 받아옴 (module model 객체를 이용)
             $oModuleModel = &getModel('module');
             $config = $oModuleModel->getModuleConfig('member');
-            if(!$config->image_name_max_width) $config->image_name_max_width = 80;
+            if(!$config->image_name_max_width) $config->image_name_max_width = 90;
             if(!$config->image_name_max_height) $config->image_name_max_height = 20;
             if(!$config->image_mark_max_width) $config->image_mark_max_width = 20;
             if(!$config->image_mark_max_height) $config->image_mark_max_height = 20;
