409f684e20e3b4b20b40ee437bb3efe464a2ca9f
zero <skklove@gmail.com>
2007-03-30 06:59:10 +0000

    git-svn-id: http://xe-core.googlecode.com/svn/trunk@816 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/page/lang/ko.lang.php
+++ modules/page/lang/ko.lang.php
@@ -1,9 +1,10 @@
 <?php
     /**
-     * @file   modules/pagemaker/lang/ko.lang.php
+     * @file   modules/page/lang/ko.lang.php
      * @author zero <zero@nzeo.com>
-     * @brief  페이지생성기 (pagemaker) 모듈의 기본 언어팩
+     * @brief  페이지생성기 (page) 모듈의 기본 언어팩
      **/
+
     $lang->about_mid = "모듈이름은 http://주소/?mid=모듈이름 처럼 직접 호출할 수 있는 값입니다. (영문+숫자만 가능)";
     $lang->about_category = "분류를 통한 관리를 할 수 있도록 합니다. 모듈 분류의 관리는 <a href=\"./?module=admin&amp;act=dispCategory&amp;mo=module\">모듈관리 > 모듈카테고리</a>에서 하실 수 있습니다.";
     $lang->about_browser_title = "브라우저의 제목에 나타나는 값입니다. RSS/Trackback에서도 사용됩니다.";
