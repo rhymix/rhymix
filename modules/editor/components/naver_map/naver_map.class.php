ff15e869955ac4fe89f85a22044d3e961a8eaf05
zero <skklove@gmail.com>
2007-04-02 02:58:57 +0000

    git-svn-id: http://xe-core.googlecode.com/svn/trunk@868 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/editor/components/naver_map/naver_map.class.php
+++ modules/editor/components/naver_map/naver_map.class.php
@@ -103,7 +103,7 @@
             $marker = $xml_obj->attrs->marker;
             $width = $xml_obj->attrs->width;
             $height = $xml_obj->attrs->height;
-            $body_code = sprintf('<div style="width:%dpx;height:%dpx;margin-bottom:5px;"><iframe src="%s?module=editor&amp;act=procCall&amp;method=displayMap&amp;component=naver_map&amp;width=%s&amp;height=%s&amp;x=%s&amp;y=%s&amp;marker=%s" frameBorder="0" style="padding:1px; border:1px solid #AAAAAA;width:%dpx;height:%dpx;margin:0px;"></iframe></div>', $width, $height, Context::getRequestUri(), $width, $height, $x, $y, $marker, $width, $height);
+            $body_code = sprintf('<div style="width:%dpx;height:%dpx;margin-bottom:5px;"><iframe src="%s?module=editor&amp;act=procEditorCall&amp;method=displayMap&amp;component=naver_map&amp;width=%s&amp;height=%s&amp;x=%s&amp;y=%s&amp;marker=%s" frameBorder="0" style="padding:1px; border:1px solid #AAAAAA;width:%dpx;height:%dpx;margin:0px;"></iframe></div>', $width, $height, Context::getRequestUri(), $width, $height, $x, $y, $marker, $width, $height);
             return $body_code;
         }
 
