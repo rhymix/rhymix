cbdbfddb54805cbcc80b36d240b4b4faf8279880
zero <skklove@gmail.com>
2007-04-02 05:22:35 +0000

    git-svn-id: http://xe-core.googlecode.com/svn/trunk@884 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/member/skins/default/js/member.js
+++ modules/member/skins/default/js/member.js
@@ -5,17 +5,7 @@ function completeInsert(ret_obj, response_tags, args, fo_obj) {
 
     alert(message);
 
-    var url = new Array();
-    var mid = fo_obj.mid.value;
-    var document_srl = fo_obj.document_srl.value;
-    var page = fo_obj.page.value;
-
-    if(mid) url[url.length] = "mid="+mid;
-    if(document_srl) url[url.length] = "document_srl="+document_srl;
-    if(page) url[url.length] = "page="+page;
-
-    if(url.length>0) location.href="./?"+url.join("&");
-    else location.href="./";
+    location.href = location.href.setQuery('act','');
 }
 
 /* 로그인 후 */
