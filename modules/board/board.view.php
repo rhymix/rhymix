70986d34d60e6050d7eb3a4660f0f55809b83742
zero <skklove@gmail.com>
2007-04-02 04:17:23 +0000

    git-svn-id: http://xe-core.googlecode.com/svn/trunk@880 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/board/board.view.php
+++ modules/board/board.view.php
@@ -443,7 +443,7 @@
         function dispBoardAdminBoardInfo() {
 
             // module_srl 값이 없다면 그냥 index 페이지를 보여줌
-            if(!Context::get('module_srl')) return $this->dispAdminContent();
+            if(!Context::get('module_srl')) return $this->dispBoardAdminContent();
 
             // 레이아웃이 정해져 있다면 레이아웃 정보를 추가해줌(layout_title, layout)
             if($this->module_info->layout_srl) {
