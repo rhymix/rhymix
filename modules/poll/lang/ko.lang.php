951c3060b69b76299491ae549b4c841d7f68afae
zero <skklove@gmail.com>
2007-04-06 04:33:44 +0000

    git-svn-id: http://xe-core.googlecode.com/svn/trunk@993 201d5d3c-b55e-5fd7-737f-ddc643e51545

---
+++ modules/poll/lang/ko.lang.php
@@ -0,0 +1,22 @@
+<?php
+    /**
+     * @file   modules/trackback/lang/ko.lang.php
+     * @author zero <zero@nzeo.com>
+     * @brief  엮인글(trackback) 모듈의 기본 언어팩
+     **/
+
+    $lang->cmd_delete_checked_trackback = '선택항목 삭제';
+
+    $lang->msg_cart_is_null = '삭제할 글을 선택해주세요';
+    $lang->msg_checked_trackback_is_deleted = '%d개의 엮인글이 삭제되었습니다';
+
+    $lang->search_target_list = array(
+        'url' => '대상 URL',
+        'blog_name' => '대상 사이트 이름',
+        'title' => '제목',
+        'excerpt' => '내용',
+        'regdate' => '등록일',
+        'ipaddress' => 'IP 주소',
+    );
+
+?>
