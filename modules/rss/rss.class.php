89d7b1e46ab9c58f9758b8e70d13911fe16a8f75
zero <skklove@gmail.com>
2007-04-13 06:22:09 +0000

    git-svn-id: http://xe-core.googlecode.com/svn/trunk@1128 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/rss/rss.class.php
+++ modules/rss/rss.class.php
@@ -17,10 +17,6 @@
          * @brief 설치시 추가 작업이 필요할시 구현
          **/
         function moduleInstall() {
-            // action forward에 등록 (관리자 모드에서 사용하기 위함)
-            $oModuleController = &getController('module');
-            $oModuleController->insertActionForward('rss', 'view', 'dispRssAdminConfig');
-            //$oModuleController->insertActionForward('rss', 'controller', 'dispRssAdminInsertConfig');
 
             return new Object();
         }
