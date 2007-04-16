d08a5b7787a2c8704617e294da3924dbbff15d37
zero <skklove@gmail.com>
2007-04-16 10:23:52 +0000

    git-svn-id: http://xe-core.googlecode.com/svn/trunk@1172 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/menu/tpl/js/menu_admin.js
+++ modules/menu/tpl/js/menu_admin.js
@@ -1,31 +1,40 @@
 /* 메뉴 입력후 */ 
-function completeInsertLayoutMenu(ret_obj) {
-    var menu_id = ret_obj['menu_id'];
-    var xml_file = ret_obj['xml_file'];
-    var menu_title = ret_obj['menu_title'];
+function completeInsertMenu(ret_obj) {
     var menu_srl = ret_obj['menu_srl'];
-
-    if(!xml_file) return;
-    loadTreeMenu(xml_file, menu_id, "menu_zone_"+menu_id, menu_title, doGetMenuInfo, menu_srl, doMoveTree);
-
-    if(!menu_srl) xInnerHtml("menu_zone_info_"+menu_id, "");
+    alert(ret_obj['message']);
+    location.href = location.href.setQuery('act','dispMenuAdminManagement').setQuery('menu_srl',menu_srl);
 } 
 
 /* 메뉴 삭제 */
-function doDeleteLayoutMenu(menu_srl, menu_id) {
-      var fo_obj = xGetElementById("fo_"+menu_id);
+function doDeleteMenu(menu_srl) {
+      var fo_obj = xGetElementById("fo_menu");
       if(!fo_obj) return;
+      fo_obj.menu_srl.value = menu_srl;
+      procFilter(fo_obj, delete_menu);
+}
 
-      procFilter(fo_obj, delete_layout_menu);
+/* 메뉴 삭제 후 */ 
+function completeDeleteMenu(ret_obj) {
+    var menu_srl = ret_obj['menu_srl'];
+    alert(ret_obj['message']);
+    location.href = location.href.setQuery('act','dispMenuAdminContent').setQuery('menu_srl','');
+} 
+
+/* 빈 메뉴 아이템 추가 */
+function doInsertLayoutMenuItem(parent_srl) {
+    if(typeof(parent_srl)=='undefined') parent_srl = 0;
+    var params = {node_srl:0, parent_srl:parent_srl}
+    doGetMenuItemInfo(params);
+    deSelectNode();
 }
 
 /* 메뉴 클릭시 적용할 함수 */
-function doGetMenuInfo(menu_id, obj) {
+function doGetMenuItemInfo(obj) {
     // layout, menu_id, node_srl을 추출
-    var fo_obj = xGetElementById("fo_layout");
-    var layout = fo_obj.layout.value;
+    var fo_obj = xGetElementById("fo_menu");
     var node_srl = 0;
     var parent_srl = 0;
+
     if(typeof(obj.getAttribute)!="undefined") { 
       node_srl = obj.getAttribute("node_srl");
     } else {
@@ -34,15 +43,22 @@ function doGetMenuInfo(menu_id, obj) {
     }
 
     var params = new Array();
-    params["menu_id"] = menu_id;
-    params["layout"] = layout;
     params["menu_srl"] = node_srl;
     params["parent_srl"] = parent_srl;
 
     // 서버에 요청하여 해당 노드의 정보를 수정할 수 있도록 한다. 
-    var response_tags = new Array('error','message','menu_id', 'tpl');
-    exec_xml('layout', 'getLayoutAdminMenuTplInfo', params, completeGetMenuInfo, response_tags, params);
+    var response_tags = new Array('error','message','tpl');
+    exec_xml('menu', 'getMenuAdminTplInfo', params, completeGetMenuItemTplInfo, response_tags, params);
 }
+
+/* 서버로부터 받아온 메뉴 정보를 출력 */
+function completeGetMenuItemTplInfo(ret_obj, response_tags) {
+    var tpl = ret_obj['tpl'];
+    xInnerHtml("menu_zone_info", tpl);
+    var fo_obj = xGetElementById("fo_menu");
+    fo_obj.menu_name.focus();
+}
+
 
 /* 메뉴를 드래그하여 이동한 후 실행할 함수 , 이동하는 node_srl과 대상 node_srl을 받음 */
 function doMoveTree(menu_id, source_node_srl, target_node_srl) {
@@ -75,26 +91,38 @@ function completeMoveLayoutMenu(ret_obj) {
     var menu_title = fo_menu.menu_title.value;
     var tmp = source_node_srl.split('_');
     var menu_srl = tmp[tmp.length-1];
-    loadTreeMenu(xml_file, menu_id, "menu_zone_"+menu_id, menu_title, doGetMenuInfo, menu_srl, doMoveTree);
+    loadTreeMenu(xml_file, menu_id, "menu_zone_"+menu_id, menu_title, doGetMenuItemInfo, menu_srl, doMoveTree);
 }
 
-/* 서버로부터 받아온 메뉴 정보를 출력 */
-function completeGetMenuInfo(ret_obj, response_tags) {
+
+
+
+
+
+
+
+
+
+
+/* 메뉴 아이템 입력후 */ 
+function completeInsertMenuItem(ret_obj) {
     var menu_id = ret_obj['menu_id'];
-    var tpl = ret_obj['tpl'];
-    xInnerHtml("menu_zone_info_"+menu_id, "");
-    xInnerHtml("menu_zone_info_"+menu_id, tpl);
+    var xml_file = ret_obj['xml_file'];
+    var menu_title = ret_obj['menu_title'];
+    var menu_srl = ret_obj['menu_srl'];
 
-    var fo_obj = xGetElementById("fo_"+menu_id);
-    fo_obj.menu_name.focus();
-}
+    if(!xml_file) return;
+    loadTreeMenu(xml_file, menu_id, "menu_zone_"+menu_id, menu_title, doGetMenuItemInfo, menu_srl, doMoveTree);
 
-/* 빈 메뉴 추가시 사용 */
-function doInsertLayoutMenu(menu_id, parent_srl) {
-    if(typeof(parent_srl)=='undefined') parent_srl = 0;
-    var params = {node_srl:0, parent_srl:parent_srl}
-    doGetMenuInfo(menu_id, params);
-    deSelectNode();
+    if(!menu_srl) xInnerHtml("menu_zone_info_"+menu_id, "");
+} 
+
+/* 메뉴 삭제 */
+function doDeleteLayoutMenu(menu_srl, menu_id) {
+      var fo_obj = xGetElementById("fo_"+menu_id);
+      if(!fo_obj) return;
+
+      procFilter(fo_obj, delete_layout_menu);
 }
 
 /* 메뉴 목록 갱신 */
