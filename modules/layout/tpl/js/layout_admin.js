311d0fb0d74618734599cd45d3ddcb3e4e8095cb
zero <skklove@gmail.com>
2007-04-17 02:27:15 +0000

    git-svn-id: http://xe-core.googlecode.com/svn/trunk@1192 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/layout/tpl/js/layout_admin.js
+++ modules/layout/tpl/js/layout_admin.js
@@ -13,3 +13,14 @@ function doDeleteLayout(layout_srl) {
 
     procFilter(fo_obj, delete_layout);
 }
+
+/* 메뉴 관리로 이동 */
+function doMenuManagement(menu_id) {
+    var fo_obj = xGetElementById("fo_layout");
+    var sel_obj = fo_obj[menu_id];
+    if(sel_obj.selectedIndex == 0) return;
+    var menu_srl = sel_obj.options[sel_obj.selectedIndex].value;
+
+    var url = location.href.setQuery('act','dispMenuAdminManagement').setQuery('menu_srl',menu_srl);
+    location.href = url;
+}
