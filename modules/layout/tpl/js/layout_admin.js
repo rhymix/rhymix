ab336f23928a8af6b64a9476ff896010ce4d250d
zero <skklove@gmail.com>
2007-04-25 06:01:51 +0000

    git-svn-id: http://xe-core.googlecode.com/svn/trunk@1319 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/layout/tpl/js/layout_admin.js
+++ modules/layout/tpl/js/layout_admin.js
@@ -14,6 +14,24 @@ function doDeleteLayout(layout_srl) {
     procFilter(fo_obj, delete_layout);
 }
 
+/* 수정된 레이아웃을 원본으로 돌림 */
+function doResetLayoutCode(layout_srl) {
+    var fo_obj = xGetElementById('fo_layout');
+    procFilter(fo_obj, reset_layout_code);
+    return false;
+}
+
+/* 수정중인 레이아웃 미리보기 */
+function doPreviewLayoutCode() {
+    var fo_obj = xGetElementById('fo_layout');
+    fo_obj.target = "_LayoutPreview";
+    fo_obj.act.value = "dispLayoutAdminPreview";
+    fo_obj.submit();
+    fo_obj.act.value = "";
+    fo_obj.target = "";
+}
+
+
 /* 메뉴 관리로 이동 */
 function doMenuManagement(menu_id) {
     var fo_obj = xGetElementById("fo_layout");
