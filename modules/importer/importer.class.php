8b16a0e086c122984eefd1202e498d798ea85646
zero <skklove@gmail.com>
2007-05-14 08:54:56 +0000

    git-svn-id: http://xe-core.googlecode.com/svn/trunk@1404 201d5d3c-b55e-5fd7-737f-ddc643e51545

---
+++ modules/importer/importer.class.php
@@ -0,0 +1,32 @@
+<?php
+    /**
+     * @class  importer
+     * @author zero (zero@nzeo.com)
+     * @brief  importer 모듈의 high class
+     **/
+
+    class importer extends ModuleObject {
+
+        /**
+         * @brief 설치시 추가 작업이 필요할시 구현
+         **/
+        function moduleInstall() {
+            return new Object();
+        }
+
+        /**
+         * @brief 설치가 이상이 없는지 체크하는 method
+         **/
+        function moduleIsInstalled() {
+            return new Object();
+        }
+
+        /**
+         * @brief 업데이트 실행
+         **/
+        function moduleUpdate() {
+            return new Object();
+        }
+
+    }
+?>
