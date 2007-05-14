e1247296dc1399083d0d6e804e5bb21ea6485668
zero <skklove@gmail.com>
2007-05-14 09:14:42 +0000

    git-svn-id: http://xe-core.googlecode.com/svn/trunk@1405 201d5d3c-b55e-5fd7-737f-ddc643e51545

---
+++ modules/importer/importer.controller.php
@@ -0,0 +1,23 @@
+<?php
+    /**
+     * @class  importerController
+     * @author zero (zero@nzeo.com)
+     * @brief  importer 모듈의 Controller class
+     **/
+
+    class importerController extends importer {
+
+        /**
+         * @brief 초기화
+         **/
+        function init() {
+        }
+
+        /**
+         * @brief import
+         **/
+        function procImporterAdminImport() {
+        }
+
+    }
+?>
---
+++ modules/importer/lang/ko.lang.php
@@ -0,0 +1,32 @@
+<?php
+    /**
+     * @file   ko.lang.php
+     * @author zero (zero@nzeo.com)
+     * @brief  Importer(importer) 모듈의 기본 언어팩
+     **/
+
+    // 버튼에 사용되는 언어
+    $lang->cmd_importer_list = 'Importer 목록';
+    $lang->cmd_module_config = 'Importer  공통 설정';
+    $lang->cmd_view_info = 'Importer 정보';
+    $lang->cmd_manage_menu = '메뉴관리';
+    $lang->cmd_make_child = '하위 카테고리 추가';
+    $lang->cmd_enable_move_category = "카테고리 위치 변경 (선택후 위 메뉴를 드래그하세요)";
+    $lang->cmd_remake_cache = '캐시파일 재생성';
+    $lang->cmd_layout_setup = '레이아웃 설정';
+    $lang->cmd_layout_edit = '레이아웃 편집';
+
+    // 항목
+    $lang->parent_category_name = '상위 카테고리명';
+    $lang->category_name = '분류명';
+    $lang->expand = '펼침';
+    $lang->category_group_srls = '그룹제한';
+
+    // 주절 주절..
+    $lang->about_category_name = '카테고리 이름을 입력해주세요';
+    $lang->about_expand = '선택하시면 늘 펼쳐진 상태로 있게 합니다';
+    $lang->about_category_group_srls = '선택하신 그룹만 현재 카테고리가 보이게 됩니다. (xml파일을 직접 열람하면 노출이 됩니다)';
+    $lang->about_layout_setup = 'Importer의 레이아웃 코드를 직접 수정할 수 있습니다. 플러그인 코드를 원하는 곳에 삽입하시거나 관리하세요';
+
+    $lang->about_importer = "Importer를 만드시고 관리할 수 있는 Importer 모듈입니다.\nImporter 모듈은 Importer 스킨에 포함된 레이아웃을 이용하니 생성후 꼭 분류 및 스킨 관리를 통해서 Importer를 꾸미시기 바랍니다.\nImporter내에 다른 게시판을 연결하시고 싶을때에는 메뉴모듈로 메뉴를 만들고 나서 스킨관리에 연결해 주시면 됩니다";
+?>
