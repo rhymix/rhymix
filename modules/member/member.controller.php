15e58c1c30bc1c8efb89a088e08a0aec94e2059f
zero <skklove@gmail.com>
2007-05-31 06:00:02 +0000

    git-svn-id: http://xe-core.googlecode.com/svn/trunk@1518 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/member/member.controller.php
+++ modules/member/member.controller.php
@@ -1369,12 +1369,11 @@
                 // 서명이 없으면 빈 내용을 등록
                 if(!$signature) {
                     $GLOBALS['_transSignatureList'][$member_srl] = $matches[0];
+                    return '';
 
                 // 서명이 있으면 글의 내용 다음에 추가
                 } else {
-
                     $document = $matches[0].'<div class="member_signature">'.$signature.'</div>';
-
                     $GLOBALS['_transSignatureList'][$member_srl] = $document;
                 }
             }
