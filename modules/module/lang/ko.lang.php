fbbad5e79e987ed0ecaa896cb637b69e51552c6a
zero <skklove@gmail.com>
2007-06-26 08:01:10 +0000

    git-svn-id: http://xe-core.googlecode.com/svn/trunk@1771 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/module/lang/ko.lang.php
+++ modules/module/lang/ko.lang.php
@@ -49,7 +49,7 @@
     $lang->about_browser_title = "브라우저의 제목에 나타나는 값입니다. RSS/Trackback에서도 사용됩니다.";
     $lang->about_mid = "모듈이름은 http://주소/?mid=모듈이름 처럼 직접 호출할 수 있는 값입니다. (영문+숫자+_ 만 가능)";
     $lang->about_default = "선택하시면 사이트에 mid값 없이 접속하였을 경우 기본으로 보여줍니다";
-    $lang->about_module_category = "분류를 통한 관리를 할 수 있도록 합니다. 모듈 분류의 관리는 <a href=\"./?module=admin&amp;act=dispCategory&amp;mo=module\">모듈관리 > 모듈분류 </a>에서 하실 수 있습니다.";
+    $lang->about_module_category = "분류를 통한 관리를 할 수 있도록 합니다. 모듈 분류의 관리는 <a href=\"./?module=admin&amp;act=dispModuleAdminCategory\">모듈관리 > 모듈분류 </a>에서 하실 수 있습니다.";
     $lang->about_description= '관리용으로 사용되는 설명입니다';
     $lang->about_default = '선택하시면 사이트에 mid값 없이 접속하였을 경우 기본으로 보여줍니다';
     $lang->about_header_text = '모듈의 상단에 표시되는 내용입니다 (html 태그 사용 가능)';
