732f87cd360552ea7327bddc812bad2d847b116c
zero <skklove@gmail.com>
2007-06-27 11:22:41 +0000

    git-svn-id: http://xe-core.googlecode.com/svn/trunk@1810 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- classes/context/Context.class.php
+++ classes/context/Context.class.php
@@ -92,6 +92,14 @@
                 $this->_set('logged_info', NULL);
             }
 
+            //////////////////
+            //$logged_info->is_admin = 'Y';
+            //$_SESSION['logged_info'] = $logged_info;
+            //$_SESSION['is_logged'] = true;
+            //$this->_set('is_logged', true);
+            //$this->_set('logged_info', $logged_info);
+            //////////////////
+
             // rewrite 모듈사용 상태 체크
             if(file_exists('./.htaccess')&&in_array('mod_rewrite',apache_get_modules())) $this->allow_rewrite = true;
 
