aa9b51fcac6a7bdc533aac6346fd18609c48079a
zero <skklove@gmail.com>
2007-07-13 08:34:23 +0000

    언어 선택 위젯 추가
    git-svn-id: http://xe-core.googlecode.com/svn/trunk@1936 201d5d3c-b55e-5fd7-737f-ddc643e51545

---
+++ widgets/language_select/language_select.class.php
@@ -0,0 +1,27 @@
+<?php
+    /**
+     * @class styx_clock
+     * @author styx (styx@bystyx.com)
+     * @brief 플래시 시계 출력
+     * @version 0.1
+     **/
+
+    class language_select extends WidgetHandler {
+
+        /**
+         * @brief 위젯의 실행 부분
+         *
+         * ./widgets/위젯/conf/info.xml 에 선언한 extra_vars를 args로 받는다
+         * 결과를 만든후 print가 아니라 return 해주어야 한다
+         **/
+        function proc($args) {
+            // 템플릿의 스킨 경로를 지정 (skin, colorset에 따른 값을 설정)
+            $tpl_path = sprintf('%sskins/%s', $this->widget_path, $args->skin);
+            $tpl_file = 'language_select';
+
+            // 템플릿 컴파일
+            $oTemplate = &TemplateHandler::getInstance();
+            return $oTemplate->compile($tpl_path, $tpl_file);
+        }
+    }
+?>
