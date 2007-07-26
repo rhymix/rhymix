1dfad7bb76d8544833b1190f7bd00991bf0daf32
zero <skklove@gmail.com>
2007-07-26 04:53:56 +0000

    swf파일을 display할때 allowScriptAccess param 추가
    git-svn-id: http://xe-core.googlecode.com/svn/trunk@2024 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- common/js/common.js
+++ common/js/common.js
@@ -150,6 +150,7 @@ function displayMultimedia(src, width, height, auto_start) {
         html = ""+
             "<object classid=\""+clsid+"\" codebase=\""+codebase+"\" width=\""+width+"\" height=\""+height+"\" >"+
             "<param name=\"wmode\" value=\"transparent\" />"+
+            "<param name=\"allowScriptAccess\" value=\"always\" />"+
             "<param name=\"movie\" value=\""+src+"\" />"+
             "<param name=\"quality\" value=\"high\" />"+
             "<embed src=\""+src+"\" autostart=\""+auto_start+"\"  width=\""+width+"\" height=\""+height+"\"></embed>"+
