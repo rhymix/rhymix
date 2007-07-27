29d8e91db06164d8b8bd150bd9f4f7a088bc7cc1
zero <skklove@gmail.com>
2007-07-27 11:38:59 +0000

    영어 번역 적용. by 나이스투님
    git-svn-id: http://xe-core.googlecode.com/svn/trunk@2065 201d5d3c-b55e-5fd7-737f-ddc643e51545

---
+++ modules/integration_search/lang/en.lang.php
@@ -0,0 +1,14 @@
+<?php
+    /**
+     * @file   modules/integration_search/lang/ko.lang.php
+     * @author zero <zero@nzeo.com>
+     * @brief  English Language Pack (For only basic things)
+     **/
+
+    $lang->integration_search = "Integration Search";
+
+    $lang->sample_code = "Sample Code";
+    $lang->about_target_module = "Only chosen modules are the target. Please be careful on setting authority";
+    $lang->about_sample_code = "You can add integration search on layout by adding code above";
+    $lang->msg_no_keyword = "Input keyword to search";
+?>
