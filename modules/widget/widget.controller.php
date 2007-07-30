a7cafa8c20aef128cd51099e625d371a3cb602f8
zero <skklove@gmail.com>
2007-07-30 08:32:02 +0000

    위젯 코드 생성기의 가로 크기 부분 수정
    git-svn-id: http://xe-core.googlecode.com/svn/trunk@2084 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/widget/widget.controller.php
+++ modules/widget/widget.controller.php
@@ -50,8 +50,8 @@
 
                 $style .= sprintf("%s:%s%s;", "width", trim($vars->widget_width), $widget_width_type);
 
-                //if($vars->widget_position) $style .= sprintf("%s:%s;", "float", trim($vars->widget_position));
-                //else $style .= "float:left;";
+                if($vars->widget_position) $style .= sprintf("%s:%s;", "float", trim($vars->widget_position));
+                else $style .= "float:left;";
                 $widget_code = sprintf('<img src="%s" class="zbxe_widget_output" widget="%s" %s style="%s" />', $blank_img_path, $widget, implode(' ',$attribute), $style);
             } else {
                 $widget_code = sprintf('<img width="%s" height="100" src="%s" class="zbxe_widget_output" style="%s" widget="%s" %s />', "100%", $blank_img_path, $style, $widget, implode(' ',$attribute));
