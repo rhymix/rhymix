a81a813f3f2d9da2285263039c3df13dc1268a97
kagami <kagami@201d5d3c-b55e-5fd7-737f-ddc643e51545>
2007-07-31 11:29:34 +0000

    프랑스어 추가
    git-svn-id: http://xe-core.googlecode.com/svn/trunk@2101 201d5d3c-b55e-5fd7-737f-ddc643e51545

---
+++ common/lang/fr.lang.php
@@ -0,0 +1,215 @@
+<?php
+    /**
+     * @file   common/lang/fr.lang.php
+     * @author zero (zero@nzeo.com)
+     * @brief  Paquet Francais de Langue (mots de base seulement) 
+     **/
+
+    // 	verbes et mots de base decrivant des actions 
+    $lang->cmd_write = 'Ecrire';
+    $lang->cmd_reply = 'Répondre';
+    $lang->cmd_delete = 'Supprimer';
+    $lang->cmd_modify = 'Modifier';
+    $lang->cmd_edit = 'Editer';
+    $lang->cmd_view = 'Regarder';
+    $lang->cmd_view_all = 'Regarder Tous';
+    $lang->cmd_list = 'Liste';
+    $lang->cmd_prev = 'Précédent ';
+    $lang->cmd_next = 'Après';
+    $lang->cmd_send_trackback = 'Envoyer Trackback';
+    $lang->cmd_registration = $lang->cmd_submit = 'Soumettre';
+    $lang->cmd_comment_registration = 'Ajouter le Commentaire';
+    $lang->cmd_insert = 'Insérer';
+    $lang->cmd_save = 'Economiser';
+    $lang->cmd_input = 'Entrer';
+    $lang->cmd_search = 'Rechercher';
+    $lang->cmd_cancel = 'Decommander';
+    $lang->cmd_back = 'Retour';
+    $lang->cmd_vote = 'Voter';
+    $lang->cmd_move = 'Deplacer';
+    $lang->cmd_move_up = 'Relever';
+    $lang->cmd_move_down = 'Abaisser';
+    $lang->cmd_add_indent = 'Ajouter le Creux';
+    $lang->cmd_remove_indent = 'Enlever le Creux';
+    $lang->cmd_management = 'Gestion';
+    $lang->cmd_make = 'Creer';
+    $lang->cmd_select = 'Choisir';
+    $lang->cmd_select_all = 'Choisir Tous';
+    $lang->cmd_unselect_all = 'Ne pas Selectionner Tous';
+    $lang->cmd_close_all = 'Cloturer Tous';
+    $lang->cmd_open_all = 'Ouvrir Tous';
+    $lang->cmd_reload = 'Recharger';
+    $lang->cmd_close = 'Fermer';
+    $lang->cmd_open = 'Cloturer';
+    $lang->cmd_setup = 'Configurer';
+    $lang->cmd_apply = 'Appliquer';
+    $lang->cmd_open_calendar = 'Choisir la Date';
+    $lang->cmd_send = 'Envoyer';
+    $lang->cmd_print = 'Imprimer';
+
+    $lang->enable = 'Permettre';
+    $lang->disable = 'Neutraliser';
+
+    // Mots essentiels
+    $lang->no = 'numéro';
+    $lang->notice = 'Notification';
+    $lang->secret = 'Secret';
+    $lang->category = 'Categorie';
+    $lang->document_srl = 'numéro du Document';
+    $lang->user_id = 'Pseudo';
+    $lang->author = 'Auteur';
+    $lang->password = 'Mot de Passe';
+    $lang->password1 = 'Mot de Passe';
+    $lang->password2 = 'Confirmer le Mot de Passe';
+    $lang->admin_id = 'Pseudo d\'Administrateur';
+    $lang->writer = 'Auteur';
+    $lang->user_name = 'Nom d\'utilisateur';
+    $lang->nick_name = 'Surnom';
+    $lang->email_address = 'E-mail';
+    $lang->homepage = 'Site Web';
+    $lang->blog = 'Blog';
+    $lang->birthday = 'Anniversaire';
+    $lang->browser_title = 'Titre de Navigateur';
+    $lang->title = 'Titre';
+    $lang->title_content = 'Titre + Contenu';
+    $lang->content = 'Contenu';
+    $lang->document = 'Article';
+    $lang->comment = 'Commentaire';
+    $lang->description = 'Description';
+    $lang->trackback = 'Trackback';
+    $lang->tag = 'Tag';
+    $lang->allow_comment = 'Permettre les Commentaires';
+    $lang->lock_comment = 'Rejeter les Commentaires';
+    $lang->allow_trackback = 'Permettre la Trackback';
+    $lang->uploaded_file = 'Dossier Téléchargé';
+    $lang->grant = 'Autorité';
+    $lang->target = 'Cible';
+    $lang->total = 'Total';
+    $lang->total_count = 'Comptage Total';
+    $lang->ipaddress = 'IP Address';
+    $lang->path = 'Chemin';
+    $lang->cart = 'Article Choisi';
+    $lang->friend = 'Les Amis';
+
+    $lang->mid = 'Titre de Module';
+    $lang->layout = 'Disposition';
+    $lang->widget = 'Widget';
+    $lang->module = 'Module';
+    $lang->skin = 'Thème';
+    $lang->colorset = 'Arrangement de couleur';
+    $lang->extra_vars = 'Variables Supplementaires';
+
+    $lang->document_url = 'URL du Document';
+    $lang->trackback_url = 'URL de la Trackback';
+    $lang->blog_name = 'Nom du Blog';
+    $lang->excerpt = 'Citation';
+
+    $lang->document_count = 'Nombre Total des Articles';
+    $lang->page_count = 'Nombre de Pages';
+    $lang->list_count = 'Nombre de Listes';
+    $lang->readed_count = 'Nombre de Fois Lues';
+    $lang->voted_count = 'Nombre de Voix';
+    $lang->member_count = 'Nombre de Membres';
+    $lang->date = 'Date';
+    $lang->regdate = 'Date d\'Enregistrement';
+    $lang->last_update = 'La Dernière Mise a Jour';
+    $lang->signup_date = 'Date d\'Enregistrement';
+    $lang->last_login = 'La Dernière Ouverture';
+    $lang->first_page = 'Première Page';
+    $lang->last_page = 'Dernière Page';
+    $lang->search_target = 'Cible de Recherche';
+    $lang->search_keyword = 'Mot-clé de Recherche';
+    $lang->is_default = 'défaut';
+
+    $lang->no_documents = 'Aucunes Entrées';
+
+    $lang->board_manager = 'Arrangements des Conseils';
+    $lang->member_manager = 'Arrangements des Membres';
+    $lang->layout_manager = 'Arrangements de Dispositions';
+
+    $lang->use = 'Utiliser';
+    $lang->notuse = 'Ne pas Utiliser';
+    $lang->not_exists = 'n\'Existe Pas';
+
+    $lang->unit_sec = 'sec';
+    $lang->unit_min = 'min';
+    $lang->unit_hour = 'heur';
+    $lang->unit_day = 'jour';
+    $lang->unit_week = 'semaine';
+    $lang->unit_month = 'mois';
+    $lang->unit_year = 'année';;
+
+    // Descriptions et information
+    $lang->about_tag = 'Quand l\'insertion des tags, insérent une virgule(,) entre chaque tags te permet d\'entrer les tags multiples.';
+    $lang->about_layout = 'es dispositions décorent l\'aspect d\'un module. Vous pouvez les configurer du menu de disposition ci-dessus.';
+
+    // Messages et alertes
+    $lang->msg_call_server = 'En demandant le serveur pour information, attendre svp.';
+    $lang->msg_db_not_setted = 'Les informations de DB n\'ont pas été placees.';
+    $lang->msg_invalid_queryid = 'La valeur indiquee d\'identification de question est inadmissi.';
+    $lang->msg_not_permitted = 'Vous n\'etes pas autorises a continuer.';
+    $lang->msg_input_password = 'Veuillez entrer votre mot de passe.';
+    $lang->msg_invalid_document = 'Vous avez demandé pour un article inadmissible.';
+    $lang->msg_invalid_request = 'Votre demande est inadmissible.';
+    $lang->msg_invalid_password = 'Vous avez entré un mot de passe inadmissible.';
+    $lang->msg_error_occured = 'Une erreur s\'est produite.';
+    $lang->msg_not_founded = 'La cible demandée n\'a pas pu être trouvée.';
+    $lang->msg_no_result = 'Aucun Resultat';
+
+    $lang->msg_not_permitted_act = 'Vous n\'êtes pas autorisés pour exécuter l\'action priée.';
+    $lang->msg_module_is_not_exists = 'Le module demandé n\'a pas pu être trouvé.';
+    $lang->msg_module_is_not_standalone = 'Le module demandé ne peut pas être exécuté indépendamment.';
+
+    $lang->success_registed = 'L\'enregistrement a été avec succès exécuté.';
+    $lang->success_updated = 'Avec succès mis a jour.';
+    $lang->success_deleted = 'Avec succès supprime';
+    $lang->success_voted = 'Avec succès voté';
+    $lang->success_moved = 'Avec succès deplacé';
+    $lang->success_sended = 'Avec succès envoyé';
+    $lang->success_reset = 'Avec succès remisé';
+    $lang->success_leaved = 'Avec succès fait cécession';
+
+    $lang->fail_to_delete = 'N\'a pas pu être supprimé.';
+    $lang->fail_to_move = 'N\'a pas pu être déplacé';
+
+    $lang->failed_voted = 'La demande de voter a échoué';
+    $lang->fail_to_delete_have_children = 'Une ou plusieurs reponses existent et ne pourraient pas être supprimees ainsi';
+
+    $lang->confirm_submit = 'Etes-vous sur vous voulez-vous soumettre?';
+    $lang->confirm_logout = 'Etes-vous sur vous voulez-vous se deconnecter?';
+    $lang->confirm_vote = 'Etes-vous sur vous voulez-vous voter?';
+    $lang->confirm_delete = 'Etes-vous sur vous voulez-vous supprimer?';
+    $lang->confirm_move = 'Etes-vous sur vous voulez-vous deplacer ceci?';
+    $lang->confirm_reset = 'Etes-vous sur vous voulez-vous remettre a zero ceci?';
+    $lang->confirm_leave = 'Etes-vous sur vous voulez-vous partir?';
+
+    $lang->column_type = 'Type de Colonne';
+    $lang->column_type_list['text'] = 'un-ligne texte';
+    $lang->column_type_list['homepage'] = 'url';
+    $lang->column_type_list['email_address'] = 'e-mail';
+    $lang->column_type_list['tel'] = 'numéro de telephone';
+    $lang->column_type_list['textarea'] = 'secteur multiligne des textes';
+    $lang->column_type_list['checkbox'] = 'boite de controle (choix multiple)';
+    $lang->column_type_list['select'] = 'boite choisie (choix simple)';
+    $lang->column_type_list['kr_zip'] = 'code postal (coréen)';
+    $lang->column_type_list['date'] = 'date (yyyy/mm/dd)';
+    //$lang->column_type_list['jp_zip'] = 'code postal (japonais)';
+    $lang->column_name = 'Nom de colonne';
+    $lang->column_title = 'Titre de colonne';
+    $lang->default_value = 'Valeur par défaut';
+    $lang->is_active = 'Actif';
+    $lang->is_required = 'Champ requis';
+
+    // Messages alertes pour le Javascript a l'aide des filtres de XML
+    $lang->filter->isnull = 'Entrer une valeur pour %s';
+    $lang->filter->outofrange = 'Aligner la longueur des textes de %s';
+    $lang->filter->equalto = "La valeur de %s est inadmissible.";
+    $lang->filter->invalid_email = "La format de %s est inadmissible. ex) zbxe@zeroboard.com";
+    $lang->filter->invalid_user_id = $lang->filter->invalid_userid = "La format de %s est inadmissible.\\nToutes les valeurs devraient se composer des alphabets, des nombres ou du soulignage (_) avec la premiere lettre etant un caractere de l'alphabet.";
+    $lang->filter->invalid_homepage = "La format de %s est inadmissible. ex) http://www.zeroboard.com";
+    $lang->filter->invalid_korean = "La format de %s est inadmissible. On permet seulement le coréen";
+    $lang->filter->invalid_korean_number = "La format de %s est inadmissible. Svp caracteres ou nombres coréens d'entrée seulement.";
+    $lang->filter->invalid_alpha = "La format de %s est inadmissible. Svp alphabets d'entrée seulement.";
+    $lang->filter->invalid_alpha_number = "La format de %s est inadmissible. Svp alphabets ou nombres d'entrée seulement.";
+    $lang->filter->invalid_number = "La format de %s est inadmissible. Svp l'entrée numérote seulement.";
+?>
