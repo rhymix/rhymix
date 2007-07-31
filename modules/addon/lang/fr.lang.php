1a6c773f6eaac98a2f8bb3ff883b376c168156c7
kagami <kagami@201d5d3c-b55e-5fd7-737f-ddc643e51545>
2007-07-31 11:33:47 +0000

    프랑스어 번역본 추가
    git-svn-id: http://xe-core.googlecode.com/svn/trunk@2102 201d5d3c-b55e-5fd7-737f-ddc643e51545

---
+++ modules/addon/lang/fr.lang.php
@@ -0,0 +1,15 @@
+<?php
+    /**
+     * @file   fr.lang.php
+     * @author zero (zero@nzeo.com)
+     * @brief  Paquet Francais de Langue
+     **/
+
+    $lang->addon = "Modules";
+
+    $lang->addon_info = 'Un resume de ce module';
+    $lang->addon_maker = 'Le createur de ce module';
+    $lang->addon_history = 'L\'histoire de ce module';
+
+    $lang->about_addon = 'Les operations de service de modules plutot que le HTML reel results.<br/>Simply basculant n\'importe quel module te permet en marche et en arret d\'employer les dispositifs utiles.';
+?>
