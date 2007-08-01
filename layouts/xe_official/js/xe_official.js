14a63031f8b97a08e1ede69cd9e621b45e81940f
zero <skklove@gmail.com>
2007-08-01 08:10:37 +0000

    XE 공식 레이아웃/ 로그인위젯/ 최근글 위젯 추가
    git-svn-id: http://xe-core.googlecode.com/svn/trunk@2112 201d5d3c-b55e-5fd7-737f-ddc643e51545

---
+++ layouts/xe_official/js/xe_official.js
@@ -0,0 +1,37 @@
+// Hide And Show Toggle
+cc=0
+function hideShow(id)
+{
+if (cc==0) 
+{
+cc=1
+document.getElementById(id).style.display="none";
+}
+else
+{
+cc=0
+document.getElementById(id).style.display="block";
+}
+}
+
+// Show And Hide Toggle
+cc=0
+function showHide(id)
+{
+if (cc==0) 
+{
+cc=1
+document.getElementById(id).style.display="block";
+}
+else
+{
+cc=0
+document.getElementById(id).style.display="none";
+}
+}
+
+// Local Navigation Toggle
+function lnbToggle(id) {
+	for(num=1; num<=3; num++) document.getElementById('D3MG'+num).style.display='none'; //D4MG1~D4MG3 까지 숨긴 다음
+	document.getElementById(id).style.display='block'; //해당 ID만 보임
+}
