14f5dc30bdecd828194c61276ec0b474f8e772b9
FruitsHake <FruitsHake@201d5d3c-b55e-5fd7-737f-ddc643e51545>
2007-08-01 15:38:24 +0000

    Board에서 Guestbook으로 변경.
    git-svn-id: http://xe-core.googlecode.com/svn/trunk@2128 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/guestbook/lang/en.lang.php
+++ modules/guestbook/lang/en.lang.php
@@ -2,16 +2,15 @@
     /**
      * @file   en.lang.php
      * @author zero (zero@nzeo.com)
-     * @brief  board modules's basic language pack
+     * @brief  Guestbook modules's basic language pack
      **/
 
-    $lang->board = "board"; 
+    $lang->board = "Guestbook"; 
 
-    // words used in button
-    $lang->cmd_board_list = 'Board List';
-    $lang->cmd_module_config = 'Common Board Configuration';
-    $lang->cmd_view_info = 'Board Info';
-    $lang->move_target_module = "Target module for changing position";
+    // Words used in button
+    $lang->cmd_board_list = 'Guestbook List';
+    $lang->cmd_module_config = 'Common Guestbook Configuration';
+    $lang->cmd_view_info = 'Guestbook Info';
 
-    $lang->about_board = "This module is used for creating and managing boards.\nSelect the module's name from the list after creating one to configurate specifically.\nBe careful with board's module name, since it will be the url. (ex : http://domain/zb/?mid=modulename)";
+    $lang->about_board = "This module is used for creating and managing guestbooks.\nSelect the module's name from the list after creating one to configurate specifically.\nBe careful with guestbook's module name, since it will be the url. (ex : http://domain/zb/?mid=modulename)";
 ?>
