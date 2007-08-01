16f08e4d266a5317851941e7fb72eb5dec221a7f
kagami <kagami@201d5d3c-b55e-5fd7-737f-ddc643e51545>
2007-08-01 10:17:33 +0000

    module/add-on 단어 교체
    git-svn-id: http://xe-core.googlecode.com/svn/trunk@2120 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/addon/lang/fr.lang.php
+++ modules/addon/lang/fr.lang.php
@@ -5,11 +5,11 @@
      * @brief  Paquet Francais de Langue
      **/
 
-    $lang->addon = "Modules";
+    $lang->addon = "Adjonctions";
 
-    $lang->addon_info = 'Un resume de ce module';
-    $lang->addon_maker = 'Le createur de ce module';
-    $lang->addon_history = 'L\'histoire de ce module';
+    $lang->addon_info = 'Un resume de ce adjonction';
+    $lang->addon_maker = 'Le createur de ce adjonction';
+    $lang->addon_history = 'L\'histoire de ce adjonction';
 
-    $lang->about_addon = 'Les operations de service de modules plutot que le HTML reel results.<br/>Simply basculant n\'importe quel module te permet en marche et en arret d\'employer les dispositifs utiles.';
+    $lang->about_addon = 'Les operations de service de adjonction plutot que le HTML reel results.<br/>Simply basculant n\'importe quel adjonction te permet en marche et en arret d\'employer les dispositifs utiles.';
 ?>
