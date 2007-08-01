ca491334442628a77f7dc5b275adafc5e40267a8
FruitsHake <FruitsHake@201d5d3c-b55e-5fd7-737f-ddc643e51545>
2007-08-01 15:26:58 +0000

    몇몇가지 단어등 수정.
    git-svn-id: http://xe-core.googlecode.com/svn/trunk@2127 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/blog/lang/en.lang.php
+++ modules/blog/lang/en.lang.php
@@ -2,21 +2,21 @@
     /**
      * @file   en.lang.php
      * @author zero (zero@nzeo.com)
-     * @brief  basic language pack for blog module
+     * @brief  Basic language pack for blog module
      **/
 
-    // words used in button
+    // Words used in button
     $lang->cmd_blog_list = 'Blog list';
     $lang->cmd_module_config = 'Common blog setting';
     $lang->cmd_view_info = 'Blog info';
     $lang->cmd_manage_menu = 'Menu management';
     $lang->cmd_make_child = 'Add child category';
-    $lang->cmd_enable_move_category = "Change category position (Drag the menu on top after selection)";
-    $lang->cmd_remake_cache = 'Rebuild cache files';
+    $lang->cmd_enable_move_category = "Change category position (Drag the top menu after selection)";
+    $lang->cmd_remake_cache = 'Rebuild cache file';
     $lang->cmd_layout_setup = 'Configure layout';
     $lang->cmd_layout_edit = 'Edit layout';
 
-    // item
+    // Item
     $lang->parent_category_name = 'Parent category';
     $lang->category_name = 'Category';
     $lang->expand = 'Expand';
@@ -27,7 +27,7 @@
     $lang->about_category_name = 'Input category name';
     $lang->about_expand = 'By selecting this option, it will be always expanded';
     $lang->about_category_group_srls = 'Only the selected group will be able to see current categories. (Manually open xml file to expose)';
-    $lang->about_layout_setup = 'You can manually modify blog layout code. Insert or manage the widget code whereever you want';
+    $lang->about_layout_setup = 'You can manually modify blog layout code. Insert or manage the widget code anywhere you want';
     $lang->about_blog_category = 'You can make blog categories.<br />When blog category is broken, try rebuilding the cache file manually.';
-    $lang->about_blog = "This module is for creating and managing a blog.\nBlog module uses the skin that is included in the layout. So after creating one, always use category and skin management to decorate your blog.\nIf you want to connect other boards inside the blog, use the menu module to create a menu and then connect it with the skin manager";
+    $lang->about_blog = "This is a blog module that can create and manage blog.\nAfter creating a blog, please decorate your blog by category and skin management because this blog module uses layout that is included in the blog skin.\nIf you want to connect other boards inside the blog, use the menu module to create a menu and then connect it with the skin manager";
 ?>
