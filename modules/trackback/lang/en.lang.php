aebdefdeef968e2b34bd3fbf0425101578240bde
FruitsHake <FruitsHake@201d5d3c-b55e-5fd7-737f-ddc643e51545>
2007-08-06 13:06:51 +0000

    기본번역본 수정
    git-svn-id: http://xe-core.googlecode.com/svn/trunk@2221 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/trackback/lang/en.lang.php
+++ modules/trackback/lang/en.lang.php
@@ -5,7 +5,7 @@
      * @brief  trackback module / basic language pack
      **/
 
-    $lang->cmd_delete_checked_trackback = 'Delete checked trackback(s)';
+    $lang->cmd_delete_checked_trackback = 'Delete selected trackback(s)';
 
     $lang->msg_cart_is_null = 'Please select a trackback to be deleted.';
     $lang->msg_checked_trackback_is_deleted = '%d trackback(s) deleted.';
