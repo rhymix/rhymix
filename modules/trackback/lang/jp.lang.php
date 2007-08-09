0d653267ebab8c6b60b38fda4b635646e1fe83ea
risapapa <risapapa@201d5d3c-b55e-5fd7-737f-ddc643e51545>
2007-08-09 16:02:25 +0000

    Added Japanese Translation.
    git-svn-id: http://xe-core.googlecode.com/svn/trunk@2285 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/trackback/lang/jp.lang.php
+++ modules/trackback/lang/jp.lang.php
@@ -19,6 +19,6 @@
         'ipaddress' => 'IPアドレス',
     );
 
-    $lang->enable_trackback = "엮인글 사용";
-    $lang->about_enable_trackback = "체크를 해제하시면 사이트내에 모든 글의 엮인글 접수를 중지합니다";
+    $lang->enable_trackback = "トラックバック使用";
+    $lang->about_enable_trackback = "チェックを外すと、すべてのコンテンツに対してトラックバックの受信を中止します。";
 ?>
