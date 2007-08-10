fef8ca75514186f3580119e1542a30ab30030731
zero <skklove@gmail.com>
2007-08-10 05:26:26 +0000

    통합검색의 다른 언어파일에 추가된 언어문자열 추가
    git-svn-id: http://xe-core.googlecode.com/svn/trunk@2296 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/integration_search/lang/jp.lang.php
+++ modules/integration_search/lang/jp.lang.php
@@ -11,4 +11,20 @@
     $lang->about_target_module = "選択されたモジュールのみを検索対象とします。権限設定に注意してください。";
     $lang->about_sample_code = "上のコードをレイアウトなどに追加すると統合検索が可能になります。";
     $lang->msg_no_keyword = "検索語を入力してください。";
+
+    $lang->is_result_text = "<strong>'%s'</strong> 에 대한 검색결과 <strong>%d</strong>건";
+
+    $lang->is_search_option = array(
+        'title' => '제목',
+        'content' => '내용',
+        'title_content' => '제목+내용',
+        //'comment' => '댓글',
+    );
+
+    $lang->is_sort_option = array(
+        'regdate' => '등록일',
+        'comment_count' => '댓글수',
+        'readed_count' => '조회수',
+        'voted_count' => '추천수',
+    );
 ?>
--- modules/integration_search/lang/zh-CN.lang.php
+++ modules/integration_search/lang/zh-CN.lang.php
@@ -10,4 +10,20 @@
     $lang->sample_code = "代码";
     $lang->about_target_module = "只把被选模块作为搜索对象。请注意权限设置。";
     $lang->about_sample_code = "可把上述代码插入到相应布局当中即可实现搜索功能。";
+
+    $lang->is_result_text = "<strong>'%s'</strong> 에 대한 검색결과 <strong>%d</strong>건";
+
+    $lang->is_search_option = array(
+        'title' => '제목',
+        'content' => '내용',
+        'title_content' => '제목+내용',
+        //'comment' => '댓글',
+    );
+
+    $lang->is_sort_option = array(
+        'regdate' => '등록일',
+        'comment_count' => '댓글수',
+        'readed_count' => '조회수',
+        'voted_count' => '추천수',
+    );
 ?>
