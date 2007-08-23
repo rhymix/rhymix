ac1d927df14bf1ac550649ce4f01f9fdb4506bc7
FruitsHake <FruitsHake@201d5d3c-b55e-5fd7-737f-ddc643e51545>
2007-08-23 19:28:04 +0000

    번역본 수정
    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@2399 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/addon/lang/en.lang.php
+++ modules/addon/lang/en.lang.php
@@ -11,5 +11,5 @@
     $lang->addon_maker = 'Author of this Addon';
     $lang->addon_history = 'Addon History';
 
-    $lang->about_addon = 'Addon is for controling actions rather than displaying the HTML results.<br />Simply by toggling any addons you want on or off,  you can use very useful functions to administer your website';
+    $lang->about_addon = 'Addon is for controlling actions rather than displaying the HTML results.<br />Simply by toggling any addons you want on or off,  you can use very useful functions to administer your website';
 ?>
