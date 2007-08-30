626d49c5021b2129969efa0be10ef41479a61249
sanghunjun <sanghunjun@201d5d3c-b55e-5fd7-737f-ddc643e51545>
2007-08-30 23:06:16 +0000

    스페인어 수정.
    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@2423 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- common/lang/es.lang.php
+++ common/lang/es.lang.php
@@ -1,75 +1,75 @@
 <?php
     /**
-     * @file   common/lang/en.lang.php
-     * @author zero (zero@nzeo.com)
-     * @brief  English Language Pack (Only basic words are included here)
+     * @archivo   common/lang/es.lang.php
+     * @autor zero (zero@nzeo.com)
+     * @sumario Paquete del Idioma Español (incluye sólo las palabras básicas )
      **/
 
-    // words for action, which is basically used
+    // palabras para la acción, las cuales son basicamente usados
     $lang->cmd_write = 'Escribir';
-    $lang->cmd_reply = 'Contestación';
-    $lang->cmd_delete = 'Cancelación';
+    $lang->cmd_reply = 'Respuesta';
+    $lang->cmd_delete = 'Borrar';
     $lang->cmd_modify = 'Modificar';
-    $lang->cmd_edit = 'Corregir';
+    $lang->cmd_edit = 'editar';
     $lang->cmd_view = 'Ver';
-    $lang->cmd_view_all = 'Ver Toda';
-    $lang->cmd_list = 'List';
-    $lang->cmd_prev = 'Prev';
-    $lang->cmd_next = 'Adelante';
+    $lang->cmd_view_all = 'Ver todo';
+    $lang->cmd_list = 'Lista';
+    $lang->cmd_prev = 'Anterior';
+    $lang->cmd_next = 'Siguiente';
     $lang->cmd_send_trackback = 'Enviar Trackback';
-    $lang->cmd_registration = $lang->cmd_submit = 'Someter';
-    $lang->cmd_comment_registration = 'Agregar El Commentario';
+    $lang->cmd_registration = $lang->cmd_submit = 'Enviar';
+    $lang->cmd_comment_registration = 'Agregar el commentario';
     $lang->cmd_insert = 'Insertar';
     $lang->cmd_save = 'Guardar';
-    $lang->cmd_input = 'Entrdada';
-    $lang->cmd_search = 'Búsqueda';
+    $lang->cmd_input = 'Ingresar';
+    $lang->cmd_search = 'Buscar';
     $lang->cmd_cancel = 'Cancelar';
-    $lang->cmd_back = 'Ir Detrás';
-    $lang->cmd_vote = 'Votar';
+    $lang->cmd_back = 'Atrás';
+    $lang->cmd_vote = 'Recomendar';
     $lang->cmd_move = 'Mover';
     $lang->cmd_move_up = 'Arriba';
     $lang->cmd_move_down = 'Abajo';
-    $lang->cmd_add_indent = 'Agregar Pedido';
-    $lang->cmd_remove_indent = 'Eliminar Pedido';
-    $lang->cmd_management = 'Manejar';
+    $lang->cmd_add_indent = 'Agregar';
+    $lang->cmd_remove_indent = 'Eliminar';
+    $lang->cmd_management = 'Administración';
     $lang->cmd_make = 'Crear';
     $lang->cmd_select = 'Seleccionar';
-    $lang->cmd_select_all = 'Seleccionar Todos';
-    $lang->cmd_unselect_all = 'Deseleccionar Todos';
-    $lang->cmd_close_all = 'Cerrar Todos';
-    $lang->cmd_open_all = 'Abrir Todos';
+    $lang->cmd_select_all = 'Seleccionar todo';
+    $lang->cmd_unselect_all = 'Cancelar todo';
+    $lang->cmd_close_all = 'Cerrar todo';
+    $lang->cmd_open_all = 'Abrir todo';
     $lang->cmd_reload = 'Actualizar';
     $lang->cmd_close = 'Cerrar';
     $lang->cmd_open = 'Abrir';
     $lang->cmd_setup = 'Configuración';
 	$lang->cmd_option = 'Opción';
-    $lang->cmd_apply = 'Applicar';
-    $lang->cmd_open_calendar = 'Seleccionar la fecha';
+    $lang->cmd_apply = 'Aplicar';
+    $lang->cmd_open_calendar = 'Abrir el calendario';
     $lang->cmd_send = 'Enviar';
-    $lang->cmd_print = 'Print';
+    $lang->cmd_print = 'Imprimir';
     $lang->cmd_scrap = 'Scrap';
-    $lang->cmd_preview = 'Preview';
-    $lang->cmd_reset = 'Reset';
+    $lang->cmd_preview = 'Previo';
+    $lang->cmd_reset = 'Rehacer';
 
-    $lang->enable = 'Enable';
-    $lang->disable = 'Disable';
+    $lang->enable = 'Activar';
+    $lang->disable = 'Desactivar';
 
-    // Essencial Words
+    // Palabras Esenciales
     $lang->no = 'No';
     $lang->notice = 'Aviso';
     $lang->secret = 'Secreto';
     $lang->category = 'Categoría';
-    $lang->document_srl = 'Doc.Num';
+    $lang->document_srl = 'Núm.Mensaje';
     $lang->user_id = 'Usuario';
     $lang->author = 'Autor';
     $lang->password = 'Contraseña';
     $lang->password1 = 'Contraseña';
-    $lang->password2 = 'Confirmar la Contraseña';
-    $lang->admin_id = 'Identificación Del Admin';
-    $lang->writer = 'Escritor';
-    $lang->user_name = 'Nombre Del Usuario';
-    $lang->nick_name = 'Nombre De la Mella';
-    $lang->email_address = 'Correo Electrónico';
+    $lang->password2 = 'Confirmar la contraseña';
+    $lang->admin_id = 'Identificación del Administrador';
+    $lang->writer = 'Autor';
+    $lang->user_name = 'Nombre De Usuario';
+    $lang->nick_name = 'Apodo';
+    $lang->email_address = 'Correo electrónico';
     $lang->homepage = 'Página web';
     $lang->blog = 'Blog';
     $lang->birthday = 'Fecha de Nacimiento';
@@ -77,125 +77,125 @@
     $lang->title = 'Título';
     $lang->title_content = 'Título+Contenido';
     $lang->content = 'Contenido';
-    $lang->document = 'Entrada';
+    $lang->document = 'Mensaje';
     $lang->comment = 'Commentario';
     $lang->description = 'Descripción';
     $lang->trackback = 'Trackback';
     $lang->tag = 'Etiqueta';
     $lang->allow_comment = 'Permitir Commentarios';
-    $lang->lock_comment = 'Cerrar Commentarios';
+    $lang->lock_comment = 'Bloquear Commentarios';
     $lang->allow_trackback = 'Permitir Trackback';
-    $lang->uploaded_file = 'Ajuntos';
-    $lang->grant = 'Autoridad';
+    $lang->uploaded_file = 'Archivos Ajuntos';
+    $lang->grant = 'Atribución';
     $lang->target = 'Objetivo';
     $lang->total = 'Total';
-    $lang->total_count = 'Visitos en Total';
+    $lang->total_count = 'Núm.Total';
     $lang->ipaddress = 'Dirección IP';
     $lang->path = 'Paso';
-    $lang->cart = 'Artículo Selecciónado';
+    $lang->cart = 'Artículo Seleccionado';
     $lang->friend = 'Amigos';
-    $lang->notify = 'Notify';
+    $lang->notify = 'Notificación';
 
-    $lang->mid = 'Module Name';
-    $lang->layout = 'Layout';
-    $lang->widget = 'Widget';
+    $lang->mid = 'Nombre del Módulo';
+    $lang->layout = 'Diseño';
+    $lang->widget = 'Herramienta';
     $lang->module = 'Módulo';
     $lang->skin = 'Tema';
-    $lang->colorset = 'Colección de colores';
+    $lang->colorset = 'Colores';
     $lang->extra_vars = 'Variables Extra';
 
-    $lang->document_url = 'Entrada URL';
-    $lang->trackback_url = 'Trackback URL';
+    $lang->document_url = 'URL del Mensaje';
+    $lang->trackback_url = 'URL del Trackback';
     $lang->blog_name = 'Titulo del Blog';
-    $lang->excerpt = 'Quotación';
+    $lang->excerpt = 'Extracción';
 
-    $lang->document_count = 'Numeros de la Entradas';
-    $lang->page_count = 'Cuentas de las Pagínas';
-    $lang->list_count = 'Cuentas del Listos';
-    $lang->readed_count = 'Cuentas Referidos';
-    $lang->voted_count = 'Cuentas Votadas';
-    $lang->member_count = 'Cuentas de Miembros';
+    $lang->document_count = 'Cantidad del Mensaje';
+    $lang->page_count = 'Cantidad de la Página';
+    $lang->list_count = 'Cantidad de la Lista';
+    $lang->readed_count = 'Leídos';
+    $lang->voted_count = 'Recomendados';
+    $lang->member_count = 'Cantidad del Usuario';
     $lang->date = 'Fecha';
-    $lang->regdate = 'Fecha Publicado';
+    $lang->regdate = 'Fecha Publicada';
     $lang->last_update = 'Última actualización';
-    $lang->signup_date = 'Fecha Inscribido';
-    $lang->last_login = 'Último Entrada';
-    $lang->first_page = 'Primera Pagína';
-    $lang->last_page = 'Última Pagína';
-    $lang->search_target = 'Buscando Objectivo';
-    $lang->search_keyword = 'Buscando Palabra Clave';
+    $lang->signup_date = 'Fecha del Registro';
+    $lang->last_login = 'Última Conección';
+    $lang->first_page = 'Primera Página';
+    $lang->last_page = 'Última Página';
+    $lang->search_target = 'Buscar Objectivo';
+    $lang->search_keyword = 'Buscar la Palabra Clave';
     $lang->is_default = 'Prefijado';
 
-    $lang->no_documents = 'No Entradas!';
+    $lang->no_documents = 'Sin Mensajes';
 
-    $lang->board_manager = 'Configuración Tablón';
-    $lang->member_manager = 'Configuración Miembros';
-    $lang->layout_manager = 'Configuración Diseño';
+    $lang->board_manager = 'Manejo de los Tableros';
+    $lang->member_manager = 'MAnejo de los usuarios';
+    $lang->layout_manager = 'MAnejo de los Diseños';
 
     $lang->use = 'Usar';
-    $lang->notuse = 'Desusar';
+    $lang->notuse = 'No usar';
     $lang->not_exists = 'No existe';
 
-    $lang->unit_sec = 'sec';
-    $lang->unit_min = 'min';
-    $lang->unit_hour = 'hr';
-    $lang->unit_day = 'dia';
+    $lang->unit_sec = 'segundo';
+    $lang->unit_min = 'minuto';
+    $lang->unit_hour = 'hora';
+    $lang->unit_day = 'día';
     $lang->unit_week = 'semana';
     $lang->unit_month = 'mes';
     $lang->unit_year = 'año';
 
-    // Related to Description 
-    $lang->about_tag = 'cuando mecanografiar las etiquetas, insertando un comma() entre cada etiqueta permite que usted entre las etiquetas múltiples';
-    $lang->about_layout = 'disposición adorna un aspecto de su módulo que usted podría configurarlo de menú de la disposición en la tapa';
+    // Relacionado a la Descripción 
+    $lang->about_tag = 'Tú puedes agregar múltiples etiquetas colocando una coma(,) entre cada etiqueta';
+    $lang->about_layout = 'El diseño decora la apariencia de tus módulos, lo cual podrás configurar en el menú Diseño en la parte                           superior';
 
-    // Related to Message
+    // Relacionado al Mensaje
     $lang->msg_call_server = 'Solicitando al servidor, espere por favor';
-    $lang->msg_db_not_setted = 'No tiene la Configuración del DB';
-    $lang->msg_invalid_queryid = 'El valor de Query ID especificado mal';
-    $lang->msg_not_permitted = 'Usted no tiene permiso';
-    $lang->msg_input_password = 'Por favor escribe sus contraseña';
-    $lang->msg_invalid_document = 'Numero de entrada invalido';
-    $lang->msg_invalid_request = 'Requesta Invalido';
-    $lang->msg_invalid_password = 'Contraseñn Invalido';
-    $lang->msg_error_occured = 'Occurrido Errores';
-    $lang->msg_not_founded = 'Objetivo no encontrado';
-    $lang->msg_no_result = 'Esta búsqueda no hay resultado';
+    $lang->msg_db_not_setted = 'Configuración de DB no ha sido creado';
+    $lang->msg_invalid_queryid = 'El valor de Query ID especificado es inválido';
+    $lang->msg_not_permitted = 'Acceso denegado';
+    $lang->msg_input_password = 'Coloque la contraseña';
+    $lang->msg_invalid_document = 'Mensaje inválido';
+    $lang->msg_invalid_request = 'Respuesta Inválida';
+    $lang->msg_invalid_password = 'Contraseña Inválida';
+    $lang->msg_error_occured = 'Error';
+    $lang->msg_not_founded = 'No encontrado';
+    $lang->msg_no_result = 'Sin resultados';
 
-    $lang->msg_not_permitted_act = 'Usted no tiene permiso para ejecutar la action';
-    $lang->msg_module_is_not_exists = 'No podía encontrar el modulo que usted solicitado';
-    $lang->msg_module_is_not_standalone = 'El modulo usted solicitado no puede ejecutado independientemente';
+    $lang->msg_not_permitted_act = 'Usted no tiene permiso para ejecutar la action solicitada';
+    $lang->msg_module_is_not_exists = 'Módulo solicitado no puede ser encontrado';
+    $lang->msg_module_is_not_standalone = 'Módulo solicitado no puede ser ejecutado independientemente';
 
-    $lang->success_registed = 'Registrado con Éxito';
-    $lang->success_updated = 'Actualizado con Éxito';
-    $lang->success_deleted = 'Boarrado con Éxito';
-    $lang->success_voted = 'Votado con Éxito';
-    $lang->success_moved = 'Movido con Éxito';
-    $lang->success_sended = 'Enviado con Éxito';
-    $lang->success_reset = 'Reajustado con Éxito';
-    $lang->success_leaved = 'Abandonó con Éxito';
+    $lang->success_registed = 'Registrado con éxito';
+    $lang->success_updated = 'Actualizado con éxito';
+    $lang->success_deleted = 'Boarrado con éxito';
+    $lang->success_voted = 'Recomendado con éxito';
+    $lang->success_moved = 'Movido con éxito';
+    $lang->success_sended = 'Enviado con éxito';
+    $lang->success_reset = 'Reajustado con éxito';
+    $lang->success_leaved = 'Abandonó con éxito';
 
-    $lang->fail_to_delete = 'Supresión Fallada';
-    $lang->fail_to_move = 'Mover Fallada';
+    $lang->fail_to_delete = 'No puede ser borrado';
+    $lang->fail_to_move = 'No puede ser movido';
 
-    $lang->failed_voted = 'No podía ser votado';
-    $lang->fail_to_delete_have_children = 'Supresión Fallda. Hay unas o más contestaciones';
+    $lang->failed_voted = 'No puede ser recomendado';
+    $lang->fail_to_delete_have_children = 'No puede ser borrados, ya que el mensaje posee respuestas ';
 
-    $lang->confirm_submit = 'Confirmar para someter?';
-    $lang->confirm_logout = 'Confirmar para salir?';
-    $lang->confirm_vote = 'Confirmar para voto?';
-    $lang->confirm_delete = 'Confirmar de supresión?';
-    $lang->confirm_move = 'Confirmar para Movido?';
-    $lang->confirm_reset = 'Confirmar para reajustar?';
-    $lang->confirm_leave = 'Confirmar para Éxito?';
+    $lang->confirm_submit = 'Estás seguro que deseas enviar?';
+    $lang->confirm_logout = 'Estás seguro que deseas desconectar?';
+    $lang->confirm_vote = 'Estás seguro que deseas recomendar?';
+    $lang->confirm_delete = 'Estás seguro que deseas borrar?';
+    $lang->confirm_move = 'Estás seguro que deseas mover?';
+    $lang->confirm_reset = 'Estás seguro que deseas rehacer?';
+    $lang->confirm_leave = 'Estás seguro que deseas salir?';
 
-    $lang->column_type = 'Tipo de la Columna';
-    $lang->column_type_list['text'] = 'Texto una linea';
+    $lang->column_type = 'Tipos de Columnas';
+    $lang->column_type_list['text'] = 'Texto de una linea';
     $lang->column_type_list['homepage'] = 'url';
     $lang->column_type_list['email_address'] = 'Correo Electrónico';
-    $lang->column_type_list['tel'] = 'Numero de fono';
+    $lang->column_type_list['tel'] = 'Número de teléfono';
     $lang->column_type_list['textarea'] = 'Texto multi-linea';
-    $lang->column_type_list['checkbox'] = 'checkbox (selecciónes multiples)';
-    $lang->column_type_list['select'] = 'select box (selección singlar)';
+    $lang->column_type_list['checkbox'] = 'Cuadro de chequeo (selecciónes múltiples)';
+    $lang->column_type_list['select'] = 'Cuadro de selección (selección individual)';
     //$lang->column_type_list['kr_zip'] = 'zip code (Korean)';
     $lang->column_type_list['date'] = 'fecha (dd/mm/aaaa)';
     //$lang->column_type_list['jp_zip'] = 'zip code (Japanese)';
@@ -203,18 +203,18 @@
     $lang->column_title = 'Título de la Columna';
     $lang->default_value = 'Valor Prefijado';
     $lang->is_active = 'Activado';
-    $lang->is_required = 'Entrada Necesario';
+    $lang->is_required = 'Campo requerido';
 
-    // Alert messages for Javascript using by XML filter
-    $lang->filter->isnull = 'Entrar un valor para %s';
-    $lang->filter->outofrange = 'Alinear el lenghth del texto de %s';
-    $lang->filter->equalto = 'Valor inválido asignado para %s';
-    $lang->filter->invalid_email = '%s es formateo inválido (eg. zbxe@zeroboard.com)';
-    $lang->filter->invalid_user_id = $lang->filter->invalid_userid = "%s es formeto inválido.\\nTomas letras, números, y el underbar(ingleses _) de cuál debe comenzar con las letras Alfabeto";
-    $lang->filter->invalid_homepage = '%s es formateo inválido. (?: http://www.zeroboard.com)';
-    $lang->filter->invalid_korean = '%s got an invalid format. Solo aceptan las letras Coreanas';
-    $lang->filter->invalid_korean_number = '%s es formateo inválido. Solamente combinación de letras y de números coreanos se acepta';
-    $lang->filter->invalid_alpha = '%s es formateo inválido. Solamente se aceptan las alfabeto';
-    $lang->filter->invalid_alpha_number = '%s es formateo inválido. Solamente se aceptan las alfabeto y números';
-    $lang->filter->invalid_number = '%s es formateo inválido. Solo aceptan los números.';
+    // Mensajes de alerta para Javascript usando el filtro XML
+    $lang->filter->isnull = 'Por favor ingresar un valor para %s';
+    $lang->filter->outofrange = 'Por favor alinear el largo del texto de %s';
+    $lang->filter->equalto = 'Valor de %s es inválido.';
+    $lang->filter->invalid_email = 'Formato de %s es inválido. (ej. zbxe@zeroboard.com)';
+    $lang->filter->invalid_user_id = $lang->filter->invalid_userid = "Formato de %s es inválido.\\n Todos los valores deberá consistir en alfabetos, números o guión abajo(_) y la primera letra deberá ser el alfabeto Español";
+    $lang->filter->invalid_homepage = 'Formato de %s es inválido. (ej: http://www.zeroboard.com)';
+    $lang->filter->invalid_korean = 'Formato de %s es inválido. Por favor ingresar sólo coreano';
+    $lang->filter->invalid_korean_number = 'Formato de %s es inválido. Por favor ingresar coreano o números';
+    $lang->filter->invalid_alpha = 'Formato de %s es inválido. Por favor ingresar sólo alfabetos';
+    $lang->filter->invalid_alpha_number = 'Formato de %s es inválido. Por favor ingresar alfabetos o números';
+    $lang->filter->invalid_number = 'Formato de %s es inválido. Por favor ingresar sólo números.';
 ?>
