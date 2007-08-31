55f0ed8813873227355ccc205ab934b9a8491159
sanghunjun <sanghunjun@201d5d3c-b55e-5fd7-737f-ddc643e51545>
2007-08-31 19:31:57 +0000

    스페인어 번역
    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@2467 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/editor/lang/es.lang.php
+++ modules/editor/lang/es.lang.php
@@ -1,36 +1,36 @@
 <?php
     /**
-     * @file   modules/editor/lang/es.lang.php
-     * @author zero <zero@nzeo.com>
-     * @brief  Paquete lingual de editor WYSIWYG
+     * @archivo  modules/editor/lang/es.lang.php
+     * @autor zero <zero@nzeo.com>
+     * @sumario  Paquete del idioma espanol para el editor de WYSIWYG
      **/
 
     $lang->editor = "Editor WYSIWYG";
     $lang->component_name = "Componente";
-    $lang->component_version = "Versión";
+    $lang->component_version = "Version";
     $lang->component_author = "Autor";
     $lang->component_link = "Enlace";
     $lang->component_date = "Fecha";
-    $lang->component_description = "Descripción";
+    $lang->component_description = "Descripcion";
     $lang->component_extra_vars = "Varibles Extras";
-    $lang->component_grant = "Autoridad"; 
+    $lang->component_grant = "Ajuste de los permisos"; 
 
-    $lang->about_component = "Sobre Componente";
-    $lang->about_component_grant = "Puede usar el funcion solo grupos seleciónado";
+    $lang->about_component = "Presentacion del Componente";
+    $lang->about_component_grant = "Solo los grupos seleccionados pueden ser usados (Si desactiva el modo se puede usar todos)";
 
-    $lang->msg_component_is_not_founded = 'No puede buscar componente editor %s';
-    $lang->msg_component_is_inserted = 'Ya habia insertado el componente';
-    $lang->msg_component_is_first_order = 'Componente seleciónado esta ordenado en primero lugar';
-    $lang->msg_component_is_last_order = 'Componente seleciónado esta ordenado en ultimo lugar';
-    $lang->msg_load_saved_doc = "Hay documento guardado automaticamente. ¿Desea recuperarlo?\nDespues de guardar el documento temporal van a eliminado.";
+    $lang->msg_component_is_not_founded = 'No se puede encontrar el componente del editor %s';
+    $lang->msg_component_is_inserted = 'El componente seleccionado ya esta insertado';
+    $lang->msg_component_is_first_order = 'El componente seleccionado se localiza en la primera posicion';
+    $lang->msg_component_is_last_order = 'El componente seleccionado se localiza en la ultima posicion';
+    $lang->msg_load_saved_doc = "Existe un documento guardado automaticamente ¢¯desea recuperarlo ?\nDespues de guardar el documento escrito, el documento autoguardado sera eliminado.";
     $lang->msg_auto_saved = "Documento guardado automaticamente";
 
-    $lang->cmd_disable = "Activado";
-    $lang->cmd_enable = "Desactivado";
+    $lang->cmd_disable = "Desactivado";
+    $lang->cmd_enable = "activado";
 
-    $lang->edit->fontname = 'Font';
-    $lang->edit->fontsize = 'Size';
-    $lang->edit->use_paragraph = 'Paragraph Function';
+    $lang->edit->fontname = 'Tipos de letra';
+    $lang->edit->fontsize = 'tamano de la letra';
+    $lang->edit->use_paragraph = 'Funcion del parrafo';
     $lang->edit->fontlist = array(
     "Gulim",
     "Dodum",
@@ -42,44 +42,44 @@
     "Arial",
     );
 
-    $lang->edit->header = "Style";
+    $lang->edit->header = "Estilo";
     $lang->edit->header_list = array(
-    "h1" => "Subject 1",
-    "h2" => "Subject 2",
-    "h3" => "Subject 3",
-    "h4" => "Subject 4",
-    "h5" => "Subject 5",
-    "h6" => "Subject 6",
+    "h1" => "Titulo 1",
+    "h2" => "Titulo 2",
+    "h3" => "Titulo 3",
+    "h4" => "Titulo 4",
+    "h5" => "Titulo 5",
+    "h6" => "Titulo 6",
     );
 
-    $lang->edit->submit = 'Submit';
+    $lang->edit->submit = 'Confirmar';
 
-    $lang->edit->help_fontcolor = "Select font color";
-    $lang->edit->help_fontbgcolor = "Select background color of font";
-    $lang->edit->help_bold = "Make font bold";
-    $lang->edit->help_italic = "Make italic font";
-    $lang->edit->help_underline = "Underline font";
-    $lang->edit->help_strike = "Strike font";
-    $lang->edit->help_redo = "Redo";
-    $lang->edit->help_undo = "Undo";
-    $lang->edit->help_align_left = "Align left";
-    $lang->edit->help_align_center = "Align center";
-    $lang->edit->help_align_right = "Align right";
-    $lang->edit->help_add_indent = "Add indent";
-    $lang->edit->help_remove_indent = "Remove indent";
-    $lang->edit->help_list_number = "Apply number list";
-    $lang->edit->help_list_bullet = "Apply bullet list";
-    $lang->edit->help_use_paragrapth = "Press Ctrl+Enter to use paragraph. (Press Alt+S to save)";
+    $lang->edit->help_fontcolor = "Selecciona el color de la letra";
+    $lang->edit->help_fontbgcolor = "Selecciona el color del fondo de la letra";
+    $lang->edit->help_bold = "Letra gruesa";
+    $lang->edit->help_italic = "Letra cursiva";
+    $lang->edit->help_underline = "Letra subrayada";
+    $lang->edit->help_strike = "Letra con linea";
+    $lang->edit->help_redo = "Rehacer";
+    $lang->edit->help_undo = "Deshacer";
+    $lang->edit->help_align_left = "Margen izquierdo";
+    $lang->edit->help_align_center = "Margen central";
+    $lang->edit->help_align_right = "Margen derecho";
+    $lang->edit->help_add_indent = "Anadir tabulacion";
+    $lang->edit->help_remove_indent = "Quitar tabulacion";
+    $lang->edit->help_list_number = "Aplicar la lista con numeros";
+    $lang->edit->help_list_bullet = "Aplicar la lista con simbolos";
+    $lang->edit->help_use_paragrapth = "Presiona Ctrl+Enter para usar el parrafo (Presiona Alt+S para guardar)";
 
-    $lang->edit->upload = 'Attachment';
-    $lang->edit->upload_file = 'Attach'; 
-    $lang->edit->link_file = 'Insert to Content';
-    $lang->edit->delete_selected = 'Delete Selected';
+    $lang->edit->upload = 'Adjuntar';
+    $lang->edit->upload_file = 'Archivo adjunto'; 
+    $lang->edit->link_file = 'Insertar en el contenido del documento';
+    $lang->edit->delete_selected = 'Eliminar lo seleccionado';
 
-    $lang->edit->icon_align_article = 'Occupy a paragraph';
-    $lang->edit->icon_align_left = 'Align Left';
-    $lang->edit->icon_align_middle = 'Align Center';
-    $lang->edit->icon_align_right = 'Align Right';
+    $lang->edit->icon_align_article = 'Ocupar un parrafo';
+    $lang->edit->icon_align_left = 'Margen izquierdo';
+    $lang->edit->icon_align_middle = 'Margen central';
+    $lang->edit->icon_align_right = 'Margen derecho';
 
-    $lang->about_dblclick_in_editor = 'You are able to set detail component configure by double-clicking on background, text, images, or quotations';
+    $lang->about_dblclick_in_editor = 'Para la configuracion mas detallada debera hacer dobleclick sobre el texto, imagen, fondo, etc.';
 ?>
