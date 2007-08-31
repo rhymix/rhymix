b7c54a5dd8c573ac8fbe9593bf98e3414a216723
sanghunjun <sanghunjun@201d5d3c-b55e-5fd7-737f-ddc643e51545>
2007-08-31 18:58:48 +0000

    스페인어 번역
    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@2464 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/document/lang/es.lang.php
+++ modules/document/lang/es.lang.php
@@ -1,37 +1,37 @@
 <?php
     /**
-     * @file   modules/document/lang/es.lang.php
-     * @author zero <zero@nzeo.com>
-     * @brief  Paquete lingual de módulo documento
+     * @archivo   modules/document/lang/es.lang.php
+     * @autor zero <zero@nzeo.com>
+     * @sumario Paquete del idioma español para el módulo de mensajes.
      **/
 
     $lang->cmd_delete_checked_document = 'Eliminar selección';
 
-    $lang->msg_cart_is_null = 'Por favor selecciona documento para eliminar';
-    $lang->msg_category_not_moved = 'No puede mover';
-    $lang->msg_is_secret = 'es secreto';
-    $lang->msg_checked_document_is_deleted = 'Eliminado %d documentos';
+    $lang->msg_cart_is_null = 'Selecciona el mensaje que desea eliminar';
+    $lang->msg_category_not_moved = 'No puede se movido';
+    $lang->msg_is_secret = 'Es un mensaje secreto';
+    $lang->msg_checked_document_is_deleted = 'Total de %d mensajes han sido eliminados';
 
-    // Artículo de busqueda en la pantalla de administración
+    // Objetivo de búsqueda en la página del administrador
     $lang->search_target_list = array(
         'title' => 'Título',
         'content' => 'Contenido',
         'user_id' => 'ID',
-        'member_srl' => 'Numero de miembro',
-        'user_name' => 'Nombre de usuario',
+        'member_srl' => 'Número del usuario',
+        'user_name' => 'Nombre del usuario',
         'nick_name' => 'Apodo',
         'email_address' => 'Correo Electrónico',
-        'homepage' => 'Pagina de web',
+        'homepage' => 'Página web',
         'is_notice' => 'Noticia',
         'is_secret' => 'Secreto',
         'tags' => 'Etiqueta',
-        'readed_count' => 'Leido',
-        'voted_count' => 'Votado',
-        'comment_count ' => 'Commentarios',
-        'trackback_count ' => 'Trackback',
-        'uploaded_count ' => 'Subido',
-        'regdate' => 'Registrado',
-        'last_update' => 'Ultimo actualizado',
+        'readed_count' => 'Número de leídos (sobre)',
+        'voted_count' => 'Número de recomnedados (sobre)',
+        'comment_count ' => 'Número de comentarios (sobre)',
+        'trackback_count ' => 'Número de Trackback (sobre)',
+        'uploaded_count ' => 'Número de archivos adjuntos (sobre)',
+        'regdate' => 'Día del registro',
+        'last_update' => 'Día de la última actualización',
         'ipaddress' => 'Dirección IP',
     );
 ?>
