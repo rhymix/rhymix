c2ccb25b0be7edb2af1a5709f0fb7d4e1774943b
sanghunjun <sanghunjun@201d5d3c-b55e-5fd7-737f-ddc643e51545>
2007-08-31 02:50:52 +0000

    수정
    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@2437 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/addon/lang/es.lang.php
+++ modules/addon/lang/es.lang.php
@@ -5,13 +5,13 @@
      * @sumario paquete del idioma español 
      **/
 
-    $lang->addon = "Adicción";
+    $lang->addon = "Addon";
 
-    $lang->addon_info = 'Información de Adición';
-    $lang->addon_maker = 'Autor de Adición';
-    $lang->addon_history = 'Historia de Adición ';
+    $lang->addon_info = 'Información de Adson';
+    $lang->addon_maker = 'Autor de Addon';
+    $lang->addon_history = 'Historia de Addon ';
 
-    $lang->about_addon = 'Adición is para controlar las acciones y no para mostrar el resultado HTML.<br /> Sólo con encender o apagar la adición que desees, podrás obtener funciones útiles para la administracion de tu sitio web.';
+    $lang->about_addon = 'Addon is para controlar las acciones y no para mostrar el resultado HTML.<br /> Sólo con encender o apagar el addon que dese, podrá obtener funciones útiles para la administracion de tu sitio web.';
 ?>
 
 
