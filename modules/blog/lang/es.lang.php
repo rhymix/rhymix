b87cf2ddf858155061f97b24216e06da272cd1f0
sanghunjun <sanghunjun@201d5d3c-b55e-5fd7-737f-ddc643e51545>
2007-08-31 03:43:30 +0000

    스페인어 번역
    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@2441 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/blog/lang/es.lang.php
+++ modules/blog/lang/es.lang.php
@@ -1,33 +1,33 @@
 <?php
     /**
-     * @file   es.lang.php
-     * @author zero (zero@nzeo.com)
-     * @brief  Paquete lingual para módulo blog simple.
+     * @archivo   es.lang.php
+     * @autor zero (zero@nzeo.com)
+     * @sumario  Paquete de idioma español para módulo de blog simple.
      **/
 
-    // Palabras en botónes
-    $lang->cmd_blog_list = 'lista de blogs';
-    $lang->cmd_module_config = 'configuración común de blogs ';
+    // Palabras en los botónes
+    $lang->cmd_blog_list = 'Lista de blog';
+    $lang->cmd_module_config = 'Configuración común de blog ';
     $lang->cmd_view_info = 'Información de blog';
-    $lang->cmd_manage_menu = 'manejar menú';
-    $lang->cmd_make_child = 'agregar sub categoria';
-    $lang->cmd_enable_move_category = "mover posición de categoria. (arrastrar y soltar la selección)";
-    $lang->cmd_remake_cache = 'rehacer caché';
-    $lang->cmd_layout_setup = 'configuración del diseño';
-    $lang->cmd_layout_edit = 'editar el diseño';
+    $lang->cmd_manage_menu = 'manejo del menú';
+    $lang->cmd_make_child = 'Agregar sub categoría';
+    $lang->cmd_enable_move_category = "Cambiar la posición de la categoría. (arrastrar y soltar luego de haber selecionado)";
+    $lang->cmd_remake_cache = 'Rehacer archivo caché';
+    $lang->cmd_layout_setup = 'Configuración del diseño';
+    $lang->cmd_layout_edit = 'Editar el diseño';
 
-    // Artículo
-    $lang->parent_category_name = 'Nombre de la categoria preferencia';
-    $lang->category_name = 'Nombre de la categoria';
-    $lang->expand = 'Abrir';
-    $lang->category_group_srls = 'Limitar grupos';
-    $lang->search_result = 'Resulto de la busqueda';
+    // Items
+    $lang->parent_category_name = 'Categoría Parental';
+    $lang->category_name = 'Nombre de la categoría';
+    $lang->expand = 'Expandir';
+    $lang->category_group_srls = 'Limitar el grupo';
+    $lang->search_result = 'Resultado de la búsqueda';
 
     // bla bla...
-    $lang->about_category_name = 'Por favor escribe nombre de la categoria.';
-    $lang->about_expand = 'Si seleccióna, sera abierto siempre.';
-    $lang->about_category_group_srls = 'Solo muestra categoria a grupo selecciónado. (pero muestra todo en XML)';
-    $lang->about_layout_setup = 'Puede modificar directamente los codigo de diseño del blog. Inserta o modifica codigo de widget.';
-    $lang->about_blog_category = 'Puede crear las categorias.<br />A veces no funcióna blog, rehacer caché manualmente puede ser solución.';
-    $lang->about_blog = "Es el módulo para crear y manejar blog.\nEl módulo de blog usa la diseña que incluydo en la carátula de blog.  Es necesarario decorar su blog manejar las categorias y carátulas.\nSi desea conectar otro boletín en su blog, crea otro menú y connecta en manejar las carátulas.";
+    $lang->about_category_name = 'Ingresar el nombre de la categoría.';
+    $lang->about_expand = 'Si seleccionas esta opción, siempre estará expandido.';
+    $lang->about_category_group_srls = 'Solo se verán los grupos seleccionados en la actual categoría. (Abrir manualmente en archivo xml para la exposición)';
+    $lang->about_layout_setup = 'Puede modificar manualmente el código del diseño de blog. Insertar o modificar el código de widget.';
+    $lang->about_blog_category = 'Puede crear las categorias de blog.<br />Cuando no funciona la categoría de blog, rehacer el archivo caché manualmente para solucionar.';
+    $lang->about_blog = "Este is el módulo de blog para crear y manejar.\nLuego de crear un blog, por favor decora tu blog por categoría y manejo de tema por que este módulo de blog usa el diseño que está incluido en el tema de blog.\nSi desea conectar otros tableros en su blog, usa el menú de módulo para crear un menú  y luego conecta con un manejo de tema.";
 ?>
