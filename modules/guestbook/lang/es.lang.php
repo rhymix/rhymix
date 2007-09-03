7c2ad6c2e3e358040e02c9656e526fd312ec4cb7
sanghunjun <sanghunjun@201d5d3c-b55e-5fd7-737f-ddc643e51545>
2007-09-03 16:27:11 +0000

    스페인어 번역
    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@2475 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/guestbook/lang/es.lang.php
+++ modules/guestbook/lang/es.lang.php
@@ -1,17 +1,16 @@
 <?php
     /**
-     * @file   es.lang.php
-     * @author zero (zero@nzeo.com)
-     * @brief  Paquete lingual para Módulo de boletín
+     * @archivo   es.lang.php
+     * @autor zero (zero@nzeo.com)
+     * @sumario  Paquete del idioma español para el módulo de Tablero de visitas
      **/
 
-    $lang->board = "Boletín"; 
+    $lang->board = "Tablero de Visitas"; 
 
-    // Palabras utiliza en botónes
-    $lang->cmd_board_list = 'Lista de boletín';
-    $lang->cmd_module_config = 'configuración comun de boletínes';
-    $lang->cmd_view_info = 'ver información de boletín';
-    $lang->move_target_module = "Módulo para mover";
-
-    $lang->about_board = "Es el módulo para crear y manejar boletínes.\nDespues de crear un boletín, haga clic en nombre de módulos para configuración mas detallado.\nEl nombre de módulo es nombre de URL, sea cuidoso deseando el nombre. (ej : http://dominio/zb/?mid=nombre de módulo)";
+    // Palabras utilizadas en los botones
+    $lang->cmd_board_list = 'Lista del teblero de visitas';
+    $lang->cmd_module_config = 'Configuración común del tablero de visitas';
+    $lang->cmd_view_info = 'Información del tablero de visitas';
+    
+    $lang->about_board = "Es módulo es usado para crear y manejar los tableros de visitas.\nLuego de crear un Tablero, seleciona el nombre de módulo en la lista para la configuración más detallada.\nSea cuidadoso con el nombre del módulo, ya que va a ser la dirección URL. (ej : http://dominio/zb/?mid=nombre del módulo)";
 ?>
