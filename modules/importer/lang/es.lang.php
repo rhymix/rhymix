1a6310656f357c4c1889347e2fe611d0b68083c7
sanghunjun <sanghunjun@201d5d3c-b55e-5fd7-737f-ddc643e51545>
2007-09-03 16:27:41 +0000

    스페인어 번역
    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@2476 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/importer/lang/es.lang.php
+++ modules/importer/lang/es.lang.php
@@ -1,52 +1,52 @@
 <?php
     /**
-     * @file   es.lang.php
-     * @author zero (zero@nzeo.com)
-     * @brief  Importer(importer) paquete lingual
+     * @archivo   es.lang.php
+     * @autor zero (zero@nzeo.com)
+     * @sumario Paquete del idioma español para importar.
      **/
 
-    // Lenguage para botones
+    // Palabras para los botones
     $lang->cmd_sync_member = 'Sincronizar';
     $lang->cmd_continue = 'Continuar';
 
-    // especificación
+    // Especificaciones
     $lang->importer = 'Transferir los datos de zeroboard';
-    $lang->source_type = 'Tipo de origen';
-    $lang->type_member = 'Datos de miembros';
-    $lang->type_module = 'Datos de articulos.';
-    $lang->type_syncmember = 'Sincronizar datos de miembros';
-    $lang->target_module = 'Modulo objetivo';
-    $lang->xml_file = 'archivo XML';
+    $lang->source_type = 'Objetivo a transferir';
+    $lang->type_member = 'Datos del usuario';
+    $lang->type_module = 'Datos de los artículos.';
+    $lang->type_syncmember = 'Sincronizar datos del usuario';
+    $lang->target_module = 'Objetivo del módulo';
+    $lang->xml_file = 'Archivo XML';
 
     $lang->import_step_title = array(
-        1 => 'Paso 1. seleccione la origen',
-        12 => 'Paso 1-2. seleccione el modulo objetivo',
-        13 => 'Paso 1-3. seleccione la categoria',
-        2 => 'Paso 2. Suvir archivo de XML',
-        3 => 'Paso 2. Sincronizar los datos de miembro y articulos',
+        1 => 'Paso 1. Seleccione el objetivo a transferir',
+        12 => 'Paso 1-2. Seleccione el objetivo del módulo ',
+        13 => 'Paso 1-3. Seleccione la categoría del módulo',
+        2 => 'Paso 2. Subir el archivo XML',
+        3 => 'Paso 2. Sincronizar los datos del usuario y de los artículos',
     );
 
     $lang->import_step_desc = array(
-        1 => 'Por favor seleccione el tipo de archivo XML para transfrerir.',
-        12 => 'Por favor seleccione el modulo que Ud. desea para transferir los datos.',
-        13 => 'Por favor seleccione la categoria para transferir.',
-        2 => "Por favor inserta la ubicación de archivo XML.\nEsto puede ser paso absoluto o relativo.",
-        3 => 'La información de miembro y articulos puede ser incorrecto. Sincroniza para la corrección de este problema.',
+        1 => 'Por favor seleccione el tipo de archivo XML a transfrerir.',
+        12 => 'Por favor seleccione el módulo para transferir los datos.',
+        13 => 'Por favor seleccione la categoría para transferir los datos.',
+        2 => "Por favor ingrese la ubicación del archivo XML para transfer los datos.\nPuede ser ruta absoluto o relativo.",
+        3 => 'La información del usuario y del artículo podría ser incorrecto luego de la transferencia. Si ese es el caso, sincroniza para la corrección basado a la ID del usuario.',
     );
 
-    // guía/ alerta
-    $lang->msg_sync_member = 'Preciónar Sincronizar para empezar sincronización de los datos miembro y articulos.';
-    $lang->msg_no_xml_file = 'No puede encontrar archivo XML. Verifique sus entrada.';
-    $lang->msg_invalid_xml_file = 'invalido tipo de archivo XML.';
-    $lang->msg_importing = 'Escribiendo datos %d de %d. (Si esta parece paralizado haga clic en "Continuar".)';
-    $lang->msg_import_finished = 'Importación de %d datos esta completo. Depende la situción, puede ser la información no puede agregar.';
-    $lang->msg_sync_completed = 'Sincronización de Miembro, ariculos, y respuestas esta completo.';
+    // Guía/ Alerta
+    $lang->msg_sync_member = 'Al presionar el botón sincronizar comenzará a sincronizar los datos usuario y el artículo.';
+    $lang->msg_no_xml_file = 'No se puede encontrar el archivo XML. Verifique su ruta.';
+    $lang->msg_invalid_xml_file = 'Tipo de archivo XML inválido.';
+    $lang->msg_importing = 'Ingresando %d dotos de %d. (Si esto mantiene paralizado presione el botón "Continuar".)';
+    $lang->msg_import_finished = '%d datos fueron completamente ingresados. Dependiendo del caso, pueden haber algunos datos no ingresados.';
+    $lang->msg_sync_completed = 'Sincronización del usuario, artículo y respuestas finalizadas.';
 
     // bla bla...
-    $lang->about_type_member = 'seleccione si desea transferir información de miembro.';
-    $lang->about_type_module = 'seleccione si desea transferir información de articulos';
-    $lang->about_type_syncmember = 'seleccione si desea sincronizar despues de transferencia';
-    $lang->about_importer = "Puede importar Zeroboard4, zb5beta o datos de otras programas a ZeroBoardXE.\nPara la transferencia utiliza<a href=\"#\" onclick=\"winopen('');return false;\">Exportador XML</a> para recrear datos en archivo XML, y subir los resultos.";
+    $lang->about_type_member = 'Seleccione esta opción si estas transferiendo la información del usuario.';
+    $lang->about_type_module = 'Seleccione esta opción si estas transfeririendo información de articulos de los tableros';
+    $lang->about_type_syncmember = 'Seleccione esta opción cuando tenga que sincronizar la información del usuario luego de haber transferodo la información del usuario y del artículo.';
+    $lang->about_importer = "Es posible trasferir los datos de Zeroboard4, zb5beta o de otros programas a ZeroBoardXE.\nPara la transferencia debe utilizar <a href=\"#\" onclick=\"winopen('');return false;\">Exportador XML</a> para transformar los datos en archivo XML, y luego subir ese archivo.";
 
-    $lang->about_target_path = "Escribe la ubicación de ZeroBoard4 para bajar el archivo.\nSi existe en mismo servidor escribe la ubicación de ZeroBoard4 por ejemplo: /home/ID/public_html/bbs o si existe en otro servidor escribe URL como ejemplo: http://dominio/bbs";
+    $lang->about_target_path = "Para bajar los archivos adjuntos de ZeroBoard4, ingresa la ubicación de ZeroBoard4 instalado.\nSi esta en el mismo servidor escribe la ubicación de ZeroBoard4 como por ejemplo: /home/ID/public_html/bbs o si esta en otro servidor escribe la ubicación de ZeroBoard4 instalado como por ejemplo: http://dominio/bbs";
 ?>
