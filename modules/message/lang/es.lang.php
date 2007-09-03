9fe0f55b2147163addee3d7038ee55ecc6d86ba2
sanghunjun <sanghunjun@201d5d3c-b55e-5fd7-737f-ddc643e51545>
2007-09-03 22:01:13 +0000

    스페인어 번역
    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@2483 201d5d3c-b55e-5fd7-737f-ddc643e51545

---
+++ modules/message/lang/es.lang.php
@@ -0,0 +1,10 @@
+<?php
+    /**
+     * @archivo   modules/message/lang/es.lang.php
+     * @autor zero <zero@nzeo.com>
+     * @sumario Paquete del idioma espanol (basico)
+ **/
+
+    $lang->message = 'Mostrar el error';
+    $lang->about_skin = "Usted puede seleccionar los temas de los mensajes de error.";
+?>
