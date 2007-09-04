02fb530d41871eff1db8a3300cb64985fbb4923d
sanghunjun <sanghunjun@201d5d3c-b55e-5fd7-737f-ddc643e51545>
2007-09-04 16:42:43 +0000

    스페인어 번역 추가
    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@2496 201d5d3c-b55e-5fd7-737f-ddc643e51545

---
+++ modules/widget/lang/es.lang.php
@@ -0,0 +1,43 @@
+<?php
+    /**
+     * @archivo   modules/widget/lang/es.lang.php
+     * @autor zero <zero@nzeo.com>
+     * @sumario Paquete del idioma espanol para el modulo Widget (basico)
+     **/
+
+    $lang->cmd_generate_code = 'Generar Codigo';
+
+    $lang->widget_name = 'Nombre de Widget';
+    $lang->widget_maker = 'Desarrollador de Widget';
+    $lang->widget_history = 'Actualizar la historia';
+    $lang->widget_info = 'informacion de Widget';
+    $lang->widget_code = 'Codigo';
+    $lang->widget_cache = 'Cache';
+
+    $lang->widget_fix_width = 'Ancho permanente';
+    $lang->widget_width = 'Ancho';
+    $lang->widget_position = 'Posicion';
+    $lang->widget_position_none = 'Proxima linea';
+    $lang->widget_position_left = 'Izquierdo';
+    $lang->widget_position_right = 'Derecho';
+    $lang->widget_margin = 'Margen';
+    $lang->widget_margin_top = 'Margen superior';
+    $lang->widget_margin_right = 'Marge derecho';
+    $lang->widget_margin_bottom = 'Margen inferior';
+    $lang->widget_margin_left= 'Margen izquierdo';
+    $lang->about_widget_fix_width = 'Si selecciona esta opcion el ancho es permanente.';
+    $lang->about_widget_width = 'Ingrese el ancho de Widget.';
+    $lang->about_widget_position = 'Selecciona la posicion si desea mostra mas widgets en una linea.';
+    $lang->about_widget_margin = "Ueste puede ingresar el margen de los widgets : superior, inferior, derecho e izquierdo.";
+    $lang->about_widget_cache = 'Se puede usar datos de cache creado durante el tiempo asignado.';
+
+    $lang->generated_code = 'Generar codigos';
+
+    $lang->msg_widget_is_not_exists = 'No existe widget %s .';
+    $lang->msg_widget_object_is_null = 'No puede ser creado el objeto de widget %s.';
+    $lang->msg_widget_proc_is_null = 'No puede ser ejecutado el proc() de widget %s.';
+
+    $lang->about_widget_code = 'Ingrese primero los valores requeridos, luego presiona el boton "Generar codigos" para mostrar el codigo en el espacio inferior para ser anadido en el archivo de los temas.';
+    $lang->about_widget_code_in_page = 'Luego de ingresar los valores requeridos, presiona el boton "Anadir" para insertar el codigo de widget dentro de la pagina.';
+    $lang->about_addon = "Un Widget is una pequena aplicacion para ser usados en los disenos o en el modulo de paginas.\n Puede ser enlazados con en modulo interno o con open api externo y con la configuracion de ella puede utilizar en variadas aplicaciones.\nSin la necesidad de utilizar los modulos de pagina o del diseno de Zeroboard XE, Usted puede directamente anadir un widget usando la funcion de [Generar Codigos].";
+?>
