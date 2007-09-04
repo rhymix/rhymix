85ead1090005b1832a1ed10918dc032ef09d377f
sanghunjun <sanghunjun@201d5d3c-b55e-5fd7-737f-ddc643e51545>
2007-09-04 04:45:00 +0000

    스페인어 번역
    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@2489 201d5d3c-b55e-5fd7-737f-ddc643e51545

---
+++ modules/rss/lang/es.lang.php
@@ -0,0 +1,18 @@
+<?php
+    /**
+     * @archivo   modules/rss/lang/es.lang.php
+     * @autor zero <zero@nzeo.com>
+     * @sumario Paquete del idioma espanol (basico)
+     **/
+
+    // palabras generales
+    $lang->rss_disable = "Desactivar RSS";
+    $lang->rss_type = "Tipo de RSS a imprimir";
+
+    // para la descripcion
+    $lang->about_rss_disable = "Si selecciona esta option, RSS sera desactivado.";
+    $lang->about_rss_type = "Usted puede asignar el tipo de RSS a imprimir.";
+
+    // mensajes de error
+    $lang->msg_rss_is_disabled = "Funcion de RSS esta desactivada.";
+?>
