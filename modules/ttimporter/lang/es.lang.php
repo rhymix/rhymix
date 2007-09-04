25c11a594e4be9d5a50e28f68141ba17e7516470
sanghunjun <sanghunjun@201d5d3c-b55e-5fd7-737f-ddc643e51545>
2007-09-04 16:14:02 +0000

    스페인어 번역 추가
    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@2495 201d5d3c-b55e-5fd7-737f-ddc643e51545

---
+++ modules/ttimporter/lang/es.lang.php
@@ -0,0 +1,21 @@
+<?php
+    /**
+     * @archivo   modules/ttimporter/lang/es.lang.php
+     * @autor zero (zero@nzeo.com)
+     * @sumario   Paquete del idioma espanol para el modulo de Ttimporter
+     **/
+
+    $lang->ttimporter = "Importar datos de la herramienta Tetter";
+    $lang->about_tt_importer = "Usted puede ingresar los datos de la herramienta Tetter dentro del modulo lo que desee.\n El archivo adjunto sera directamento descargado via web, por lo que Usted debe activar el blog original de la herrameienta Tetter.";
+
+    $lang->target_module = "Modulo objetivo";
+    $lang->target_file = "Archivo xml objetivo";
+    $lang->target_url = "URL del Blog";
+
+    $lang->cmd_continue = 'Continuar';
+
+    $lang->msg_no_xml_file = 'No se puede encontrar el archivo XML! Por favor chequee la URL.';
+    $lang->msg_invalid_xml_file = 'Tipo de archivo XML invalido!';
+    $lang->msg_importing = 'Importando %d datos (si permanece parado mucho tiempo, por favor presiona el boton "Continuar".)';
+    $lang->msg_import_finished = '%d datos importados (dependiendo de la condicion, algunos datos pueden no ser importados correctamente.)';
+?>
