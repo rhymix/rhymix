1020077bed56d1b87a5517b8442dcff00413f4e8
sanghunjun <sanghunjun@201d5d3c-b55e-5fd7-737f-ddc643e51545>
2007-09-04 15:52:34 +0000

    수페인어 번역 추가
    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@2494 201d5d3c-b55e-5fd7-737f-ddc643e51545

---
+++ modules/trackback/lang/es.lang.php
@@ -0,0 +1,24 @@
+<?php
+    /**
+     * @archivo  modules/trackback/lang/es.lang.php
+     * @autor zero <zero@nzeo.com>
+     * @sumario Paquete del idioma español para el módulo de trackback (básico)
+     **/
+
+    $lang->cmd_delete_checked_trackback = 'Trackback(s) es eliminado';
+
+    $lang->msg_cart_is_null = 'Seleccione un trackback para ser eliminado.';
+    $lang->msg_checked_trackback_is_deleted = '%d trackback(s) eliminado.';
+
+    $lang->search_target_list = array(
+        'url' => 'URL objetivo',
+        'blog_name' => 'Ojetivo del nombre de sitio',
+        'title' => 'Título',
+        'excerpt' => 'Contenido',
+        'regdate' => 'Fecha del posteo',
+        'ipaddress' => 'Dirección IP',
+    );
+
+    $lang->enable_trackback = "Usar trackback";
+    $lang->about_enable_trackback = "Si no selecciona esta opción, desactiva la inscripción de trackback en todos los documentos del sitio";
+?>
