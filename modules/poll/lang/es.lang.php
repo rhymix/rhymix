89dcad5218101615bbacb0de27b23f3a04a45799
sanghunjun <sanghunjun@201d5d3c-b55e-5fd7-737f-ddc643e51545>
2007-09-04 04:40:00 +0000

    스페인어 번역
    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@2488 201d5d3c-b55e-5fd7-737f-ddc643e51545

---
+++ modules/poll/lang/es.lang.php
@@ -0,0 +1,37 @@
+<?php
+    /**
+     * @archivo   modules/poll/lang/es.lang.php
+     * @autor zero <zero@nzeo.com>
+     * @sumario Paquete del idioma espa;ol para el modulo de encuesta 
+     **/
+
+    $lang->poll = "Encuesta"; 
+    $lang->poll_stop_date = "Fecha de expiracion";
+    $lang->poll_join_count = "Participantes";
+    $lang->poll_checkcount = "Numero de articulos requeridos.";
+
+    $lang->cmd_poll_list = 'Ver la lista de la encuesta';
+    $lang->cmd_delete_checked_poll = 'Eliminar la encuesta seleccionada'; 
+    $lang->cmd_apply_poll = 'Aplicar la encuesta';
+    $lang->cmd_delete_checked_poll = 'Eliminar la encuesta seleccionada';  
+
+    $lang->success_poll = 'Gracias por participar en la encuesta.';
+
+    $lang->msg_already_poll = 'Usted ya realizo la encuesta!';
+    $lang->msg_cart_is_null = 'Por favor seleciona un articulo a eliminar.';
+    $lang->msg_checked_poll_is_deleted = '%d encustas son eliminados.';
+    $lang->msg_check_poll_item = 'Por favor selecciona un articulo de la encuesta.\n(Articulos requeridos pueden ser diferentes in cada encuesta.)';
+    $lang->msg_cart_is_null = 'Por favor selecciona una encuesta a eliminar.';
+    $lang->msg_checked_poll_is_deleted = '%d encuestas son eliminados.';
+    $lang->msg_poll_not_exists = 'Encuesta seleccionada no existe.';
+
+    $lang->cmd_null_item = "No existe ningun valor para la encuesta. Por favor trate nuevamente.";
+
+    $lang->confirm_poll_submit = "¢¯Confirma la participacion de la encuesta?";
+
+    $lang->search_target_list = array(
+        'title' => 'Titulo',
+        'regdate' => 'Fecha de la participacion',
+        'ipaddress' => 'Direccion IP',
+    );
+?>
