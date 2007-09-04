4afac9015811bc83d37b1c388e7e4cbdde420ba1
sanghunjun <sanghunjun@201d5d3c-b55e-5fd7-737f-ddc643e51545>
2007-09-04 00:13:30 +0000

    스페인어 수정
    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@2486 201d5d3c-b55e-5fd7-737f-ddc643e51545

---
+++ modules/page/lang/es.lang.php
@@ -0,0 +1,11 @@
+<?php
+    /**
+     * @archivo   modules/page/lang/es.lang.php
+     * @autor zero (zero@nzeo.com)
+     * @sumario Paquete del idioma espanol para la pagina de modulo (basico)
+     **/
+
+    $lang->page = "Pagina";
+    $lang->about_page = "Esto es un modulo de blog, lo cual usted puede crear una pagina completa.\nUsando los ultimos u otros widgets, Usted puede crear una pagina dinamica. A traves del componente del editor, tambien puede crear paginas de gran variedad.\nURL de coneccion es el mismo que de los otros modulos como mid=Nombre del modulo.\n Si selcciona como predefinido esta pagina sera la pagina principal del sitio.";
+    $lang->cmd_page_modify = "Modificar";
+?>
