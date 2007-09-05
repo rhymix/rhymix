bb3aa07e547126bd6abd8b27d393869fcfc933d0
sanghunjun <sanghunjun@201d5d3c-b55e-5fd7-737f-ddc643e51545>
2007-09-05 15:25:52 +0000

    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@2535 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/comment/lang/es.lang.php
+++ modules/comment/lang/es.lang.php
@@ -4,7 +4,11 @@
      * @auhor zero <zero@nzeo.com>
      * @sumario  Paquete del idioma español para el módulo de comentarios.
      **/
+<<<<<<< .mine
+    $lang->cmd_delete_checked_comment = 'Eliminar lo seleccionado' ;
+=======
     $lang->cmd_delete_checked_comment = 'Eliminar lo seleccionado ';
+>>>>>>> .r2534
 
     $lang->msg_cart_is_null = 'Selecciona el commentario que desea eliminar';
     $lang->msg_checked_comment_is_deleted = '%d comentario eliminado correctamente.';
