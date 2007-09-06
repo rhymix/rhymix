d1d2d63a118c8c4cdaaea4ba7b9fd6c1228a69a2
sanghunjun <sanghunjun@201d5d3c-b55e-5fd7-737f-ddc643e51545>
2007-09-06 02:12:09 +0000

    수정
    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@2543 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/layout/lang/es.lang.php
+++ modules/layout/lang/es.lang.php
@@ -13,18 +13,13 @@
     $lang->layout_history = "Actualización";
     $lang->layout_info = "Información del Diseño";
     $lang->layout_list = 'Lista de los Diseños';
-
     $lang->menu_count = 'Número del Menú';
 	$lang->downloaded_list = 'Lista de la descarga';
-
-    $lang->menu_count = 'Número del Menú';
-    $lang->downloaded_list = 'Lista de descarga';
-
     $lang->layout_preview_content = 'Los contenidos son mostrados aquí';
     $lang->not_apply_menu = 'Aplicar Diseños';
-
+  
     $lang->cmd_move_to_installed_list = "Ver la lista creada";
-
+  
     $lang->about_downloaded_layouts = "Ver la lista de diseños descargados";
     $lang->about_title = 'Ingresar un nombre fácil de distinguir cuando conecta al módulo';
     $lang->about_not_apply_menu = 'Al seleccionar esta opción, cambia todos los diseños conectados.';
