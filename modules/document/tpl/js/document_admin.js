d087acfec3beb868c3009cddad7449183afef7cf
venister <venister@201d5d3c-b55e-5fd7-737f-ddc643e51545>
2007-09-08 03:38:21 +0000

    추후 확장을 위해 doCheckAll에 doToggleAll을 추가.
    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@2564 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/document/tpl/js/document_admin.js
+++ modules/document/tpl/js/document_admin.js
@@ -1,3 +1,16 @@
+function doToggleAll() {
+    var fo_obj = xGetElementById('fo_list');
+    for(var i=0;i<fo_obj.length;i++) {
+        if(fo_obj[i].name == 'cart'){
+			if( fo_obj[i].checked == true ){
+				fo_obj[i].checked = false;
+			} else {
+				fo_obj[i].checked = true;
+			}
+		}
+    }
+}
+
 function doCheckAll() {
     var fo_obj = xGetElementById('fo_list');
     for(var i=0;i<fo_obj.length;i++) {
