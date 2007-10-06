ff17cebf5222435f36aba48ac424b3cd688612e9
haneul <syhan@cs.washington.edu>
2007-10-06 05:03:25 +0000

    admin language 영어 번역
    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@2683 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/admin/lang/en.lang.php
+++ modules/admin/lang/en.lang.php
@@ -9,11 +9,11 @@
     
     $lang->env_setup = "Setting";
 
-    $lang->env_information = "환경 정보";
-    $lang->current_version = "설치된 버전";
-    $lang->current_path = "설치된 경로";
-    $lang->released_version = "최신 버전";
-    $lang->about_download_link = "설치된 버전보다 최신의 버전이 배포되었습니다. download링크를 클릭하시면 다운 받으실 수 있습니다.";
+    $lang->env_information = "Environment Information";
+    $lang->current_version = "Current Version";
+    $lang->current_path = "Installed Path";
+    $lang->released_version = "Latest Version";
+    $lang->about_download_link = "Newer version of Zerboard XE is available. To download the latest version, click download link.";
     
     $lang->item_module = "Module List";
     $lang->item_addon  = "Addon List";
@@ -35,7 +35,7 @@
 
     $lang->welcome_to_zeroboard_xe = 'Welcome to the admin page of Zeroboard XE';
     $lang->about_admin_page = "Admin page is still being developing,\nWe will add essential contents by accepting many good suggestions during Closebeta.";
-    $lang->about_lang_env = "위 설정한 언어셋을 처음 방문하는 사용자들에게 동일하게 적용하기 위해서는 원하는 언어로 변경후 아래 [저장] 버튼을 클릭하시면 됩니다";
+    $lang->about_lang_env = "To apply selected language set to users as default, click the [Save] button after changing it.";
 
     $lang->zeroboard_xe_user_links = 'Links for Users';
     $lang->zeroboard_xe_developer_links = 'Links for Developers';
