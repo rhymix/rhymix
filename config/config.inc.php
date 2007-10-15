894b9e5f952b2568907e1003c5c91f40f2b74bc1
zero <skklove@gmail.com>
2007-10-15 11:25:27 +0000

    버전 업그레이드
    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@2780 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- config/config.inc.php
+++ config/config.inc.php
@@ -14,7 +14,7 @@
      * 이 내용은 제로보드XE의 버전을 관리자 페이지에 표시하기 위한 용도이며
      * config.inc.php의 수정이 없더라도 공식 릴리즈시에 수정되어 함께 배포되어야 함
      **/
-    define('__ZBXE_VERSION__', '0.2.1');
+    define('__ZBXE_VERSION__', '0.2.2');
 
     /**
      * @brief 디버깅 메세지 출력
@@ -37,7 +37,7 @@
      * 0 : 출력하지 않음
      * 1 : files/_debug_db_query.php 에 연결하여 출력
      **/
-    define('__DEBUG_DB_OUTPUT__', 1);
+    define('__DEBUG_DB_OUTPUT__', 0);
 
     /**
      * @brief DB 쿼리중 정해진 시간을 넘기는 쿼리의 로그 남김
@@ -45,7 +45,7 @@
      * 0 이상 : 단위를 초로 하여 지정된 초 이상의 실행시간이 걸린 쿼리를 로그로 남김
      * 로그파일은 ./files/_db_slow_query.php 파일로 저장됨
      **/
-    define('__LOG_SLOW_QUERY__', 1);
+    define('__LOG_SLOW_QUERY__', 0);
 
     /**
      * @brief ob_gzhandler를 이용한 압축 기능을 강제로 사용하거나 끄는 옵션
