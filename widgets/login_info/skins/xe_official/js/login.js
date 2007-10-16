9144e3a0c2042361d1e51af17b4fd67e862712ea
zero <skklove@gmail.com>
2007-10-16 06:52:01 +0000

    scroll이 조금이라도 되어 있으면 로그인폼의 사용자 아이디에 포커스가 가지 않도록 수정
    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@2797 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- widgets/login_info/skins/xe_official/js/login.js
+++ widgets/login_info/skins/xe_official/js/login.js
@@ -4,14 +4,12 @@ function doFocusUserId(fo_id) {
     if(xGetCookie('user_id')) {
         fo_obj.user_id.value = xGetCookie('user_id');
         fo_obj.remember_user_id.checked = true;
-        try{
-            fo_obj.password.focus();
-        } catch(e) {};
-    } else {
-        try{
-            fo_obj.user_id.focus();
-        } catch(e) {};
     }
+
+    try {
+        if(xScrollTop()) return;
+        fo_obj.user_id.focus();
+    } catch(e) { }
 }
 
 /* 로그인 후 */
