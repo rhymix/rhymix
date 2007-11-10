4e9e4c9e3e2bc757321e8cf570ae71f5bc7b96e2
zero <skklove@gmail.com>
2007-11-10 04:28:36 +0000

    에디터내부에 base href를 제거하여 이미지등을 copy하여 왔을때 해당 경로가 제대로 보이도록 수정
    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@2955 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/editor/tpl/js/editor.js
+++ modules/editor/tpl/js/editor.js
@@ -158,7 +158,7 @@ function editorStart(editor_sequence, primary_key, content_key, resizable, edito
     var contentHtml = ''+
         '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">'+
         '<html lang="ko" xmlns="http://www.w3.org/1999/xhtml><head><meta http-equiv="content-type" content="text/html; charset=utf-8"/>'+
-		'<base href="'+request_uri+'" />'+
+		//'<base href="'+request_uri+'" />'+
         '<link rel="stylesheet" href="'+request_uri+'common/css/default.css" type="text/css" />'+
         '<link rel="stylesheet" href="'+request_uri+editor_path+'css/editor.css" type="text/css" />'+
         '<style style="text/css">'+
