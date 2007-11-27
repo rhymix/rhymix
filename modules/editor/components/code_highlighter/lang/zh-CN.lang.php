ffb5c432937b5677fd5a75e89b3df6dfbd07c8ba
haojilin <haojilin@201d5d3c-b55e-5fd7-737f-ddc643e51545>
2007-11-27 08:18:31 +0000

    중국어 - 추가부분 번역
    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@3090 201d5d3c-b55e-5fd7-737f-ddc643e51545

---
+++ modules/editor/components/code_highlighter/lang/zh-CN.lang.php
@@ -0,0 +1,8 @@
+<?php
+    /**
+     * @file   /modules/editor/components/emoticon/lang/ko.lang.php
+     * @author zero <zero@nzeo.com>
+     * @brief  编辑器(editor) 模块 >  引用句 (quotation) 编辑器语言包
+     **/
+    $lang->code_type = '语言类型';
+?>
