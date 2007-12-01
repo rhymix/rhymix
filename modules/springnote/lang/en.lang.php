8ca3d71d84eb14a9c38cdcd3333b6faa72a09fdb
supershop <supershop@201d5d3c-b55e-5fd7-737f-ddc643e51545>
2007-12-01 12:38:27 +0000

    영어 - 추가 번역
    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@3128 201d5d3c-b55e-5fd7-737f-ddc643e51545

---
+++ modules/springnote/lang/en.lang.php
@@ -0,0 +1,24 @@
+<?php
+    /**
+     * @file   ko.lang.php
+     * @author zero (zero@nzeo.com)
+     * @brief  스프링노트(springnote) 모듈의 기본 언어팩
+     **/
+
+    $lang->springnote = "Springnote";
+    $lang->springnote_openid = "OpenID";
+    $lang->springnote_userkey = "User Key";
+    $lang->springnote_pageid = "Page Number";
+
+    $lang->page_url = "Original URL";
+    $lang->page_modified = "Last Modification";
+    $lang->page_modifier = "Last Modifier";
+
+    $lang->cmd_springnote_list = 'Springnote List';
+    $lang->cmd_view_info = 'Springnote Info';
+
+    $lang->about_springnote = "Springnote is a Wiki Service that Openmaru provides.<br />Springnote ZeroboardXE module displays specific springnote pages as internal documents.";
+    $lang->about_springnote_openid = "Please input your OpenID that created Springnote";
+    $lang->about_springnote_userkey = 'User Key is needed to enable Springnote module.<br />Please input generated User Key after entering your OpenID at [<a href="https://api.openmaru.com/delegate_key/springnote?app_id=www.zeroboard.com&openid=" onclick="window.open(this.href);return false;">Get User Key</a>] .';
+    $lang->about_springnote_pageid = 'Please input pageid if you want to display specific page first.';
+?>
