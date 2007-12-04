b396c811c791c37d01d5e68e075360e6b7cf5f37
haneul <syhan@cs.washington.edu>
2007-12-04 03:09:39 +0000

    [Xquared] added align to allowed attributes
    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@3150 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/editor/skins/xquared/js/xe_interface.js
+++ modules/editor/skins/xquared/js/xe_interface.js
@@ -30,11 +30,8 @@ function editorGetContent_xq(editor_sequence) {
 
 function editorStart_xq(editor, element, editor_sequence, content_key, editor_height, primary_key) {
     editor = new xq.Editor(element);
-    var additionalAttributes = ['editor_component', 'poll_srl','multimedia_src', 'auto_start', 'link_url', 'editor_sequence', 'use_folder', 'folder_opener', 'folder_closer', 'color', 'border_thickness', 'border_color', 'bg_color', 'border_style', 'margin', 'padding', 'bold', 'nx', 'ny', 'gx', 'gy', 'address', 'reg_sinpic', 'language'];
-    var additionalTags = ['embed', 'param', 'object'];
-    additionalAttributes.each( function (item, index) {
-	editor.config.allowedAttributes.push(item); } );
-    additionalTags.each( function (item, index) { editor.config.allowedTags.push(item); } );
+    editor.config.allowedAttributes.push('editor_component', 'poll_srl','multimedia_src', 'auto_start', 'link_url', 'editor_sequence', 'use_folder', 'folder_opener', 'folder_closer', 'color', 'border_thickness', 'border_color', 'bg_color', 'border_style', 'margin', 'padding', 'bold', 'nx', 'ny', 'gx', 'gy', 'address', 'reg_sinpic', 'language','align');
+    editor.config.allowedTags.push('embed', 'param', 'object');
 
     editorRelKeys[editor_sequence] = new Array();
     editorRelKeys[editor_sequence]['editor'] = editor; 
