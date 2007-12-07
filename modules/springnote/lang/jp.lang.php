ad1ae7aecb1a9ac8f96dbe8a504703a0dc14aee7
risapapa <risapapa@201d5d3c-b55e-5fd7-737f-ddc643e51545>
2007-12-07 05:18:28 +0000

    Added Japanese Translation.
    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@3174 201d5d3c-b55e-5fd7-737f-ddc643e51545

---
+++ modules/springnote/lang/jp.lang.php
@@ -0,0 +1,24 @@
+<?php
+    /**
+     * @file   ko.lang.php
+     * @author zero (zero@nzeo.com)
+     * @brief  スプリングノート(springnote) モジュールの基本言語パッケージ 日本語翻訳：RisaPapa
+     **/
+
+    $lang->springnote = "SpringNote";
+    $lang->springnote_openid = "オープンID";
+    $lang->springnote_userkey = "ユーザーKey";
+    $lang->springnote_pageid = "ページ番号";
+
+    $lang->page_url = "元のURL";
+    $lang->page_modified = "最終修正";
+    $lang->page_modifier = "最終修正者";
+
+    $lang->cmd_springnote_list = 'SpringNote リスト';
+    $lang->cmd_view_info = 'SpringNote 情報';
+
+    $lang->about_springnote = "スプリングノートは、「openmaru.com」から提供されるウィキサービスです。<br />スプリングノート連動モジュールは、スプリングノートの特定のページを自分のコンテンツの閲覧することができるようにするモジュールです。";
+    $lang->about_springnote_openid = "スプリングノートでページを作成したオープンIDを入力してください。";
+    $lang->about_springnote_userkey = 'スプリングノートとの連動するためのユーザKeyを入力してください。<br />[<a href="https://api.openmaru.com/delegate_key/springnote?app_id=www.zeroboard.com&openid=" onclick="window.open(this.href);return false;">ユーザーキー受信</a>]をクリックして入力して生成されるキーの値を入力してください。';
+    $lang->about_springnote_pageid = '使用するスプリングノートの中の特定ページを出力したい場合、「pageid」の値を入力してください。';
+?>
