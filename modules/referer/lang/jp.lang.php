649f68e6ad9ac245094c5053f078192eea88379a
risapapa <risapapa@201d5d3c-b55e-5fd7-737f-ddc643e51545>
2007-12-07 05:33:10 +0000

    Added New Japanese Translation File.
    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@3178 201d5d3c-b55e-5fd7-737f-ddc643e51545

---
+++ modules/referer/lang/jp.lang.php
@@ -0,0 +1,10 @@
+<?php
+    /**
+     * @file module/referer/lang/ko.lang.php
+     * @author haneul <haneul0318@gmail.com>
+     * @brief 日本語言語パッケージ　翻訳：RisaPapa
+     */
+
+    $lang->referer = "レファラー";
+    $lang->ranking = "順位";
+?>
