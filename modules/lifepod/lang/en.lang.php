f97abc2016da91f0dcbe7cb01f953089b8b9f770
supershop <supershop@201d5d3c-b55e-5fd7-737f-ddc643e51545>
2007-12-15 13:35:38 +0000

    영어 - 기존 번역문 수정
    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@3364 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/lifepod/lang/en.lang.php
+++ modules/lifepod/lang/en.lang.php
@@ -2,12 +2,12 @@
     /**
      * @file   en.lang.php
      * @author haneul (haneul0318@gmail.com)
-     * @brief  라이프팟(lifepod) 모듈의 기본 언어팩
+     * @brief  default language pack of lifepod module
      **/
 
     $lang->lifepod = "Lifepod";
 
-    $lang->calendar_address = "Calendar's xml address";
+    $lang->calendar_address = "Calendar's XML Address";
     $lang->cmd_lifepod_list = 'Lifepod List';
     $lang->cmd_view_info = 'Lifepod Info.';
 
--- modules/spamfilter/lang/en.lang.php
+++ modules/spamfilter/lang/en.lang.php
@@ -14,19 +14,19 @@
     $lang->denied_ip = "IP to blacklist";
     $lang->interval = "Interval for spam filtering";
     $lang->limit_count = "No. of posting limitation";
-    $lang->check_trackback = "Check trackback";
+    $lang->check_trackback = "Check trackbacks";
     $lang->word = "Word";
 
     // for description word
     $lang->about_interval = "All articles attempted for posting within the assigned time will be blocked.";
     $lang->about_limit_count = "If you exceed the posting limitation,\n your article will be recognized as a spam, and your IP address will be blacklisted.";
     $lang->about_denied_ip = "You can blacklist IP address range like 127.0.0.* by using *.";
-    $lang->about_denied_word = "When you add a word to Word Blacklist,\n articles with that word will not be posted.";
+    $lang->about_denied_word = "When you add a word to Word Blacklist,\n articles including that word will not be posted.";
     $lang->about_check_trackback = "Only the trackback by one IP per an article could be allowed.";
 
     // to post a message
-    $lang->msg_alert_limited_by_config = 'Posting an article within %s second is not allowed.\n If you try it again and again, your IP address may be blacklisted.';
+    $lang->msg_alert_limited_by_config = 'Posting an article within %s second is not allowed.\n If you keep trying, your IP address may be blacklisted.';
     $lang->msg_alert_denied_word = 'The word "%s" is not allowed to be posted.';
-    $lang->msg_alert_registered_denied_ip = 'Your IP address was blacklisted,\n so you may have limitations on normal using of this site.\n If you have any questions on that matter, please contact to the site administrator.'; 
+    $lang->msg_alert_registered_denied_ip = 'Your IP address is blacklisted,\n so you may have limitations on normal using of this site.\n If you have any questions on that matter, please contact to the site administrator.'; 
     $lang->msg_alert_trackback_denied = 'Only one trackback per an article is allowed.';
 ?>
