167d2dabed68f63908c0569d7a66aab622bbed95
chinaskyking <chinaskyking@201d5d3c-b55e-5fd7-737f-ddc643e51545>
2007-12-24 02:53:23 +0000

    SpringNote중문 언어팩.
    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@3411 201d5d3c-b55e-5fd7-737f-ddc643e51545

---
+++ modules/springnote/lang/zh-CN.lang.php
@@ -0,0 +1,30 @@
+<?php
+    /**
+     * @file   ko.lang.php
+     * @author zero (zero@nzeo.com)
+     * @brief  模块springnote基本语言包
+     **/
+
+    $lang->springnote = "SpringNote";
+    $lang->springnote_openid = "OpenID";
+    $lang->springnote_userkey = "Key";
+    $lang->springnote_pageid = "编号";
+    $lang->springnote_pageid_setup = '设定编号';
+    $lang->springnote_pageid_option_only = '显示指定页面(不显示目录)';
+    $lang->springnote_pageid_option_list = '显示指定页面 (显示目录)';
+    $lang->springnote_domain = "设定域名";
+
+    $lang->page_url = "原本 URL";
+    $lang->page_modified = "最终修改";
+    $lang->page_modifier = "最终修改者";
+
+    $lang->cmd_springnote_list = 'SpringNote目录';
+    $lang->cmd_view_info = 'SpringNote信息';
+
+    $lang->about_springnote = "SpringNote是Openmaru提供的WIKI服务。<br />SpringNote zeroboardXE模块是特定页面显示成内部文件的模块。";
+    $lang->about_springnote_openid = "请输入SpringNote作者的OpenID。";
+    $lang->about_springnote_userkey = '请输入连接SpringNote的Key。<br />点击[<a href="https://api.openmaru.com/delegate_key/springnote?app_id=www.zeroboard.com&openid=" onclick="window.open(this.href);return false;">生成Key</a>]后输入OpenID生成Key。';
+    $lang->about_springnote_pageid = '显示指定SpringNote中的页面时输入编号。';
+    $lang->about_springnote_pageid_setup = '设定显示指定页面部包括目录，或者不包括目录。';
+    $lang->about_springnote_domain = '设定其他SpringNote.  (http://域名.springnote.com 中输入《域名》值。)';
+?>
