b73c48ecbcedb51f4c07b856e6d547f5346d4ac3
zero <skklove@gmail.com>
2008-01-08 03:33:25 +0000

    제로보드XE의 기본 스타일에서 body,p,div,ul,ol,li,dd,dt,span,fieldset,h1,h2,h3,h4,h5,h6 에 대해서만 margin, padding을 0으로 수정. 이로 인해 기존 스킨들에서 문제가 생길 수 있으며 계속 디버깅 해야 함
    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@3492 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/editor/tpl/js/editor.js
+++ modules/editor/tpl/js/editor.js
@@ -118,11 +118,10 @@ function editorStart(editor_sequence, primary_key, content_key, editor_height) {
         '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">'+
         '<html lang="ko" xmlns="http://www.w3.org/1999/xhtml><head><meta http-equiv="content-type" content="text/html; charset=utf-8"/>'+
 		//'<base href="'+request_uri+'" />'+
-        '<link rel="stylesheet" href="'+request_uri+'common/css/default.css" type="text/css" />'+
-        '<link rel="stylesheet" href="'+request_uri+editor_path+'css/editor.css" type="text/css" />'+
+        //'<link rel="stylesheet" href="'+request_uri+'common/css/default.css" type="text/css" />'+
+        //'<link rel="stylesheet" href="'+request_uri+editor_path+'css/editor.css" type="text/css" />'+
         '<style style="text/css">'+
-        'body {font-size:9pt;margin:0px; height:'+editor_height+'px; background-color:transparent; line-height:140%;}'+
-        'blockquote, ol, ul { margin-left:40px; }'+
+        'body {font-size:9pt;height:'+editor_height+'px; background-color:transparent; line-height:140%;}'+
         '</style>'+
         '</head><body editor_sequence="'+editor_sequence+'">'+
         content+
