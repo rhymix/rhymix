2c85caaff900ff508a1f3244918ba164cf712489
zero <skklove@gmail.com>
2008-01-28 06:54:05 +0000

    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@3605 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- config/func.inc.php
+++ config/func.inc.php
@@ -470,4 +470,36 @@
         if(function_exists('php_sapi_name') && php_sapi_name()=='cgi') return preg_replace('/index.php/i','',$_SERVER['PATH_INFO']);
         return preg_replace('/index.php/i','',$_SERVER['SCRIPT_NAME']);
     }
+
+    function utf8RawUrlDecode ($source) {
+        $decodedStr = "";
+        $pos = 0;
+        $len = strlen ($source);
+        while ($pos < $len) {
+            $charAt = substr ($source, $pos, 1);
+            if ($charAt == '%') {
+                $pos++;
+                $charAt = substr ($source, $pos, 1);
+                if ($charAt == 'u') {
+                    // we got a unicode character
+                    $pos++;
+                    $unicodeHexVal = substr ($source, $pos, 4);
+                    $unicode = hexdec ($unicodeHexVal);
+                    $entity = "&#". $unicode . ';';
+                    $decodedStr .= utf8_encode ($entity);
+                    $pos += 4;
+                }
+                else {
+                    // we have an escaped ascii character
+                    $hexVal = substr ($source, $pos, 2);
+                    $decodedStr .= chr (hexdec ($hexVal));
+                    $pos += 2;
+                }
+            } else {
+                $decodedStr .= $charAt;
+                $pos++;
+            }
+        }
+        return $decodedStr;
+    }
 ?>
