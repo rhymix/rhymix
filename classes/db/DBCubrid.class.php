8151402ad5dd29828b7584162762c8d4dc186713
rokmcssu <rokmcssu@201d5d3c-b55e-5fd7-737f-ddc643e51545>
2008-01-29 07:04:39 +0000

    #394 큐브리드 모듈의 페이징 버그 오류 수정
    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@3617 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- classes/db/DBCubrid.class.php
+++ classes/db/DBCubrid.class.php
@@ -573,16 +573,16 @@
                     $index_list[] = sprintf('%s %s', $val[0], $val[1]);
                   }
                   if(count($index_list)) $query .= ' order by '.implode(',',$index_list);
-                  $query = sprintf('%s for orderby_num() between %d and %d', $query, $start_count, $list_count);
+                  $query = sprintf('%s for orderby_num() between %d and %d', $query, $start_count + 1, $list_count + $start_count);
                 }
                 else {
                   if (count($output->groups))
-                    $query = sprintf('%s having groupby_num() between %d and %d', $query, $start_count, $list_count);
+                    $query = sprintf('%s having groupby_num() between %d and %d', $query, $start_count + 1, $list_count + $start_count);
                   else {
                     if ($condition)
-                      $query = sprintf('%s and inst_num() between %d and %d', $query, $start_count, $list_count);
+                      $query = sprintf('%s and inst_num() between %d and %d', $query, $start_count + 1, $list_count + $start_count);
                     else 
-                      $query = sprintf('%s where inst_num() between %d and %d', $query, $start_count, $list_count);
+                      $query = sprintf('%s where inst_num() between %d and %d', $query, $start_count + 1, $list_count + $start_count);
                   }
                 }
 
@@ -696,16 +696,16 @@
                 $index_list[] = sprintf('%s %s', $val[0], $val[1]);
               }
               if(count($index_list)) $query .= ' order by '.implode(',',$index_list);
-              $query = sprintf('%s for orderby_num() between %d and %d', $query, $start_count, $list_count);
+              $query = sprintf('%s for orderby_num() between %d and %d', $query, $start_count + 1, $list_count + $start_count);
             }
             else {
               if (count($output->groups))
-                $query = sprintf('%s having groupby_num() between %d and %d', $query, $start_count, $list_count);
+                $query = sprintf('%s having groupby_num() between %d and %d', $query, $start_count + 1, $list_count + $start_count);
               else {
                 if ($condition)
-                  $query = sprintf('%s and inst_num() between %d and %d', $query, $start_count, $list_count);
+                  $query = sprintf('%s and inst_num() between %d and %d', $query, $start_count + 1, $list_count + $start_count);
                 else 
-                  $query = sprintf('%s where inst_num() between %d and %d', $query, $start_count, $list_count);
+                  $query = sprintf('%s where inst_num() between %d and %d', $query, $start_count + 1, $list_count + $start_count);
               }
             }
 
