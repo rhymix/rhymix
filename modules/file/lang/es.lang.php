fffcc7d8d6264b452154a525b2ed4e24632d8d79
sspa3141 <sspa3141@201d5d3c-b55e-5fd7-737f-ddc643e51545>
2008-02-14 05:29:21 +0000

    에스파냐어, 러시아어 번역
    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@3656 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/file/lang/es.lang.php
+++ modules/file/lang/es.lang.php
@@ -16,7 +16,7 @@
     $lang->allowed_filesize = 'Límite del tamaño del archivo adjunto';
     $lang->allowed_attach_size = 'Límite del tamaño total de los archivos adjuntos por documento';
     $lang->allowed_filetypes = 'Tipos de archivos permitidos';
-    $lang->enable_download_group = '다운로드 가능 그룹';
+    $lang->enable_download_group = 'Descargar permitió grupos';
 
     $lang->about_allowed_filesize = 'Puede definir el límite del tamaño del archivo adjunto. (exceptuando el administrador)';
     $lang->about_allowed_attach_size = 'Puede definir el límite del tamaño total de los archivos adjuntos por documento. (exceptuando el administrador)';
@@ -26,7 +26,7 @@
     $lang->cmd_move_to_document = 'Mover hacia el doncumento';
     $lang->cmd_download = 'Descargar';
 
-    $lang->msg_not_permitted_download = '다운로드 할 수 있는 권한이 없습니다';
+    $lang->msg_not_permitted_download = 'Usted no tiene ningún permiso para descargar';
     $lang->msg_cart_is_null = 'Seleccione el archivo a eliminar';
     $lang->msg_checked_file_is_deleted = 'Total de %d archivos eliminados';
     $lang->msg_exceeds_limit_size = 'Ha excedido el límite del tamaño total de los archivos adjuntos';
