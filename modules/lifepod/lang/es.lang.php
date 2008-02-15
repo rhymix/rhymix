75630968bae39350ffc630bc2bf40b4ec99ee7bc
sspa3141 <sspa3141@201d5d3c-b55e-5fd7-737f-ddc643e51545>
2008-02-15 06:15:02 +0000

    에스파냐어(스페인어) 번역
    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@3681 201d5d3c-b55e-5fd7-737f-ddc643e51545

---
+++ modules/lifepod/lang/es.lang.php
@@ -0,0 +1,15 @@
+<?php
+    /**
+     * @file   es.lang.php
+     * @author haneul (haneul0318@gmail.com)
+     * @brief  Paquete de idioma por defecto de lifepod módulo
+     **/
+
+    $lang->lifepod = "Lifepod";
+
+    $lang->calendar_address = "Calendario de la Dirección XML";
+    $lang->cmd_lifepod_list = 'Lista Lifepod';
+    $lang->cmd_view_info = 'Lifepod Información.';
+
+    $lang->about_lifepod = "Lifepod calendario es un servicio proporcionado por Openmaru Studio.<br />Lifepod Zeroboard XE módulo desplays Lifepod específicas de calendarios como documentos internos.";
+?>
