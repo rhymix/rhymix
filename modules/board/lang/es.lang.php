6c1dd09f6be6ecb595566779a396a9613e87eb5f
zero <skklove@gmail.com>
2008-02-15 00:55:27 +0000

    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@3676 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/board/lang/es.lang.php
+++ modules/board/lang/es.lang.php
@@ -7,7 +7,7 @@
 
     $lang->board = "Tablero"; 
 
-    $lang->except_notice = "공지사항 제외";
+    $lang->except_notice = "Excluir Avisos";
     $lang->cmd_manage_menu = 'Manejo del menú';
     $lang->cmd_make_child = 'Agregar sub categoría';
     $lang->cmd_enable_move_category = "Cambiar la posición de la categoría. (arrastrar y soltar luego de haber selecionado)";
@@ -34,7 +34,7 @@
     $lang->about_category_group_srls = 'Solo se verán los grupos seleccionados en la actual categoría. (Abrir manualmente en archivo xml para la exposición)';
     $lang->about_layout_setup = 'Puede modificar manualmente el código del diseño de tableros. Insertar o modificar el código de widget.';
     $lang->about_board_category = 'Puede crear las categorias de tableros.<br />Cuando no funciona la categoría de tableros, rehacer el archivo caché manualmente para solucionar.';
-    $lang->about_except_notice = "목록 상단에 늘 나타나는 공지사항을 일반 목록에서 공지사항을 출력하지 않도록 합니다.";
+    $lang->about_except_notice = "Aviso de los artículos no se mostrará en la lista normal.";
     $lang->about_board = "Este módulo es para crear y manejar los tableros.\nLuego de crear un Tablero, seleciona el nombre del módulo para la configuración más detallada.\nSea cuidadoso con el nombre del módulo, ya que ese nombre va a ser la dirección URL. (ej : http://dominio/zb/?mid=nombre del módulo)";
     $lang->about_consultation = "상담 기능은 관리권한이 없는 회원은 자신이 쓴 글만 보이도록 하는 기능입니다\n단 상담기능 사용시 비회원 글쓰기는 자동으로 금지됩니다.";
 ?>
