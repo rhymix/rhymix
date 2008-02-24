2cf27f7afd98e184aab30f850fbbd0b675ac5b43
risapapa <risapapa@201d5d3c-b55e-5fd7-737f-ddc643e51545>
2008-02-24 17:40:19 +0000

    Added and Corrected Japanese Translation(modules/springnote/lang/jp.lang.php).
    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@3769 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/springnote/lang/jp.lang.php
+++ modules/springnote/lang/jp.lang.php
@@ -9,10 +9,10 @@
     $lang->springnote_openid = "オープンID";
     $lang->springnote_userkey = "ユーザーKey";
     $lang->springnote_pageid = "ページ番号";
-    $lang->springnote_pageid_setup = '페이지 번호 설정';
-    $lang->springnote_pageid_option_only = '지정된 페이지만 보여주기 (목록 보이지 않음)';
-    $lang->springnote_pageid_option_list = '지정된 페이지부터 보이기 (목록 보임)';
-    $lang->springnote_domain = "도메인 지정";
+    $lang->springnote_pageid_setup = 'ページ番号設定';
+    $lang->springnote_pageid_option_only = '指定ページのみ表示（リスト非表示）';
+    $lang->springnote_pageid_option_list = '指定ページから表示（リスト表示）';
+    $lang->springnote_domain = "ドメイン指定";
 
     $lang->page_url = "元のURL";
     $lang->page_modified = "最終修正";
@@ -23,8 +23,8 @@
 
     $lang->about_springnote = "スプリングノートは、「openmaru.com」から提供されるウィキサービスです。<br />スプリングノート連動モジュールは、スプリングノートの特定のページを自分のコンテンツの閲覧することができるようにするモジュールです。";
     $lang->about_springnote_openid = "スプリングノートでページを作成したオープンIDを入力してください。";
-    $lang->about_springnote_userkey = 'スプリングノートとの連動するためのユーザKeyを入力してください。<br />[<a href="https://api.openmaru.com/delegate_key/springnote?app_id=www.zeroboard.com&openid=" onclick="window.open(this.href);return false;">ユーザーキー受信</a>]をクリックして入力して生成されるキーの値を入力してください。';
-    $lang->about_springnote_pageid = '使用するスプリングノートの中の特定ページを出力したい場合、「pageid」の値を入力してください。';
-    $lang->about_springnote_pageid_setup = '設定することができます\'選択されたページのみを表示（隠すリスト） \' 、 \'ショーから選択されたページ（番組リスト） \' etcときにページ番号を設定します。';
-    $lang->about_springnote_domain = 'springnote他のドメインの設定をお勧めします。 （ドメインのみを入力してくださいフォームからの\' http://domain.springnote.com \' ）';
+    $lang->about_springnote_userkey = 'スプリングノートと連動させるためのユーザKeyを入力してください。<br />[<a href="https://api.openmaru.com/delegate_key/springnote?app_id=www.zeroboard.com&openid=" onclick="window.open(this.href);return false;">ユーザーキー受信</a>]をクリックして入力して生成されたキーの値を入力してください。';
+    $lang->about_springnote_pageid = '使用するスプリングノートの特定ページを出力させたい場合、「pageid」の値を入力してください。';
+    $lang->about_springnote_pageid_setup = 'ページ番号を指定する際に、該当ページのみ表示（リストを含まない）、該当ページから表示（リストを含む）などを指定することができます。';
+    $lang->about_springnote_domain = '自分のスプリングノートではない他のドメインのスプリングノートを入力することができます（ 「http://domain.springnote.com」でドメインの値を入力してください ）。';
 ?>
