b380e571b1007c49dcecc7d155503c95ce106be7
duvent <duvent@201d5d3c-b55e-5fd7-737f-ddc643e51545>
2008-02-25 19:14:08 +0000

    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@3797 201d5d3c-b55e-5fd7-737f-ddc643e51545

---
+++ modules/admin/lang/fr.lang.php
@@ -0,0 +1,88 @@
+<?php
+    /**
+     * @file   fr.lang.php
+     * @author zero (zero@nzeo.com)
+     * @brief  Paquet de la Langue française (Les Mots base seulement sont  inclus ici)
+     **/
+
+    $lang->admin_info = 'Information d\'Administrateur';
+    $lang->admin_index = 'Page de l\'indice pour l\'Administrateur';
+
+    $lang->module_category_title = array(
+        'service' => 'Modules pour Service',
+        'manager' => 'Modules pour Sdministrer',
+        'utility' => 'Modules des Utilités',
+        'accessory' => 'Modules Additionnels',
+        'base' => 'Modules par Défaut',
+    );
+
+    $lang->newest_news = "Nouvelles Dernières";
+    
+    $lang->env_setup = "Configuration";
+
+    $lang->env_information = "Informations de l\'Environnement";
+    $lang->current_version = "Version Courante";
+    $lang->current_path = "Chemin Installé";
+    $lang->released_version = "Version Dernière";
+    $lang->about_download_link = "Nouvelle version est disponible. Pour télécharger la version dernière, cliquez le lien à télécharger.";
+    
+    $lang->item_module = "Liste des Modules";
+    $lang->item_addon  = "Liste des Compagnons";
+    $lang->item_widget = "Liste des Widgets";
+    $lang->item_layout = "Liste des Mises en Pages";
+
+    $lang->module_name = "Nom de Module";
+    $lang->addon_name = "Nom de Compagnon";
+    $lang->version = "Version";
+    $lang->author = "Auteur";
+    $lang->table_count = "Somme de Tables";
+    $lang->installed_path = "Chemin Installé";
+
+    $lang->cmd_shortcut_management = "Editer Menu";
+
+    $lang->msg_is_not_administrator = 'Administrateur seulement';
+    $lang->msg_manage_module_cannot_delete = 'Raccourci pour modules, compagnons, mises en page, widgets ne peuvent pas être supprimé';
+    $lang->msg_default_act_is_null = 'Raccourci ne peut pas être enrégistré  parce que l\'Action par défaut de l\'Administrateur n\'est pas configuré';
+
+    $lang->welcome_to_zeroboard_xe = 'Bienvenue sur la Page d\'Administration du  Zeroboard XE';
+    $lang->about_admin_page = "La Page d\'Administration est encore en train de développer,\nNous allons ajouter des contenus essentiels par accepter beauoup de bons suggestions pendant Closebeta.";
+    $lang->about_lang_env = "Pour appliquer  par défaut le Répertoire de la Langue choisie aux utilisateurs, cliquez le boutton [Conserver] après changer ça.";
+
+    $lang->zeroboard_xe_user_links = 'Liens pour les Utilisateurs';
+    $lang->zeroboard_xe_developer_links = 'Liens pour les Développeur';
+
+    $lang->xe_user_links = array(
+        'Official Website' => 'http://www.zeroboard.com',
+        //'Close Beta website' => 'http://spring.zeroboard.com',
+        //'Module morgue' => 'http://www.zeroboard.com',
+        //'Addon morgue' => 'http://www.zeroboard.com',
+        //'Widget morgue' => 'http://www.zeroboard.com',
+        //'Module Skin morgue' => 'http://www.zeroboard.com',
+        //'Widget Skin morgue' => 'http://www.zeroboard.com',
+        //'Layout Skin morgue' => 'http://www.zeroboard.com',
+    );
+
+    $lang->xe_developer_links = array(
+        //'Manual' => 'http://www.zeroboard.com/wiki/manual',
+        "Developer's forum" => 'http://spring.zeroboard.com',
+        'Issue Tracking' => 'http://trac.zeroboard.com',
+        'SVN Repository' => 'http://svn.zeroboard.com',
+        'doxygen document' => 'http://doc.zeroboard.com',
+        'PDF Documentation' => 'http://doc.zeroboard.com/zeroboard_xe.pdf',
+    );
+
+    $lang->zeroboard_xe_usefulness_module = 'Useful Modules';
+    $lang->xe_usefulness_modules = array(
+        'dispEditorAdminIndex' => 'Editor Manager',
+        'dispDocumentAdminList' => 'Article Manager',
+        'dispCommentAdminList' => 'Comment Manager',
+        'dispFileAdminList' => 'Attachment Manager',
+        'dispPollAdminList' => 'Poll Manager',
+        'dispSpamfilterAdminConfig' => 'Spam Filter Manager',
+        'dispCounterAdminIndex' => 'Counter Log',
+
+    );
+
+    $lang->xe_license = 'Zeroboard XE complies with the GPL';
+    $lang->about_shortcut = 'You may remove shortcuts of modules which are registered on frequently using module list';
+?>
