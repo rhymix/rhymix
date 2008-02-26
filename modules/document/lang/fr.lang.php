a07a6879c4adf818bfb98289abbb9cc199eaea55
duvent <duvent@201d5d3c-b55e-5fd7-737f-ddc643e51545>
2008-02-26 15:27:42 +0000

    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@3832 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/document/lang/fr.lang.php
+++ modules/document/lang/fr.lang.php
@@ -16,37 +16,37 @@
 
     $lang->cmd_search_next = 'Recherche Suivante';
 
-    $lang->cmd_temp_save = 'Conservé temporaire';
+    $lang->cmd_temp_save = 'Conserver temporairement';
 
-	$lang->cmd_toggle_checked_document = 'Reverse selected items';
-    $lang->cmd_delete_checked_document = 'Delete selected';
-    $lang->cmd_document_do = 'You would..';
+	$lang->cmd_toggle_checked_document = 'Renverser les items choisis';
+    $lang->cmd_delete_checked_document = 'Supprimer les documents choisis';
+    $lang->cmd_document_do = 'Vous voudriez..';
 
-    $lang->msg_cart_is_null = 'Please select the articles to delete';
-    $lang->msg_category_not_moved = 'Could not be moved';
-    $lang->msg_is_secret = 'This article is secret';
-    $lang->msg_checked_document_is_deleted = '%d article(s) was(were) deleted';
+    $lang->msg_cart_is_null = 'Choisissez les articles à supprimer, SVP.';
+    $lang->msg_category_not_moved = 'Ne peut(peuvent) pas être bougé(s)';
+    $lang->msg_is_secret = 'Cet article est secret';
+    $lang->msg_checked_document_is_deleted = '%d article(s) est(sont) supprimé';
 
     // Search targets in admin page
         $lang->search_target_list = array(
-        'title' => 'Subject',
-        'content' => 'Content',
-        'user_id' => 'User ID',
-        'member_srl' => 'Member Serial Number',
-        'user_name' => 'User Name',
-        'nick_name' => 'Nickname',
-        'email_address' => 'Email',
-        'homepage' => 'Homepage',
+        'title' => 'Titre',
+        'content' => 'Contenu',
+        'user_id' => 'Compte d\'Utilisateur',
+        'member_srl' => 'Nombre Sériel de Membre',
+        'user_name' => 'Nom d\'Utilisateur',
+        'nick_name' => 'Surnom',
+        'email_address' => 'Courriel',
+        'homepage' => 'Page d\'accueil',
         'is_notice' => 'Notice',
         'is_secret' => 'Secret',
         'tags' => 'Tag',
-        'readed_count' => 'Number of Views (over)',
-        'voted_count' => 'Number of Votes (over)',
-        'comment_count ' => 'Number of Comments (over)',
-        'trackback_count ' => 'Number of Trackbacks (over)',
-        'uploaded_count ' => 'Number of Attachments (over)',
-        'regdate' => 'Date',
-        'last_update' => 'Last Modified Date',
-        'ipaddress' => 'IP Address',
+        'readed_count' => 'Somme de Vues (dessus)',
+        'voted_count' => 'Somme de Recommandations (dessus)',
+        'comment_count ' => 'Somme de Commentaires (dessus)',
+        'trackback_count ' => 'Somme de Retroliens (dessus)',
+        'uploaded_count ' => 'Somme de Fichiers Attachés (dessus)',
+        'regdate' => 'Jour d\'Enregistrement',
+        'last_update' => 'La Derniere Mise a Jour',
+        'ipaddress' => 'Addresse IP',
     );
 ?>
