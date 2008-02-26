2d50a97e18a66475306db0225fc7a14d1cc3466b
duvent <duvent@201d5d3c-b55e-5fd7-737f-ddc643e51545>
2008-02-26 13:54:48 +0000

    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@3831 201d5d3c-b55e-5fd7-737f-ddc643e51545

---
+++ modules/document/lang/fr.lang.php
@@ -0,0 +1,52 @@
+<?php
+    /**
+     * @file   modules/document/lang/fr.lang.php
+     * @author zero <zero@nzeo.com>  Traduit par Pierre Duvent <Pierreduvent@gmail.com>
+     * @brief  Paquet de la langue française fondamentale du module du Document
+     **/
+
+    $lang->document_list = 'Liste des Documents';
+    $lang->thumbnail_type = 'Type de la Vignette';
+    $lang->thumbnail_crop = 'Rogner';
+    $lang->thumbnail_ratio = 'Proportion';
+    $lang->cmd_delete_all_thumbnail = 'Supprimer toutes les vignettes';
+    $lang->move_target_module = "Module à déménager";
+    $lang->title_bold = 'Gras';
+    $lang->title_color = 'Couleur';
+
+    $lang->cmd_search_next = 'Recherche Suivante';
+
+    $lang->cmd_temp_save = 'Conservé temporaire';
+
+	$lang->cmd_toggle_checked_document = 'Reverse selected items';
+    $lang->cmd_delete_checked_document = 'Delete selected';
+    $lang->cmd_document_do = 'You would..';
+
+    $lang->msg_cart_is_null = 'Please select the articles to delete';
+    $lang->msg_category_not_moved = 'Could not be moved';
+    $lang->msg_is_secret = 'This article is secret';
+    $lang->msg_checked_document_is_deleted = '%d article(s) was(were) deleted';
+
+    // Search targets in admin page
+        $lang->search_target_list = array(
+        'title' => 'Subject',
+        'content' => 'Content',
+        'user_id' => 'User ID',
+        'member_srl' => 'Member Serial Number',
+        'user_name' => 'User Name',
+        'nick_name' => 'Nickname',
+        'email_address' => 'Email',
+        'homepage' => 'Homepage',
+        'is_notice' => 'Notice',
+        'is_secret' => 'Secret',
+        'tags' => 'Tag',
+        'readed_count' => 'Number of Views (over)',
+        'voted_count' => 'Number of Votes (over)',
+        'comment_count ' => 'Number of Comments (over)',
+        'trackback_count ' => 'Number of Trackbacks (over)',
+        'uploaded_count ' => 'Number of Attachments (over)',
+        'regdate' => 'Date',
+        'last_update' => 'Last Modified Date',
+        'ipaddress' => 'IP Address',
+    );
+?>
