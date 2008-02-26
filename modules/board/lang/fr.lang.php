6c8e14f7d8fa4ce8868aa96ff2e36a0586bb3354
duvent <duvent@201d5d3c-b55e-5fd7-737f-ddc643e51545>
2008-02-26 07:49:51 +0000

    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@3822 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/board/lang/fr.lang.php
+++ modules/board/lang/fr.lang.php
@@ -2,10 +2,10 @@
     /**
      * @file   fr.lang.php
      * @author zero (zero@nzeo.com) Traduit par Pierre Duvent(PierreDuvent@gamil.com)
-     * @brief  Paquet de la langue fondamental pour le module de Tableaux d\'Affichage
+     * @brief  Paquet de la langue fondamental pour le module de Panneau d\'Affichage
      **/
 
-    $lang->board = "Tableaux d\'Affichage"; 
+    $lang->board = "Panneau"; 
 
     $lang->except_notice = "Exclure des Notices";
 
@@ -22,17 +22,17 @@
     $lang->consultation = 'Consultation';
 
     // Mots utilisés en bouton
-    $lang->cmd_board_list = 'Liste des Tableaux d\'Affichage';
-    $lang->cmd_module_config = 'Configuration commun pour les Tableaux d\'Affichage';
-    $lang->cmd_view_info = 'Information des Tableaux d\'Affichage';
+    $lang->cmd_board_list = 'Liste des Panneaux';
+    $lang->cmd_module_config = 'Configuration commun pour les Panneau';
+    $lang->cmd_view_info = 'Information des Panneau';
 
     // blah blah..
     $lang->about_category_title = 'Entrez le nom de la catégorie, SVP.';
     $lang->about_expand = 'Si vous cochez la case, ce sera toujours étendu';
     $lang->about_category_group_srls = 'Le groupe sélectionné seulement pourra voir ces catégories. (Ouvrir manuellement le fiche de xml, c\'est l\'exposer)';
-    $lang->about_layout_setup = 'Vous pouvez manuellement modifier le code de Mise en Page du Tableau d\'Affichage. Insérez ou arrangez le code de Widget n\'importe où vous voulez.';
+    $lang->about_layout_setup = 'Vous pouvez manuellement modifier le code de Mise en Page du Panneau. Insérez ou arrangez le code de Widget n\'importe où vous voulez.';
     $lang->about_board_category = 'Vous pouvez créer des catégories d\'affichage dans le tableau. Quand la catégorie d\'affichage est cassé, essayez manuellement rétablir la fichier cache.';
     $lang->about_except_notice = "L\'Article de Notice ne sera exposé sur la liste normale.";
-    $lang->about_board = "Ce module se sert à créer et arranger des Tableaux d\'Affichage.\nAprès avoir créé un module, si vous cliquez le nom sur le leste, vous pouvez configurer specifiquement.\nFaites attention quand vous choisissez le nom du module du Tableaux d\'Affichage, car ce sera URL. (ex : http://domain/zb/?mid=nom_de_module)"; 
+    $lang->about_board = "Ce module se sert à créer et arranger des Panneau.\nAprès avoir créé un module, si vous cliquez le nom sur le leste, vous pouvez configurer specifiquement.\nFaites attention quand vous choisissez le nom du module du Panneau, car ce sera URL. (ex : http://domain/zb/?mid=nom_de_module)"; 
 	$lang->about_consultation = "Les membres non-administratifs verront seulement les ariticles d\'eux-même.\nNon-membres ne pourraient pas écrire des articles quand la Consultation est appliqué.";
 ?>
