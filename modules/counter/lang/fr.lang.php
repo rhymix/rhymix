4d110c2fbd87497071d0351a09070fe9d5e7b438
duvent <duvent@201d5d3c-b55e-5fd7-737f-ddc643e51545>
2008-02-26 12:56:59 +0000

    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@3828 201d5d3c-b55e-5fd7-737f-ddc643e51545

---
+++ modules/counter/lang/fr.lang.php
@@ -0,0 +1,22 @@
+<?php
+    /**
+     * @file   modules/counter/lang/fr.lang.php
+     * @author zero <zero@nzeo.com>  Traduit par Pierre Duvent <PierreDuvent@gmail.com>
+     * @brief  Paquet de la langue française (Contenu fondamental est seulement lesté)
+     **/
+
+    $lang->counter = "Coumpteur";
+    $lang->cmd_select_date = 'Choisir un Jour';
+    $lang->cmd_select_counter_type = array(
+        'hour' => 'Par Heure',
+        'day' => 'Par Jour',
+        'month' => 'Par Mois',
+        'year' => 'Par An',
+    );
+
+    $lang->total_counter = 'Statut Total';
+    $lang->selected_day_counter = 'Statut du Jour Choisi';
+
+    $lang->unique_visitor = 'Visiteurs';
+    $lang->pageview = 'Vues de la Page';
+?>
