d6720850cc9cb2ee3e60184e01d9ea522f4611a7
duvent <duvent@201d5d3c-b55e-5fd7-737f-ddc643e51545>
2008-03-02 20:00:46 +0000

    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@3890 201d5d3c-b55e-5fd7-737f-ddc643e51545

---
+++ modules/integration_search/lang/fr.lang.php
@@ -0,0 +1,31 @@
+<?php
+    /**
+     * @file   modules/integration_search/lang/fr.lang.php
+     * @author zero <zero@nzeo.com>  Traduit par Pierre Duvent <PierreDuvent@gmail.com>
+     * @brief  Paquet de la langue française  (Choses fondamentales seulement)
+     **/
+
+    $lang->integration_search = "Recherche Intégrée";
+
+    $lang->sample_code = "Code d\'échantillon";
+    $lang->about_target_module = "Les modules choisis seulement seront les cibles. Faites attention quand vous mettez la permission, SVP.";
+    $lang->about_sample_code = "Vous pouvez utiliser la Reherche Intégrée en ajoutant le code ci-dessus dans le Mise en Page ou d\'autres choses.";
+    $lang->msg_no_keyword = "Entrez le mot de clé à rechercher, SVP.";
+
+    $lang->is_result_text = "Il y a <strong>%d</strong> résultat(s) pour <strong>'%s'</strong>";
+
+    $lang->is_search_option = array(
+        'title' => 'Titre',
+        'content' => 'Contenu',
+        'title_content' => 'Titre+Contenu',
+        'comment' => 'Commentaire',
+        'tag' => 'Tags',
+    );
+
+    $lang->is_sort_option = array(
+        'regdate' => 'Enrégistré',
+        'comment_count' => 'Commentaires',
+        'readed_count' => 'Vues',
+        'voted_count' => 'Recommendés',
+    );
+?>
