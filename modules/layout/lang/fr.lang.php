f902cb1d26eb3f78d86c48b10334403baf47dcc9
duvent <duvent@201d5d3c-b55e-5fd7-737f-ddc643e51545>
2008-03-03 00:35:57 +0000

    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@3896 201d5d3c-b55e-5fd7-737f-ddc643e51545

---
+++ modules/layout/lang/fr.lang.php
@@ -0,0 +1,32 @@
+<?php
+    /**
+     * @file   modules/layout/lang/fr.lang.php
+     * @author zero <zero@nzeo.com>  Traduit par Pierre Duvent <PierreDuvent@gmail.com>
+     * @brief  Paquet de la langue fondamentale du module pour Mise en Page
+     **/
+
+    $lang->cmd_layout_management = 'Configuration du Mise en Page';
+    $lang->cmd_layout_edit = 'Edit Layout';
+
+    $lang->layout_name = 'Layout Name';
+    $lang->layout_maker = "Layout Developer";
+    $lang->layout_history = "Updates";
+    $lang->layout_info = "Layout Info";
+    $lang->layout_list = 'Layout List';
+    $lang->menu_count = 'Menus';
+    $lang->downloaded_list = 'Download List';
+    $lang->layout_preview_content = 'The content gets displayed here';
+    $lang->not_apply_menu = 'Apply Layouts';
+
+    $lang->cmd_move_to_installed_list = "View created list";
+
+    $lang->about_downloaded_layouts = "List of downloaded layouts";
+    $lang->about_title = 'Please input the title that is easy to verify when connecting to module';
+    $lang->about_not_apply_menu = 'All connected module\'s layout will be changed by checking this option.';
+
+    $lang->about_layout = "Layout module helps you to create the site's layout easily.<br />By using layout setting and menu connection, website's completed shape will be displayed with various modules.<br />* Those layouts which are unabled to delete or modify are the blog or other module's layout. ";
+    $lang->about_layout_code = 
+        "It will be applied to the service when you save the layout code after editing it.
+        Please first preview your code and then save it.
+        You can refer grammar of Zeroboard XE's template from <a href=\"#\" onclick=\"winopen('http://trac.zeroboard.com/trac/wiki/TemplateHandler');return false;\">ZeroboardXE Template</a>.";
+?>
