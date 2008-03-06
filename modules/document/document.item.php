4d5c9246afbeb6e25221e7ed6ddc23b9af23bab9
bnu <bnufactory@gmail.com>
2008-03-06 21:12:55 +0000

    #409 서명이 높이제한 이하일 때 맞추어 표시하도록 수정
    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@3913 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/document/document.item.php
+++ modules/document/document.item.php
@@ -644,7 +644,7 @@
             }
             if($signature) {
                 $max_signature_height = $GLOBALS['__member_signature_max_height'];
-                if($max_signature_height) $signature = sprintf('<div style="height:%dpx;overflow-y:auto;overflow-x:hidden;">%s</div>',$max_signature_height, $signature);
+                if($max_signature_height) $signature = sprintf('<div style="max-height:%dpx;overflow:auto;overflow-x:hidden;height:expression(this.scrollHeight > %d ? \'%dpx\': \'auto\')">%s</div>', $max_signature_height, $max_signature_height, $max_signature_height, $signature);
             }
 
             return $signature;
