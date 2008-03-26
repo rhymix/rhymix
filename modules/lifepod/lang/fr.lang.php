dbcc5d8a5b5fc06289c67c533d6caa15ad79d90e
duvent <duvent@201d5d3c-b55e-5fd7-737f-ddc643e51545>
2008-03-26 12:58:54 +0000

    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@4047 201d5d3c-b55e-5fd7-737f-ddc643e51545

---
+++ modules/lifepod/lang/fr.lang.php
@@ -0,0 +1,15 @@
+<?php
+    /**
+     * @file   fr.lang.php
+     * @author haneul (haneul0318@gmail.com)  Traduit par Pierre Duvent (PierreDuvent@gmail.com)
+     * @brief  default language pack of lifepod module
+     **/
+
+    $lang->lifepod = "Lifepod";
+
+    $lang->calendar_address = "Addresse de XML du Calendrier";
+    $lang->cmd_lifepod_list = 'Liste de Lifepod';
+    $lang->cmd_view_info = 'Information de Lifepod.';
+
+    $lang->about_lifepod = "Lifepod est un service pourvu par Openmaru Studio.<br />Le module de Lifepod Zeroboard XE se présente les calendriers spécifiques de Lifepod comme les documents internes.";
+?>
