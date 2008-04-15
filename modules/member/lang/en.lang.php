44a4a7ba4a711d126e664b9a76a471460a695db8
haneul <syhan@cs.washington.edu>
2008-04-15 08:16:55 +0000

    로긴 유지 메시지 번역, fileencoding 변경된 것 수정
    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@4106 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/member/lang/en.lang.php
+++ modules/member/lang/en.lang.php
@@ -63,8 +63,8 @@
     $lang->webmaster_name = "Webmaster's Name";
     $lang->webmaster_email = "Webmaster's Email";
 
-    $lang->about_keep_signed = '브라우저를 닫더라도 로그인이 계속 유지될 수 있습니다.\n\n로그인 유지 기능을 사용할 경우 다음 접속부터는 로그인을 하실 필요가 없습니다.\n\n단, 게임방, 학교 등 공공장소에서 이용시 개인정보가 유출될 수 있으니 꼭 로그아웃을 해주세요';
-	$lang->about_webmaster_name = "Please input webmaster's name which will be used for certification mails or other site administration. (default : webmaster)";
+    $lang->about_keep_signed = 'Though the browser is closed, you might be kept signed.\n\nIf you use this feature at the shared computer, your personal information might be accessed by others, thus do not use this feature at the shared computer. ';
+    $lang->about_webmaster_name = "Please input webmaster's name which will be used for certification mails or other site administration. (default : webmaster)";
     $lang->about_webmaster_email = "Please input webmaster's email address.";
 
     $lang->search_target_list = array(
