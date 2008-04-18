1c37f1f49a00e4e941413871b579192736ef197a
aerofleet <aerofleet@201d5d3c-b55e-5fd7-737f-ddc643e51545>
2008-04-18 07:54:02 +0000

    번역수정
    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@4120 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- addons/member_extra_info/lang/en.lang.php
+++ addons/member_extra_info/lang/en.lang.php
@@ -1,3 +1,3 @@
 <?php
-    $lang->alert_new_message_arrived = 'You got a new message. Do you want to check now?';
+    $lang->alert_new_message_arrived = 'You have a new message. Do you want to check now?';
 ?>
