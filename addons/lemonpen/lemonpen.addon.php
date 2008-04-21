028d3b26f2c49d05769a193828b002ff9e970c68
zero <skklove@gmail.com>
2008-04-21 06:55:20 +0000

    yongirl님의 지적으로 레몬펜 애드온의 소스 코드 주석 오류 수정
    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@4125 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- addons/lemonpen/lemonpen.addon.php
+++ addons/lemonpen/lemonpen.addon.php
@@ -2,14 +2,10 @@
     if(!defined("__ZBXE__")) exit();
 
     /**
-     * @file rainbow.addon.php
+     * @file lemonpen.addon.php
      * @author zero (zero@nzeo.com)
-     * @brief Rainbow link addon
-     *
-     * 링크가 걸린 텍스트에 마우스 오버를 하면 무지개색으로 변하게 하는 애드온입니다.
-     * rainbow.js 파일만 추가하는 것으로 끝납니다.
-     * rainbow.js는 http://www.dynamicdrive.com에서 제작하였으며 저작권을 가지고 있습니다.
-     * before_display_content 에서만 요청이 됩니다.
+     * @brief LemonPen addon
+     * 스프링노트의 레몬펜을 사이트에 설치하는 애드온입니다
      **/
 
     if(Context::getResponseMethod()=="XMLRPC") return;
