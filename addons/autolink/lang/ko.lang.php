fbfd975aae816c02bff8ec59e2b0199a16814f02
zero <skklove@gmail.com>
2008-06-04 07:10:21 +0000

    자동링크 애드온의 언어파일 제거
    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@4233 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- addons/autolink/lang/ko.lang.php
+++
@@ -1,9 +0,0 @@
-<?php
-    /**
-     * @file   addons/autolink/lang/ko.lang.php
-     * @author zero (zero@zeroboard.com)
-     * @brief  한국어 언어팩 (기본적인 내용만 수록)
-     **/
-    $lang->open_cur_window = "현재창";
-    $lang->open_new_window = "새창";
-?>
--- addons/autolink/lang/ru.lang.php
+++
@@ -1,9 +0,0 @@
-<?php
-    /**
-     * @file   addons/autolink/lang/ru.lang.php
-     * @author zero (zero@zeroboard.com)
-     * @brief  한국어 언어팩 (기본적인 내용만 수록)
-     **/
-    $lang->open_cur_window = "текущем окне";
-    $lang->open_new_window = "Новое окно";
-?>
