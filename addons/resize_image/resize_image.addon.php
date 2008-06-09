e2b3f664d55d32d3a1f3a36b006cf489f7e5d555
zero <skklove@gmail.com>
2008-06-09 10:08:00 +0000

    본문내 이미지 크기 조절 애드온의 동작 방식 변경 및 원본 이미지 보기 기능 변경
    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@4256 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- addons/resize_image/resize_image.addon.php
+++ addons/resize_image/resize_image.addon.php
@@ -9,6 +9,5 @@
 
     if($called_position == 'after_module_proc' && Context::getResponseMethod()!="XMLRPC") {
         Context::addJsFile('./addons/resize_image/js/resize_image.js');
-        Context::addCSSFile('./addons/resize_image/css/resize_image.css');
     }
 ?>
