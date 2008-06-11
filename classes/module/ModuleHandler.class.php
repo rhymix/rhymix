b80ed9eb00cebd405a4f86cbd53b3ad21786f230
zero <skklove@gmail.com>
2008-06-11 03:25:58 +0000

    1. 로그인과 관련된 html에서 중복 id를 제거 2. 여러개의 게시글을 호출할때 20개까지로 제한이 되는 문제점 제거 3. ModuleHandler내의 잘못된 변수 기입 수정
    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@4269 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- classes/module/ModuleHandler.class.php
+++ classes/module/ModuleHandler.class.php
@@ -104,7 +104,7 @@
 
             // 모듈정보에 module과 mid를 강제로 지정
             $this->module_info->module = $this->module;
-            $this->mid = $this->mid;
+            $this->module_info->mid = $this->mid;
 
             // 여기까지도 모듈 정보를 찾지 못했다면 깔끔하게 시스템 오류 표시
             if(!$this->module) $this->error = 'msg_module_is_not_exists';
