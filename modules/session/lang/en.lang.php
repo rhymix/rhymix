f1b149a52d8568e00b0905f50bc92abb75628f3a
supershop <supershop@201d5d3c-b55e-5fd7-737f-ddc643e51545>
2008-06-19 11:42:58 +0000

    영어 - 추가 번역
    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@4311 201d5d3c-b55e-5fd7-737f-ddc643e51545

---
+++ modules/session/lang/en.lang.php
@@ -0,0 +1,13 @@
+<?php
+    /**
+     * @file   modules/session/lang/ko.lang.php
+     * @author zero <zero@nzeo.com>
+     * @brief  한국어 언어팩 (기본적인 내용만 수록)
+     **/
+
+    $lang->session = 'Session';
+    $lang->about_session = "This module manages sessions\nFrequent session arrangement will provide better environment for your site.";
+
+    $lang->cmd_clear_session = 'Arrange Session';
+    $lang->session_cleared = 'Session data have been arranged succesfully';
+?>
