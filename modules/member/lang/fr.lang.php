f238db7625c46ca09cecc765009d55635a827272
duvent <duvent@201d5d3c-b55e-5fd7-737f-ddc643e51545>
2008-07-19 02:32:53 +0000

    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@4385 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/member/lang/fr.lang.php
+++ modules/member/lang/fr.lang.php
@@ -6,8 +6,8 @@
      **/
 
     $lang->member = 'Membre';
-    $lang->member_default_info = 'Informations fondamentales';
-    $lang->member_extend_info = 'Informations additionnelles';
+    $lang->member_default_info = 'Information fondamentale';
+    $lang->member_extend_info = 'Information additionnelle';
     $lang->default_group_1 = "Membre Associé";
     $lang->default_group_2 = "Membre Régulier";
     $lang->admin_group = "Groupe des administrateurs";
@@ -38,7 +38,7 @@
     $lang->image_mark_max_height = 'Hauteur Maximum';
 	$lang->signature_max_height = 'Hauteur Maximum de la Signature';
     $lang->enable_openid = 'Permettre OpenID';
-    $lang->enable_join = 'Permettre à s\'inscrire';
+    $lang->enable_join = 'Permettre à inscrire';
     $lang->enable_confirm = 'Utiliser Authentification par mél';
 	$lang->enable_ssl = 'Utiliser SSL';
 	$lang->security_sign_in = 'Ouvrir une Session en utilisant sécurité rehaussé';
@@ -49,7 +49,7 @@
     $lang->redirect_url = 'URL après Inscription';
     $lang->agreement = 'Accord d\'Inscription comme Membre';
 	$lang->accept_agreement = 'D\'accord';
-    $lang->member_info = 'Informations du Membre';
+    $lang->member_info = 'Information de Membre';
     $lang->current_password = 'Mot de Passe courrant';
     $lang->openid = 'OpenID';
 
@@ -70,116 +70,117 @@
         'extra_vars' => 'Variables extra ',
     );
 
-    $lang->cmd_login = 'Login';
-    $lang->cmd_logout = 'Logout';
-    $lang->cmd_signup = 'Join';
-    $lang->cmd_modify_member_info = 'Modify Member Info';
-    $lang->cmd_modify_member_password = 'Modify Password';
-    $lang->cmd_view_member_info = 'Member Info';
-    $lang->cmd_leave = 'Leave';
-    $lang->cmd_find_member_account = 'Find Account Info';
+    $lang->cmd_login = 'Connexion ';
+    $lang->cmd_logout = 'Déconnexion';
+    $lang->cmd_signup = 'Inscription';
+    $lang->cmd_modify_member_info = 'Modifier Mon Information';
+    $lang->cmd_modify_member_password = 'Modifier le Mot de Passe';
+    $lang->cmd_view_member_info = 'Voir Mon Information';
+    $lang->cmd_leave = 'Quitter';
+    $lang->cmd_find_member_account = 'J\'ai perdu le compte / le mot de passe';
 
-    $lang->cmd_member_list = 'Member List';
-    $lang->cmd_module_config = 'Default Setting';
+    $lang->cmd_member_list = 'Liste de Membres';
+    $lang->cmd_module_config = 'Mise par Défaut';
     $lang->cmd_member_group = 'Manage Groups';
-    $lang->cmd_send_mail = 'Send Mail';
-    $lang->cmd_manage_id = 'Manage Prohibited IDs';
-    $lang->cmd_manage_form = 'Manage Join Form';
-    $lang->cmd_view_own_document = 'Written Articles';
-    $lang->cmd_trace_document = 'Trace Written Articles';
-    $lang->cmd_trace_comment = 'Trace Written Comments';
-    $lang->cmd_view_scrapped_document = 'Scraps';
-    $lang->cmd_view_saved_document = 'Saved Articles';
-    $lang->cmd_send_email = 'Send Mail';
+    $lang->cmd_send_mail = 'Envoyer des Méls';
+    $lang->cmd_manage_id = 'Administrer les Comptes Interdits';
+    $lang->cmd_manage_form = 'Administrer la Forme d\'Inscription';
+    $lang->cmd_view_own_document = 'Voir les Articles écrits';
+    $lang->cmd_trace_document = 'Tracer les Articles écrits';
+    $lang->cmd_trace_comment = 'Trace les Commentaires écrits';
+    $lang->cmd_view_scrapped_document = 'Voir les Coupures';
+    $lang->cmd_view_saved_document = 'Voir les Articles conservés';
+    $lang->cmd_send_email = 'Envoyer des Méls';
 
-    $lang->msg_email_not_exists = "You have entered an invalid email address";
+    $lang->msg_email_not_exists = "Vous avez entré un adresse mél invalide.";
 
-    $lang->msg_alreay_scrapped = 'This article is already scrapped';
+    $lang->msg_alreay_scrapped = 'Vous avez déjà la coupure de cet article';
 
-    $lang->msg_cart_is_null = 'Please select the target';
-    $lang->msg_checked_file_is_deleted = '%d attached file(s) is(are) deleted';
+    $lang->msg_cart_is_null = 'Choisissez l\'Objet, S.V.P.';
+    $lang->msg_checked_file_is_deleted = '%d fichier(s) attaché(s) est(sont) suprimé(s)';
 
-    $lang->msg_find_account_title = 'Account Info';
-    $lang->msg_find_account_info = 'This is requested account info.';
-    $lang->msg_find_account_comment = 'The password will be modified as above one as you click below link.<br />Please modify the password after login.';
-    $lang->msg_confirm_account_title = 'Authentication confirmation mail';
-    $lang->msg_confirm_account_info = 'This is the registered account information:';
-    $lang->msg_confirm_account_comment = 'Click the following confirmation link to complete your sign up.';
-    $lang->msg_auth_mail_sent = 'The certification mail has been sent to %s. Please check your mail.';
-    $lang->msg_confirm_mail_sent = 'We just sent you a confirmation email to %s. Click on the confirmation link in the email to complete your sign up.';
-    $lang->msg_invalid_auth_key = 'This is an invalid request of certification.<br />Please retry finding account info or contact to administrator.';
-    $lang->msg_success_authed = 'Your account has been successfully certificated and logged on.\n Please modify the password to your own one with the password in the mail.';
-    $lang->msg_success_confirmed = 'The autentication completed successfully.';
+    $lang->msg_find_account_title = 'Information de compte';
+    $lang->msg_find_account_info = 'Voilà votre information de compte.';
+    $lang->msg_find_account_comment = 'Le Mot de passe sera modifié comme celui ci-dessus si vous cliquez le lien ci-dessous.<br />Modifiez le Mot de passe après ouvrir la connexion, S.V.P.';
+    $lang->msg_confirm_account_title = 'Mél à confirmer l\'Authentification';
+    $lang->msg_confirm_account_info = 'Voilà votre Information enregistré du Compte:';
+    $lang->msg_confirm_account_comment = 'Cliquez le lien de confirmation suivant pour compléter votre inscription.';
+    $lang->msg_auth_mail_sent = 'Le mél de certification a été envoyé  à %s. Vérifiez votre mél.';
+    $lang->msg_confirm_mail_sent = 'On a justement envoyé un mél de confirmation à %s. Cliquez sur le lien de confirmation dans le mél pour compléter votre inscription.';
+    $lang->msg_invalid_auth_key = 'Cette requête à certifier est invalide.<br />Essayez encore une fois à retrouver votre information de compte ou contactez l\'administrateur.';
+    $lang->msg_success_authed = 'Votre compte a été certifié avec succès et ouvert une session. \n Modifiez le mot de passe après vous ouvrez une session en utilisant le mot de passe dans le mél.';
+    $lang->msg_success_confirmed = 'L\'authentification est complétée avec succèss.';
 
-    $lang->msg_new_member = 'Add Member';
-    $lang->msg_update_member = 'Modify Member Info';
-    $lang->msg_leave_member = 'Leave';
-    $lang->msg_group_is_null = 'There is no registered group';
-    $lang->msg_not_delete_default = 'Default items cannot be deleted';
-    $lang->msg_not_exists_member = "Invalid member";
-    $lang->msg_cannot_delete_admin = 'Admin ID cannot be deleted. Please remove the ID from administration and try again.';
-    $lang->msg_exists_user_id = 'This ID already exists. Please try with another one.';
-    $lang->msg_exists_email_address = 'This email address already exists. Please try with another one.';
-    $lang->msg_exists_nick_name = 'This nickname already exists. Please try with another one.';
-    $lang->msg_signup_disabled = 'You are not able to sign up';
-    $lang->msg_already_logged = 'You have already signed up';
-    $lang->msg_not_logged = 'Please login first';
-    $lang->msg_insert_group_name = 'Please input the name of group';
-    $lang->msg_check_group = 'Please select the group';
+    $lang->msg_new_member = 'Ajouter un membre';
+    $lang->msg_update_member = 'Modifier l\'Information de Membre';
+    $lang->msg_leave_member = 'Quitter';
+    $lang->msg_group_is_null = 'Il n\'y a pas de groupe enrégistré';
+    $lang->msg_not_delete_default = 'Elément par Défaut ne pourra pas être supprimé';
+    $lang->msg_not_exists_member = "Membre Invalide";
+    $lang->msg_cannot_delete_admin = 'Le Compte de l\'Administrateur ne pourra pas être supprimé. Disqualifiez l\'administration du compte et essayez encore une fois.';
+    $lang->msg_exists_user_id = 'Le compte existe déjà. Essayez un autre.';
+    $lang->msg_exists_email_address = 'L\'adresse mél existe déjà. Essayez une autre.';
+    $lang->msg_exists_nick_name = 'Le surnom existe déjà. Essayez un autre.';
+    $lang->msg_signup_disabled = 'Vous ne pouvez pas inscrire.';
+    $lang->msg_already_logged = 'Vous avez déjà inscrit.';
+    $lang->msg_not_logged = 'Ouvrez une session d\'abord';
+    $lang->msg_insert_group_name = 'Entrez le nom de groupe, S.V.P.';
+    $lang->msg_check_group = 'Choisissez le groupe';
 
-    $lang->msg_not_uploaded_profile_image = 'Profile image could not be registered';
-    $lang->msg_not_uploaded_image_name = 'Image name could not be registered';
-    $lang->msg_not_uploaded_image_mark = 'Image mark could not be registered';
+    $lang->msg_not_uploaded_profile_image = 'L\'image de Profil n\'a pas pu être enrégistré';
+    $lang->msg_not_uploaded_image_name = 'Le nom d\'image n\'a pas pu être enrégistré';
+    $lang->msg_not_uploaded_image_mark = 'La marque d\'image n\'a pas pu être enrégistré';
 
-    $lang->msg_accept_agreement = 'You have to agree the agreement'; 
+    $lang->msg_accept_agreement = 'Vous devez agréer l\'accord'; 
 
-    $lang->msg_user_denied = 'You have entered a prohibited ID';
-    $lang->msg_user_not_confirmed = 'You are not authenticated yet. Please check your email.';
-    $lang->msg_user_limited = 'You have entered an ID that can be used after %s';
+    $lang->msg_user_denied = 'Vous avez entré un compte interdit';
+    $lang->msg_user_not_confirmed = 'Vous n\'avez pas encore authentifié. Verifiez votre mél, S.V.P.';
+    $lang->msg_user_limited = 'Vous avez entré un compte qui peut êrtre utilisé depuis %s';
 
-    $lang->about_user_id = 'User ID should be 3~20 letters long and consist of alphabet+number with alphabet as first letter.';
-    $lang->about_password = 'Password should be 6~20 letters long';
-    $lang->about_user_name = 'Name should be 2~20 letters long';
-    $lang->about_nick_name = 'Nickname should be 2~20 letters long';
-    $lang->about_email_address = 'Email address will be used to modify/find password after email certification';
-    $lang->about_homepage = 'Please input if you have your websites';
-    $lang->about_blog_url = 'Please input if you have your blogs';
-    $lang->about_birthday = 'Please input your birth date';
-    $lang->about_allow_mailing = "If you don't join mailing, you will not able to receive group mail";
-    $lang->about_denied = 'Check to prohibit the ID';
-    $lang->about_is_admin = 'Check to give Superadmin permission';
-    $lang->about_description = "Administrator's memo about members";
-    $lang->about_group = 'An ID can belong to many groups';
+    $lang->about_user_id = 'Le compte d\'utilisateur doit être long de 3~20 lettres et se composer des alphabets et des chiffres avec un alphabet comme le premier lettre.';
+    $lang->about_password = 'Le Mot de passe doit être long de 6~20 lettres.';
+    $lang->about_user_name = 'Le Nom doit être long de 2~20 lettres.';
+    $lang->about_nick_name = 'Le Surnom doit être long de 2~20 lettres.';
+    $lang->about_email_address = 'L\'Adresse mél sera utilisé à modifier/trouver le mot de passe après la certification en mél.';
+    $lang->about_homepage = 'Entrez  si vous avez un site web.';
+    $lang->about_blog_url = 'Entrez si vous avez un blogue.';
+    $lang->about_birthday = 'Entrez votre anniversaire.';
+    $lang->about_allow_mailing = "Si vous n'inscrivez pas sur mailing, vous ne pouvez pas recevoir mél du groupe.";
+    $lang->about_denied = 'Cocher pour interdire le compte';
+    $lang->about_is_admin = 'Cocher pour autoriser la permission de Superadministrateur';
+    $lang->about_description = "L\'Administrateur peut noter sur le membre";
+    $lang->about_group = 'Un compte peut appartenir aux plusieurs groupes.';
 
-    $lang->about_column_type = 'Please set the format of additional sign up form';
-    $lang->about_column_name = 'Please input English name that can be used in template (name as variable)';
-    $lang->about_column_title = 'This will be displayed on sign up or modifying/viewing member info form';
-    $lang->about_default_value = 'You can set default values';
-    $lang->about_active = 'You have to check on active items to show on sign up form';
-    $lang->about_form_description = 'If you input in description form, it will be displayed on sign up form';
-    $lang->about_required = 'If you check, it will be essential item for sign up';
+    $lang->about_column_type = 'Choisissez la formule du champ que vous voulez ajouter';
+    $lang->about_column_name = 'Entrez le nom composé en alphabet qui peut être utilisé dans le modèle (nom comme variable)';
+    $lang->about_column_title = 'Ce titre sera exposé sur la formule pour inscrire ou sur l\'écran pour modifier/voir les informations de membre';
+    $lang->about_default_value = 'Vous pouvez mettre les valeurs par défaut';
+    $lang->about_active = 'Vous devez cocher sur les éléments actifs pour les exposer sur la formule d\'inscription';
+    $lang->about_form_description = 'Si vous entrez dans le champ du description, elle sera exposé sur la formule d\'incription';
+    $lang->about_required = 'Si vous cochez, ce sera obligatoire pour inscrire';
 
-    $lang->about_enable_openid = 'Allow users to sign up as OpenID';
-    $lang->about_enable_join = 'Allow users to sign up';
-    $lang->about_enable_confirm = 'Send confirmation email to complete signup.';
-    $lang->about_enable_ssl = 'Personal information from Join/Modify Member Info/Login can be sent as SSL(https) mode if server provides SSL service.';
-    $lang->about_limit_day = 'You can limit certification date after sign up';
-    $lang->about_limit_date = 'User cannot login until assigned date';
-    $lang->about_after_login_url = 'You can set URL after login. Blank means current page.';
-    $lang->about_after_logout_url = 'You can set URL after logout. Blank means current page.';
-    $lang->about_redirect_url = 'Please input URL where users will go after sign up. When this is empty, it will be set as the previous page of sign up page.';
-    $lang->about_agreement = "Sign up agreement will only be displayed when it's not empty";
+    $lang->about_enable_openid = 'Cochez si vous voulez permettre OpenID';
+    $lang->about_enable_join = 'Cochez si vous voulez permettre à inscrire';
+    $lang->about_enable_confirm = 'Envoyer mél de confirmation pour compléter inscription.';
+    $lang->about_enable_ssl = 'Les informations personnelles (Inscription / Modification des informations du membre / Connexion) peuvent être envoyées comme mode SSL(https) si le serveur offre le service SSL.';
+    $lang->about_limit_day = 'Vous pouvez limiter le jour de certification après l\'inscription';
+    $lang->about_limit_date = 'Utilisateur ne peut pas ouverir la connexion jusqu\'au jour assigné';
+    $lang->about_after_login_url = 'Vous pouvez indiquer URL où l\'on va après la connexion. Le vide signifie la page courrante.';
+    $lang->about_after_logout_url = 'Vous pouvez indiquer URL où l\'on va après la déconnexion. Le vide signifie la page courrante.';
+    $lang->about_redirect_url = 'Entrez URL où l\'utilisateur irra après l\'inscription, S.V.P. Si c\'est vide, ce sera la page précédente de la page d\'inscrire.';
+    $lang->about_agreement = "L\'Accord d\'Inscription comme Membre sera exposé seulement quand il n'est pas vide.";
 
-    $lang->about_image_name = "Allow users to use image name instead of text name";
-    $lang->about_image_mark = "Allow users to use mark in front of their names";
-    $lang->about_profile_image = 'Allow users to use profile images';
-    $lang->about_accept_agreement = "I have read the agreement and agree"; 
+    $lang->about_image_name = "Permettre les utilisateurs à utiliser une image pour présenter leurs noms au lieu des lettres";
+    $lang->about_image_mark = "Permettre les utilisateurs à utiliser une marque devent leurs noms";
+    $lang->about_profile_image = 'Permettre les utilisateurs à utiliser une image de profil';
+    $lang->about_accept_agreement = "J\'ai lu l\'Accord et je suis d\'accord."; 
 
-    $lang->about_member_default = 'It will be set as default group on sign up';
+    $lang->about_member_default = 'On sera dans ce groupe après l\'inscription par défaut';
 
-    $lang->about_openid = 'When you join as OpenID, basic info like ID or email address will be saved on this site, process for password and certification management will be done on current OpenID offering service';
-    $lang->about_openid_leave = 'The secession of OpenID means deletion of your member info from this site.<br />If you login after secession, it will be recognized as a new member, so you will no longer have the permission for your ex-written articles.';
+    $lang->about_openid = 'Si vous inscrivez avec OpenID, vos informations primaires comme le Compte(ID) soit l\'address mél sera gardés sur ce site. Mais le procès pour le Mot de Passe et la certification sera fait sur le service courrant qui offre OpenID';
+    $lang->about_openid_leave = 'La sécession de OpenID nous fait effacer vos informations du membre dans notre site.<br />Si vous ouvrez la connexion après la sécession, nous vous reconnaîtrons comme un nouveau membre, c\'est-à-dire, vous n\'aura plus la permission sur les articles que vous avez écrits avant.';
 
-    $lang->about_member = "This is a module for creating/modifying/deleting members and managing group or join form.\nYou can manage members by creating new groups, and get additional information by managing join form";
-    $lang->about_find_member_account = 'Your account info will be noticed by registered email address.<br />Please input email address which you have input on registration, and press "Find Account Info" button.<br />';
+    $lang->about_find_member_account = 'L\information de votre compte sera annoncé par l\'address mél d\'inscription. <br />Entrez l\'address mél que vous avez entré en inscrivant et appuyez le bouton "Recherce l\'Information du Compte" button.<br />';
+
+	$lang->about_member = "C'est le module pour administrer des membres avec lequel vous pouvez créer/modifier/effacer des membres ou administrer les groupes et la formule d'inscription.\nVous pouvez administrer membres par création un nouveau groupe, ou gagner les informations additionnelles par l'administration la formule d'inscrioption.";
 ?>
