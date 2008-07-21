5e3e63cdd1e12c8c6db2fb97fb6a1148b9af4f9c
duvent <duvent@201d5d3c-b55e-5fd7-737f-ddc643e51545>
2008-07-21 10:20:28 +0000

    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@4400 201d5d3c-b55e-5fd7-737f-ddc643e51545

---
+++ modules/message/lang/fr.lang.php
@@ -0,0 +1,10 @@
+<?php
+    /**
+     * @file   modules/message/lang/en.lang.php
+     * @author zero <zero@nzeo.com>
+     * @brief  English language pack (basic)
+     **/
+
+    $lang->message = 'Montrer Erreurs';
+    $lang->about_skin = "Vous pouvez choisir un habillage de message des erreurs";
+?>
