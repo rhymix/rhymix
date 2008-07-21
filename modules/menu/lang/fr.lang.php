3b72bd138cc329dd121519177830838043239459
duvent <duvent@201d5d3c-b55e-5fd7-737f-ddc643e51545>
2008-07-21 06:48:50 +0000

    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@4396 201d5d3c-b55e-5fd7-737f-ddc643e51545

---
+++ modules/menu/lang/fr.lang.php
@@ -0,0 +1,54 @@
+<?php
+    /**
+     * @file   modules/menu/lang/fr.lang.php
+     * @author zero <zero@nzeo.com>
+     * @brief  Paque de la langue primaire du module de Menu
+	 * @Traduit par duvent <duvent@gamail.com>
+     **/
+
+    $lang->cmd_menu_insert = 'Créer Menu';
+    $lang->cmd_menu_management = 'Configurer des Menu';
+
+    $lang->menu = 'Menu'; 
+    $lang->menu_count = 'Numéro de menu';
+    $lang->menu_management = 'Administration de Menu';
+    $lang->depth = 'Niveau';
+    $lang->parent_menu_name = 'Nom de Menu parent';
+    $lang->menu_name = 'Nom de Menu';
+    $lang->menu_srl = 'Numéro sériel de Menu';
+    $lang->menu_id = 'Nom d\'Identité de Menu';
+    $lang->menu_url = 'URL de Menu';
+    $lang->menu_open_window = 'Ouvrire une nouvelle fenêtre';
+    $lang->menu_expand = 'Étendre';
+    $lang->menu_img_btn = 'Button en Image';
+    $lang->menu_normal_btn = 'Normal';
+    $lang->menu_hover_btn = 'Survolé';
+    $lang->menu_active_btn = 'Choisi';
+    $lang->menu_group_srls = 'Groupes qui peuvent approcher';
+    $lang->layout_maker = "Auteur de la Mise en Page";
+    $lang->layout_history = "Histoire des Mises à Jour";
+    $lang->layout_info = "Information de la Mise en Page";
+    $lang->layout_list = 'Liste des Mises en Page';
+    $lang->downloaded_list = 'Liste de Téléchargement';
+    $lang->limit_menu_depth = 'Niveau permis exposer';
+
+    $lang->cmd_make_child = 'Ajouter un menu enfant';
+    $lang->cmd_move_to_installed_list = "Voir la liste créé";
+    $lang->cmd_enable_move_menu = "Bouger le Menu (glisser-déposer un menu après cocher)";
+    $lang->cmd_search_mid = "Rechercher mid";
+
+    $lang->msg_cannot_delete_for_child = 'Un menu qui a des enfants ne peut pas être effacé.';
+
+    $lang->about_title = 'Entrez le titre qui est facile à vérifier quand on le connecte à un module.';
+    $lang->about_menu_management = "Management de Menu vous permet composer le menu dans la mise en page management choisie.\nVous pouvez créer le menu jusqu'au niveau permis et entrer des informations détaillées en cliquant le menu.\nMenu sera étendu en cliquant l'image de dossier.\nSi le menu n'est pas représenté normalement, rafraîchir les informations en cliquant le button \"Recréer fichier cache\".\n* Menu cré qui passe plus que le niveau permis ne pourra pas être représenté correctement.";
+    $lang->about_menu_name = 'Le nom sera représenté comme le nom de menu si ce n\'est pas le bouton en image ou le bouton pour administrer.';
+    $lang->about_menu_url = "C'est le URL où l'on bouge quand on choisit le menu.<br />Vous pouvez enter la valeur d'identité(nom d'idendité) seulement pour lier à un autre module.<br />Si nul contenu n'existe, rien aura lieu même si l'on clique le menu.";
+    $lang->about_menu_open_window = 'Vous pouvez faire ouvrir une page dans une nouvelle fenêtre quand le menu est cliqué.';
+    $lang->about_menu_expand = 'L\'Arbre de Menu(tree_menu.js) peut faire resté le menu étendu toujours.';
+    $lang->about_menu_img_btn = 'Si vous enrégistez un bouton en image, l\'image remplacera automatiquement le bouton en texte, et ce sera représenté dans la Mise en Page.';
+    $lang->about_menu_group_srls = 'Si vous choisissez un groupe, les membres de ce groupe seulement peuvent voir le menu. (Si un fichier xml est ouvert, il sera vu.)';
+
+    $lang->about_menu = "Le Module de Menu vous aidrera à créer un site complet par le management commode qui arrange les modules créés et liens à la mise en page sans aucun travaux manuels.\nMenu n'est pas un Manager du Site, mais il a seulement l'information qui peut lier les modules à la mise en page, et on peut représenter les menu en formes diverses par la mise en page.";
+
+    $lang->alert_image_only = "Fichiers d'image seulement peuvent être enrégistrés.";
+?>
