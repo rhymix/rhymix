c6ac6523103cc7ce81e18c0479cabbb3f0cbe490
duvent <duvent@201d5d3c-b55e-5fd7-737f-ddc643e51545>
2008-07-29 03:29:03 +0000

    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@4420 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/krzip/lang/fr.lang.php
+++ modules/krzip/lang/fr.lang.php
@@ -12,9 +12,9 @@
     $lang->krzip_server_query = "Chemin de serveur pour vérifier le code postal";
 
     // descriptions
-    $lang->about_krzip_server_hostname = "Entrez le domaine de serveur pour vérifier le code postal et recevoir le liste des résultats, SVP.";
-    $lang->about_krzip_server_port = "Entrez le nombre de port de serveur pour vérifier le code postal, SVP";
-    $lang->about_krzip_server_query = "Entrez l'URL à requérir qui sera requis pour vérifier le code postal";
+    $lang->about_krzip_server_hostname = "Entrez le domaine de serveur pour vérifier le code postal et recevoir le liste des résultats, S.V.P.";
+    $lang->about_krzip_server_port = "Entrez le numéro de port de serveur pour vérifier le code postal, SVP";
+    $lang->about_krzip_server_query = "Entrez l'URL à Requérir qui sera requis pour vérifier le code postal";
 
     // messages des erreurs
     $lang->msg_not_exists_addr = "Objectifs à rechercher n'existe pas";
