ca4c3845ace21555d0b3fd693a7d5cd3f9aef9e5
duvent <duvent@201d5d3c-b55e-5fd7-737f-ddc643e51545>
2008-07-30 10:39:36 +0000

    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@4427 201d5d3c-b55e-5fd7-737f-ddc643e51545

---
+++ modules/page/lang/fr.lang.php
@@ -0,0 +1,11 @@
+<?php
+    /**
+     * @file   modules/page/lang/fr.lang.php
+     * @author zero (zero@nzeo.com) Traduit par Pierre Duvent (PierreDuvent@gmail.com)
+     * @brief  Paquet du langage fondamental pour le module de Page
+     **/
+
+    $lang->page = "Page";
+    $lang->about_page = "C'est un module qui peut créer une page complet.\nVous pouvez créer une page dynamique en utilisant des gadgets des Documents derniers ou d'autres. Vous pouvez aussi créer une page avec variété par le composant d'editeur.\nL'URL d'accès est égal celui d'autre module comme mid=module.\nSi c'est choisi par défaut, ce sera la première page du site.";
+    $lang->cmd_page_modify = "Modifier";
+?>
