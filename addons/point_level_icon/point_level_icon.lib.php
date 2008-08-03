bf03c31634ebf79f1aa3ade8f1bd132226836d57
bnu <bnufactory@gmail.com>
2008-08-03 01:08:46 +0000

    #517 레벨아이콘의 alt속성 값을 간소화
    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@4432 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- addons/point_level_icon/point_level_icon.lib.php
+++ addons/point_level_icon/point_level_icon.lib.php
@@ -7,7 +7,7 @@
         if($member_srl<1) return $matches[0];
 
         if(!isset($GLOBALS['_pointLevelIcon'][$member_srl])) {
-            // 포인트 설정을 구해옴 
+            // 포인트 설정을 구해옴
             if(!$GLOBALS['_pointConfig']) {
                 $oModuleModel = &getModel('module');
                 $GLOBALS['_pointConfig'] = $oModuleModel->getModuleConfig('point');
@@ -26,7 +26,7 @@
             $text = $matches[5];
 
             // 레벨 아이콘의 위치를 구함
-            $level_icon = sprintf("./modules/point/icons/%s/%d.gif", $config->level_icon, $level);
+            $level_icon = sprintf('./modules/point/icons/%s/%d.gif', $config->level_icon, $level);
 
             // 최고 레벨이 아니면 다음 레벨로 가기 위한 per을 구함
             if($level < $config->max_level) {
@@ -34,12 +34,13 @@
                 if($next_point > 0) $per = (int)($point / $next_point*100);
             }
 
-            $title = sprintf("%s:%s%s %s, %s:%s/%s", Context::getLang('point'), $point, $config->point_name, $per?"(".$per."%)":"", Context::getLang('level'), $level, $config->max_level);
+            $title = sprintf('%s:%s%s %s, %s:%s/%s', Context::getLang('point'), $point, $config->point_name, $per?'('.$per.'%)':'', Context::getLang('level'), $level, $config->max_level);
+            $alt = sprintf('[%s:%s]', Context::getLang('level'), $level);
 
-            $text = sprintf('<span class="nowrap member_%s" style="cursor:pointer"><img src="%s" alt="%s" title="%s" style="vertical-align:middle;margin-right:3px"/>%s</span>', $member_srl, $level_icon, $title, $title, $text);
+            $text = sprintf('<span class="nowrap member_%s" style="cursor:pointer"><img src="%s" alt="%s" title="%s" style="vertical-align:middle;margin-right:3px"/>%s</span>', $member_srl, $level_icon, $alt, $title, $text);
 
             $GLOBALS['_pointLevelIcon'][$member_srl] = $text;
-        } 
+        }
 
         return $GLOBALS['_pointLevelIcon'][$member_srl];
     }
