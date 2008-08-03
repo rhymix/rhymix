789dfe530136b92331cf63a00d5458196a9fc3e7
duvent <duvent@201d5d3c-b55e-5fd7-737f-ddc643e51545>
2008-08-03 07:02:36 +0000

    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@4435 201d5d3c-b55e-5fd7-737f-ddc643e51545

---
+++ modules/rss/lang/fr.lang.php
@@ -0,0 +1,25 @@
+<?php
+    /**
+     * @file   modules/rss/lang/en.lang.php
+     * @author zero <zero@nzeo.com>
+     * @brief  English language pack (basic)
+     **/
+
+    // general words
+    $lang->rss_disable = "Invalider RSS";
+    $lang->rss_type = "Le Format pour imprimer RSS";
+    $lang->open_rss = 'Exposer RSS';
+    $lang->open_rss_types = array(
+        'Y' => 'Exposer Tout',
+        'H' => 'Exposer Résumé',
+        'N' => 'Ne pas exposer',
+    );
+
+    // for description
+    $lang->about_rss_disable = "Si vous cochez, RSS sera invalidé.";
+    $lang->about_rss_type = "Vous pouvez désignez le format pour inprimer RSS.";
+    $lang->about_open_rss = 'Vous pouvez exposez au publique le RSS du module courant ou non.\nN\'importe comment est la permission de l\'article, RSS sera exposé au publique selon son option.';
+
+    // for error message
+    $lang->msg_rss_is_disabled = "La fonction RSS est invalidé.";
+?>
