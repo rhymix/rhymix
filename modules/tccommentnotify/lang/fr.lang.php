1d3bf71d42295278d042d09f23acbd3efb295a65
duvent <duvent@201d5d3c-b55e-5fd7-737f-ddc643e51545>
2008-08-03 21:28:53 +0000

    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@4438 201d5d3c-b55e-5fd7-737f-ddc643e51545

---
+++ modules/tccommentnotify/lang/fr.lang.php
@@ -0,0 +1,5 @@
+<?php
+    $lang->commentnotify = 'Annonceur de Commentaire';
+    $lang->msg_checked_comment_is_deleted = '%d commentaire(s) a(ont) est(sont) supprimé(s)';
+    $lang->cmd_delete_checked_comment = 'Supprimer les Item choisis';
+?>
