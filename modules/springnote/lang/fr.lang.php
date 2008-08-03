5ffb3f388041979a2bd4131cbd4216346120a5aa
duvent <duvent@201d5d3c-b55e-5fd7-737f-ddc643e51545>
2008-08-03 21:25:52 +0000

    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@4437 201d5d3c-b55e-5fd7-737f-ddc643e51545

---
+++ modules/springnote/lang/fr.lang.php
@@ -0,0 +1,30 @@
+<?php
+    /**
+     * @fichier   fr.lang.php
+     * @auteur zero (zero@nzeo.com) Traduit par Pierre Duvent (PierreDuvent@gmail.com)
+     * @brève  Paquet du langage en français pour le module de Springnote(cahier relié par un ressort)
+     **/
+
+    $lang->springnote = "Springnote";
+    $lang->springnote_openid = "OpenID";
+    $lang->springnote_userkey = "Clé d'Utilisateur";
+    $lang->springnote_pageid = "Numéro de Page";
+    $lang->springnote_pageid_setup = 'Configurer Numéro de Page';
+    $lang->springnote_pageid_option_only = 'Exposer seulement la page choisie (cacher la liste)';
+    $lang->springnote_pageid_option_list = 'Exposer de la Page choisie (montrer la list)';
+    $lang->springnote_domain = "Désigner le Domaine";
+
+    $lang->page_url = "URL d'original";
+    $lang->page_modified = "Modification dernière";
+    $lang->page_modifier = "Modificateur dernier";
+
+    $lang->cmd_springnote_list = 'Liste des Springnotes';
+    $lang->cmd_view_info = 'Information des Springnotes';
+
+    $lang->about_springnote = "Springnote est un Service de Wiki qui est pourvu par Openmaru.<br />Le module de Springnote représente une page particulier du Springnote comme un document interne";
+    $lang->about_springnote_openid = "Entrez votre OpenID qui a créé le Springnote, S.V.P.";
+    $lang->about_springnote_userkey = 'Le Clé d\'Utilisateur est necessaire pour valider le module de Springnote.<br />Entrez le Clé d\'Utilisateur produit après avoir entré votre OpenID à [<a href="https://api.openmaru.com/delegate_key/springnote?app_id=www.zeroboard.com&openid=" onclick="window.open(this.href);return false;">Get User Key</a>], S.V.P.';
+    $lang->about_springnote_pageid = 'Entrez pageid(identité de page) si vous voulez représenter une page particulière premièrement.';
+    $lang->about_springnote_pageid_setup = "Vous pouvez désigner 'Exposer seulement la page choisie (cacher la liste)', 'Exposer de la Page choisie (montrer la list)' etc quand vous désignez le numéro de page.";
+    $lang->about_springnote_domain = "Vous pouvez aussi désigner Springnote d'autre domaine. (Entrez seulement la valeur de DOMAINE dans la formule de 'http://DOMAINE.springnote.com' )";
+?>
