fe9161bdd6b46a07848610f34861e8dd1d545b8d
zero <skklove@gmail.com>
2008-09-19 04:59:33 +0000

    호스팅 환경을 감안하여 엮인글의 IP주소중 C클래스딴의 여러 엮인글 등록시도시 스팸으로 간주하는 루틴을 제거. 17235346
    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@4525 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/spamfilter/spamfilter.controller.php
+++ modules/spamfilter/spamfilter.controller.php
@@ -132,12 +132,14 @@
             }
 
             // 30분 이내에 1개 이상의 한 C클래스의 ip에서 엮인글 등록 시도시 금지 아이피로 지정하고 해당 ip의 글을 모두 삭제
+            /* 호스팅 환경을 감안하여 일단 이 부분은 동작하지 않도록 주석 처리
             $count = $oTrackbackModel->getRegistedTrackback(30*60, $ipaddress, $obj->url, $obj->blog_name, $obj->title, $obj->excerpt);
             if($count > 1) {
                 $oTrackbackController->deleteTrackbackSender(3*60, $ipaddress, $obj->url, $obj->blog_name, $obj->title, $obj->excerpt);
                 $this->insertIP($ipaddress.'.*');
                 return new Object(-1,'msg_alert_trackback_denied');
             }
+            */
 
             return new Object();
         }
