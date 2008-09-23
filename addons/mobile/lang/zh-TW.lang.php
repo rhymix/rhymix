77548731679f595645e9c69365a44f0e669e8c95
royallin <royallin@201d5d3c-b55e-5fd7-737f-ddc643e51545>
2008-09-23 05:35:12 +0000

    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@4545 201d5d3c-b55e-5fd7-737f-ddc643e51545

---
+++ addons/mobile/lang/zh-TW.lang.php
@@ -0,0 +1,10 @@
+<?php
+    /**
+     * @file   addons/mobile/lang/zh-TW.lang.php
+     * @author zero (zero@nzeo.com)
+     * @brief  正體中文語言包 (只有基本內容)
+     **/
+
+    $lang->cmd_go_upper = '回上頁';
+    $lang->cmd_go_home = '回首頁';
+?>
