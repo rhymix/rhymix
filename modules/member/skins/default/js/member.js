32b0cab1d24212834b8558f7b4f69e4a0e094627
zero <skklove@gmail.com>
2008-10-08 09:11:51 +0000

    회원가입창을 새창으로 띄웠을경우(get query에 popup=1 명시) 회원가입후 창닫고 부모창을 리로드하도록 코드 변경
    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@4607 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/member/skins/default/js/member.js
+++ modules/member/skins/default/js/member.js
@@ -6,8 +6,13 @@ function completeInsert(ret_obj, response_tags, args, fo_obj) {
 
     alert(message);
 
-    if(redirect_url) location.href = redirect_url;
-    else location.href = current_url.setQuery('act','');
+    if(current_url.getQuery('popup')==1) {
+        if(typeof(opener)!='undefined') opener.location.reload();
+        window.close();
+    } else {
+        if(redirect_url) location.href = redirect_url;
+        else location.href = current_url.setQuery('act','');
+    }
 }
 
 /* 정보 수정 */
