3d25a24b3d9571793b4ae08c8ed5f60077b9605c
royallin <royallin@201d5d3c-b55e-5fd7-737f-ddc643e51545>
2008-10-20 08:07:00 +0000

    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@4648 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/issuetracker/lang/zh-TW.lang.php
+++ modules/issuetracker/lang/zh-TW.lang.php
@@ -6,15 +6,15 @@
      **/
 
      $lang->issuetracker = '問題追蹤';
-     $lang->about_issuetracker = '里程碑管理，原始碼，問題與發佈等問題追蹤。';
+     $lang->about_issuetracker = '版本管理，原始碼，問題與發佈等問題追蹤。';
 
-     $lang->cmd_project_list = '專案列表';
+     $lang->cmd_project_list = '專案清單';
      $lang->cmd_view_info = '專案資訊';
      $lang->cmd_project_setting = '專案設置';
      $lang->cmd_release_setting = '發佈設置';
      $lang->cmd_insert_package = '新增套裝包';
      $lang->cmd_insert_release = '新增發佈版';
-     $lang->cmd_attach_file = '新增附件';
+     $lang->cmd_attach_file = '新增附加檔案';
      $lang->cmd_display_item = '顯示專案';
 
      $lang->cmd_resolve_as = '修改狀態';
@@ -29,8 +29,8 @@
      $lang->about_diff_cmd = '為了比較 SVN revisions，請輸入diff應用程式位置。 (ex: /usr/bin/diff)';
 
      $lang->issue = '問題';
-     $lang->total_issue = '全部問題';
-     $lang->milestone = $lang->milestone_srl = '里程碑';
+     $lang->total_issue = '所有問題';
+     $lang->milestone = $lang->milestone_srl = '版本';
      $lang->priority = $lang->priority_srl = '優先順序';
      $lang->type = $lang->type_srl = '種類';
      $lang->component = $lang->component_srl = '組件';
@@ -46,14 +46,14 @@
      $lang->deadline = '完成期限';
      $lang->name = '名稱';
      $lang->complete = '完成';
-     $lang->completed_date = '結束日';
+     $lang->completed_date = '結束日期';
      $lang->order = '順序';
      $lang->package = $lang->package_srl = '套裝包';
-     $lang->release = $lang->release_srl = '發佈版';
+     $lang->release = $lang->release_srl = '已發佈版本';
      $lang->release_note = '發佈記錄';
      $lang->release_changes = '更新日誌';
      $lang->occured_version = $lang->occured_version_srl = '目前版本';
-     $lang->attached_file = '附件';
+     $lang->attached_file = '附加檔案';
      $lang->filename = '檔案名稱';
      $lang->filesize = '檔案大小';
 
@@ -74,19 +74,19 @@
      $lang->about_component = '設置問題組件。';
 
      $lang->project_menus = array(
-             'dispIssuetrackerViewIssue' => '檢視問題',
-             'dispIssuetrackerNewIssue' => '提交問題',
+             'dispIssuetrackerViewIssue' => '問題清單',
+             'dispIssuetrackerNewIssue' => '發表問題',
              'dispIssuetrackerViewMilestone' => '開發計劃',
              'dispIssuetrackerViewSource' => '檢視原始碼',
              'dispIssuetrackerDownload' => '下載',
              'dispIssuetrackerAdminProjectSetting' => '設置',
     );
 
-    $lang->msg_not_attched = '請新增附件。';
+    $lang->msg_not_attched = '請新增附加檔案。';
     $lang->msg_attached = '檔案已新增。';
-    $lang->msg_no_releases = '沒有被新增的發佈版。';
+    $lang->msg_no_releases = '尚未被新增的發佈版本。';
 
     $lang->cmd_document_do = '將把此問題.. ';
     $lang->not_assigned = '沒有分配';
-    $lang->not_assigned_description = '沒被分配的問題列表';
+    $lang->not_assigned_description = '尚未被分配的問題清單';
 ?>
