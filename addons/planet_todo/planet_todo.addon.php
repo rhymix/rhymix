d0aa5cbd6659731317adca761f002790caa43352
ngleader <ngleader@gmail.com>
2008-11-04 07:55:24 +0000

    addon planet todo
    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@4750 201d5d3c-b55e-5fd7-737f-ddc643e51545

---
+++ addons/planet_todo/planet_todo.addon.php
@@ -0,0 +1,21 @@
+<?php
+    if(!defined("__ZBXE__")) exit();
+
+    /**
+     * @file autolink.addon.php
+     * @author SOL군 (sol@ngleader.com)
+     * @brief 
+     **/
+
+
+
+    if($called_position == 'after_module_proc' && Context::getResponseMethod()!="XMLRPC") {
+        $config = Context::get('config');
+        if($config->mid == 'planet'){
+            $oPlanet = Context::get('planet');
+            if($oPlanet->isMyPlanet()){
+                Context::addJsFile('./addons/planet_tab/planet_todo.js');
+            }
+        }
+    }
+?>
