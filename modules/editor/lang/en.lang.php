1234e4b2fbaa0ff6323c3450a0150caf8c564c28
haneul <syhan@cs.washington.edu>
2008-11-10 13:46:17 +0000

    영문 번역
    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@4836 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/editor/lang/en.lang.php
+++ modules/editor/lang/en.lang.php
@@ -109,9 +109,9 @@
     $lang->about_dblclick_in_editor = 'You may set detail component configures by double-clicking background, text, images, or quotations';
 
 
-    $lang->edit->rich_editor = '스타일 편집기';
-    $lang->edit->html_editor = 'HTML 편집기';
-    $lang->edit->extension ='확장 컴포넌트';
-    $lang->edit->help = '도움말';
+    $lang->edit->rich_editor = 'Rich Text Editor';
+    $lang->edit->html_editor = 'HTML Editor';
+    $lang->edit->extension ='Extension Components';
+    $lang->edit->help = 'Help';
     $lang->edit->help_command = '단축키 안내';
 ?>
