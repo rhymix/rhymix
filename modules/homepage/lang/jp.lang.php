dd4e8fe3cea6fbd2482217f466d66e65989f0961
heemin <heemin@201d5d3c-b55e-5fd7-737f-ddc643e51545>
2008-11-27 07:31:24 +0000

    일본어 번역 수정 및 추가 :: homepage 모듈
    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@5005 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/homepage/lang/jp.lang.php
+++ modules/homepage/lang/jp.lang.php
@@ -34,7 +34,7 @@
         ),
         'menu' => array(
             'first' => '基本メニュー',
-            'second' => 'サブメニュー',
+            'second' => '`フッターメニュー',
         ),
         'widget' => array(
             'download_rank' => 'ダウンロードランキング',
