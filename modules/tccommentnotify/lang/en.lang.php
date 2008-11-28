d254e6d8ae9165a494555f64298b6f0b1484f547
haneul <syhan@cs.washington.edu>
2008-11-28 04:02:48 +0000

    17495478 : modified keywords in template file to support multi-language
    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@5020 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/tccommentnotify/lang/en.lang.php
+++ modules/tccommentnotify/lang/en.lang.php
@@ -2,4 +2,5 @@
     $lang->commentnotify = 'Comment Notifier';
     $lang->msg_checked_comment_is_deleted = '%d comment(s) has(have) been deleted';
     $lang->cmd_delete_checked_comment = 'Delete Selected Items';
+    $lang->site_name = 'Site Name';
 ?>
