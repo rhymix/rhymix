8caba4c579b2c619910ad054a9074b3b703e5660
ngleader <ngleader@gmail.com>
2008-12-17 06:09:08 +0000

    document/comment update 시 스팸필터 트리거 추가
    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@5105 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/spamfilter/spamfilter.class.php
+++ modules/spamfilter/spamfilter.class.php
@@ -22,6 +22,12 @@
             $oModuleController->insertTrigger('comment.insertComment', 'spamfilter', 'controller', 'triggerInsertComment', 'before');
             $oModuleController->insertTrigger('trackback.insertTrackback', 'spamfilter', 'controller', 'triggerInsertTrackback', 'before');
 
+
+            //2008-12-17 글 수정시 스펨필터 추가
+            $oModuleController->insertTrigger('comment.updateComment', 'spamfilter', 'controller', 'triggerInsertComment', 'before');
+            $oModuleController->insertTrigger('document.updateDocument', 'spamfilter', 'controller', 'triggerInsertDocument', 'before');
+
+
             return new Object();
         }
 
@@ -36,6 +42,9 @@
             if(!$oModuleModel->getTrigger('comment.insertComment', 'spamfilter', 'controller', 'triggerInsertComment', 'before')) return true;
             if(!$oModuleModel->getTrigger('trackback.insertTrackback', 'spamfilter', 'controller', 'triggerInsertTrackback', 'before')) return true;
 
+            //2008-12-17 글 수정시 스펨필터 추가
+            if(!$oModuleModel->getTrigger('comment.updateComment', 'spamfilter', 'controller', 'triggerInsertComment', 'before')) return true;
+            if(!$oModuleModel->getTrigger('document.updateDocument', 'spamfilter', 'controller', 'triggerInsertDocument', 'before')) return true;
             return false;
         }
 
@@ -54,6 +63,14 @@
             if(!$oModuleModel->getTrigger('trackback.insertTrackback', 'spamfilter', 'controller', 'triggerInsertTrackback', 'before')) 
                 $oModuleController->insertTrigger('trackback.insertTrackback', 'spamfilter', 'controller', 'triggerInsertTrackback', 'before');
 
+            //2008-12-17 글 수정시 스펨필터 추가
+            if(!$oModuleModel->getTrigger('comment.updateComment', 'spamfilter', 'controller', 'triggerInsertComment', 'before')){
+                $oModuleController->insertTrigger('comment.updateComment', 'spamfilter', 'controller', 'triggerInsertComment', 'before');
+            }
+            //2008-12-17 글 수정시 스펨필터 추가
+            if(!$oModuleModel->getTrigger('document.updateDocument', 'spamfilter', 'controller', 'triggerInsertDocument', 'before')){
+                $oModuleController->insertTrigger('document.updateDocument', 'spamfilter', 'controller', 'triggerInsertDocument', 'before');
+            }
             return new Object(0,'success_updated');
         }
 
