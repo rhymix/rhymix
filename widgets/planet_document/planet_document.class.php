5eb6ca353cd24f51e2347d49eb1f4daf22ef7306
zero <skklove@gmail.com>
2008-12-24 07:31:20 +0000

    1. 플래닛 글 위젯 추가 2. 최신글 위젯 스킨에 xe_official 스킨 추가
    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@5176 201d5d3c-b55e-5fd7-737f-ddc643e51545

---
+++ widgets/planet_document/planet_document.class.php
@@ -0,0 +1,44 @@
+<?php
+    /**
+     * @class planet_document 
+     * @author zero (zero@nzeo.com)
+     * @brief 플래닛 글 목록 출력
+     * @version 0.1
+     **/
+
+    class planet_document extends WidgetHandler {
+
+        /**
+         * @brief 위젯의 실행 부분
+         *
+         * ./widgets/위젯/conf/info.xml 에 선언한 extra_vars를 args로 받는다
+         * 결과를 만든후 print가 아니라 return 해주어야 한다
+         **/
+        function proc($args) {
+            // 제목
+            $title = $args->title;
+
+            // 출력된 목록 수
+            $list_count = (int)$args->list_count;
+            if(!$list_count) $list_count = 5;
+
+            // 플래닛 글 목록 구함
+            $oPlanetModel = &getModel('planet');
+            Context::set('planet', $planet = $oPlanetModel->getPlanet());
+            $output = $oPlanetModel->getNewestContentList(null, $planet->getContentLastDay(),  $page, $list_count, 'documents.list_order', 'asc', null);
+            Context::set('planet_list', $output->data);
+
+            // 템플릿의 스킨 경로를 지정 (skin, colorset에 따른 값을 설정)
+            $tpl_path = sprintf('%sskins/%s', $this->widget_path, $args->skin);
+            Context::set('colorset', $args->colorset);
+
+            // 템플릿 파일을 지정
+            $tpl_file = 'list';
+
+            // 템플릿 컴파일
+            $oTemplate = &TemplateHandler::getInstance();
+            $output = $oTemplate->compile($tpl_path, $tpl_file);
+            return $output;
+        }
+    }
+?>
