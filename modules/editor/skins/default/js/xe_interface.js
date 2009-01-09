34819438c77fd26037489da953ddfcd21de46d1d
misol <misol.kr@gmail.com>
2009-01-09 02:35:53 +0000

    #17620466 스킨에서 정하게
    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@5291 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/editor/skins/default/js/xe_interface.js
+++ modules/editor/skins/default/js/xe_interface.js
@@ -1,5 +1,6 @@
 function editorStartTextarea(editor_sequence, content_key, primary_key) {
     var obj = xGetElementById('editor_'+editor_sequence);
+    var use_html = xGetElementById('htm_'+editor_sequence).value;
     obj.form.setAttribute('editor_sequence', editor_sequence);
 
     obj.style.width = '99%';
@@ -10,13 +11,26 @@ function editorStartTextarea(editor_sequence, content_key, primary_key) {
     editorRelKeys[editor_sequence]["func"] = editorGetContentTextarea;
 
     var content = obj.form[content_key].value;
-    content = content.replace(/<br([^>]*)>/ig,"\n");
+    if(use_html=='n') {
+        content = content.replace(/<br([^>]*)>/ig,"\n");
+        content = content.replace(/&lt;/g, "<");
+        content = content.replace(/&gt;/g, ">");
+        content = content.replace(/&quot;/g, '"');
+        content = content.replace(/&amp;/g, "&");
+    }
     obj.value = content;
 }
 
 function editorGetContentTextarea(editor_sequence) {
     var obj = xGetElementById('editor_'+editor_sequence);
+    var use_html = xGetElementById('htm_'+editor_sequence).value;
     var content = obj.value.trim();
-    content = content.replace(/(\r\n|\n)/g, "<br />$1");
+    if(use_html=='n') {
+        content = content.replace(/&/g, "&amp;");
+        content = content.replace(/</g, "&lt;");
+        content = content.replace(/>/g, "&gt;");
+        content = content.replace(/\"/g, "&quot;");
+        content = content.replace(/(\r\n|\n)/g, "<br />");
+    }
     return content;
 }
