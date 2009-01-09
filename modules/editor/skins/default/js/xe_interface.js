759521e27a3e04af2182f363f0549aa61705a76d
misol <misol.kr@gmail.com>
2009-01-09 02:50:50 +0000

    이전 설정 유지해도 차이 없게.
    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@5292 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/editor/skins/default/js/xe_interface.js
+++ modules/editor/skins/default/js/xe_interface.js
@@ -11,12 +11,14 @@ function editorStartTextarea(editor_sequence, content_key, primary_key) {
     editorRelKeys[editor_sequence]["func"] = editorGetContentTextarea;
 
     var content = obj.form[content_key].value;
-    if(use_html=='n') {
+    if(use_html) {
         content = content.replace(/<br([^>]*)>/ig,"\n");
-        content = content.replace(/&lt;/g, "<");
-        content = content.replace(/&gt;/g, ">");
-        content = content.replace(/&quot;/g, '"');
-        content = content.replace(/&amp;/g, "&");
+        if(use_html!='br') {
+            content = content.replace(/&lt;/g, "<");
+            content = content.replace(/&gt;/g, ">");
+            content = content.replace(/&quot;/g, '"');
+            content = content.replace(/&amp;/g, "&");
+        }
     }
     obj.value = content;
 }
@@ -25,11 +27,13 @@ function editorGetContentTextarea(editor_sequence) {
     var obj = xGetElementById('editor_'+editor_sequence);
     var use_html = xGetElementById('htm_'+editor_sequence).value;
     var content = obj.value.trim();
-    if(use_html=='n') {
-        content = content.replace(/&/g, "&amp;");
-        content = content.replace(/</g, "&lt;");
-        content = content.replace(/>/g, "&gt;");
-        content = content.replace(/\"/g, "&quot;");
+    if(use_html) {
+        if(use_html!='br') {
+            content = content.replace(/&/g, "&amp;");
+            content = content.replace(/</g, "&lt;");
+            content = content.replace(/>/g, "&gt;");
+            content = content.replace(/\"/g, "&quot;");
+        }
         content = content.replace(/(\r\n|\n)/g, "<br />");
     }
     return content;
