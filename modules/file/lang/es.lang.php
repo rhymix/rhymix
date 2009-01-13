67198cc350d9d7e27128d4ba53c698abaed9ac4c
misol <misol.kr@gmail.com>
2009-01-13 07:19:46 +0000

    언어파일 복구
    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@5349 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/file/lang/es.lang.php
+++ modules/file/lang/es.lang.php
@@ -7,41 +7,41 @@
 
     $lang->file = 'Adjuntar archivos';
     $lang->file_name = 'Nombre del archivo';
-    $lang->file_size = 'TamaA±o del archivo';
+    $lang->file_size = 'Tamaño del archivo';
     $lang->download_count = 'Cantidad Bajado';
     $lang->status = 'Estado';
-    $lang->is_valid = 'VA¡lido';
+    $lang->is_valid = 'Válido';
     $lang->is_stand_by = 'En espera';
     $lang->file_list = 'Lista de archivos adjuntos';
     $lang->allow_outlink = '파일 외부 링크';
-    $lang->allowed_filesize = 'LA­mite del tamaA±o del archivo adjunto';
-    $lang->allowed_attach_size = 'LA­mite del tamaA±o total de los archivos adjuntos por documento';
+    $lang->allowed_filesize = 'Límite del tamaño del archivo adjunto';
+    $lang->allowed_attach_size = 'Límite del tamaño total de los archivos adjuntos por documento';
     $lang->allowed_filetypes = 'Tipos de archivos permitidos';
-    $lang->enable_download_group = 'Descargar permitiA³ grupos';
+    $lang->enable_download_group = 'Descargar permitió grupos';
 
     $lang->about_allow_outlink = '리퍼러에 따라 파일 외부 링크를 차단할 수 있습니다.(*.wmv, *.mp3등 미디어 파일 제외)';
-    $lang->about_allowed_filesize = 'Puede definir el lA­mite del tamaA±o del archivo adjunto. (exceptuando el administrador)';
-    $lang->about_allowed_attach_size = 'Puede definir el lA­mite del tamaA±o total de los archivos adjuntos por documento. (exceptuando el administrador)';
-    $lang->about_allowed_filetypes = 'Puede definir las extensiones de los archivos permitidos. Para permitir una extensiA³n use "*.extensiA³n". Para permitir mA¡s de una extensiA³n use ";".<br />ej) *.* o *.jpg;*.gif;etc.<br />(exceptuando el administrador)';
+    $lang->about_allowed_filesize = 'Puede definir el límite del tamaño del archivo adjunto. (exceptuando el administrador)';
+    $lang->about_allowed_attach_size = 'Puede definir el límite del tamaño total de los archivos adjuntos por documento. (exceptuando el administrador)';
+    $lang->about_allowed_filetypes = 'Puede definir las extensiones de los archivos permitidos. Para permitir una extensión use "*.extensión". Para permitir más de una extensión use ";".<br />ej) *.* o *.jpg;*.gif;etc.<br />(exceptuando el administrador)';
 
     $lang->cmd_delete_checked_file = 'Eliminar el archivo seleccionado';
     $lang->cmd_move_to_document = 'Mover hacia el doncumento';
     $lang->cmd_download = 'Descargar';
 
-    $lang->msg_not_permitted_download = 'Usted no tiene ningAºn permiso para descargar';
+    $lang->msg_not_permitted_download = 'Usted no tiene ningún permiso para descargar';
     $lang->msg_cart_is_null = 'Seleccione el archivo a eliminar';
     $lang->msg_checked_file_is_deleted = 'Total de %d archivos eliminados';
-    $lang->msg_exceeds_limit_size = 'Ha excedido el lA­mite del tamaA±o total de los archivos adjuntos';
+    $lang->msg_exceeds_limit_size = 'Ha excedido el límite del tamaño total de los archivos adjuntos';
 
     $lang->file_search_target_list = array(
         'filename' => 'Nombre del archivo',
-        'filesize' => 'TamaA±o del archivo (Byte, sobre)',
+        'filesize' => 'Tamaño del archivo (Byte, sobre)',
         'filesize_mega' => '파일크기 (Mb, 이상)',
         'download_count' => 'Descargados (Sobre)',
         'user_id' => '아이디',
         'user_name' => '이름',
         'nick_name' => '닉네임',
         'regdate' => 'La fecha registrada',
-        'ipaddress' => 'DirecciA³n IP',
+        'ipaddress' => 'Dirección IP',
     );
 ?>
