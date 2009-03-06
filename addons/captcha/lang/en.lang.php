5a7698952f992b964cc4398531aebb19c7e7c1f7
haneul <syhan@cs.washington.edu>
2009-03-06 06:17:10 +0000

    english language pack
    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@5787 201d5d3c-b55e-5fd7-737f-ddc643e51545

---
+++ addons/captcha/lang/en.lang.php
@@ -0,0 +1,21 @@
+<?php
+    /**
+     * @file   en.lang.php
+     * @author haneul (haneul0318@gmail.com)
+     * @brief  English Language Pack 
+     **/
+
+    $lang->about_captcha = "Choose an image whose name is listed below";
+    $lang->target_captcha = array(
+        "airplane" => "airplane",
+        "apple" => "apple",
+        "book" => "book",
+        "camera" => "camera",
+        "dog" => "dog",
+        "earth" => "earth",
+        "flag" => "flag",
+        "mobile" => "mobile",
+        "note" => "note",
+        "skeleton" => "skeleton",
+    );
+?>
