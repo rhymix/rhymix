d6dcfb05172bd4529d9fd9c1a2bbc129105a14d6
haneul <syhan@cs.washington.edu>
2009-04-10 01:25:40 +0000

    #17920436 : fixed the problem occurred when using php4
    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@6074 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/board/board.view.php
+++ modules/board/board.view.php
@@ -179,8 +179,8 @@
             }
 
             // 스킨에서 사용할 oDocument 변수 세팅
-            Context::set('oDocument', $oDocument);
             $oDocument->add('module_srl', $this->module_srl);
+            Context::set('oDocument', $oDocument);
 
             /** 
              * 사용되는 javascript 필터 추가
