437b0092f587dca73843687f290c2525692ac271
mooo <mooo@201d5d3c-b55e-5fd7-737f-ddc643e51545>
2009-04-12 12:43:28 +0000

    #17870061 : 사용자 플래닛 생성시 기본 모듈 이름 등과 겹치지 않도록 memberModel::isDeniedID() 함수를 이용하여 검사하도록 수정
    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@6092 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/planet/lang/jp.lang.php
+++ modules/planet/lang/jp.lang.php
@@ -18,7 +18,7 @@
 
     $lang->planet_reply_content = "プラネットのコメント内容";
 
-    $lang->about_planet = 
+    $lang->about_planet =
        "microBlogのXEモジュールです。会員別のプラネットを作成して運営することが出来ます。
         ホームページパッケージで登録されているドメインからプラネットへアクセス出来ない場合もあります。
         プラネットを初期ページとして利用する場合は、ホームページパッケージで登録されているドメインと異なるドメインを登録して下さい。";
@@ -45,6 +45,7 @@
 
     $lang->msg_not_logged = "ログインしていません。";
     $lang->msg_planet_exists = "既に作成したプラネットがありますので、新しく作成することが出来ません。";
+    $lang->msg_denied_planet_title = 'Denied planet title';
 
     $lang->planet_userinfo = "個人情報";
     $lang->planet_change_userinfo = "個人情報変更";
