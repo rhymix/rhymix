251ab8a928712090c3bfde1da385cba5dee89d5c
zero <skklove@gmail.com>
2009-04-15 06:29:28 +0000

    ID기반의 가상사이트를 위해 통합검색폼/ 기본 레이아웃의 통합검색폼/ 개별 카페에서 레이아웃 설정 바꿀때 정상동작하도록 코드 변경
    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@6111 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/integration_search/integration_search.admin.view.php
+++ modules/integration_search/integration_search.admin.view.php
@@ -53,7 +53,7 @@
             Context::set('mid_list',$module_categories);
 
             // 샘플코드
-            Context::set('sample_code', htmlspecialchars('<form action="{getUrl()}" method="get"><input type="hidden" name="mid" value="{$mid}" /><input type="hidden" name="act" value="IS" /><input type="text" name="is_keyword" class="inputTypeText" value="{$is_keyword}" /><span class="button"><input type="submit" value="{$lang->cmd_search}" /></span></form>') );
+            Context::set('sample_code', htmlspecialchars('<form action="{getUrl()}" method="get"><input type="hidden" name="vid" value="{$vid}" /><input type="hidden" name="mid" value="{$mid}" /><input type="hidden" name="act" value="IS" /><input type="text" name="is_keyword" class="inputTypeText" value="{$is_keyword}" /><span class="button"><input type="submit" value="{$lang->cmd_search}" /></span></form>') );
 
             $this->setTemplateFile("index");
         }
