caf26ad7406e33630e3fb3dee5741a856f978259
royallin <royallin@201d5d3c-b55e-5fd7-737f-ddc643e51545>
2009-04-18 06:23:32 +0000

    Modified traditional Chinese
    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@6139 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- common/lang/zh-TW.lang.php
+++ common/lang/zh-TW.lang.php
@@ -124,7 +124,7 @@
     $lang->file = '檔案';
 
     $lang->mid = '模組名稱';
-    $lang->sid = 'Site Name';
+    $lang->sid = '網站名稱';
     $lang->layout = '版面';
     $lang->widget = '控件 ';
     $lang->module = '模組';
