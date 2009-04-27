011efbdb6a389a3b739fe178b034850e92fff1d2
zero <skklove@gmail.com>
2009-04-27 07:26:31 +0000

    위키 링크 애드온의 배경 투명을 없애고 UI 살짝 변경. 가상사이트에서 위키 문서 연결시 이상없이 동작하도록 수정
    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@6201 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- addons/wiki_link/wiki_link.addon.php
+++ addons/wiki_link/wiki_link.addon.php
@@ -6,9 +6,7 @@
         if(!$module_info->module) return;
         if($module_info->module != 'wiki') return;
         Context::loadJavascriptPlugin('hotkeys');
-        Context::addJsFile('./addons/wiki_link/wikilink.js');
-        Context::addCssFile('./addons/wiki_link/wikilink.css');
-        $footer = "<div id='link'> <div class='linkForm'>Link Target: <input type='text' id='linktarget' class='inputTypeText w100' /><a href='#' onclick='setText(); return false;'>AddLink</a></div></div>";
-        Context::addHtmlFooter($footer);
+        Context::addJsFile('./addons/wiki_link/wikilink.js',false);
+        Context::addCssFile('./addons/wiki_link/wikilink.css',false);
     }
 ?>
