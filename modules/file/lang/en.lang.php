900f6982a7e370d11e82b67c96f938c4c5dc4d74
nicetwo <nicetwo@201d5d3c-b55e-5fd7-737f-ddc643e51545>
2009-05-27 15:04:21 +0000

    영어 - 추가 번역
    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@6428 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/file/lang/en.lang.php
+++ modules/file/lang/en.lang.php
@@ -13,17 +13,17 @@
     $lang->is_valid = 'Valid';
     $lang->is_stand_by = 'Stand by';
     $lang->file_list = 'Attachments List';
-    $lang->allow_outlink = '파일 외부 링크';
-    $lang->allow_outlink_site = '파일 외부 허용 사이트';
-    $lang->allow_outlink_format = '파일 외부 링크 허용 확장자';
+    $lang->allow_outlink = 'External Link';
+    $lang->allow_outlink_site = 'Allowed Websites';
+    $lang->allow_outlink_format = 'Allowed Formats';
     $lang->allowed_filesize = 'Maximum File Size';
     $lang->allowed_attach_size = 'Maximum Attachments';
     $lang->allowed_filetypes = 'Allowed Extensions';
     $lang->enable_download_group = 'Download Allowed Groups';
 
-    $lang->about_allow_outlink = '리퍼러에 따라 파일 외부 링크를 차단할 수 있습니다.(*.wmv, *.mp3등 미디어 파일 제외)';
-    $lang->about_allow_outlink_format = '파일 외부 링크 설정에 관계 없이 허용하는 파일 확장자입니다. 여러개 입력시에 쉼표(,)을 이용해서 구분해주세요.<br />eg)hwp,doc,zip,pdf';
-    $lang->about_allow_outlink_site = '파일 외부 링크 설정에 관계 없이 허용하는 사이트 주소입니다. 여러개 입력시에 줄을 바꿔서 구분해주세요.<br />ex)http://www.zeroboard.com';
+    $lang->about_allow_outlink = 'You can shut external links according to referers. (except media files like *.wmv, *.mp3)';
+    $lang->about_allow_outlink_format = 'These formats will always be allowed. Please use comma(,) for multiple input.<br />eg)hwp,doc,zip,pdf';
+    $lang->about_allow_outlink_site = 'These websites will alyways be allowed. Please use new line for multiple input.<br />ex)http://www.zeroboard.com';
 	$lang->about_allowed_filesize = 'You can assign file size limit for each file. (Exclude administrators)';
     $lang->about_allowed_attach_size = 'You can assign file size limit for each document. (Exclude administrators)';
     $lang->about_allowed_filetypes = 'Only allowed extentsions can be attached. To allow an extension, use "*.[extention]". To allow multiple extensions, use ";" between each extension.<br />ex) *.* or *.jpg;*.gif;<br />(Exclude Administrators)';
@@ -36,7 +36,7 @@
     $lang->msg_cart_is_null = 'Please select file(s) to delete';
     $lang->msg_checked_file_is_deleted = '%d attachment(s) was(were) deleted';
     $lang->msg_exceeds_limit_size = 'File size of attachment is bigger than allowed size.';
-    $lang->msg_file_not_found = '요청하신 파일을 찾을 수 없습니다.';
+    $lang->msg_file_not_found = 'Could not find requested file.';
 
 
     $lang->file_search_target_list = array(
