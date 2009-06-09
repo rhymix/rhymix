b4230cc2eb4d51e713877b38a86305baac7d3f93
misol <misol.kr@gmail.com>
2009-06-09 10:43:04 +0000

    글 수정시 파일 옮기지 않도록 수정
    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@6501 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/editor/editor.model.php
+++ modules/editor/editor.model.php
@@ -348,7 +348,7 @@
 
             // 자동저장 데이터에 문서번호가 있고 이 번호에 파일이 있다면 파일을 모두 이동하고
             // 해당 문서 번호를 editor_sequence로 세팅함
-            if($saved_doc->document_srl && $upload_target_srl) {
+            if($saved_doc->document_srl && $upload_target_srl && !Context::get('document_srl')) {
                 $module_srl = Context::get('module_srl');
                 $oFileController = &getController('file');
                 $oFileController->moveFile($saved_doc->document_srl, $module_srl, $upload_target_srl);
