9d7a921f3b4b61a5ebd4cd70e51ab32c38459d60
zero <skklove@gmail.com>
2009-06-27 13:48:59 +0000

    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@6676 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/wiki/wiki.model.php
+++ modules/wiki/wiki.model.php
@@ -28,9 +28,8 @@
             $oModuleModel = &getModel('module');
 
             $mid = Context::get('mid');
-
             $cache_file = sprintf('%sfiles/cache/wiki/%d.xml', _XE_PATH_,$this->module_srl);
-            if(!file_exists($cache_file)) FileHandler::writeFile($cache_file, $this->loadWikiTreeList($this->module_srl));
+            FileHandler::writeFile($cache_file, $this->loadWikiTreeList($this->module_srl));
 
             print FileHandler::readFile($cache_file);
             Context::close();
