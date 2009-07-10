245f6d2d079f4608d1db8379928231dceea80908
zero <skklove@gmail.com>
2009-07-10 10:35:43 +0000

    XE Core 패키지를 위해서 별도로 분리할 모듈들을 모두 제거
    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@6696 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- layouts/cafeXE/lang/zh-TW.lang.php
+++
@@ -1,5 +0,0 @@
-<?php
-    $lang->new_members = '最新會員';
-    $lang->document_ranking = '主題排行';
-    $lang->comment_ranking = '評論排行';
-?>
--- modules/tccommentnotify/lang/en.lang.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $lang->commentnotify = 'Comment Notifier';
-    $lang->msg_checked_comment_is_deleted = '%d comment(s) has(have) been deleted';
-    $lang->cmd_delete_checked_comment = 'Delete Selected Items';
-    $lang->site_name = 'Site Name';
-?>
