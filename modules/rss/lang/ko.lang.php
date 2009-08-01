17e397c2c4bd2a5a5fc2cee86fe4a7b0d9f528da
misol <misol.kr@gmail.com>
2009-08-01 09:40:52 +0000

    띄어쓰기, 마침표 등 2차 수정
    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@6717 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/rss/lang/ko.lang.php
+++ modules/rss/lang/ko.lang.php
@@ -24,13 +24,13 @@
     // 설명문
     $lang->about_rss_disable = '체크하시면 피드(Feed) 출력을 하지 않습니다.';
     $lang->about_rss_type = '출력하실 피드(Feed) 형식을 지정하실 수 있습니다.';
-    $lang->about_open_rss = '현재 모듈에 대한 피드(Feed) 공개를 선택하실 수 있습니다. 글 보는 권한과 상관없이 옵션에 따라 RSS가 공개됩니다.';
+    $lang->about_open_rss = '현재 모듈에 대한 피드(Feed) 공개를 선택하실 수 있습니다. 글 열람 권한과 상관없이 옵션에 따라 RSS가 공개됩니다.';
     $lang->about_feed_description = '발행될 피드에 대한 설명을 입력하실 수 있습니다. 설명을 입력하지 않으실 경우, 해당 모듈에 설정된 관리용 설명이 포함됩니다.';
-    $lang->about_feed_copyright = '발행될 피드의 글에 대한 저작권 정보입니다.';
+    $lang->about_feed_copyright = '발행될 피드에 대한 저작권 정보를 입력하실 수 있습니다.';
     $lang->about_part_feed_copyright = '입력하지 않으면 전체 피드 저작권 설정과 동일하게 적용됩니다.';
-    $lang->about_feed_document_count = '피드 한페이지에 공개되는 글의 수. (기본값 : 15)';
+    $lang->about_feed_document_count = '피드 한 페이지에 공개되는 글 수. (기본 값 : 15)';
 
-    // 에러 메세지들
-    $lang->msg_rss_is_disabled = '피드(Feed) 기능이 잠겨 있습니다';
+    // 에러 메시지들
+    $lang->msg_rss_is_disabled = '피드(Feed) 기능이 잠겨 있습니다.';
     $lang->msg_rss_invalid_image_format = '이미지의 형식이 잘못되었습니다.\nJPEG, GIF, PNG 파일만 지원합니다.';
 ?>
--- modules/widget/lang/ko.lang.php
+++ modules/widget/lang/ko.lang.php
@@ -16,9 +16,9 @@
     $lang->widget_cache = '캐시';
 
     $lang->widget_fix_width = '크기 고정';
-    $lang->widget_width = '가로크기';
+    $lang->widget_width = '가로 크기';
     $lang->widget_position = '위치';
-    $lang->widget_position_none = '줄바꿈';
+    $lang->widget_position_none = '줄 바꿈';
     $lang->widget_position_left = '왼쪽';
     $lang->widget_position_right = '오른쪽';
     $lang->widget_margin = '외부 여백';
@@ -26,23 +26,23 @@
     $lang->widget_margin_right = '오른쪽';
     $lang->widget_margin_bottom = '아래 ';
     $lang->widget_margin_left= '왼쪽';
-    $lang->about_widget_fix_width = '선택하시면 가로크기가 고정됩니다.';
+    $lang->about_widget_fix_width = '선택하시면 가로 크기가 고정됩니다.';
     $lang->about_widget_width = '가로 크기를 입력해주세요.';
-    $lang->about_widget_position = '한 줄에 여러개의 위젯을 나열하고 싶을때에는 위치를 선택해주세요.';
-    $lang->about_widget_margin = '지정된 위젯의 상하좌우 여백을 지정할 수 있습니다';
+    $lang->about_widget_position = '한 줄에 여러 개의 위젯을 나열하고 싶을 때는 위치를 선택해주세요.';
+    $lang->about_widget_margin = '지정된 위젯의 상하좌우 여백을 지정할 수 있습니다.';
     $lang->about_widget_cache = '지정된 시간동안 캐시된 데이터를 이용할 수 있습니다.';
 
     $lang->generated_code = '생성된 코드';
     $lang->widgetstyle= '위젯스타일';
 
-    $lang->msg_widget_is_not_exists = '%s 위젯을 찾을 수 없습니다';
-    $lang->msg_widget_object_is_null = '%s 위젯의 객체 생성을 할 수가 없습니다';
-    $lang->msg_widget_proc_is_null = '%s 위젯의 proc() 를 실행할 수가 없습니다';
-    $lang->msg_widget_skin_is_null = '위젯의 스킨을 선택해 주셔야 정상적으로 동작할 수 있습니다';
+    $lang->msg_widget_is_not_exists = '%s 위젯을 찾을 수 없습니다.';
+    $lang->msg_widget_object_is_null = '%s 위젯의 객체 생성을 할 수 없습니다.';
+    $lang->msg_widget_proc_is_null = '%s 위젯의 proc() 를 실행할 수 없습니다.';
+    $lang->msg_widget_skin_is_null = '위젯의 스킨을 선택해 주셔야 정상적으로 동작할 수 있습니다.';
 
-    $lang->about_widget_code = '선택하신 위젯에서 요구하는 아래 항목들의 값을 넣고 [코드생성]버튼을 누르시면 제일 아래 칸에 템플릿 파일에 적용할 수 있는 코드가 출력 됩니다';
-    $lang->about_widget_code_in_page = '아래 필요한 값들을 입력하신 후 추가 버튼을 누르시면 페이지 내에 위젯이 삽입이 됩니다';
-    $lang->about_widget = "위젯은 레이아웃이나 페이지 모듈에서 사용되는 작은 구성요소입니다.\n내부 모듈 또는 외부 open api와 연동될 수도 있고 설정을 통해서 다양한 응용이 가능합니다.\nXE의 페이지 모듈이나 레이아웃 모듈을 사용하지 않더라도 [코드생성] 기능을 통해 직접 위젯 추가도 할 수 있습니다";
+    $lang->about_widget_code = '선택하신 위젯에서 요구하는 아래 항목들의 값을 넣고 [코드생성]버튼을 누르시면 제일 아래 칸에 템플릿 파일에 적용할 수 있는 코드가 출력 됩니다.';
+    $lang->about_widget_code_in_page = '아래 필요한 값들을 입력하신 후 추가 버튼을 누르시면 페이지 내에 위젯이 삽입됩니다.';
+    $lang->about_widget = "위젯은 레이아웃이나 페이지 모듈에서 사용되는 작은 구성요소입니다.\n위젯은 내부 모듈 또는 외부 open api와 연동될 수도 있고, 설정을 통해 다양하게 응용될 수 있습니다.\nXE의 페이지 모듈이나 레이아웃 모듈을 사용하지 않더라도 [코드생성] 기능을 통해 직접 위젯 추가도 할 수 있습니다.";
 
     $lang->cmd_content_insert = '내용 직접 추가';
     $lang->cmd_box_widget_insert = '상자 위젯 추가';
