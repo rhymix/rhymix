2c8902c81541f614cf7d433c9e3acf6987569ad3
zero <skklove@gmail.com>
2009-08-28 02:22:48 +0000

    #18261235 displayMultimedia() 함수에서 swf 파일에 대한 검증시 ?get변수를 고려하지 못해 wmode가 반영되지 못하던 문제 수정
    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@6761 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- common/js/common.js
+++ common/js/common.js
@@ -459,7 +459,9 @@ function _displayMultimedia(src, width, height, options) {
 
     if(/\.(gif|jpg|jpeg|bmp|png)$/i.test(src)){
         html = '<img src="'+src+'" width="'+width+'" height="'+height+'" />';
-    } else if(/\.swf$/i.test(src)) {
+    } else if(/\.flv$/i.test(src) || /\.mov$/i.test(src) || /\.moov$/i.test(src) || /\.m4v$/i.test(src)) {
+        html = '<embed src="'+request_uri+'common/tpl/images/flvplayer.swf" allowfullscreen="true" autostart="'+autostart+'" width="'+width+'" height="'+height+'" flashvars="&file='+src+'&width='+width+'&height='+height+'&autostart='+autostart+'" wmode="'+params.wmode+'" />';
+    } else if(/\.swf/i.test(src)) {
         clsid = 'clsid:D27CDB6E-AE6D-11cf-96B8-444553540000';
         codebase = "http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=9,0,28,0";
         html = '<object classid="'+clsid+'" codebase="'+codebase+'" width="'+width+'" height="'+height+'" flashvars="'+params.flashvars+'">';
@@ -472,8 +474,6 @@ function _displayMultimedia(src, width, height, options) {
         html += ''
             + '<embed src="'+src+'" autostart="'+autostart+'"  width="'+width+'" height="'+height+'" flashvars="'+params.flashvars+'" wmode="'+params.wmode+'"></embed>'
             + '</object>';
-    } else if(/\.flv$/i.test(src) || /\.mov$/i.test(src) || /\.moov$/i.test(src) || /\.m4v$/i.test(src)) {
-        html = '<embed src="'+request_uri+'common/tpl/images/flvplayer.swf" allowfullscreen="true" autostart="'+autostart+'" width="'+width+'" height="'+height+'" flashvars="&file='+src+'&width='+width+'&height='+height+'&autostart='+autostart+'" wmode="'+params.wmode+'" />';
     }  else {
         html = '<embed src="'+src+'" autostart="'+autostart+'" width="'+width+'" height="'+height+'"';
         if(params.wmode == 'transparent') {
