9b10cc2df11d1628f671c773f48a0e6e3670638c
haneul <syhan@cs.washington.edu>
2009-10-27 05:24:09 +0000

    #18400372 : Context close in xml cache codes
    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@6893 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/menu/menu.admin.controller.php
+++ modules/menu/menu.admin.controller.php
@@ -411,7 +411,8 @@
                 'header("Cache-Control: no-store, no-cache, must-revalidate"); '.
                 'header("Cache-Control: post-check=0, pre-check=0", false); '.
                 'header("Pragma: no-cache"); '.
-                '%s'.
+                '%s '.
+                '$oContext->close(); '.
                 '?>'.
                 '<root>%s</root>',
                 $header_script,
