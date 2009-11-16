4d0d49df8cd0f81c15e497dfebad58bbd479be68
royallin <royallin@201d5d3c-b55e-5fd7-737f-ddc643e51545>
2009-11-16 03:16:07 +0000

    Modify CHT language
    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@6944 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/autoinstall/lang/zh-TW.lang.php
+++ modules/autoinstall/lang/zh-TW.lang.php
@@ -1,30 +1,30 @@
 <?php
     /**
-     * @file   ko.lang.php
-     * @author sol (sol@ngleader.com)
-     * @brief  Autoinstall(autoinstall) 모듈의 기본 언어팩
+     * @file   zh-TW.lang.php
+     * @author sol (sol@ngleader.com) 翻譯：royallin
+     * @brief  自動安裝(autoinstall)模組正體中文語言
      **/
 
-    $lang->autoinstall = '쉬운 설치';
-    $lang->about_autoinstall = 'XpressEngine의 여러 프로그램을 쉽게 설치/업그레이드할 수 있도록 도와주는 모듈입니다.';
-    $lang->package_update = '최근 업데이트';
-    $lang->package_downloaded_count = '전체 다운로드';
-    $lang->need_update = "업데이트가 필요합니다.";
+    $lang->autoinstall = '自動安裝';
+    $lang->about_autoinstall = '可幫助您安裝/更新XE程式及面板。';
+    $lang->package_update = '最近更新日';
+    $lang->package_downloaded_count = '全部下載';
+    $lang->need_update = "需要更新。";
 
-    $lang->order_newest = "신규 등록";
-    $lang->order_popular = "인기";
-    $lang->order_download = "다운로드";
-    $lang->success_installed = "설치가 성공하였습니다.";
-    $lang->view_all_package = "전체 보기";
-    $lang->description_ftp_note = "FTP 설정이 안되어있으면 설치가 진행되지 않습니다. FTP설정을 해주세요";
-    $lang->description_update = "새로 설치한 모듈의 버전 정보등은 업데이트를 눌러주시면 반영됩니다.";
-    $lang->install = "설치";
-    $lang->update = "업데이트";
-    $lang->current_version = "현재 설치 버전";
-    $lang->depending_programs = "의존하고 있는 프로그램";
-    $lang->require_update = "업데이트가 필요합니다.";
-    $lang->require_installation = "설치가 필요합니다.";
-    $lang->description_install = "설치를 진행하면, 이 프로그램이 의존하고 있는 모든 프로그램을 업데이트/설치 합니다.";
-    $lang->description_download = "FTP를 이용할 수 없는 경우, 직접 다운로드 하여 해당 path에 설치하셔야 합니다. (한칸 상위에서 압축을 푸시면 됩니다. ./modules/board의 경우 ./modules에서 tar를 푸세요)";
-    $lang->path = "설치경로";
+    $lang->order_newest = "最新";
+    $lang->order_popular = "熱門";
+    $lang->order_download = "下載";
+    $lang->success_installed = "安裝成功";
+    $lang->view_all_package = "全部檢視";
+    $lang->description_ftp_note = "請先將FTP設定好，否則無法執行自動安裝功能。";
+    $lang->description_update = "如果您最近不是用自動安裝模組更新或安裝，請點擊更新按鈕更新。";
+    $lang->install = "安裝";
+    $lang->update = "更新";
+    $lang->depending_programs = "This program depends on ";
+    $lang->require_update = "需要更新";
+    $lang->require_installation = "需要安裝";
+    $lang->description_install = "Installation process also install/update programs which this program depends on";
+    $lang->current_version = "安裝版本";
+    $lang->description_download = "When FTP is disabled, you should download it and extract it into target path. (if target path is ./modules/board, extract it at ./modules)";
+    $lang->path = "路徑";
 ?>
