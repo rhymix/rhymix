7ccf96fda45971e385fda1c32160374a22e9a5b0
haneul <syhan@cs.washington.edu>
2009-11-23 06:38:45 +0000

    #18454344 : modify link to parent directory
    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@6958 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/admin/tpl/js/config.js
+++ modules/admin/tpl/js/config.js
@@ -6,7 +6,28 @@ function completeGetFtpInfo(ret_obj)
     for(var i=0;i<ret_obj['list']['item'].length;i++)
     {   
         var v = ret_obj['list']['item'][i];
-        list = list + "<li><a href='"+current_url.setQuery('pwd',pwd+v)+"#ftpSetup'>"+v+"</a></li>";
+        if(v == "../")
+        {
+            if(pwd == "/")
+            {
+                continue;
+            }
+
+            arr = pwd.split("/");
+            arr.pop();
+            arr.pop();
+            arr.push("");
+            target = arr.join("/");
+            list = list + "<li><a href='"+current_url.setQuery('pwd',target)+"#ftpSetup'>"+v+"</a></li>";
+        } 
+        else if( v == "./")
+        {
+            continue;
+        }
+        else
+        {
+            list = list + "<li><a href='"+current_url.setQuery('pwd',pwd+v)+"#ftpSetup'>"+v+"</a></li>";
+        }
     }
     list = "<td><ul>"+list+"</ul></td>";
     e.append(jQuery(list));
