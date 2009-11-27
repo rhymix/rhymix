85d2b674e8061ecf63da7dc82c3da93b46270536
supershop <supershop@201d5d3c-b55e-5fd7-737f-ddc643e51545>
2009-11-27 09:21:03 +0000

    english translation
    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@6995 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/autoinstall/lang/en.lang.php
+++ modules/autoinstall/lang/en.lang.php
@@ -1,31 +1,31 @@
 <?php
     /**
-     * @file   ko.lang.php
+     * @file   en.lang.php
      * @author sol (sol@ngleader.com)
      * @brief  Autoinstall(autoinstall) 모듈의 기본 언어팩
      **/
 
-    $lang->autoinstall = 'Easy-install';
-    $lang->about_autoinstall = 'It helps you install/upgrade progams(skins) of XE.';
+    $lang->autoinstall = 'One Click Installer';
+    $lang->about_autoinstall = 'One Click Installer will help you install/upgrade programs(skins) for XE.';
     $lang->package_update = 'Recent Update';
     $lang->package_downloaded_count = 'Download Count';
-    $lang->need_update = "You need to update package list.";
+    $lang->need_update = "Please update package list first.";
 
     $lang->order_newest = "Newest";
     $lang->order_popular = "Popular";
     $lang->order_download = "Download";
-    $lang->success_installed = "Installations succeeded.";
+    $lang->success_installed = "Successfully Installed";
     $lang->view_all_package = "View All";
     $lang->description_ftp_note = "If FTP configuration is not set, installation would not work. Please configure FTP information";
-    $lang->description_update = "If you recently upgrade or install programs without easy installation module, please press update button to keep information of the programs updated.";
+    $lang->description_update = "If you have upgraded or installed programs without One Click Installer, please press update button to renew new information.";
     $lang->install = "Install";
     $lang->update = "Update";
-    $lang->current_version = "Installed version";
-    $lang->depending_programs = "This program depends on ";
+    $lang->current_version = "Version";
+    $lang->depending_programs = "This program is depending on ";
     $lang->require_update = "Update is required.";
     $lang->require_installation = "Installation is required.";
-    $lang->description_install = "Installation process also install/update programs which this program depends on";
-    $lang->description_download = "When FTP is disabled, you should download it and extract it into target path. (if target path is ./modules/board, extract it at ./modules)";
+    $lang->description_install = "One Click Installer will also install/update all other programs which this program is depending on";
+    $lang->description_download = "If FTP is unavailable, you should manually download it and extract it into target path. (if target path is ./modules/board, extract it at ./modules)";
     $lang->path = "Path";
     $lang->cmd_download = "Download";
 ?>
