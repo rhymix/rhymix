4ebc072262677ccf9baac99ee563b393d2ef01fa
haneul <syhan@cs.washington.edu>
2009-11-30 05:40:00 +0000

    #18510611 , #18516093 : fixed a bug occurred when there exists one file && added .. to parent dir.
    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@7000 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/admin/tpl/js/config.js
+++ modules/admin/tpl/js/config.js
@@ -1,24 +1,28 @@
 function completeGetFtpInfo(ret_obj)
 {
-
     var e = jQuery("#ftplist").empty();
     var list = "";
+    if(!jQuery.isArray(ret_obj['list']['item']))
+    {
+        ret_obj['list']['item'] = [ret_obj['list']['item']];
+    }
+
+    if(pwd != "/")
+    {
+        arr = pwd.split("/");
+        arr.pop();
+        arr.pop();
+        arr.push("");
+        target = arr.join("/");
+        list = list + "<li><a href='"+current_url.setQuery('pwd',target)+"#ftpSetup'>../</a></li>";
+    }
+    
     for(var i=0;i<ret_obj['list']['item'].length;i++)
     {   
         var v = ret_obj['list']['item'][i];
         if(v == "../")
         {
-            if(pwd == "/")
-            {
-                continue;
-            }
-
-            arr = pwd.split("/");
-            arr.pop();
-            arr.pop();
-            arr.push("");
-            target = arr.join("/");
-            list = list + "<li><a href='"+current_url.setQuery('pwd',target)+"#ftpSetup'>"+v+"</a></li>";
+            continue;
         } 
         else if( v == "./")
         {
@@ -29,6 +33,7 @@ function completeGetFtpInfo(ret_obj)
             list = list + "<li><a href='"+current_url.setQuery('pwd',pwd+v)+"#ftpSetup'>"+v+"</a></li>";
         }
     }
+
     list = "<td><ul>"+list+"</ul></td>";
     e.append(jQuery(list));
 }
