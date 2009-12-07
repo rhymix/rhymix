91c61c08905ca4ec45216a8496df07bc3967399b
ChanMyeong <dece24@gmail.com>
2009-12-07 08:11:26 +0000

    로그인 UI 개선
    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@7008 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/message/skins/default/message.js
+++ modules/message/skins/default/message.js
@@ -9,19 +9,3 @@ function completeMessageOpenIDLogin(ret_obj, response_tags) {
     var redirect_url =  ret_obj['redirect_url'];
     location.href = redirect_url;
 }
-
-
-/* 오픈 아이디 폼 변환 */
-function toggleLoginForm(obj) {
-    if(xGetElementById('messageLogin').style.display != "none") {
-        xGetElementById('messageLogin').style.display = "none";
-        xGetElementById('messageOpenidLogin').style.display = "block";
-        xGetElementById('messageOpenIDForm').use_open_id_2.checked = true;
-        xGetElementById('messageOpenIDForm').openid.focus();
-    } else {
-        xGetElementById('messageLogin').style.display = "block";
-        xGetElementById('messageOpenidLogin').style.display = "none";
-        xGetElementById('messageLoginForm').use_open_id.checked = false;
-        xGetElementById('messageLoginForm').user_id.focus();
-    }
-}
