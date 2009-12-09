6ce72677660fd3bdaba7501bc664f7b2dfc18414
haneul <syhan@cs.washington.edu>
2009-12-09 13:17:42 +0000

    #18534804 : blogapi specification
    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@7019 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- addons/blogapi/blogapi.addon.php
+++ addons/blogapi/blogapi.addon.php
@@ -263,8 +263,7 @@
                     if(!$output->toBool()) {
                         $content = getXmlRpcFailure(1, $output->getMessage());
                     } else {
-                        //$content = getXmlRpcResponse(Context::getRequestUri().$this->mid.'/'.$document_srl);
-                        $content = getXmlRpcResponse($document_srl);
+                        $content = getXmlRpcResponse(strval($document_srl));
                     }
                     FileHandler::removeDir($tmp_uploaded_path);
 
@@ -362,7 +361,7 @@
                     if(!$output->toBool()) {
                         $content = getXmlRpcFailure(1, $output->getMessage());
                     } else {
-                        $content = getXmlRpcResponse(getFullUrl('','document_srl',$document_srl));
+                        $content = getXmlRpcResponse(true);
                         FileHandler::removeDir($tmp_uploaded_path);
                     }
 
