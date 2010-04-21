feb2a1e0542db05bf7d4a95696d38d6c8f1819e0
ngleader <ngleader@gmail.com>
2010-04-21 02:03:44 +0000

    rollback [7245]
    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@7377 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/editor/editor.model.php
+++ modules/editor/editor.model.php
@@ -542,10 +542,10 @@
          * @brief 에디터 컴포넌트 목록 캐시 파일 이름 return
          **/
         function getCacheFile($filter_enabled= true, $site_srl = 0) {
-            $lang_type = Context::getLangType();
+            $lang = Context::getLangType();
             $cache_path = _XE_PATH_.'files/cache/editor/cache/';
             if(!is_dir($cache_path)) FileHandler::makeDir($cache_path);
-            $cache_file = $cache_path.'component_list.' . $lang_type .'.';
+            $cache_file = $cache_path.'component_list.' . $lang .'.';
             if($filter_enabled) $cache_file .= 'filter.';
             if($site_srl) $cache_file .= $site_srl.'.';
             $cache_file .= 'php';
