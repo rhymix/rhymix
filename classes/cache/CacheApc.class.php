f07cc09e4f7800eb5f57b6ab2277eebeedf53bc4
ngleader <ngleader@gmail.com>
2010-05-12 08:34:23 +0000

    #18885380 add Cache Handler
    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@7450 201d5d3c-b55e-5fd7-737f-ddc643e51545

---
+++ classes/cache/CacheApc.class.php
@@ -0,0 +1,70 @@
+<?php
+	class CacheApc extends CacheBase {
+		var $valid_time = 3600;
+
+		function getInstance(){
+            if(!$GLOBALS['__CacheApc__']) {
+                $GLOBALS['__CacheApc__'] = new CacheApc();
+            }
+            return $GLOBALS['__CacheApc__'];
+		}
+
+		function CacheApc(){
+		}
+
+		function isSupport(){
+			return apc_store('xe', 'xe', 1);
+		}
+
+		function put($key, $buff, $valid_time = 0){
+			if($valid_time == 0) $valid_time = $this->valid_time;
+			return apc_store(md5(_XE_PATH_.$key), array(time(), $buff), $valid_time);
+		}
+
+		function isValid($key, $modified_time = 0)
+		{
+			$key = md5(_XE_PATH_.$key);
+			$obj = apc_fetch($key, $success);
+			if(!$success || !is_array($obj)) return false;
+			unset($obj[1]);
+
+			if($modified_time > 0 && $modified_time > $obj[0])
+			{
+				$this->delete($_key);
+				return false;
+			}
+			
+			return true;
+		}
+
+		function get($key, $modified_time = 0)
+		{
+			$key = md5(_XE_PATH_.$key);
+			$obj = apc_fetch($key, $success);
+			if(!$success || !is_array($obj)) return false;
+
+			if($modified_time > 0 && $modified_time > $obj[0])
+			{
+				$this->_delete($key);
+				return false;
+			}
+
+			return $obj[1];
+		}
+
+		function _delete($_key)
+		{
+			$this->put($_key,null,1);
+		}
+
+		function delete($key)
+		{
+			$this->_delete(md5(_XE_PATH_.$key));
+		}
+
+		function truncate() 
+		{
+			apc_clear_cache('user');
+		}
+	}
+?>
---
+++ classes/cache/CacheHandler.class.php
@@ -0,0 +1,94 @@
+<?php
+    /**
+     * @class CacheHandler
+     * @author sol (sol@nhn.com)
+     * @brief Cache Handler
+     * @version 0.1
+	 *
+     **/
+
+    class CacheHandler extends Handler {
+			
+		var $handler = null;
+
+        function &getInstance($target='object') {
+			return new CacheHandler($target);
+        }
+
+		function CacheHandler($target, $info=null) {
+			if(!$info) $info = Context::getDBInfo();
+
+			if($target == 'object'){
+				if($info->use_template_cache =='apc') $type = 'apc';
+				else if(substr($info->use_template_cache,0,8)=='memcache'){
+					$type = 'memcache'; 
+					$url = $info->use_template_cache;
+				}
+			}else if($target == 'template'){
+				if($info->use_template_cache =='apc') $type = 'apc';
+				else if(substr($info->use_template_cache,0,8)=='memcache'){
+					$type = 'memcache'; 
+					$url = $info->use_template_cache;
+				}
+			}
+
+			if($type){
+				$class = 'Cache' . ucfirst($type);
+				include_once sprintf('%sclasses/cache/%s.class.php', _XE_PATH_, $class);
+				$this->handler = call_user_func(array($class,'getInstance'), $url);
+			}
+		}
+
+		function isSupport(){
+			if($this->handler && $this->handler->isSupport()) return true;
+			return false;
+		}
+
+		function get($key, $modified_time = 0){
+			if(!$this->handler) return false;
+			return $this->handler->get($key, $modified_time);
+		}
+
+		function put($key, $obj, $valid_time = 0){
+			if(!$this->handler) return false;
+			return $this->handler->put($key, $obj, $valid_time);
+		}
+
+		function delete($key){
+			if(!$this->handler) return false;
+			return $this->handler->delete($key);
+		}
+
+		function isValid($key, $modified_time){
+			if(!$this->handler) return false;
+			return $this->handler->isValid($key, $modified_time);
+		}
+
+		function truncate(){
+			if(!$this->handler) return false;
+			return $this->handler->truncate();
+		}
+    }
+
+	class CacheBase{
+		function get($key, $modified_time = 0){
+			return false;
+		}
+
+		function put($key, $obj, $valid_time = 0){
+			return false;
+		}
+
+		function isValid($key, $modified_time = 0){
+			return false;
+		}
+
+		function isSupport(){
+			return false;
+		}
+
+		function truncate(){
+			return false;
+		}
+	}
+?>
