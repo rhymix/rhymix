0fbdb9b716ff63b86d40fd2cdd69b8dbb90b3eb9
royallin <royallin@201d5d3c-b55e-5fd7-737f-ddc643e51545>
2010-05-15 15:19:43 +0000

    update Taiwanese
    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@7458 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/autoinstall/lang/zh-TW.lang.php
+++ modules/autoinstall/lang/zh-TW.lang.php
@@ -17,7 +17,7 @@
     $lang->success_installed = "安裝成功";
     $lang->view_all_package = "全部檢視";
     $lang->description_ftp_note = "請先將 FTP 設定好，否則無法執行自動安裝功能。";
-    $lang->description_update = "如果您最近不是用自動安裝模組更新或安裝，請點擊更新按鈕更新。";
+    $lang->description_update = "如果您最近不是用自動安裝模組更新或安裝，請按更新按鈕更新。";
     $lang->install = "安裝";
     $lang->update = "更新";
     $lang->current_version = "版本";
@@ -28,8 +28,8 @@
     $lang->description_download = "如果 FTP 無法使用的話，必須要手動下載並解壓縮到目標路徑。(假設目標路徑為 ./modules/board的話，將檔案解壓縮到 ./modules就可以了)";
     $lang->path = "路徑";
     $lang->cmd_download = "下載";
-    $lang->view_installed_packages = "已安裝套裝軟體";
+    $lang->view_installed_packages = "已安裝程式";
     $lang->msg_ftp_password_input = "請輸入 FTP 密碼";
-	$lang->dependant_list = "이 패키지에 의존하는 패키지 목록";
-	$lang->description_uninstall = "패키지를 삭제합니다. 모듈의 경우 모든 데이터가 사라집니다.";
+	$lang->dependant_list = "與此程式相關程式列表";
+	$lang->description_uninstall = "移除模組，所有資料將會被刪除。";
 ?>
--- modules/file/lang/zh-TW.lang.php
+++ modules/file/lang/zh-TW.lang.php
@@ -14,7 +14,7 @@
     $lang->is_stand_by = '等待';
     $lang->file_list = '檔案清單';
     $lang->allow_outlink = '外部檔案連結';
-    $lang->allow_outlink_site = '允許的外連網站';
+    $lang->allow_outlink_site = '允許外連的網站';
     $lang->allow_outlink_format = '允許外連的副檔名';
     $lang->allowed_filesize = '檔案大小限制';
     $lang->allowed_attach_size = '上傳限制';
@@ -22,11 +22,11 @@
     $lang->enable_download_group = '允許下載的群組';
 
     $lang->about_allow_outlink = '是否允許連結外部檔案。(*.wmv, *.mp3等影音檔案除外)';
-    $lang->about_allow_outlink_format = '設定允許外部連結的檔案格式。可以用(,)來區隔多個副檔名。<br />例)hwp,doc,zip,pdf';
-    $lang->about_allow_outlink_site = '可設置允許外部檔案連結的網站名單。當數量太多時，可換行輸入。<br />例)http://www.zeroboard.com';
-	$lang->about_allowed_filesize = '最大單一上傳檔案大小(管理員不受此限制)。';
-    $lang->about_allowed_attach_size = '每個主題最大上傳檔案大小(管理員不受此限制)。';
-    $lang->about_allowed_filetypes = '設定允許上傳的檔案類型。可以用"*.副檔名"來指定或用";"來區隔多個副檔名。<br />例) *.* or *.jpg;*.gif;<br />(管理員不受此限制)';
+    $lang->about_allow_outlink_format = '設定允許外部連結的檔案格式。可以用逗號(,)來區隔多個副檔名。<br />例) hwp, doc, zip, pdf';
+    $lang->about_allow_outlink_site = '可設置允許外部檔案連結的網站名單。當數量太多時，可換行輸入。<br />例) http://www.zeroboard.com';
+	$lang->about_allowed_filesize = '最大單一上傳檔案大小 (管理員不受此限制)。';
+    $lang->about_allowed_attach_size = '每個主題最大上傳檔案大小 (管理員不受此限制)。';
+    $lang->about_allowed_filetypes = '設定允許上傳的檔案類型。可以用"*.副檔名"來指定或用分號";"來區隔多個副檔名。<br />例) *.* or *.jpg; *.gif;<br />(管理員不受此限制)';
 
     $lang->cmd_delete_checked_file = '刪除所選項目';
     $lang->cmd_move_to_document = '檢視原始主題';
@@ -34,7 +34,7 @@
 
     $lang->msg_not_permitted_download = '您不具備下載的權限。';
     $lang->msg_cart_is_null = ' 請選擇要刪除的檔案。';
-    $lang->msg_checked_file_is_deleted = '已刪除%d個檔案！';
+    $lang->msg_checked_file_is_deleted = '已刪除 %d 個檔案！';
     $lang->msg_exceeds_limit_size = '已超過系統指定的檔案大小！';
     $lang->msg_file_not_found = '找不到檔案。';
 
@@ -42,8 +42,8 @@
         'filename' => '檔案名稱',
         'filesize_more' => '檔案大小 (byte, 以上)',
         'filesize_mega_more' => '檔案大小 (Mb, 以上)',
-		'filesize_less' => '파일크기 (byte, 이하)',
-		'filesize_mega_less' => '파일크기 (Mb, 이하)',
+		'filesize_less' => '檔案大小 (byte, 以下)',
+		'filesize_mega_less' => '檔案大小 (Mb, 以下)',
         'download_count' => '下載次數 (以上)',
         'user_id' => '帳號',
         'user_name' => '姓名',
@@ -51,5 +51,5 @@
         'regdate' => '登錄日期',
         'ipaddress' => 'IP位址',
     );
-	$lang->msg_not_allowed_outlink = 'It is not allowed to download files not from this site.'; 
+	$lang->msg_not_allowed_outlink = '無法從網站下載檔案。'; 
 ?>
