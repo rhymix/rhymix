99e0e8062051e2355ebe646dc5f486bbcd44dc4d
haneul <syhan@cs.washington.edu>
2010-06-17 10:09:16 +0000

    #18947649 : still modifying
    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@7518 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- classes/mobile/Mobile.class.php
+++ classes/mobile/Mobile.class.php
@@ -1,13 +1,38 @@
 <?php
 
 class Mobile {
+	var $ismobile = null;
+
+	function &getInstance() {
+		static $theInstance;
+		if(!isset($theInstance)) $theInstance = new Mobile();
+		return $theInstance;
+	}
+
 	function isFromMobilePhone() {
-		if(Context::get('full_browse') || $_COOKIE["FullBrowse"])
+		$oMobile =& Mobile::getInstance();
+		return $oMobile->_isFromMobilePhone();
+	}
+
+	function _isFromMobilePhone() {
+		if(isset($this->ismobile)) return $this->ismobile;
+		$db_info = Context::getDBInfo();
+		if($db_info->use_mobile_view != "Y" || Context::get('full_browse') || $_COOKIE["FullBrowse"])
 		{
-			return false;
+			$this->ismobile = false;
+		}
+		else
+		{
+			$this->ismobile = Context::get('mobile') || preg_match('/(iPod|iPhone|Android|SCH\-M[0-9]+)/',$_SERVER['HTTP_USER_AGENT']);
 		}
 
-		return Context::get('mobile') || preg_match('/(iPod|iPhone|Android|SCH\-M[0-9]+)/',$_SERVER['HTTP_USER_AGENT']);
+		return $this->ismobile;
+	}
+
+	function setMobile($ismobile)
+	{
+		$oMobile =& Mobile::getInstance();
+		$oMobile->ismobile = $ismobile;
 	}
 }
 
