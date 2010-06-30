c199130aba25277e25aeb9e8cc5269af58b7ae39
haneul <syhan@cs.washington.edu>
2010-06-30 07:41:44 +0000

    #18973574 : language select for mobile view
    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@7568 201d5d3c-b55e-5fd7-737f-ddc643e51545

---
+++ modules/module/module.mobile.php
@@ -0,0 +1,8 @@
+<?php
+
+class moduleMobile extends moduleObject {
+	function dispModuleChangeLang() { 
+		$this->setTemplatePath(sprintf("%stpl/",$this->module_path));
+		$this->setTemplateFile('lang.html');
+	}
+}
