e4575cd65adbf6a92e75e24eb17a35aa742fc8e1
ngleader <ngleader@gmail.com>
2010-07-05 07:31:44 +0000

    #r18940383 파일 업로드시 실패 message 추가
    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@7582 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/file/lang/vi.lang.php
+++ modules/file/lang/vi.lang.php
@@ -56,4 +56,7 @@
         'ipaddress' => 'IP',
     );
 	$lang->msg_not_allowed_outlink = 'Không cho phép tải file từ những trang khác ngoài trang này.'; 
+    $lang->msg_not_permitted_create = '파일 또는 디렉토리를 생성할 수 없습니다.';
+	$lang->msg_file_upload_error = '파일 업로드 중 에러가 발생하였습니다.';
+
 ?>
