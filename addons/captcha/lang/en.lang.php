5a1bb0cdd7b8dbc26a48a9dddef440a542b72ef1
supershop <supershop@201d5d3c-b55e-5fd7-737f-ddc643e51545>
2010-08-24 10:04:05 +0000

    english translation
    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@7645 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- addons/captcha/lang/en.lang.php
+++ addons/captcha/lang/en.lang.php
@@ -1,6 +1,6 @@
 <?php
-    $lang->about_captcha = "위 영어 알파벳을 순서대로 입력해 주세요. 대소문자는 구분하지 않습니다.";
-	$lang->captcha_reload = '이미지 새로고침';
-	$lang->captcha_play = '음성으로 듣기';
-	$lang->captcha_denied = '잘못 입력하셨습니다';
+    $lang->about_captcha = "Please type alphabets above in order. They are not case-sensitive.";
+	$lang->captcha_reload = 'Refresh Image';
+	$lang->captcha_play = 'Play sound of words';
+	$lang->captcha_denied = 'You have typed wrong alphabets.';
 ?>
