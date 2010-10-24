395788999b77145455ab30a1f31292f3210131e2
supershop <supershop@201d5d3c-b55e-5fd7-737f-ddc643e51545>
2010-10-24 08:12:56 +0000

    english translation
    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@7769 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/addon/lang/en.lang.php
+++ modules/addon/lang/en.lang.php
@@ -13,5 +13,5 @@
     $lang->addon_history = 'Addon History';
 
     $lang->about_addon_mid = "Addons can select targets.<br />(All targets will be selected when nothing is selected)";
-    $lang->about_addon = 'Addon is for controlling actions rather than displaying the HTML results.<br />Simply by toggling any addons you want on or off,  you can use very useful functions to administer your website';
+    $lang->about_addon = 'Addons control many actions performed in your site rather than display HTML results.<br />You can control useful functions simply by toggling ON/OFF switch';
 ?>
