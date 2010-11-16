6e163b4ff09060c9d0d8c6084b35588fdb9e5407
zero <skklove@gmail.com>
2010-11-16 09:41:45 +0000

    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@7841 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- classes/template/TemplateHandler.class.php
+++ classes/template/TemplateHandler.class.php
@@ -351,8 +351,10 @@
 				$next = substr($buff,$pos);
 
 				$pre_pos = strrpos($pre, '<');
-				$np = strpos($next,'"',strpos($next,'"')+1);
-				$next_pos = strpos(substr($next,$np), '>')+$np+1;
+
+				preg_match('/<(\/|[a-z])/i',$next,$m);
+				if(!$m[0]) return $buff;
+				$next_pos = strpos($next, $m[0]);
 
 				$tag = substr($pre, $pre_pos). substr($next, 0, $next_pos);
 				$pre = substr($pre, 0, $pre_pos);
