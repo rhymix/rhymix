e31cea38e325474d8c47ac6e41d481f30fc1e4f9
flourscent <flourscent@gmail.com>
2010-11-22 12:19:27 +0000

    일본어 번역 추가
    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@7868 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/autoinstall/lang/jp.lang.php
+++ modules/autoinstall/lang/jp.lang.php
@@ -28,8 +28,8 @@
     $lang->description_download = "FTPの利用が出来ない場合は、直接ダウンロードし、サーバー上の該当パスにてインストールして下さい。 (一つ上階層にて解凍します。　./modules/board　の場合 ./modulesにて tarを解凍して下さい。)";
     $lang->path = "インストールパス";
     $lang->cmd_download = "ダウンロード";
-    $lang->view_installed_packages = "Installed Packages";
-    $lang->msg_ftp_password_input = "Please input FTP password.";
-	$lang->dependant_list = "이 패키지에 의존하는 패키지 목록";
-	$lang->description_uninstall = "패키지를 삭제합니다. 모듈의 경우 모든 데이터가 사라집니다.";
+    $lang->view_installed_packages = "インストールされたパッケージ";
+    $lang->msg_ftp_password_input = "FTPパスワードを入力して下さい。";
+	$lang->dependant_list = "このパッケージに依存するパッケージのリスト";
+	$lang->description_uninstall = "パッケージを削除します。モジュールの場合、すべてのデータを失います。";
 ?>
