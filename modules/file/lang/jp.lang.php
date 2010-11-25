6b2e60d9e4f8dee1827677259b6b059b9c60b19e
flourscent <flourscent@gmail.com>
2010-11-25 01:39:55 +0000

    일본어 언어파일 내 번역 추가.
    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@7895 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/file/lang/jp.lang.php
+++ modules/file/lang/jp.lang.php
@@ -51,8 +51,8 @@
         'regdate' => '登録日',
         'ipaddress' => 'IPアドレス',
     );
-	$lang->msg_not_allowed_outlink = 'It is not allowed to download files not from this site.'; 
-    $lang->msg_not_permitted_create = '파일 또는 디렉토리를 생성할 수 없습니다.';
-	$lang->msg_file_upload_error = '파일 업로드 중 에러가 발생하였습니다.';
+	$lang->msg_not_allowed_outlink = '外部リンクからのダウンロードは許可されていません。'; 
+    $lang->msg_not_permitted_create = 'ファイルまたはディレクトリを生成できません。';
+	$lang->msg_file_upload_error = 'ファイルアップロードに失敗しました。';
 
 ?>
