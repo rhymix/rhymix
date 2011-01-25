04bbfc6cfbb9fc0bd0dad1bca3d6cf0b5a89d2a1
ngleader <ngleader@gmail.com>
2011-01-25 05:51:26 +0000

    add Turkey langauge
    git-svn-id: http://xe-core.googlecode.com/svn/sandbox@8056 201d5d3c-b55e-5fd7-737f-ddc643e51545

---
+++ modules/poll/lang/tr.lang.php
@@ -0,0 +1,38 @@
+<?php
+    /**
+     * @file   modules/poll/lang/en.lang.php
+     * @author NHN (developers@xpressengine.com)
+     * @brief  poll module / basic language pack 
+     **/
+
+    $lang->poll = "Oylama"; 
+    $lang->poll_stop_date = "Kullanım Süresi Bitimi";
+    $lang->poll_join_count = "Katılımcılar";
+    $lang->poll_checkcount = "Gerekli ögelerin sayısı";
+
+    $lang->cmd_poll_list = 'Oylama listesini gör';
+    $lang->cmd_delete_checked_poll = 'Seçili oylamayı sil'; 
+    $lang->cmd_apply_poll = 'Oylamayı uygula';
+    $lang->cmd_view_result = 'Sonuç önizlemesi';
+    $lang->cmd_delete_checked_poll = 'Seçili oylamayı sil';  
+
+    $lang->success_poll = 'Oylamaya katıldığınız için teşekkürler.';
+
+    $lang->msg_already_poll = 'Zaten oylamada bulundunuz!';
+    $lang->msg_cart_is_null = 'Lütfen silmek için bir makale seçiniz.';
+    $lang->msg_checked_poll_is_deleted = '%d oylama silindi.';
+    $lang->msg_check_poll_item = 'Lütfen oylama yapmak için oylama ögesi seçiniz.\n(Gerekli oylama ögeleri her oylamada farklılık gösterebilir.)';
+    $lang->msg_cart_is_null = 'Lütfen silmek için bir oylama seçiniz.';
+    $lang->msg_checked_poll_is_deleted = '%d oylama silindi.';
+    $lang->msg_poll_not_exists = 'Seçili oylama mevcut değil.';
+
+    $lang->cmd_null_item = "Oylama göndermek için hiçbir öge değeri yok. Lütfen tekrar deneyiniz.";
+
+    $lang->confirm_poll_submit = "Oylamayı göndermeyi onaylıyor musunuz?";
+
+    $lang->search_target_list = array(
+        'title' => 'Başlık',
+        'regdate' => 'Mesaj Tarihi',
+        'ipaddress' => 'IP Adresi',
+    );
+?>
---
+++ modules/trackback/lang/tr.lang.php
@@ -0,0 +1,28 @@
+<?php
+    /**
+     * @file   modules/trackback/lang/en.lang.php
+     * @author NHN (developers@xpressengine.com)
+     * @brief  trackback module / basic language pack
+     **/
+	$lang->cmd_toggle_checked_trackback = 'Seçili geri izlemeleri iptal et';
+    $lang->cmd_delete_checked_trackback = 'Seçili geri izlemeleri sil';
+
+    $lang->msg_cart_is_null = 'Lütfen silinecek geri izlemeyi seçin.';
+    $lang->msg_checked_trackback_is_deleted = '%d geri izlemeleri silindi.';
+
+	$lang->send_trackback_url = 'Hedef URL';
+    $lang->msg_trackback_url_is_invalid = 'Hedef URL geçersiz.';
+    $lang->msg_trackback_send_success = 'Başarıyla gönderildi';
+
+    $lang->search_target_list = array(
+        'url' => 'Hedef URL',
+        'blog_name' => 'Hedef Site Adı',
+        'title' => 'Başlık',
+        'excerpt' => 'Alıntı',
+        'regdate' => 'Gönderilme Tarihi',
+        'ipaddress' => 'IP Adresi',
+    );
+
+    $lang->enable_trackback = "Geri izleme kullan.";
+	$lang->about_enable_trackback = "Kontrol devredışı bırakıldığında, sitedeki tüm geri izleme koleksiyonları durdurulacaktır.";
+?>
