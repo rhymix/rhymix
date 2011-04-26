ba796d9badf4333ed034b19ea2be0312d5ad27de
ovclas <ovclas@gmail.com>
2011-04-26 08:00:58 +0000

    #19705602 problem fixed, when specified column select
    git-svn-id: http://xe-core.googlecode.com/svn/branches/1.5.0@8339 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- classes/db/DBMssql.class.php
+++ classes/db/DBMssql.class.php
@@ -707,8 +707,14 @@
 			{
 				$columns = array();
 				foreach($output->arg_columns as $col){
-					if(strpos($col,'[')===false && strpos($col,' ')==false) $columns[] = '['.$col.']'; 
-					else $columns[] = $col;
+					unset($tmpCol);
+					$tmpCol = explode('.', $col);
+					if(isset($tmpCol[1])) $col = $tmpCol[1];
+
+					if(strpos($col,'[')===false && strpos($col,' ')==false) $col = '['.$col.']'; 
+					if(isset($tmpCol[1])) $col = $tmpCol[0].'.'.$col;
+
+					$columns[] = $col;
 				}
 				
 				$columns = join(',',$columns);
@@ -826,8 +832,14 @@
 			{
 				$columns = array();
 				foreach($output->arg_columns as $col){
-					if(strpos($col,'[')===false && strpos($col,' ')==false) $columns[] = '['.$col.']'; 
-					else $columns[] = $col;
+					unset($tmpCol);
+					$tmpCol = explode('.', $col);
+					if(isset($tmpCol[1])) $col = $tmpCol[1];
+
+					if(strpos($col,'[')===false && strpos($col,' ')==false) $col = '['.$col.']'; 
+					if(isset($tmpCol[1])) $col = $tmpCol[0].'.'.$col;
+
+					$columns[] = $col;
 				}
 				
 				$columns = join(',',$columns);
