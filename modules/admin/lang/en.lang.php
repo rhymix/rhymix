412f95de4813d40bf636f745e04a4cb17e98b08b
taggon <gonom9@gmail.com>
2011-05-12 01:11:59 +0000

    #19745331 Converted four spaces to a tab in language files of the admin module.
    git-svn-id: http://xe-core.googlecode.com/svn/branches/1.5.0@8366 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/admin/lang/en.lang.php
+++ modules/admin/lang/en.lang.php
@@ -1,92 +1,95 @@
 <?php
-    /**
-     * @file   en.lang.php
-     * @author NHN (developers@xpressengine.com)
-     * @brief  English Language Pack (Only basic words are included here)
-     **/
+/**
+ * @file   en.lang.php
+ * @author NHN (developers@xpressengine.com)
+ * @brief  English Language Pack (Only basic words are included here)
+ **/
 
-    $lang->admin_info = 'Administrator Info';
-    $lang->admin_index = 'Index Admin Page';
-    $lang->control_panel = 'Dashboard';
-    $lang->start_module = 'Default Module';
-    $lang->about_start_module = 'You can specify the default module for the site.';
+$lang->admin_info = 'Administrator Info';
+$lang->admin_index = 'Index Admin Page';
+$lang->control_panel = 'Dashboard';
+$lang->start_module = 'Default Module';
+$lang->about_start_module = 'You can specify the default module for the site.';
 
-    $lang->module_category_title = array(
-        'service' => 'Services',
-        'member' => 'Members',
-        'content' => 'Contents',
-        'statistics' => 'Statistics',
-        'construction' => 'Construction',
-        'utility' => 'Utilities',
-        'interlock' => 'Embedded',
-        'accessory' => 'Accessories',
-        'migration' => 'Data Migration',
-        'system' => 'System Setting',
-    );
+$lang->module_category_title = array(
+	'service' => 'Services',
+	'member' => 'Members',
+	'content' => 'Contents',
+	'statistics' => 'Statistics',
+	'construction' => 'Construction',
+	'utility' => 'Utilities',
+	'interlock' => 'Embedded',
+	'accessory' => 'Accessories',
+	'migration' => 'Data Migration',
+	'system' => 'System Setting',
+);
 
-    $lang->newest_news = "Latest News";
-    
-    $lang->env_setup = "Setting";
-    $lang->default_url = "Default URL";
-    $lang->about_default_url = "If you use a virtual site feature (e.g., cafeXE), input default URL (parent-site's address), then SSO would be enabled, thus connection to documents/modules works properly.";
+$lang->newest_news = "Latest News";
 
-    $lang->env_information = "Environment Information";
-    $lang->current_version = "Current Version";
-    $lang->current_path = "Installed Path";
-    $lang->released_version = "Latest Version";
-    $lang->about_download_link = "New version of Zerboard XE is now available!\nPlease click the download link to get the latest version.";
-    
-    $lang->item_module = "Module List";
-    $lang->item_addon  = "Addon List";
-    $lang->item_widget = "Widget List";
-    $lang->item_layout = "Layout List";
+$lang->env_setup = "Setting";
+$lang->default_url = "Default URL";
+$lang->about_default_url = "If you use a virtual site feature (e.g., cafeXE), input default URL (parent-site's address), then SSO would be enabled, thus connection to documents/modules works properly.";
 
-    $lang->module_name = "Module Name";
-    $lang->addon_name = "Addon Name";
-    $lang->version = "Version";
-    $lang->author = "Developer";
-    $lang->table_count = "Number of Table";
-    $lang->installed_path = "Installed Path";
+$lang->env_information = "Environment Information";
+$lang->current_version = "Current Version";
+$lang->current_path = "Installed Path";
+$lang->released_version = "Latest Version";
+$lang->about_download_link = "New version of Zerboard XE is now available!\nPlease click the download link to get the latest version.";
 
-    $lang->cmd_shortcut_management = "Edit Menu";
+$lang->item_module = "Module List";
+$lang->item_addon  = "Addon List";
+$lang->item_widget = "Widget List";
+$lang->item_layout = "Layout List";
 
-    $lang->msg_is_not_administrator = 'Administrator Only';
-    $lang->msg_manage_module_cannot_delete = 'Shortcuts of module, addon, layout, widget cannot be removed';
-    $lang->msg_default_act_is_null = 'Shortcut could not be registered because the default admin Action is not set';
+$lang->module_name = "Module Name";
+$lang->addon_name = "Addon Name";
+$lang->version = "Version";
+$lang->author = "Developer";
+$lang->table_count = "Number of Table";
+$lang->installed_path = "Installed Path";
 
-    $lang->welcome_to_xe = 'Welcome to the admin page of XE';
-    $lang->about_admin_page = "Admin page is still under development,\nWe will add essential contents by accepting many good suggestions during the Closed-beta.";
-    $lang->about_lang_env = "To set the selected language as the default language, click on the Save button.";
+$lang->cmd_shortcut_management = "Edit Menu";
 
-    $lang->xe_license = 'XE complies with the GPL';
-    $lang->about_shortcut = 'You may remove shortcuts of modules which are registered on frequently using module list';
+$lang->msg_is_not_administrator = 'Administrator Only';
+$lang->msg_manage_module_cannot_delete = 'Shortcuts of module, addon, layout, widget cannot be removed';
+$lang->msg_default_act_is_null = 'Shortcut could not be registered because the default admin Action is not set';
 
-    $lang->yesterday = "Yesterday";
-    $lang->today = "Today";
+$lang->welcome_to_xe = 'Welcome to the admin page of XE';
+$lang->about_admin_page = "Admin page is still under development,\nWe will add essential contents by accepting many good suggestions during the Closed-beta.";
+$lang->about_lang_env = "To set the selected language as the default language, click on the Save button.";
 
-    $lang->cmd_lang_select = "Language";
-    $lang->about_cmd_lang_select = "Only selected languages will be served.";
-    $lang->about_recompile_cache = "You can delete useless or invalid cache files.";
-    $lang->use_ssl = "Use SSL";
-    $lang->ssl_options = array(
-        'none' => "Never",
-        'optional' => "Optional",
-        'always' => "Always"
-    );
-    $lang->about_use_ssl = "In case of 'Optional', SSL will be used for actions such as signing up / changing information. And for 'Always', your site will be served only via https.";
-    $lang->server_ports = "Server Port";
-    $lang->about_server_ports = "If your web server does not use 80 for HTTP or 443 for HTTPS port, you should specify server ports";
-    $lang->use_db_session = 'Use Session DB';
-    $lang->about_db_session = 'It will use php session with DB when authenticating.<br/>Websites with infrequent usage of web server may expect faster response when this function is disabled.<br/>However session DB will make it unable to get current users, so you cannot use related functions.';
-    $lang->sftp = "Use SFTP";
-    $lang->ftp_get_list = "Get List";
-    $lang->ftp_remove_info = 'Remove FTP Info.';
-	$lang->msg_ftp_invalid_path = 'Failed to read the specified FTP Path.';
-	$lang->msg_self_restart_cache_engine = 'Please restart Memcached or cache daemon.';
-	$lang->mobile_view = 'Mobile View';
-	$lang->about_mobile_view = 'Mobile View will display the best layout when accessing with smartphones.';
-    $lang->autoinstall = 'EasyInstall';
+$lang->xe_license = 'XE complies with the GPL';
+$lang->about_shortcut = 'You may remove shortcuts of modules which are registered on frequently using module list';
 
-    $lang->last_week = 'Last week';
-    $lang->this_week = 'This week';
+$lang->yesterday = "Yesterday";
+$lang->today = "Today";
+
+$lang->cmd_lang_select = "Language";
+$lang->about_cmd_lang_select = "Only selected languages will be served.";
+$lang->about_recompile_cache = "You can delete useless or invalid cache files.";
+$lang->use_ssl = "Use SSL";
+$lang->ssl_options = array(
+	'none' => "Never",
+	'optional' => "Optional",
+	'always' => "Always"
+);
+$lang->about_use_ssl = "In case of 'Optional', SSL will be used for actions such as signing up / changing information. And for 'Always', your site will be served only via https.";
+$lang->server_ports = "Server Port";
+$lang->about_server_ports = "If your web server does not use 80 for HTTP or 443 for HTTPS port, you should specify server ports";
+$lang->use_db_session = 'Use Session DB';
+$lang->about_db_session = 'It will use php session with DB when authenticating.<br/>Websites with infrequent usage of web server may expect faster response when this function is disabled.<br/>However session DB will make it unable to get current users, so you cannot use related functions.';
+$lang->sftp = "Use SFTP";
+$lang->ftp_get_list = "Get List";
+$lang->ftp_remove_info = 'Remove FTP Info.';
+$lang->msg_ftp_invalid_path = 'Failed to read the specified FTP Path.';
+$lang->msg_self_restart_cache_engine = 'Please restart Memcached or cache daemon.';
+$lang->mobile_view = 'Mobile View';
+$lang->about_mobile_view = 'Mobile View will display the best layout when accessing with smartphones.';
+$lang->autoinstall = 'EasyInstall';
+
+$lang->last_week = 'Last week';
+$lang->this_week = 'This week';
+
+$lang->use_html5 = 'HTML5 DTD';
+$lang->about_html5 = 'Use HTML5 DTD';
 ?>
--- modules/admin/lang/tr.lang.php
+++ modules/admin/lang/tr.lang.php
@@ -1,95 +1,95 @@
 <?php
-    /**
-     * @file   en.lang.php
-     * @author NHN (developers@xpressengine.com)
-     * @brief  English Language Pack (Only basic words are included here)
-     **/
+/**
+ * @file   tr.lang.php
+ * @author NHN (developers@xpressengine.com)
+ * @brief  Turkish Language Pack
+ **/
 
-    $lang->admin_info = 'Yönetici Bilgisi';
-    $lang->admin_index = 'Indeks Yönetici Sayfası';
-    $lang->control_panel = 'Dashboard';
-    $lang->start_module = 'Varsayılan Modül';
-    $lang->about_start_module = 'Sitenin varsayılan modülünü belirleyebilirsiniz.';
+$lang->admin_info = 'Yönetici Bilgisi';
+$lang->admin_index = 'Indeks Yönetici Sayfası';
+$lang->control_panel = 'Dashboard';
+$lang->start_module = 'Varsayılan Modül';
+$lang->about_start_module = 'Sitenin varsayılan modülünü belirleyebilirsiniz.';
 
-    $lang->module_category_title = array(
-        'service' => 'Hizmetler',
-        'member' => 'Yöneticiler',
-        'content' => 'İçerikler',
-        'statistics' => 'İstatistikler',
-        'construction' => 'Yapı',
-        'utility' => 'Yardımcı Uygulamalar',
-        'interlock' => 'Gömülü',
-        'accessory' => 'Donatılar',
-        'migration' => 'Veri Geçişi',
-        'system' => 'Sistem Ayarları',
-    );
+$lang->module_category_title = array(
+	'service' => 'Hizmetler',
+	'member' => 'Yöneticiler',
+	'content' => 'İçerikler',
+	'statistics' => 'İstatistikler',
+	'construction' => 'Yapı',
+	'utility' => 'Yardımcı Uygulamalar',
+	'interlock' => 'Gömülü',
+	'accessory' => 'Donatılar',
+	'migration' => 'Veri Geçişi',
+	'system' => 'Sistem Ayarları',
+);
 
-    $lang->newest_news = "Son Gelişmeler";
-    
-    $lang->env_setup = "Ayarlar";
-    $lang->default_url = "Varsayılan URL";
-    $lang->about_default_url = "Eğer sanal site özelliği kullanıyorsanız (örneğin, cafeXE), varsayılan URL girdisini yapınız (üst-sitenin adresi), SSO etkinleştirilecektir, böylece belgelere/modüllere sağlanan bağlantı uygun bir şekilde çalışacaktır. ";
+$lang->newest_news = "Son Gelişmeler";
 
-    $lang->env_information = "Ortam Bilgisi";
-    $lang->current_version = "Güncel Sürüm";
-    $lang->current_path = "Yükleme Yolu";
-    $lang->released_version = "Son Sürüm";
-    $lang->about_download_link = "Zeroboard XE'nin yeni sürümü yayımlandı!\nLütfen son sürümü için indirme linkine tıklayınız.";
-    
-    $lang->item_module = "Modül Listesi";
-    $lang->item_addon  = "Eklenti Listesi";
-    $lang->item_widget = "Widget Listesi";
-    $lang->item_layout = "Yerleşim Düzeni Listesi";
+$lang->env_setup = "Ayarlar";
+$lang->default_url = "Varsayılan URL";
+$lang->about_default_url = "Eğer sanal site özelliği kullanıyorsanız (örneğin, cafeXE), varsayılan URL girdisini yapınız (üst-sitenin adresi), SSO etkinleştirilecektir, böylece belgelere/modüllere sağlanan bağlantı uygun bir şekilde çalışacaktır. ";
 
-    $lang->module_name = "Modül İsmi";
-    $lang->addon_name = "Eklenti İsmi";
-    $lang->version = "Sürüm";
-    $lang->author = "Geliştirici";
-    $lang->table_count = "Tablo Numarası";
-    $lang->installed_path = "Yükleme Yolu";
+$lang->env_information = "Ortam Bilgisi";
+$lang->current_version = "Güncel Sürüm";
+$lang->current_path = "Yükleme Yolu";
+$lang->released_version = "Son Sürüm";
+$lang->about_download_link = "Zeroboard XE'nin yeni sürümü yayımlandı!\nLütfen son sürümü için indirme linkine tıklayınız.";
 
-    $lang->cmd_shortcut_management = "Menü Düzenle";
+$lang->item_module = "Modül Listesi";
+$lang->item_addon  = "Eklenti Listesi";
+$lang->item_widget = "Widget Listesi";
+$lang->item_layout = "Yerleşim Düzeni Listesi";
 
-    $lang->msg_is_not_administrator = 'Sadece Yöneticiler';
-    $lang->msg_manage_module_cannot_delete = 'Modüllerin, eklentilerin, yerleşim düzenlerinin, widgetların kısayolları silinemez.';
-    $lang->msg_default_act_is_null = 'Kısayol varsayılan yönetici eylemi ayarlanmadığından kayıt edilemiyor.';
+$lang->module_name = "Modül İsmi";
+$lang->addon_name = "Eklenti İsmi";
+$lang->version = "Sürüm";
+$lang->author = "Geliştirici";
+$lang->table_count = "Tablo Numarası";
+$lang->installed_path = "Yükleme Yolu";
 
-    $lang->welcome_to_xe = 'XE Yönetici Sayfasına Hoşgeldiniz';
-    $lang->about_admin_page = "Yönetici sayfası hâla geliştirilmektedir,\nClosebeta sürecinde birçok iyi öneriyi kabul ederek gerekli içerikleri ekleyeceğiz.";
-    $lang->about_lang_env = "Seçilen dili varsayılan dil olarak uygulamak için, lütfen Kaydet tuşuna basınız.";
+$lang->cmd_shortcut_management = "Menü Düzenle";
 
-    $lang->xe_license = 'XE GPL ile uyumludur';
-    $lang->about_shortcut = 'Sık kullanılan modüller listesine kaydedilmiş modüllerin kısayollarını silebilirsiniz.';
+$lang->msg_is_not_administrator = 'Sadece Yöneticiler';
+$lang->msg_manage_module_cannot_delete = 'Modüllerin, eklentilerin, yerleşim düzenlerinin, widgetların kısayolları silinemez.';
+$lang->msg_default_act_is_null = 'Kısayol varsayılan yönetici eylemi ayarlanmadığından kayıt edilemiyor.';
 
-    $lang->yesterday = "Dün";
-    $lang->today = "Bugün";
+$lang->welcome_to_xe = 'XE Yönetici Sayfasına Hoşgeldiniz';
+$lang->about_admin_page = "Yönetici sayfası hâla geliştirilmektedir,\nClosebeta sürecinde birçok iyi öneriyi kabul ederek gerekli içerikleri ekleyeceğiz.";
+$lang->about_lang_env = "Seçilen dili varsayılan dil olarak uygulamak için, lütfen Kaydet tuşuna basınız.";
 
-    $lang->cmd_lang_select = "Dil";
-    $lang->about_cmd_lang_select = "Sadece seçili dillerde hizmet verecektir.";
-    $lang->about_recompile_cache = "Gereksiz veya geçersiz önbellek dosyalarını silebilirsiniz.";
-    $lang->use_ssl = "SSL Kullan";
-    $lang->ssl_options = array(
-        'none' => "Hiçbir zaman",
-        'optional' => "İsteğe Bağlı",
-        'always' => "Her zaman"
-    );
-    $lang->about_use_ssl = "'İsteği Bağlı' seçiminde; SSL, kayıt olma/bilgi değiştirme gibi eylemler için kullanılacaktır. 'Her zaman' seçiminde, siteniz sadece http yoluyla hizmet verecektir.";
-    $lang->server_ports = "Sunucu Bağlantı Noktası (port)";
-    $lang->about_server_ports = "Eğer web sunucunuz, HTTP bağlantı noktaları için 80 ya da HTTPS 443 portunu kullanmıyorsa, sunucu bağlantı noktalarını belirtmeniz gerekmektedir.";
-    $lang->use_db_session = 'Oturum Veritabanı Kullanımı';
-    $lang->about_db_session = 'Yetersiz web sunucusu kullanımı olan websiteleri için, bu özellik devredışı bırakıldığı zaman daha hızlı bir tepki beklenebilir.<br/>Ancak oturum veritabanı, mevcut kullanıcılar için veritabanını erişilemez hâle getirecektir ve ilgili işler kullanılamaz hale gelecektir.';
-    $lang->sftp = "SFTP Kullan";
-    $lang->ftp_get_list = "Listeyi Al";
-    $lang->ftp_remove_info = 'FTP Bilgisini Sil.';
-	$lang->msg_ftp_invalid_path = 'Belirtilen FTP Yolunu okuma işlemi başarız oldu.';
-	$lang->msg_self_restart_cache_engine = 'Lütfen önbellek geri plan yordamını veya Memcached\' ı yeniden başlatınız.';
-	$lang->mobile_view = 'Hareketli Görünümü';
-	$lang->about_mobile_view = 'Hareketli görünümü, mobil cihazlarla giriş yapılırken, mobil cihazlara uygun en iyi yerleşim düzenini göstermek içindir.';
-    $lang->autoinstall = 'KolayKurulum';
+$lang->xe_license = 'XE GPL ile uyumludur';
+$lang->about_shortcut = 'Sık kullanılan modüller listesine kaydedilmiş modüllerin kısayollarını silebilirsiniz.';
 
-    $lang->last_week = 'Geçen Hafta';
-    $lang->this_week = 'Bu Hafta';
+$lang->yesterday = "Dün";
+$lang->today = "Bugün";
 
-	$lang->use_html5 = 'HTML5 DTD';
-	$lang->about_html5 = 'Use HTML5 DTD';
+$lang->cmd_lang_select = "Dil";
+$lang->about_cmd_lang_select = "Sadece seçili dillerde hizmet verecektir.";
+$lang->about_recompile_cache = "Gereksiz veya geçersiz önbellek dosyalarını silebilirsiniz.";
+$lang->use_ssl = "SSL Kullan";
+$lang->ssl_options = array(
+	'none' => "Hiçbir zaman",
+	'optional' => "İsteğe Bağlı",
+	'always' => "Her zaman"
+);
+$lang->about_use_ssl = "'İsteği Bağlı' seçiminde; SSL, kayıt olma/bilgi değiştirme gibi eylemler için kullanılacaktır. 'Her zaman' seçiminde, siteniz sadece http yoluyla hizmet verecektir.";
+$lang->server_ports = "Sunucu Bağlantı Noktası (port)";
+$lang->about_server_ports = "Eğer web sunucunuz, HTTP bağlantı noktaları için 80 ya da HTTPS 443 portunu kullanmıyorsa, sunucu bağlantı noktalarını belirtmeniz gerekmektedir.";
+$lang->use_db_session = 'Oturum Veritabanı Kullanımı';
+$lang->about_db_session = 'Yetersiz web sunucusu kullanımı olan websiteleri için, bu özellik devredışı bırakıldığı zaman daha hızlı bir tepki beklenebilir.<br/>Ancak oturum veritabanı, mevcut kullanıcılar için veritabanını erişilemez hâle getirecektir ve ilgili işler kullanılamaz hale gelecektir.';
+$lang->sftp = "SFTP Kullan";
+$lang->ftp_get_list = "Listeyi Al";
+$lang->ftp_remove_info = 'FTP Bilgisini Sil.';
+$lang->msg_ftp_invalid_path = 'Belirtilen FTP Yolunu okuma işlemi başarız oldu.';
+$lang->msg_self_restart_cache_engine = 'Lütfen önbellek geri plan yordamını veya Memcached\' ı yeniden başlatınız.';
+$lang->mobile_view = 'Hareketli Görünümü';
+$lang->about_mobile_view = 'Hareketli görünümü, mobil cihazlarla giriş yapılırken, mobil cihazlara uygun en iyi yerleşim düzenini göstermek içindir.';
+$lang->autoinstall = 'KolayKurulum';
+
+$lang->last_week = 'Geçen Hafta';
+$lang->this_week = 'Bu Hafta';
+
+$lang->use_html5 = 'HTML5 DTD';
+$lang->about_html5 = 'Use HTML5 DTD';
 ?>
