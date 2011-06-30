4d26d3b9801dda388307965b516aef2d6920f046
ovclas <ovclas@gmail.com>
2011-06-30 06:33:24 +0000

    issue 46 apply validator to syndication module
    git-svn-id: http://xe-core.googlecode.com/svn/branches/1.5.0@8554 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/syndication/syndication.admin.controller.php
+++ modules/syndication/syndication.admin.controller.php
@@ -37,6 +37,11 @@
 			}
 
             $this->setMessage('success_applied');
+			if(!in_array(Context::getRequestMethod(),array('XMLRPC','JSON'))) {
+				$returnUrl = Context::get('success_return_url') ? Context::get('success_return_url') : getNotEncodedUrl('', 'module', 'admin', 'act', 'dispSyndicationAdminConfig');
+				header('location:'.$returnUrl);
+				return;
+			}
         }
 
 		function procSyndicationAdminCheckSitePingResult(){
