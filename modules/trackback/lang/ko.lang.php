ef99b1c24011cc046de9ab856e864b83e7378b63
ChanMyeong <dece24@gmail.com>
2011-08-25 05:09:31 +0000

    Admin setting 'title, image' UI added.
    git-svn-id: http://xe-core.googlecode.com/svn/branches/1.5.0@8883 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/trackback/lang/ko.lang.php
+++ modules/trackback/lang/ko.lang.php
@@ -7,10 +7,10 @@
 	$lang->cmd_toggle_checked_trackback = '선택항목 반전';
     $lang->cmd_delete_checked_trackback = '선택항목 삭제';
     $lang->msg_cart_is_null = '삭제할 글을 선택해주세요.';
-    $lang->msg_checked_trackback_is_deleted = '%d개의 엮인글이 삭제되었습니다.';
+    $lang->msg_checked_trackback_is_deleted = '%d개의 엮인글을 삭제했습니다.';
     $lang->send_trackback_url = '발송할 주소';
-    $lang->msg_trackback_url_is_invalid = '발송할 대상 url이 올바르지 않습니다.';
-    $lang->msg_trackback_send_success = '엮인글 발송에 성공하였습니다.';
+    $lang->msg_trackback_url_is_invalid = '발송할 대상의 주소가 올바르지 않습니다.';
+    $lang->msg_trackback_send_success = '엮인글을 발송했습니다.';
     $lang->search_target_list = array(
         'url' => '대상 URL',
         'blog_name' => '대상 사이트 이름',
@@ -20,7 +20,7 @@
         'ipaddress' => 'IP 주소',
     );
     $lang->enable_trackback = '엮인글 사용';
-    $lang->about_enable_trackback = '체크를 해제하시면 사이트 내에 모든 글의 엮인글 접수를 중지합니다.';
+    $lang->about_enable_trackback = '체크를 해제하면 모든 글의 엮인글 접수를 중지합니다.';
 	$lang->no_trackbacks = '엮인글이 없습니다.';
 	$lang->trackback_manager = '선택한 엮인글 관리';
 	$lang->selected_trackback = '선택한 엮인글';
