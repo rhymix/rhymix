e4031ad4db5566759082233fcb6e8c33e9d98918
flyskyko <sinsy200@gmail.com>
2011-09-08 08:06:27 +0000

    issue 157, change default escape char.
    git-svn-id: http://xe-core.googlecode.com/svn/branches/1.5.0@9121 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- classes/db/DB.class.php
+++ classes/db/DB.class.php
@@ -604,7 +604,7 @@
 		// HACK This is needed because on installation, the XmlQueryParer is used without any configured database
 		// TODO Change this or make sure the query cache files created before db.config exists are deleted
 		function getParser(){
-			return new DBParser('"');
+			return new DBParser('`');
 		}
 
         function _getSlaveConnectionStringIndex() {
