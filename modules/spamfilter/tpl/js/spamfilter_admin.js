3c3481f377a899d08e8db0f8c4873b37d5a3bb12
chschy <chschy@gmail.com>
2011-09-21 06:53:02 +0000

    issue 160, modify javscript in spamfilter page and language
    git-svn-id: http://xe-core.googlecode.com/svn/branches/1.5.0@9250 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/spamfilter/tpl/js/spamfilter_admin.js
+++ modules/spamfilter/tpl/js/spamfilter_admin.js
@@ -20,23 +20,30 @@ function doDeleteDeniedWord(word) {
 	fo_obj.submit();
 }
 
-function doInsertDeniedSome(some){
+function checkSomeValid(some){
 	var fo_obj = get_by_id('spamfilterInsert');
 
-	if(some == 'ipaddressList'){
-		var reg = /^(\d{1,3}(?:.(\d{1,3}|\*)){3}\s*(\/\/[^\r\n]*)?[\r\n]*)*$/;
-		var matchStr = fo_obj.ipaddressList.value;
+	var reg_ipaddress = /^(\d{1,3}(?:.(\d{1,3}|\*)){3}\s*(\/\/[^\r\n]*)?[\r\n]*)*$/;
+	var matchStr_ipaddress = fo_obj.ipaddressList.value;
+	
+	var reg_word = /^(.{2,40}[\r\n]+)*.{0,40}$/;
+	var matchStr_word = fo_obj.wordList.value;
+	
+	var valid_word = true;
+	var valid_ip = true;
+	
+	if(!matchStr_ipaddress.match(reg_ipaddress)) valid_ip = false;
+	if(!matchStr_word.match(reg_word)) valid_word = false;
+
+	if(some == 'ip'){
 		fo_obj.wordList.value = '';
-	}
-	else if(some == 'wordList'){
-		var reg = /^(.{2,40}[\r\n]+)*.{2,40}$/;
-		var matchStr = fo_obj.wordList.value;
+		valid_word = true;
+	} else if(some == 'word'){
 		fo_obj.ipaddressList.value = '';
+		valid_ip = true;
 	}
 
-	if(!matchStr.match(reg)){
-		alert('형식에 맞게 입력하세요'); return false;
-	}
-
-	fo_obj.submit();
+	if(valid_ip && valid_word) return true; 
+	else return false;
 }
+
