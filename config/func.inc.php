2a6f6e3c04d6dd86c6ecaf780860607e95688617
flyskyko <sinsy200@gmail.com>
2011-09-21 01:11:34 +0000

    issue 160, fix a bug for garbled text after copy module instance
    git-svn-id: http://xe-core.googlecode.com/svn/branches/1.5.0@9244 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- config/func.inc.php
+++ config/func.inc.php
@@ -988,6 +988,23 @@
 		}
 	}
 
+	function htmlHeader()
+	{
+		echo <<<HTMLHEADER
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html lang="ko" xml:lang="ko" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+</head>
+<body>
+HTMLHEADER;
+	}
+
+	function htmlFooter()
+	{
+		echo '</body></html>';
+	}
+
 	function alertScript($msg)
 	{
 		if(!$msg) return;
