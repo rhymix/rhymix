ac866a7a317746a81f517fe04cc4c4650bb9f54b
ovclas <ovclas@gmail.com>
2011-09-28 09:37:35 +0000

    issue 160 admin logging addon init
    git-svn-id: http://xe-core.googlecode.com/svn/branches/1.5.0@9407 201d5d3c-b55e-5fd7-737f-ddc643e51545

---
+++ addons/adminlogging/adminlogging.addon.php
@@ -0,0 +1,17 @@
+<?php
+    if(!defined("__ZBXE__")) exit();
+
+    /**
+     * @file adminlogging.addon.php
+     * @author NHN (developers@xpressengine.com)
+     * @brief Automatic link add-on
+     **/
+	$logged_info = Context::get('logged_info');
+	$act = Context::get('act');
+	$kind = strpos(strtolower($act),'admin')!==false?'admin':'';
+
+    if($called_position == 'before_module_proc' && $kind == 'admin' && $logged_info->is_admin == 'Y') {
+		$oAdminloggingController = &getController('adminlogging');
+		$oAdminloggingController->insertLog($this->module, $this->act);
+    }
+?>
