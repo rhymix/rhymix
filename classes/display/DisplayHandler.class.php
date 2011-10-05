c0da2956cbd0f006207d6142368d9fae20d71eb3
lickawtl <lickawtl@gmail.com>
2011-10-05 12:28:05 +0000

    git-svn-id: http://xe-core.googlecode.com/svn/branches/1.5.0@9497 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- classes/display/DisplayHandler.class.php
+++ classes/display/DisplayHandler.class.php
@@ -159,6 +159,7 @@
                     $buff .= sprintf("\tTemplate compile elapsed time\t: %0.5f sec (%d called)\n", $GLOBALS['__template_elapsed__'], $GLOBALS['__TemplateHandlerCalled__']);
                     $buff .= sprintf("\tXmlParse compile elapsed time\t: %0.5f sec\n", $GLOBALS['__xmlparse_elapsed__']);
                     $buff .= sprintf("\tPHP elapsed time \t\t: %0.5f sec\n", $end-__StartTime__-$GLOBALS['__template_elapsed__']-$GLOBALS['__xmlparse_elapsed__']-$GLOBALS['__db_elapsed_time__']-$GLOBALS['__elapsed_class_load__']);
+		    $buff .= sprintf("\tDB class elapsed time \t\t: %0.5f sec\n", $GLOBALS['__dbclass_elapsed_time__'] -$GLOBALS['__db_elapsed_time__']);
                     // widget execution time
                     $buff .= sprintf("\n\tWidgets elapsed time \t\t: %0.5f sec", $GLOBALS['__widget_excute_elapsed__']);
                     // layout execution time
