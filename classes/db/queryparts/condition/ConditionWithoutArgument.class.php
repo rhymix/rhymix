873d3e704ab0de43fe70d60333e66744f59a7043
ucorina <corina.udrescu@gmail.com>
2011-10-13 15:01:15 +0000

    Issue 490: Patch for /branches/1.5.0/classes/db/queryparts/condition/ConditionWithoutArgument.class.php
    git-svn-id: http://xe-core.googlecode.com/svn/branches/1.5.0@9644 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- classes/db/queryparts/condition/ConditionWithoutArgument.class.php
+++ classes/db/queryparts/condition/ConditionWithoutArgument.class.php
@@ -3,7 +3,7 @@
 	class ConditionWithoutArgument extends Condition {
 		function ConditionWithoutArgument($column_name, $argument, $operation, $pipe = ""){
                     parent::Condition($column_name, $argument, $operation, $pipe);
-                    if(in_array($operation, array('in', 'not in')))
+                    if(in_array($operation, array('in', 'notin')))
                             $this->_value = '('. $argument .')';
                     else
                         $this->_value = $argument;
