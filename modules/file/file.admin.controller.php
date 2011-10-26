0b423d7f927db3e72a4adbf8d23557b37ce7f52c
ovclas <ovclas@gmail.com>
2011-10-26 02:00:15 +0000

    file module config modify
    git-svn-id: http://xe-core.googlecode.com/svn/branches/1.5.0@9748 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/file/file.admin.controller.php
+++ modules/file/file.admin.controller.php
@@ -137,7 +137,7 @@
             }
 
             $this->setError(-1);
-            $this->setMessage('success_updated');
+            $this->setMessage('success_updated', 'info');
 			if(!in_array(Context::getRequestMethod(),array('XMLRPC','JSON'))) {
 				$returnUrl = Context::get('success_return_url') ? Context::get('success_return_url') : getNotEncodedUrl('', 'module', 'admin', 'act', 'dispBoardAdminContent');
 				header('location:'.$returnUrl);
