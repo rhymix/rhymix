22591f44b0a27a8e7a9a895e398ac78b822fb6ce
flyskyko <sinsy200@gmail.com>
2011-10-27 06:40:37 +0000

    version up to 1.5.0.8
    git-svn-id: http://xe-core.googlecode.com/svn/branches/1.5.0@9770 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- config/config.inc.php
+++ config/config.inc.php
@@ -13,7 +13,7 @@
      * @brief display XE's full version
      * Even The file should be revised when releasing altough no change is made
      **/
-    define('__ZBXE_VERSION__', '1.5.0.7');
+    define('__ZBXE_VERSION__', '1.5.0.8');
 
     /**
      * @brief The base path to where you installed zbXE Wanted
