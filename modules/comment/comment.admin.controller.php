f4f3639f487ba7c05769d35534302b490f7087d6
flyskyko <sinsy200@gmail.com>
2011-11-07 05:29:36 +0000

    after action, search value memory
    git-svn-id: http://xe-core.googlecode.com/svn/branches/1.5.0@9802 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/comment/comment.admin.controller.php
+++ modules/comment/comment.admin.controller.php
@@ -86,7 +86,7 @@
             $this->setMessage($msgCode, 'info');
 
 			if(!in_array(Context::getRequestMethod(),array('XMLRPC','JSON'))) {
-				$returnUrl = Context::get('success_return_url') ? Context::get('success_return_url') : getNotEncodedUrl('', 'module', 'admin', 'act', 'dispCommentAdminList');
+				$returnUrl = Context::get('success_return_url') ? Context::get('success_return_url') : getNotEncodedUrl('', 'module', 'admin', 'act', 'dispCommentAdminList', 'search_keyword', $search_keyword, 'search_target', $search_target);
 				header('location:'.$returnUrl);
 				return;
 			}
