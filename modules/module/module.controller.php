b4bac3ffac877266b92d7453b27b7ad8d7741304
misol <misol.kr@gmail.com>
2012-04-26 08:07:58 +0000

    Make clearer the condition of regular expression of user_lang than before...
    git-svn-id: http://xe-core.googlecode.com/svn/branches/1.5.0@10622 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/module/module.controller.php
+++ modules/module/module.controller.php
@@ -696,7 +696,7 @@
          * @brief Change user-defined language
          **/
         function replaceDefinedLangCode(&$output) {
-            $output = preg_replace_callback('!\$user_lang->([a-z0-9\_]+)!is', array($this,'_replaceLangCode'), $output);
+            $output = preg_replace_callback('!\$user\_lang\-\>([A-Za-z0-9\_]+)!s', array($this,'_replaceLangCode'), $output);
         }
         function _replaceLangCode($matches) {
             static $lang = null;
