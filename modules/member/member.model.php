f59eec72f0598019fae0427325fb3a60b5e1166a
devjin <namnyams@gmail.com>
2012-08-03 10:05:40 +0000

    issue 873 retouch.
    git-svn-id: http://xe-core.googlecode.com/svn/branches/1.5.3.1@10974 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/member/member.model.php
+++ modules/member/member.model.php
@@ -35,8 +35,7 @@
 			}
 
             // Get terms of user
-            $agreement_file = _XE_PATH_.'files/member_extra_info/agreement_' . Context::get('lang_type') . '.txt';
-            if(is_readable($agreement_file)) $config->agreement = FileHandler::readFile($agreement_file);
+			$config->agreement = $this->_getAgreement();
 
             if(!$config->webmaster_name) $config->webmaster_name = 'webmaster';
             if(!$config->image_name_max_width) $config->image_name_max_width = 90;
@@ -56,6 +55,34 @@
 
             return $config;
         }
+
+		function _getAgreement()
+		{
+            $agreement_file = _XE_PATH_.'files/member_extra_info/agreement_' . Context::get('lang_type') . '.txt';
+			if(is_readable($agreement_file))
+			{
+				return FileHandler::readFile($agreement_file);
+			}
+
+            $db_info = Context::getDBInfo();
+			$agreement_file = _XE_PATH_.'files/member_extra_info/agreement_' . $db_info->lang_type . '.txt';
+			if(is_readable($agreement_file))
+			{
+				return FileHandler::readFile($agreement_file);
+			}
+
+			$lang_selected = Context::loadLangSelected();
+			foreach($lang_selected as $key => $val)
+			{
+				$agreement_file = _XE_PATH_.'files/member_extra_info/agreement_' . $key . '.txt';
+				if(is_readable($agreement_file))
+				{
+					return FileHandler::readFile($agreement_file);
+				}
+			}
+
+			return null;
+		}
 
         /**
          * @brief Display menus of the member
