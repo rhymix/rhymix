2db5e7ca5378fca368d2407a7812d2a7b2d19d0f
flyskyko <sinsy200@gmail.com>
2012-08-22 13:40:54 +0000

    issue 2242, added multilingual on widget style
    git-svn-id: http://xe-core.googlecode.com/svn/branches/1.5.3.2@11062 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/admin/tpl/js/admin.js
+++ modules/admin/tpl/js/admin.js
@@ -1076,8 +1076,17 @@ $('.multiLangEdit')
 					var $li = $('<li />').appendTo($langlist);
 
 					var anchor_id = $layer.data('layer_index');
+
+					var lang_text = this[xe.current_lang];
+					if(!lang_text) {
+						for(lang_code in this) {
+							lang_text = this[lang_code];
+							break;
+						}
+					}
+
 					$('<a href="#langInput_'+anchor_id+'" class="langItem" />')
-						.text(this[xe.current_lang])
+						.text(lang_text)
 						.data('multilang-name', key)
 						.appendTo($li);
 				});
