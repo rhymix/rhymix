2c0157362566337bf47be893f7a61db48ace0ac9
nagoon97 <nagoon97@gmail.com>
2012-09-10 08:55:43 +0000

    Issue 2356 : Fixes for the new module selector made in Luminous branch is merged
    git-svn-id: http://xe-core.googlecode.com/svn/branches/1.5.3.2@11196 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/widget/tpl/js/mid.js
+++ modules/widget/tpl/js/mid.js
@@ -13,7 +13,7 @@ xe.MidManager = xe.createApp("MidManager", {
 			return false;
 		});
 
-		$keyObj.siblings('button.delete').bind('click', function(){
+		$keyObj.parent().find('button.delete,a.delete').bind('click', function(){
 			$keyObj.val('').next().val('');
 			return false;
 		});
@@ -24,6 +24,13 @@ xe.MidManager = xe.createApp("MidManager", {
 			$finder.slideUp(100);
 			return false;
 		});
+		
+		jQuery(function($){
+			$('.moduleSearch').bind('moduleSelect', function(e, aSelected){
+				self.cast('MID_SYNC_NEW', aSelected);
+			});
+		});
+
 	},
 
 	API_MID_SYNC: function(){
@@ -31,6 +38,27 @@ xe.MidManager = xe.createApp("MidManager", {
 		var $finder = self.$keyObj.closest('.modulefinder');
 
 		var module_srl = this.$keyObj.val();
+		if (!module_srl) return;
+
+		function on_complete(data){
+			if (data.error){
+				alert(data.message);
+				return;
+			}
+
+			self.$keyObj.val(data.module_list[0].module_srl);
+			self.$keyObj.next().val(data.module_list[0].browser_title+' ('+data.module_list[0].mid+', '+data.module_list[0].module_name+')');
+		}
+
+		$.exec_json('module.getModuleAdminModuleList', {'module_srls': module_srl}, on_complete);
+	},
+	
+	API_MID_SYNC_NEW: function(htBase, aParam){
+		var self = this;
+
+		htSelected = aParam[0];
+		
+		var module_srl = htSelected.module_srl;
 		if (!module_srl) return;
 
 		function on_complete(data){
