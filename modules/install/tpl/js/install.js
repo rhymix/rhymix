4429e5ae16dacc9e46290a917217c920423a5ef0
ChanMyeong <dece24@gmail.com>
2012-10-09 08:31:57 +0000

    Issue 2050 started. Installation > admin info setting.
    git-svn-id: http://xe-core.googlecode.com/svn/branches/maserati@11642 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/install/tpl/js/install.js
+++ modules/install/tpl/js/install.js
@@ -13,88 +13,14 @@ jQuery(function($){
 		}
 		return false;
 	});
-
-	// Input Clear
-	var iText = $('.item>.iLabel').next('.iText');
-	$('.item>.iLabel').css('position','absolute');
-	iText
-		.focus(function(){
-			$(this).prev('.iLabel').css('visibility','hidden');
-		})
-		.blur(function(){
-			if($(this).val() == ''){
-				$(this).prev('.iLabel').css('visibility','visible');
-			} else {
-				$(this).prev('.iLabel').css('visibility','hidden');
-			}
-		})
-		.change(function(){
-			if($(this).val() == ''){
-				$(this).prev('.iLabel').css('visibility','visible');
-			} else {
-				$(this).prev('.iLabel').css('visibility','hidden');
-			}
-		})
-		.blur();
-	// Common
-	var select_root = $('div.select');
-	var select_value = $('.myValue');
-	var select_a = $('div.select ul>li>a');
-	var select_input = $('div.select ul>li>input[type=radio]');
-	var select_label = $('div.select ul>li>label');
-	
-	// Radio Default Value
-	$('div.myValue').each(function(){
-		var default_value = $(this).next('.iList').find('input[checked]').next('label').text();
-		$(this).append(default_value);
+// SUBMIT disabled 
+	$('input[required]').change(function(){
+		var invalid = $('input[required]').is('[value=""], [value=" "], [value="  "], [value="   "]');
+		var $submit = $('[type="submit"]');
+		if(!invalid){
+			$submit.removeClass('x_disabled');
+		} else {
+			$submit.addClass('x_disabled');
+		}
 	});
-	
-	// Line
-	select_value.bind('focusin',function(){$(this).addClass('outLine');});
-	select_value.bind('focusout',function(){$(this).removeClass('outLine');});
-	select_input.bind('focusin',function(){$(this).parents('div.select').children('div.myValue').addClass('outLine');});
-	select_input.bind('focusout',function(){$(this).parents('div.select').children('div.myValue').removeClass('outLine');});
-	
-	// Show
-	function show_option(){
-		$(this).parents('div.select:first').toggleClass('open');
-	}
-	
-	// Hover
-	function i_hover(){
-		$(this).parents('ul:first').children('li').removeClass('hover');
-		$(this).parents('li:first').toggleClass('hover');
-	}
-	
-	// Hide
-	function hide_option(){
-		var t = $(this);
-		setTimeout(function(){
-			t.parents('div.select:first').removeClass('open');
-		}, 1);
-	}
-	
-	// Set Input
-	function set_label(){
-		var v = $(this).next('label').text();
-		$(this).closest('.select').find('>.myValue').text(v).addClass('selected');
-	}
-	
-	// Set Anchor
-	function set_anchor(){
-		var v = $(this).text();
-		$(this).closest('.select').find('>.myValue').text(v).addClass('selected');
-	}
-
-	// Anchor Focus Out
-	$(window).mousedown(function(evt){
-		if($(evt.target).closest('.select').length) return;
-		$('.aList,.iList').parent('.select').removeClass('open');
-	});
-			
-	select_value.click(show_option);
-	select_root.removeClass('open');
-	select_a.click(set_anchor).click(hide_option).focus(i_hover).hover(i_hover);
-	select_input.change(set_label).focus(set_label);
-	select_label.hover(i_hover).click(hide_option);
 });
