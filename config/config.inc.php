69809301b27aacc550527efa1bb3af4c4b5d7023
flyskyko <sinsy200@gmail.com>
2012-10-15 08:59:38 +0000

    rollback mistake in r11701
    git-svn-id: http://xe-core.googlecode.com/svn/branches/maserati@11707 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- config/config.inc.php
+++ config/config.inc.php
@@ -189,7 +189,7 @@
 		 * Only particular servers may have a problem in IE browser when sending a compression
 		 * </pre>
 		 */
-		define('__OB_GZHANDLER_ENABLE__', 0);
+		define('__OB_GZHANDLER_ENABLE__', 1);
 	}
 
 	if(!defined('__ENABLE_PHPUNIT_TEST__'))
