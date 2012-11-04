c60b34725a3c5c7026816dff89663f035821a2ab
chschy <chschy@gmail.com>
2012-11-04 06:50:16 +0000

    foggyLayer added
    git-svn-id: http://xe-core.googlecode.com/svn/branches/maserati@12088 201d5d3c-b55e-5fd7-737f-ddc643e51545

---
+++ common/js/foggyLayer.js
@@ -0,0 +1,29 @@
+jQuery(function($){
+	var $foggyLayer = $("<div>");
+	$foggyLayer.css({
+		position: 'absolute',
+		top:0,
+		left:0,
+		backgroundColor:'#000',
+		opacity: 0,
+		display:'none',
+		zIndex:100
+	});
+	$($.find("body")).append($foggyLayer);
+	
+	$(window).resize(function(){
+		$foggyLayer.css({
+			width: 0,
+			height: 0
+		});
+	
+		setTimeout(function(){
+			$foggyLayer.css({
+				width: $(document).width(),
+				height: $(document).height()
+			});
+		}, 0);
+	});
+	
+	$foggyLayer.show();
+}
