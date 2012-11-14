f1f7d80540eb353a5b0f98971ecb074c00850b97
ovclas <ovclas@gmail.com>
2012-11-14 06:46:49 +0000

    issue 2662 queryparts, condition coding convention
    git-svn-id: http://xe-core.googlecode.com/svn/branches/maserati@12214 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- classes/db/queryparts/condition/ConditionSubquery.class.php
+++ classes/db/queryparts/condition/ConditionSubquery.class.php
@@ -1,23 +1,24 @@
 <?php
+/**
+ * @author NHN (developers@xpressengine.com)
+ * @package /classes/db/queryparts/condition
+ * @version 0.1
+ */
+class ConditionSubquery extends Condition
+{
 	/**
-	 * @author NHN (developers@xpressengine.com)
-	 * @package /classes/db/queryparts/condition
-	 * @version 0.1
+	 * constructor
+	 * @param string $column_name
+	 * @param mixed $argument
+	 * @param string $operation
+	 * @param string $pipe
+	 * @return void
 	 */
-	class ConditionSubquery extends Condition {
-
-		/**
-		 * constructor
-		 * @param string $column_name
-		 * @param mixed $argument
-		 * @param string $operation
-		 * @param string $pipe
-		 * @return void
-		 */
-            function ConditionSubquery($column_name, $argument, $operation, $pipe = ""){
-                parent::Condition($column_name, $argument, $operation, $pipe);
-                $this->_value = $this->argument->toString();
-            }
+	function ConditionSubquery($column_name, $argument, $operation, $pipe = "")
+	{
+		parent::Condition($column_name, $argument, $operation, $pipe);
+		$this->_value = $this->argument->toString();
 	}
-
-?>
+}
+/* End of file ConditionSubquery.class.php */
+/* Location: ./classes/db/queryparts/condition/ConditionSubquery.class.php */
--- classes/db/queryparts/condition/ConditionWithoutArgument.class.php
+++ classes/db/queryparts/condition/ConditionWithoutArgument.class.php
@@ -1,29 +1,32 @@
 <?php
+/**
+ * @author NHN (developers@xpressengine.com)
+ * @package /classes/db/queryparts/condition
+ * @version 0.1
+ */
+class ConditionWithoutArgument extends Condition
+{
 	/**
-	 * @author NHN (developers@xpressengine.com)
-	 * @package /classes/db/queryparts/condition
-	 * @version 0.1
+	 * constructor
+	 * @param string $column_name
+	 * @param mixed $argument
+	 * @param string $operation
+	 * @param string $pipe
+	 * @return void
 	 */
-	class ConditionWithoutArgument extends Condition {
-		/**
-		 * constructor
-		 * @param string $column_name
-		 * @param mixed $argument
-		 * @param string $operation
-		 * @param string $pipe
-		 * @return void
-		 */
-		function ConditionWithoutArgument($column_name, $argument, $operation, $pipe = ""){
-                    parent::Condition($column_name, $argument, $operation, $pipe);
-					$tmpArray = array('in'=>1, 'notin'=>1, 'not_in'=>1);
-                    if(isset($tmpArray[$operation])){
-                        if(is_array($argument)) $argument = implode($argument, ',');
-                        $this->_value = '('. $argument .')';
-                    }
-                    else
-                        $this->_value = $argument;
-
+	function ConditionWithoutArgument($column_name, $argument, $operation, $pipe = "")
+	{
+		parent::Condition($column_name, $argument, $operation, $pipe);
+		$tmpArray = array('in'=>1, 'notin'=>1, 'not_in'=>1);
+		if(isset($tmpArray[$operation]))
+		{
+			if(is_array($argument)) $argument = implode($argument, ',');
+			$this->_value = '('. $argument .')';
 		}
+		else
+			$this->_value = $argument;
 	}
+}
 
-?>
+/* End of file ConditionWithoutArgument.class.php */
+/* Location: ./classes/db/queryparts/condition/ConditionWithoutArgument.class.php */
