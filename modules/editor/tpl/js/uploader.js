96c66ef733a242ac6075f659e54436179e7baac5
chschy <chschy@gmail.com>
2012-11-22 08:10:32 +0000

    issue 2704, Script for error in editor module is wrong
    git-svn-id: http://xe-core.googlecode.com/svn/branches/luminous@12285 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/editor/tpl/js/uploader.js
+++ modules/editor/tpl/js/uploader.js
@@ -142,7 +142,7 @@ function _true(){ return true };
 
 defaultHandlers = {
 	onFileQueued : _true,
-	onFileQueueError : function(flie, errorCode, message) {
+	onFileQueueError : function(file, errorCode, message) {
 		try {
 			switch(errorCode) {
 				case SWFUpload.QUEUE_ERROR.QUEUE_LIMIT_EXCEEDED :
