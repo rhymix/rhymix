66c9ae5bb7ea61508bd0ba4823d4610ba93cb469
misol <misol.kr@gmail.com>
2012-12-25 19:55:15 +0000

    On the mobile page, autolink addon need jQuery and XE script lib, and this commit fix the file to load them.
    git-svn-id: http://xe-core.googlecode.com/svn/branches/maserati@12458 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- addons/autolink/autolink.addon.php
+++ addons/autolink/autolink.addon.php
@@ -8,6 +8,10 @@ if(!defined('__XE__')) exit();
  */
 if($called_position == 'after_module_proc' && Context::getResponseMethod()!="XMLRPC")
 {
+	if(Mobile::isFromMobilePhone()) {
+		Context::addJsFile('./common/js/jquery.min.js',false, '',-1000000);
+		Context::addJsFile('./common/js/xe.min.js',false, '',-1000000);
+	}
 	Context::loadFile(array('./addons/autolink/autolink.js', 'body', '', null), true);
 }
 /* End of file autolink.addon.php */
