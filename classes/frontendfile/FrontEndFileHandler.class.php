eac2272e050800a64724930e65531aaacc7acb4e
chschy <chschy@gmail.com>
2013-01-23 05:38:57 +0000

    loadFile() error
    git-svn-id: http://xe-core.googlecode.com/svn/branches/luminous@12581 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- classes/frontendfile/FrontEndFileHandler.class.php
+++ classes/frontendfile/FrontEndFileHandler.class.php
@@ -102,7 +102,10 @@
 			$file->filePath = $this->_getAbsFileUrl($pathInfo['dirname']);
 			$file->fileRealPath = FileHandler::getRealPath($pathInfo['dirname']);
 			$file->fileExtension = strtolower($pathInfo['extension']);
-			$file->fileNameNoExt = preg_replace('/\.min$/', '', $pathInfo['filename']);
+
+			// Remove .min
+			$file->fileNameNoExt = preg_replace("/\.{$file->fileExtension}$/", '', $file->fileName);
+			$file->fileNameNoExt = preg_replace("/\.min$/", '', $file->fileNameNoExt);
 			$file->keyName = implode('.', array($file->fileNameNoExt, $file->fileExtension));
 
 			if(strpos($file->filePath, '://') === FALSE)
