228c8d7f71d1afa5dabe32c4eb903d0d9cdb1437
ChanMyeong <dece24@gmail.com>
2013-01-31 09:34:10 +0000

    Issue 2874 fixed. Site edit page input style cosmetic issue.
    git-svn-id: http://xe-core.googlecode.com/svn/branches/maserati@12650 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/admin/tpl/js/admin.js
+++ modules/admin/tpl/js/admin.js
@@ -2371,11 +2371,9 @@ jQuery(function($){
 
 jQuery(function($){
 	_alert = alert;
-	
 	alert = function(){
 		return $.xeMsgBox.alert.apply($.xeMsgBox, arguments);
 	}
-	
 	setTimeout(function(){$('div.message.info').fadeOut(1000);}, 2500);
 });
 
