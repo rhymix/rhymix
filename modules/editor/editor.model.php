b1c312cae36113e4ced2ee54095eee93cfce8adf
ovclas <ovclas@gmail.com>
2013-02-05 05:26:29 +0000

    change singleton instance
    git-svn-id: http://xe-core.googlecode.com/svn/branches/maserati@12700 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/editor/editor.model.php
+++ modules/editor/editor.model.php
@@ -353,7 +353,7 @@ class editorModel extends editor
 		// load editor skin lang
 		Context::loadLang($tpl_path.'lang');
 		// Return the compiled result from tpl file
-		$oTemplate = new TemplateHandler();
+		$oTemplate = TemplateHandler::getInstance();
 		return $oTemplate->compile($tpl_path, $tpl_file);
 	}
 
