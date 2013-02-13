3ba585c33f172346ee75d090c04b3af7e9d16d25
devjin <namnyams@gmail.com>
2013-02-13 07:14:06 +0000

    issue 2673
    git-svn-id: http://xe-core.googlecode.com/svn/branches/maserati@12742 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- modules/module/module.class.php
+++ modules/module/module.class.php
@@ -395,6 +395,24 @@ class module extends ModuleObject
 		}
 
 		// XE 1.7
+		if(!$oDB->isColumnExists("modules", "is_mskin_fix"))
+		{
+			$oDB->addColumn('modules', 'is_mskin_fix', 'char', 1, 'N');
+			$output = executeQueryArray('module.getAllMobileSkinSetModule');
+			if($output->toBool() && $output->data)
+			{
+				$module_srls = array();
+				foreach($output->data as $val)
+				{
+					$module_srls[] = $val->module_srl;
+				}
+				unset($args);
+				$args->module_srls = implode(',', $module_srls);
+				$args->is_mskin_fix = 'Y';
+				$output = executeQuery('module.updateMobileSkinFixModules', $args);
+			}
+		}
+
 		$output = executeQueryArray('module.getNotLinkedModuleGroupSiteSrl');
 		if($output->toBool() && $output->data && count($output->data) > 0)
 		{
@@ -427,23 +445,6 @@ class module extends ModuleObject
 
 		}
 
-		if(!$oDB->isColumnExists("modules", "is_mskin_fix"))
-		{
-			$oDB->addColumn('modules', 'is_mskin_fix', 'char', 1, 'N');
-			$output = executeQueryArray('module.getAllMobileSkinSetModule');
-			if($output->toBool() && $output->data)
-			{
-				$module_srls = array();
-				foreach($output->data as $val)
-				{
-					$module_srls[] = $val->module_srl;
-				}
-				unset($args);
-				$args->module_srls = implode(',', $module_srls);
-				$args->is_mskin_fix = 'Y';
-				$output = executeQuery('module.updateMobileSkinFixModules', $args);
-			}
-		}
 		return new Object(0, 'success_updated');
 	}
 
