bbc50b994fcd56896fe7d480ce1b79b131575d9f
bnu <bnufactory@gmail.com>
2013-12-03 18:44:23 +0900

    NOISSUE 설치 시 문제 수정.

--- modules/module/module.model.php
+++ modules/module/module.model.php
@@ -93,18 +93,17 @@ class moduleModel extends module
 		$mid = Context::get('mid');
 
 		// Set up
-		// test.xe.com
 		$domain = '';
 		if($default_url && $default_url_parse['host'] != $request_url_parse['host'])
 		{
 			$url_info = parse_url($request_url);
 			$hostname = $url_info['host'];
 			$path = $url_info['path'];
-			if(substr_compare($path, '/', -1) === 0) $path = substr($path, 0, -1);
+			if(strlen($path) >= 1 && substr_compare($path, '/', -1) === 0) $path = substr($path, 0, -1);
 
 			$domain = sprintf('%s%s%s', $hostname, $url_info['port']&&$url_info['port']!=80?':'.$url_info['port']:'',$path);
 		}
-		// xe.com/blog
+
 		if($domain === '')
 		{
 			if(!$vid) $vid = $mid;
