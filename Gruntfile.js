ca400dec278cb5f9135871b89a57b07aa5c3d53b
bnu <bnufactory@gmail.com>
2013-12-05 10:47:59 +0900

    NOISSUE. build 시 board 모듈을 포함하도록 수정.

--- Gruntfile.js
+++ Gruntfile.js
@@ -213,7 +213,7 @@ module.exports = function(grunt) {
 
 	grunt.registerTask('build', '', function(A, B) {
 		var _only_export = false;
-		var tasks = ['krzip', 'syndication'];
+		var tasks = ['board', 'krzip', 'syndication'];
 
 		if(!A) {
 			grunt.fail.warn('Undefined build target.');
@@ -330,10 +330,19 @@ module.exports = function(grunt) {
 				cmd: "tar",
 				args: ['xf', 'build/temp.full.tar', '-C', 'build/xe']
 			}, function (error, result, code) {
+				// board
+				grunt.util.spawn({
+					cmd: "git",
+					args: ['clone', '-b', 'master', 'git@github.com:xpressengine/xe-module-board.git', 'build/xe/modules/board']
+				}, function (error, result, code) {
+					grunt.file.delete('build/xe/modules/board/.git');
+					taskDone();
+				});
+
 				// krzip
 				grunt.util.spawn({
 					cmd: "git",
-					args: ['clone', 'git@github.com:xpressengine/xe-module-krzip.git', 'build/xe/modules/krzip']
+					args: ['clone', '-b', 'master', 'git@github.com:xpressengine/xe-module-krzip.git', 'build/xe/modules/krzip']
 				}, function (error, result, code) {
 					grunt.file.delete('build/xe/modules/krzip/.git');
 					taskDone();
@@ -342,7 +351,7 @@ module.exports = function(grunt) {
 				// syndication
 				grunt.util.spawn({
 					cmd: "git",
-					args: ['clone', 'git@github.com:xpressengine/xe-module-syndication.git', 'build/xe/modules/syndication']
+					args: ['clone', '-b', 'master', 'git@github.com:xpressengine/xe-module-syndication.git', 'build/xe/modules/syndication']
 				}, function (error, result, code) {
 					grunt.file.delete('build/xe/modules/syndication/.git');
 					taskDone();
