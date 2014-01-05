8191a184a8cb07c50f8019311068ae65c2a51a1d
bnu <bnufactory@gmail.com>
2014-01-06 01:10:42 +0900

    revert r12601

--- common/js/xe.js
+++ common/js/xe.js
@@ -420,11 +420,6 @@ if(jQuery) jQuery.noConflict();
 /* jQuery(document).ready() */
 jQuery(function($) {
 
-	// Anchor: focus move to target
-	$('a[href^="#"]').click(function(){
-		$($(this).attr('href')).attr('tabindex','0').focus();
-	});
-
     /* select - option의 disabled=disabled 속성을 IE에서도 체크하기 위한 함수 */
     if($.browser.msie) {
         $('select').each(function(i, sels) {
