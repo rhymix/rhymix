191d77480fd5f21e335bc0fa7d6fcafa2081d1e7
bnu <bnufactory@gmail.com>
2014-01-15 16:56:58 +0900

    #312 oEmbed API를 활용해 글 본문의 URL을 embed 코드로 변환하는 애드온 추가. - jQuery oEmbed API Wrapper : https://github.com/starfishmod/jquery-oembed-all

---
+++ addons/oembed/oembed.addon.php
@@ -0,0 +1,15 @@
+<?php
+/* Copyright (C) NAVER <http://www.navercorp.com> */
+
+if(!defined('__XE__'))
+{
+	exit();
+}
+
+if($called_position == 'after_module_proc' && Context::getResponseMethod() == 'HTML')
+{
+	Context::loadFile(array('./addons/oembed/jquery.oembed.js', 'body', '', null), true);
+	Context::loadFile(array('./addons/oembed/oembed.js', 'body', '', null), true);
+}
+
+/* End of file */
