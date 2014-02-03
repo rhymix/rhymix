c88d99aaef13f84801cbe0ae52d2ba02062eb37e
akasima <akasima@nate.com>
2014-02-03 14:16:02 +0900

    db_info->admin_ip_list 없을 때 오류 수정

--- modules/admin/admin.admin.view.php
+++ modules/admin/admin.admin.view.php
@@ -426,6 +426,8 @@ class adminAdminView extends admin
 		$whitelist = implode("\r\n", $db_info->sitelock_whitelist);
 		Context::set('sitelock_whitelist', $whitelist);
 
+		if(gettype($db_info->admin_ip_list)!="array")
+			$db_info->admin_ip_list = array();
 		if(!in_array('127.0.0.1', $db_info->admin_ip_list)) $db_info->admin_ip_list[] = '127.0.0.1';
 		if(!in_array($_SERVER['REMOTE_ADDR'], $db_info->admin_ip_list)) $db_info->admin_ip_list[] = $_SERVER['REMOTE_ADDR'];
 
