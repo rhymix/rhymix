7e2e93262c5bde1c6b68f1d5f09ffd5f8e77ed72
khongchi <sungbum00@gmail.com>
2014-02-04 14:55:20 +0900

    #135 trackback 모듈이 없을 경우 documentitem allowTrackback은 무조건 false를 리턴

--- modules/document/document.item.php
+++ modules/document/document.item.php
@@ -194,22 +194,33 @@ class documentItem extends Object
 		static $allow_trackback_status = null;
 		if(is_null($allow_trackback_status))
 		{
-			// If the trackback module is configured to be disabled, do not allow. Otherwise, check the setting of each module.
-			$oModuleModel = getModel('module');
-			$trackback_config = $oModuleModel->getModuleConfig('trackback');
-			if(!$trackback_config)
+			
+			// Check the tarckback module exist
+			if(!getClass('trackback'))
 			{
-				$trackback_config = new stdClass();
+				$allow_trackback_status = false;
 			}
-			if(!isset($trackback_config->enable_trackback)) $trackback_config->enable_trackback = 'Y';
-			if($trackback_config->enable_trackback != 'Y') $allow_trackback_status = false;
 			else
 			{
-				$module_srl = $this->get('module_srl');
-				// Check settings of each module
-				$module_config = $oModuleModel->getModulePartConfig('trackback', $module_srl);
-				if($module_config->enable_trackback == 'N') $allow_trackback_status = false;
-				else if($this->get('allow_trackback')=='Y' || !$this->isExists()) $allow_trackback_status = true;
+				// If the trackback module is configured to be disabled, do not allow. Otherwise, check the setting of each module.
+				$oModuleModel = getModel('module');
+				$trackback_config = $oModuleModel->getModuleConfig('trackback');
+				
+				if(!$trackback_config)
+				{
+					$trackback_config = new stdClass();
+				}
+				
+				if(!isset($trackback_config->enable_trackback)) $trackback_config->enable_trackback = 'Y';
+				if($trackback_config->enable_trackback != 'Y') $allow_trackback_status = false;
+				else
+				{
+					$module_srl = $this->get('module_srl');
+					// Check settings of each module
+					$module_config = $oModuleModel->getModulePartConfig('trackback', $module_srl);
+					if($module_config->enable_trackback == 'N') $allow_trackback_status = false;
+					else if($this->get('allow_trackback')=='Y' || !$this->isExists()) $allow_trackback_status = true;
+				}
 			}
 		}
 		return $allow_trackback_status;
