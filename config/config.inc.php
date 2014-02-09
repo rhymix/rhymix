1a361bb958f1078bcbef3ba542876a56149fcdc2
bnu <bnufactory@gmail.com>
2014-02-09 15:25:17 +0900

    warning 메시지를 표시하지 않도록 변경

--- config/config.inc.php
+++ config/config.inc.php
@@ -9,11 +9,11 @@
  */
 if(version_compare(PHP_VERSION, '5.4.0', '<'))
 {
-	@error_reporting(E_ALL ^ E_NOTICE ^ E_DEPRECATED);
+	@error_reporting(E_ALL ^ E_NOTICE ^ E_DEPRECATED ^ E_WARNING);
 }
 else
 {
-	@error_reporting(E_ALL ^ E_NOTICE ^ E_DEPRECATED ^ E_STRICT);
+	@error_reporting(E_ALL ^ E_NOTICE ^ E_DEPRECATED ^ E_WARNING ^ E_STRICT);
 }
 
 if(!defined('__XE__'))
