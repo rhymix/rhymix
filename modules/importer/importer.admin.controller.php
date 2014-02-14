6fb4b7a61022b88038fcf93a32eebfd74b92e49a
Jonghwan Hyeon <hyeon0135@gmail.com>
2014-02-14 18:34:08 +0900

    $file_obj가 정상적으로 초기화 되도록 수정

--- modules/importer/importer.admin.controller.php
+++ modules/importer/importer.admin.controller.php
@@ -1010,13 +1010,13 @@ class importerAdminController extends importer
 
 		while(!feof($fp))
 		{
-			$file_obj = new stdClass;
 			$str = trim(fgets($fp, 1024));
 			// If it ends with </attaches>, break
 			if(trim($str) == '</attaches>') break;
 			// If it starts with <attach>, collect attachments
 			if(trim($str) == '<attach>')
 			{
+				$file_obj = new stdClass;
 				$file_obj->file_srl = getNextSequence();
 				$file_obj->upload_target_srl = $upload_target_srl;
 				$file_obj->module_srl = $module_srl;
