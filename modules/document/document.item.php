2013e4af35ef044084b616f9e1cdb6f55672ca9c
bnu <bnufactory@gmail.com>
2014-03-19 14:57:58 +0900

    fix #551 #470 페이지 모듈에서 다국어 문서 적용이 안 되는 문제 해결

--- modules/document/document.item.php
+++ modules/document/document.item.php
@@ -139,13 +139,12 @@ class documentItem extends Object
 		}
 
 		$oDocumentModel = getModel('document');
-		$GLOBALS['XE_DOCUMENT_LIST'][$this->document_srl] = $this;
 		if($load_extra_vars)
 		{
-			$oDocumentModel->getDocumentExtraVarsFromDB($this->document_srl);
-			$this->add('title', $this->get('title'));
-			$this->add('content', $this->get('content'));
+			$GLOBALS['XE_DOCUMENT_LIST'][$attribute->document_srl] = $this;
+			$oDocumentModel->setToAllDocumentExtraVars();
 		}
+		$GLOBALS['XE_DOCUMENT_LIST'][$this->document_srl] = $this;
 	}
 
 	function isExists()
