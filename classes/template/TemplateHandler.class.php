b9a515c1013b9ba8f0704ffd8cf47cd7a4fe8a96
YJSoft <yjsoft@yjsoft.pe.kr>
2014-04-26 18:16:08 +0900

    #588 템플릿 파일 오류시 오류 정보와 해당 오류가 발생한 템플릿 파일을 출력
    eval에서 발생하는 오류 대신 실제 오류가 발생한 템플릿 파일을 출력함으로 디버깅시 참고 가능하게함
    
    개선 코드(by @lansi951 ) 반영

--- classes/template/TemplateHandler.class.php
+++ classes/template/TemplateHandler.class.php
@@ -370,7 +370,12 @@ class TemplateHandler
 		else
 		{
 			$eval_str = "?>" . $buff;
-			eval($eval_str);
+			@eval($eval_str);
+			$error_info = error_get_last();
+			if ($error_info['type'] == 4)
+			{
+			    echo "<p>Error Pharsing Template - {$error_info['message']} in template file {$this->file}</p>";
+			}
 		}
 
 		return ob_get_clean();
