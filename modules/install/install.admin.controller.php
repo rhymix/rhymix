7b53f2443578a2c3defe2087bdd0d9ed4596b37b
upgle <admin@upgle.com>
2014-05-14 16:18:53 +0545

    파비콘이 가상 사이트에 대응하도록 개선
    파비콘 저장시
    files/attach/xeicon/{가상사이트srl}/favicon.ico
    와 같이 저장하도록 개선
    
    이전 버전과 호환을 위해 기본 사이트의 경우 files/attach/xeicon/favicon.ico 와 같이 기존 주소를 그대로
    이용하도록 처리.

--- modules/install/install.admin.controller.php
+++ modules/install/install.admin.controller.php
@@ -327,8 +327,12 @@ class installAdminController extends install
 
 	private function saveIconTmp($icon, $iconname)
 	{
+
+		$site_info = Context::get('site_module_info');
+		if($site_info->site_srl) $virtual_site = $site_info->site_srl . '/';
+
 		$target_file = $icon['tmp_name'];
-		$relative_filename = 'files/attach/xeicon/tmp/'.$iconname;
+		$relative_filename = 'files/attach/xeicon/'.$virtual_site.'tmp/'.$iconname;
 		$target_filename = _XE_PATH_.$relative_filename;
 
 		list($width, $height, $type_no, $attrs) = @getimagesize($target_file);
@@ -367,7 +371,11 @@ class installAdminController extends install
 	}
 
 	private function updateIcon($iconname, $deleteIcon = false) {
-		$image_filepath = _XE_PATH_.'files/attach/xeicon/';
+
+		$site_info = Context::get('site_module_info');
+		if($site_info->site_srl) $virtual_site = $site_info->site_srl . '/';
+
+		$image_filepath = _XE_PATH_.'files/attach/xeicon/' . $virtual_site;
 
 		if($deleteIcon) {
 			FileHandler::removeFile($image_filepath.$iconname);
