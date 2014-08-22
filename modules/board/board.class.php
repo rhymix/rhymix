875149975a214302ae84780637c04c11fa1e2e33
AJKJ <thename@requery.me>
2014-08-22 15:46:01 +0900

    board module 검색 순서 변경

--- modules/board/board.class.php
+++ modules/board/board.class.php
@@ -9,7 +9,7 @@
 
 class board extends ModuleObject
 {
-	var $search_option = array('title','content','title_content','comment','user_name','nick_name','user_id','tag'); ///< 검색 옵션
+	var $search_option = array('title_content','title','content','comment','user_name','nick_name','user_id','tag'); ///< 검색 옵션
 
 	var $order_target = array('list_order', 'update_order', 'regdate', 'voted_count', 'blamed_count', 'readed_count', 'comment_count', 'title'); // 정렬 옵션
 
