21bd343048698b1baa91c21e75715750a3734302
akasima <akasima@nate.com>
2014-08-26 12:34:15 +0900

    #930 modify Router class call position

--- classes/context/Context.class.php
+++ classes/context/Context.class.php
@@ -242,9 +242,18 @@ class Context
 			}
 		}
 
+		// check if using rewrite module
+		$this->allow_rewrite = ($this->db_info->use_rewrite == 'Y' ? TRUE : FALSE);
+
 		// If XE is installed, get virtual site information
 		if(self::isInstalled())
 		{
+			// If using rewrite module, initializes router
+			if($this->allow_rewrite)
+			{
+				Router::proc();
+			}
+
 			$oModuleModel = getModel('module');
 			$site_module_info = $oModuleModel->getDefaultMid();
 
@@ -364,15 +373,6 @@ class Context
 		// load common language file
 		$this->lang = &$GLOBALS['lang'];
 		$this->loadLang(_XE_PATH_ . 'common/lang/');
-
-		// check if using rewrite module
-		$this->allow_rewrite = ($this->db_info->use_rewrite == 'Y' ? TRUE : FALSE);
-
-		// If using rewrite module, initializes router
-		if($this->allow_rewrite)
-		{
-			Router::proc();
-		}
 
 		// set locations for javascript use
 		if($_SERVER['REQUEST_METHOD'] == 'GET')
