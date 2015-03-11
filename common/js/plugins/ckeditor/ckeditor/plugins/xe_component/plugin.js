89fa0e9ecf96c0f3c720b9d67ab4885caa11b4c8
bnu <bnufactory@gmail.com>
2015-03-11 15:34:07 +0900

    #1087 XE 확장 컴포넌트를 dropdown list에서 개별 버튼으로 출력하도록 변경

--- common/js/plugins/ckeditor/ckeditor/plugins/xe_component/plugin.js
+++ common/js/plugins/ckeditor/ckeditor/plugins/xe_component/plugin.js
@@ -1,52 +1,30 @@
-/*
- * @author Arnia <dev@karybu.org>
- * @modifier XpressEngine <developers@xpressengine.com>
- */
 CKEDITOR.plugins.add('xe_component', {
-	requires: 'menubutton',
-	icons: 'xe_component',
+	requires: 'button',
 
 	init: function(editor) {
-		var config=editor.config;
-		var items = {};
+		var config = editor.config;
 
-		editor.addMenuGroup('xe_component');
-
-		var openSelectComponent = function() {
-			window.editorPrevSrl = config.xe_editor_sequence;
-			openComponent(this.name, config.xe_editor_sequence);
-		};
+		editor.ui.addToolbarGroup('xecomponent');
 
 		for(var key in config.xe_component_arrays) {
 			var component_name = key;
 			var component_title = config.xe_component_arrays[key];
 
-			items[component_name] = {
+			(function(component_name) {
+				editor.addCommand('openXeComponent_' + component_name , {
+					exec: function() {
+						window.openComponent(component_name, config.xe_editor_sequence);
+					}
+				});
+			})(component_name);
+
+			editor.ui.addButton( component_name, {
 				label: component_title,
-				group: 'xe_component',
 				icon : request_uri + 'modules/editor/components/' + component_name + '/component_icon.gif',
-				onClick: openSelectComponent
-			};
+				command: 'openXeComponent_' + component_name,
+				toolbar: 'xecomponent'
+			} );
 		}
-
-		editor.addMenuItems( items );
-
-		editor.ui.add('xe_component', CKEDITOR.UI_MENUBUTTON, {
-			label: '추가 기능', // @TODO lang
-			modes: {
-				wysiwyg: 1
-			},
-			onMenu: function() {
-				var active = {};
-
-				for(var p in items)
-				{
-					active[p] = CKEDITOR.TRISTATE_OFF;
-				}
-
-				return active;
-			}
-		});
 
 		editor.on( 'doubleclick', function( evt ) {
 			var element = evt.data.element;
@@ -74,4 +52,3 @@ CKEDITOR.plugins.add('xe_component', {
 		});
 	}
 });
-
