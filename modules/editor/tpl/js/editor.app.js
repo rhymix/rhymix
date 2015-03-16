aac394b852ed3e8569d9013c68fd4c7062548b15
bnu <bnufactory@gmail.com>
2015-03-16 19:17:29 +0900

    #1087 툴바 조정

--- modules/editor/tpl/js/editor.app.js
+++ modules/editor/tpl/js/editor.app.js
@@ -13,6 +13,7 @@
 			'/',
 			{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
 			{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
+			'/',
 			{ name: 'styles' },
 			{ name: 'colors' },
 			{ name: 'xecomponent' },
