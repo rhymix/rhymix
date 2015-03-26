86b3caa28f04d05887e8ab1f014c12e99432ceaf
bnu <bnufactory@gmail.com>
2015-03-26 13:42:20 +0900

    fix #1347 관리페이지의 상단에 사이트 주소를 표시하지 못하는 문제 수정

--- modules/admin/admin.admin.view.php
+++ modules/admin/admin.admin.view.php
@@ -24,6 +24,21 @@ class adminAdminView extends admin
 	 */
 	var $easyinstallCheckFile = './files/env/easyinstall_last';
 
+	function __construct()
+	{
+		$db_info = Context::getDBInfo();
+
+		if(strpos($db_info->default_url, 'xn--') !== FALSE)
+		{
+			$xe_default_url = Context::decodeIdna($db_info->default_url);
+		}
+		else
+		{
+			$xe_default_url = $db_info->default_url;
+		}
+		Context::set('xe_default_url', $xe_default_url);
+	}
+
 	/**
 	 * Initilization
 	 * @return void
@@ -67,16 +82,6 @@ class adminAdminView extends admin
 		{
 			Context::set('https_port', $db_info->https_port);
 		}
-
-		if(strpos($db_info->default_url, 'xn--') !== FALSE)
-		{
-			$xe_default_url = Context::decodeIdna($db_info->default_url);
-		}
-		else 
-		{
-			$xe_default_url = $db_info->default_url;
-		}
-		Context::set('xe_default_url', $xe_default_url);
 
 		$this->showSendEnv();
 		$this->checkEasyinstall();
@@ -156,7 +161,7 @@ class adminAdminView extends admin
 		$moduleActionInfo = $oModuleModel->getModuleActionXml($module);
 		$currentAct = Context::get('act');
 		$subMenuTitle = '';
-		
+
 		foreach((array) $moduleActionInfo->menu as $key => $value)
 		{
 			if(isset($value->acts) && is_array($value->acts) && in_array($currentAct, $value->acts))
@@ -437,7 +442,7 @@ class adminAdminView extends admin
 		Context::set('use_sitelock', $db_info->use_sitelock);
 		Context::set('sitelock_title', $db_info->sitelock_title);
 		Context::set('sitelock_message', htmlspecialchars($db_info->sitelock_message, ENT_COMPAT | ENT_HTML401, 'UTF-8', false));
-		
+
 		$whitelist = implode("\r\n", $db_info->sitelock_whitelist);
 		Context::set('sitelock_whitelist', $whitelist);
 
@@ -582,7 +587,7 @@ class adminAdminView extends admin
 				$str = urldecode($arr[1]);
 				$arrModuleName = explode("|", $str);
 				$oModuleModel = getModel("module");
-				$mInfo = array();	
+				$mInfo = array();
 				foreach($arrModuleName as $moduleName) {
 					$moduleInfo = $oModuleModel->getModuleInfoXml($moduleName);
 					$mInfo[] = "{$moduleName}({$moduleInfo->version})";
@@ -592,7 +597,7 @@ class adminAdminView extends admin
 				$str = urldecode($arr[1]);
 				$arrAddonName = explode("|", $str);
 				$oAddonModel = getAdminModel("addon");
-				$mInfo = array();	
+				$mInfo = array();
 				foreach($arrAddonName as $addonName) {
 					$addonInfo = $oAddonModel->getAddonInfoXml($addonName);
 					$mInfo[] = "{$addonName}({$addonInfo->version})";
@@ -602,7 +607,7 @@ class adminAdminView extends admin
 				$str = urldecode($arr[1]);
 				$arrWidgetName = explode("|", $str);
 				$oWidgetModel = getModel("widget");
-				$mInfo = array();	
+				$mInfo = array();
 				foreach($arrWidgetName as $widgetName) {
 					$widgetInfo = $oWidgetModel->getWidgetInfo($widgetName);
 					$mInfo[] = "{$widgetName}({$widgetInfo->version})";
@@ -612,7 +617,7 @@ class adminAdminView extends admin
 				$str = urldecode($arr[1]);
 				$arrWidgetstyleName = explode("|", $str);
 				$oWidgetModel = getModel("widget");
-				$mInfo = array();	
+				$mInfo = array();
 				foreach($arrWidgetstyleName as $widgetstyleName) {
 					$widgetstyleInfo = $oWidgetModel->getWidgetStyleInfo($widgetstyleName);
 					$mInfo[] = "{$widgetstyleName}({$widgetstyleInfo->version})";
@@ -623,7 +628,7 @@ class adminAdminView extends admin
 				$str = urldecode($arr[1]);
 				$arrLayoutName = explode("|", $str);
 				$oLayoutModel = getModel("layout");
-				$mInfo = array();	
+				$mInfo = array();
 				foreach($arrLayoutName as $layoutName) {
 					$layoutInfo = $oLayoutModel->getLayoutInfo($layoutName);
 					$mInfo[] = "{$layoutName}({$layoutInfo->version})";
