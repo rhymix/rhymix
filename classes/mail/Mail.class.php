1af4c9521694b1597e068f92af8adc88c2e221bc
YJSoft <yjsoft@yjsoft.pe.kr>
2015-04-08 13:44:52 +0900

    #1370 불필요한 PHP4용 코드 삭제
    XE 최소 PHP 버전이 5.3임에도 남아있는 PHP5 미만용 코드를 삭제하였습니다.

--- classes/mail/Mail.class.php
+++ classes/mail/Mail.class.php
@@ -1,14 +1,7 @@
 <?php
 /* Copyright (C) NAVER <http://www.navercorp.com> */
 
-if(version_compare(PHP_VERSION, '5.0.0', '>='))
-{
-	require_once _XE_PATH_ . "libs/phpmailer/phpmailer.php";
-}
-else
-{
-	require_once _XE_PATH_ . "libs/phpmailer/class.phpmailer.php";
-}
+require_once _XE_PATH_ . "libs/phpmailer/phpmailer.php";
 
 /**
  * Mailing class for XpressEngine
