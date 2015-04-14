c8a2b700cf02fb4e033f8b1dd8f2b9785de9de55
Xvezda <xvezda@naver.com>
2015-04-15 01:39:04 +0900

    check manual inserted document

--- modules/document/document.controller.php
+++ modules/document/document.controller.php
@@ -293,7 +293,7 @@ class documentController extends document
 			$oModuleModel = getModel('module');
 			$editor_config = $oModuleModel->getModuleConfig('editor');
 			
-			if(substr_compare($editor_config->sel_editor_colorset, 'nohtml', -6) === 0)
+			if(substr_compare($editor_config->sel_editor_colorset, 'nohtml', -6) === 0 && !$manual_inserted)
 			{
 				$obj->content = preg_replace('/\r|\n/', '', nl2br(htmlspecialchars($obj->content, ENT_COMPAT | ENT_HTML401, 'UTF-8', false)));
 			}
