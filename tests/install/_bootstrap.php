b07cd5f596652b97e82f358af017aaaad936697e
bnu <bnufactory@gmail.com>
2015-04-16 15:12:08 +0900

    설치 테스트 추가

---
+++ tests/install/_bootstrap.php
@@ -0,0 +1,5 @@
+<?php
+// Here you can initialize variables that will be available to your tests
+
+\Codeception\Util\Autoload::registerSuffix('Steps', __DIR__.DIRECTORY_SEPARATOR.'_steps');
+\Codeception\Util\Autoload::registerSuffix('Page', __DIR__.DIRECTORY_SEPARATOR.'_pages');
