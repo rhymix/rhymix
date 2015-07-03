64085a2f6373ee5b3bf78b90b23f00df0f234a96
dlehdanakf <dlehdanakf@xpressengine.com>
2015-07-03 15:19:22 +0900

    XEDITION 업데이트

--- layouts/xedition/js/layout.js
+++ layouts/xedition/js/layout.js
@@ -102,60 +102,10 @@
     })
 })(jQuery);
 
-(function($) {
-    "use strict";
-    var $window = $(window);
-    var windowHeight = $window.height();
-
-    $window.resize(function() {
-        windowHeight = $window.height()
-    });
-
-    $.fn.parallax = function(xpos, speedFactor, outerHeight) {
-        var $this = $(this);
-        var getHeight;
-        var firstTop;
-        $this.each(function() {
-            firstTop = $this.offset().top;
-        });
-
-        if (outerHeight) {
-          getHeight = function(object) {
-            return object.outerHeight(true)
-          }
-        } else {
-          getHeight = function(object) {
-            return object.height()
-          }
-        }
-        if (arguments.length < 1 || xpos === null)
-          xpos = "50%";
-        if (arguments.length < 2 || speedFactor === null)
-          speedFactor = 0.1;
-        if (arguments.length < 3 || outerHeight === null)
-          outerHeight = true;
-        function update() {
-          var pos = $window.scrollTop();
-          $this.each(function() {
-            var $element = $(this);
-            var top = $element.offset().top;
-            var height = getHeight($element);
-
-            if (top + height < pos || top > pos + windowHeight) {
-              return
-            }
-            $this.css('backgroundPosition', xpos + " " + Math.round((firstTop - pos) * speedFactor) + "px")
-          })
-        }
-        $window.bind('scroll', update).resize(update);
-        update()
-  }
-})(jQuery);
-
 (function($){
 	 $(function(){
 		$('.bg-holder').parallaxScroll({
-		  friction: 0.5
+		  friction: 0.2
 		});
     });
 })(jQuery);
