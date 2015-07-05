6857b46c58768b76a88fa66371428ff5986a792c
hjhj3686 <nettermhell@gmail.com>
2015-07-05 20:48:30 +0900

    지금은 쓰이지 않는 주소 현재 주소로 변경

--- config/config.inc.php
+++ config/config.inc.php
@@ -72,12 +72,12 @@ else
 	/**
 	 * Location site
 	 */
-	define('_XE_LOCATION_SITE_', 'http://www.xpressengine.org/');
+	define('_XE_LOCATION_SITE_', 'http://www.xpressengine.com/');
 
 	/**
 	 * Download server
 	 */
-	define('_XE_DOWNLOAD_SERVER_', 'http://en.download.xpressengine.org/');
+	define('_XE_DOWNLOAD_SERVER_', 'http://download.xpressengine.com/');
 }
 
 /*
