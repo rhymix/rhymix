34f9a6245a995a3bb6b24dd5857f6f547e3ec8af
bnu <bnufactory@gmail.com>
2015-07-06 10:53:03 +0900

    Revert "gruntjs 업데이"
    This reverts commit 2565e9e5fc07683c7c53e14a3dfc2a50855532fd.

--- Gruntfile.js
+++ Gruntfile.js
@@ -156,7 +156,6 @@ module.exports = function(grunt) {
 					'layouts/xedition/css/widget.login.min.css': ['layouts/xedition/css/widget.login.css'],
 					'layouts/xedition/css/xeicon.min.css': ['layouts/xedition/css/xeicon.css'],
 					'layouts/xedition/css/swiper.min.css': ['layouts/xedition/css/swiper.css'],
-					'layouts/xedition/css/idangerous.swiper.min.css': ['layouts/xedition/css/idangerous.swiper.css'],
 				}
 			},
 		},
