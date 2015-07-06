7ec38cae2cd5743988bc39cfb7a47d92f496e342
bnu <bnufactory@gmail.com>
2015-07-06 10:50:48 +0900

    Revert "제거된 css 파일에 대한 minify 제거"
    This reverts commit d3076aa2df6a17a1015361aca23218d257ffb6ca.

--- Gruntfile.js
+++ Gruntfile.js
@@ -154,6 +154,7 @@ module.exports = function(grunt) {
 					'layouts/xedition/css/welcome.min.css': ['layouts/xedition/css/welcome.css'],
 					'layouts/xedition/css/widget.login.min.css': ['layouts/xedition/css/widget.login.css'],
 					'layouts/xedition/css/xeicon.min.css': ['layouts/xedition/css/xeicon.css'],
+					'layouts/xedition/css/swiper.min.css': ['layouts/xedition/css/swiper.css'],
 					'layouts/xedition/css/idangerous.swiper.min.css': ['layouts/xedition/css/idangerous.swiper.css'],
 				}
 			},
