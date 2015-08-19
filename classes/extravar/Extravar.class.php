5d6e6015a305e309d7c3400d67201361b2fb6c60
Kijin Sung <kijin@kijinsung.com>
2015-08-20 08:27:39 +0900

    ExtraItem 클래스에도 PHP 5 방식의 생성자 (__construct) 사용

--- classes/extravar/Extravar.class.php
+++ classes/extravar/Extravar.class.php
@@ -157,7 +157,7 @@ class ExtraItem
 	 * @param string $eid Unique id of extra variable in module
 	 * @return void
 	 */
-	function ExtraItem($module_srl, $idx, $name, $type = 'text', $default = null, $desc = '', $is_required = 'N', $search = 'N', $value = null, $eid = '')
+	function __construct($module_srl, $idx, $name, $type = 'text', $default = null, $desc = '', $is_required = 'N', $search = 'N', $value = null, $eid = '')
 	{
 		if(!$idx)
 		{
