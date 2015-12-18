dc4c22d64a5de39b677ae704e6e81177fdca91b0
conory <conorycom@gmail.com>
2015-12-18 18:45:12 +0900

    다시 나타난 HTTP_RAW_POST_DATA  문제 수정

--- classes/context/Context.class.php
+++ classes/context/Context.class.php
@@ -201,9 +201,12 @@ class Context
 	function init()
 	{
 		// fix missing HTTP_RAW_POST_DATA in PHP 5.6 and above
-		if(!isset($GLOBALS['HTTP_RAW_POST_DATA']) && version_compare(PHP_VERSION, '5.6.0', '>=') === true) {
+		if(!isset($GLOBALS['HTTP_RAW_POST_DATA']) && version_compare(PHP_VERSION, '5.6.0', '>=') === TRUE)
+		{
 			$GLOBALS['HTTP_RAW_POST_DATA'] = file_get_contents("php://input");
-			if(!preg_match('/^[<\{\[]/', $GLOBALS['HTTP_RAW_POST_DATA']))
+			
+			// If content is not XML JSON, unset
+			if(!preg_match('/^[\<\{\[]/', $GLOBALS['HTTP_RAW_POST_DATA']) && strpos($_SERVER['CONTENT_TYPE'], 'json') === FALSE && strpos($_SERVER['HTTP_CONTENT_TYPE'], 'json') === FALSE)
 			{
 				unset($GLOBALS['HTTP_RAW_POST_DATA']);
 			}
