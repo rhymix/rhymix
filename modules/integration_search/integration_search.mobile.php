64acd714e02764aa89c0c1a85d679e0ebcacacf1
MinSoo Kim <misol.kr@gmail.com>
2015-12-19 20:34:52 +0900

    Make be able to search on your mobile phone
    Enhance the search module to view mobile page, and add a default skin,
    sutable for mobile environment.

---
+++ modules/integration_search/integration_search.mobile.php
@@ -0,0 +1,21 @@
+<?php
+/* Copyright (C) XETOWN CMS <https://www.xetown.com> */
+/**
+ * The view class of the integration_search module
+ *
+ * @author MinSoo Kim <misol.kr@gmail.com>
+ */
+require_once(_XE_PATH_.'modules/board/board.view.php');
+
+class integration_searchMobile extends integration_searchView
+{
+	/**
+	 * Search Result
+	 *
+	 * @return Object
+	 */
+	function IS()
+	{
+		parent::IS();
+	}
+}
