e0402e101901db0a7ddcd244c807b117b1bb4c04
conory <conorycom@gmail.com>
2015-12-21 16:17:31 +0900

    php7를 위한 phpmailer 수정 (추후 phpmailer 업데이트를 하면 좋겠음)

--- libs/phpmailer/phpmailer.php
+++ libs/phpmailer/phpmailer.php
@@ -1724,10 +1724,10 @@ class PHPMailer {
 
     switch (strtolower($position)) {
       case 'phrase':
-        $encoded = preg_replace("/([^A-Za-z0-9!*+\/ -])/e", "'='.sprintf('%02X', ord('\\1'))", $encoded);
+		$encoded = preg_replace_callback('/([^A-Za-z0-9!*+\/ -])/', function($n) { return '='.sprintf('%02X', ord('\\1')); }, $encoded);
         break;
       case 'comment':
-        $encoded = preg_replace("/([\(\)\"])/e", "'='.sprintf('%02X', ord('\\1'))", $encoded);
+		$encoded = preg_replace_callback('/([\(\)\"])/', function($n) { return '='.sprintf('%02X', ord('\\1')); }, $encoded);
       case 'text':
       default:
         // Replace every high ascii, control =, ? and _ characters
