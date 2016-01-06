f92219f32860ff02e921daa72bd6bd6b6fa01929
Kijin Sung <kijin@kijinsung.com>
2016-01-06 11:25:23 +0900

    After install, allow login by both user_id and email_address by default

--- modules/install/tpl/js/install.js
+++ modules/install/tpl/js/install.js
@@ -6,4 +6,13 @@ jQuery(function($){
 			$("p.db_type_" + $(this).val()).show();
 		}).triggerHandler("click");
 	}
+	if($("input[name='user_id']").size() && $("input[name='email_address']").size()) {
+		var user_id_input = $("input[name='user_id']");
+		var email_input = $("input[name='email_address']");
+		email_input.on("blur", function() {
+			if (user_id_input.val() == "") {
+				user_id_input.val(email_input.val().replace(/@.+$/g, "").replace(/[^a-zA-Z0-9_]/g, ""));
+			}
+		});
+	}
 });
