8d6068ab2bdd48516e2c93abba4f6158a73874b9
Kijin Sung <kijin@kijinsung.com>
2016-01-09 21:04:51 +0900

    Immediately invalidate PHP scripts from opcache if modified or deleted

--- classes/cache/CacheFile.class.php
+++ classes/cache/CacheFile.class.php
@@ -78,10 +78,6 @@ class CacheFile extends CacheBase
 		$content[] = 'if(!defined(\'__XE__\')) { exit(); }';
 		$content[] = 'return \'' . addslashes(serialize($obj)) . '\';';
 		FileHandler::writeFile($cache_file, implode(PHP_EOL, $content));
-		if(function_exists('opcache_invalidate'))
-		{
-			@opcache_invalidate($cache_file, true);
-		}
 	}
 
 	/**
@@ -145,10 +141,6 @@ class CacheFile extends CacheBase
 	function _delete($_key)
 	{
 		$cache_file = $this->getCacheFileName($_key);
-		if(function_exists('opcache_invalidate'))
-		{
-			@opcache_invalidate($cache_file, true);
-		}
 		FileHandler::removeFile($cache_file);
 	}
 
