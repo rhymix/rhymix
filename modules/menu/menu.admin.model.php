1b6ccd69a33d2067511ae8d32cd51108f4ad4473
Kijin Sung <kijin@kijinsung.com>
2016-01-16 11:24:13 +0900

    Fix E_WARNING when handling an empty menu

--- modules/menu/menu.admin.model.php
+++ modules/menu/menu.admin.model.php
@@ -497,8 +497,19 @@ class menuAdminModel extends menu
 			$isMenuFixed = false;
 			$output = $this->getMenu($menuSrl);
 			$php_file = sprintf(_XE_PATH_ . 'files/cache/menu/%s.php',$output->menu_srl);
-			if(file_exists($php_file)) include($php_file);
-			else $oMenuAdminController->makeXmlFile($menuSrl);
+			if(file_exists($php_file))
+			{
+				include($php_file);
+			}
+			else 
+			{
+				$oMenuAdminController->makeXmlFile($menuSrl);
+			}
+			if(!$menu)
+			{
+				$menu = new stdClass;
+				$menu->list = array();
+			}
 
 			if(count($menu->list)>0)
 			{
@@ -545,6 +556,11 @@ class menuAdminModel extends menu
 						}
 
 						include($value->php_file);
+						if(!$menu)
+						{
+							$menu = new stdClass;
+							$menu->list = array();
+						}
 
 						$isMenuFixed = false;
 						if(count($menu->list) > 0)
