fed4f950bff954fab6c328176b8ce9fcdabfcfff
Kijin Sung <kijin@kijinsung.com>
2016-02-06 14:57:16 +0900

    Fix encoding of numeric arrays in XMLRPC->JSON conversion

--- classes/display/JSONDisplayHandler.php
+++ classes/display/JSONDisplayHandler.php
@@ -9,14 +9,53 @@ class JSONDisplayHandler
 	 * @param ModuleObject $oModule the module object
 	 * @return string
 	 */
-	function toDoc(&$oModule)
+	public function toDoc($oModule)
 	{
 		$variables = $oModule->getVariables();
 		$variables['error'] = $oModule->getError();
 		$variables['message'] = $oModule->getMessage();
+		
+		if (Context::getRequestMethod() === 'XMLRPC')
+		{
+			$temp = array();
+			foreach ($variables as $key => $value)
+			{
+				if (self::_isNumericArray($value))
+				{
+					$temp[$key] = array_values($value);
+				}
+				else
+				{
+					$temp[$key] = $value;
+				}
+			}
+			$variables = $temp;
+		}
+		
 		return json_encode($variables);
 	}
-
+	
+	/**
+	 * Check if an array only has numeric keys.
+	 * 
+	 * @param array $array
+	 * @return bool
+	 */
+	protected static function _isNumericArray($array)
+	{
+		if (!is_array($array) || !count($array))
+		{
+			return false;
+		}
+		foreach ($array as $key => $value)
+		{
+			if (intval($key) != $key)
+			{
+				return false;
+			}
+		}
+		return true;
+	}
 }
 /* End of file JSONDisplayHandler.class.php */
 /* Location: ./classes/display/JSONDisplayHandler.class.php */
