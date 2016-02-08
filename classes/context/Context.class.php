dcba0bec5873a27ad5bb1ac1527bda12acf73c10
Kijin Sung <kijin@kijinsung.com>
2016-02-08 22:25:56 +0900

    Change sitelock HTTP code to 503 Service Unavailable

--- classes/context/Context.class.php
+++ classes/context/Context.class.php
@@ -1442,7 +1442,7 @@ class Context
 		define('_XE_SITELOCK_TITLE_', config('lock.title'));
 		define('_XE_SITELOCK_MESSAGE_', config('lock.message'));
 		
-		header("HTTP/1.1 403 Forbidden");
+		header('HTTP/1.1 503 Service Unavailable');
 		if(FileHandler::exists(RX_BASEDIR . 'common/tpl/sitelock.user.html'))
 		{
 			include RX_BASEDIR . 'common/tpl/sitelock.user.html';
