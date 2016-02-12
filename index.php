48a570615a8bf136181b7d7676b18bab0da3bf14
Kijin Sung <kijin@kijinsung.com>
2016-02-12 14:15:06 +0900

    Do not catch all exceptions in index.php

--- index.php
+++ index.php
@@ -53,18 +53,9 @@ if($oContext->checkSSO())
 {
 	$oModuleHandler = new ModuleHandler();
 
-	try
+	if($oModuleHandler->init())
 	{
-		if($oModuleHandler->init())
-		{
-			$oModuleHandler->displayContent($oModuleHandler->procModule());
-		}
-	}
-	catch(Exception $e)
-	{
-		htmlHeader();
-		echo Context::getLang($e->getMessage());
-		htmlFooter();
+		$oModuleHandler->displayContent($oModuleHandler->procModule());
 	}
 }
 
