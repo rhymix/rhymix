55d7fe9df4f9cd2d53751b7c3be10509258cb9e3
qw5414 <sosifam@070805.co.kr>
2016-02-13 11:25:40 +0900

    라이믹스를 덮어 씌웠을때, 애드온이 남아있어도 작동을 하지 않도록 함.

---
+++ addons/member_communication/member_communication.addon.php
@@ -0,0 +1,6 @@
+<?php
+/* Copyright (C) NAVER <http://www.navercorp.com> */
+/* 이 애드온은 communication 모둘로 흡수 되었습니다. */
+
+/* End of file member_communication.addon.php */
+/* Location: ./addons/member_communication/member_communication.addon.php */
