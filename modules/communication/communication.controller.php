160129879a39d7129fb5c5f27279c112a94ad284
qw5414 <sosifam@070805.co.kr>
2016-02-18 16:49:15 +0900

    쪽지 발송시 이메일로 험깨 발송되는 콘텐츠의 필터링 보충 추가 (xe1.8.16)

--- modules/communication/communication.controller.php
+++ modules/communication/communication.controller.php
@@ -130,7 +130,7 @@ class communicationController extends communication
 			$view_url = Context::getRequestUri();
 			$content = sprintf("%s<br /><br />From : <a href=\"%s\" target=\"_blank\">%s</a>", $content, $view_url, $view_url);
 			$oMail = new Mail();
-			$oMail->setTitle($title);
+			$oMail->setTitle(htmlspecialchars($title, ENT_COMPAT | ENT_HTML401, 'UTF-8', false));
 			$oMail->setContent(utf8_mbencode(removeHackTag($content)));
 			$oMail->setSender($logged_info->nick_name, $logged_info->email_address);
 			$oMail->setReceiptor($receiver_member_info->nick_name, $receiver_member_info->email_address);
