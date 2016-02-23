e335aa477e7f35826fb90b25a691b6c5af19bf6b
Kijin Sung <kijin@kijinsung.com>
2016-02-23 21:37:44 +0900

    Initial implementation of blacklist function

---
+++ common/defaults/blacklist.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Rhymix Default Blacklist for Deprecated Plugins
+ * 
+ * Copyright (c) Rhymix Developers and Contributors
+ */
+return array(
+	'member_communication' => true,
+	'zipperupper' => true,
+);
