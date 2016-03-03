9290c5312225a18c9d54609b7c5f131deaf7deb1
conory <conorycom@gmail.com>
2016-03-03 19:46:59 +0900

    #327 언어파일 정리2

---
+++ layouts/xedition/lang/ko.php
@@ -0,0 +1,2 @@
+<?php
+$lang->cmd_xedition_search_close = '검색창 닫기';
---
+++ modules/editor/skins/ckeditor/lang/en.php
@@ -0,0 +1,4 @@
+<?php
+$lang->ckeditor_about_file_drop_area = 'Drag and drop your files here, or Click attach files button.';
+$lang->ckeditor_file_uploading = 'Uploading...';
+$lang->ckeditor_file_count = '<span class="file_count">0</span> file(s) attached';
