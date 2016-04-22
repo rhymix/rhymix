7cdb30f160d8384f840d9a72f3206d4a89337582
BJRambo <sosifam@070805.co.kr>
2016-04-22 21:52:00 +0900

    fixed engilsh Language

--- modules/ncenterlite/lang/en.php
+++ modules/ncenterlite/lang/en.php
@@ -1,6 +1,7 @@
 <?php
 $lang->ncenterlite = 'Notification Center Lite';
 $lang->ncenterlite_install_version = 'Installed version';
+$lang->ncenterlite_advenced_config = 'Advenced Setting';
 $lang->ncenterlite_document = 'document';
 $lang->ncenterlite_comment = 'comment';
 $lang->ncenterlite_type_message = 'message';
