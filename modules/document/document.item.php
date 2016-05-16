78cbaea1a4c3844b50bcdac66007ba0966bb6257
Kijin Sung <kijin@kijinsung.com>
2016-05-16 22:35:53 +0900

    Limit minimum original image size for thumbnails of external images
    cf. xpressengine/xe-core a4a54f9 by @bnu
    Exclude external images less than 30% of the thumbnail size
    
    TODO: Make the limit configurable

--- modules/document/document.item.php
+++ modules/document/document.item.php
@@ -980,6 +980,10 @@ class documentItem extends Object
 						if($is_img = @getimagesize($tmp_file))
 						{
 							list($_w, $_h, $_t, $_a) = $is_img;
+							if($_w < ($width * 0.3) && $_h < ($height * 0.3))
+							{
+								continue;
+							}
 						}
 						else
 						{
