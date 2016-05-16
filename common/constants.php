a7eb59bdf6b1f9dfb96a3f89b5b6ae7f63a7fded
Kijin Sung <kijin@kijinsung.com>
2016-05-16 22:38:11 +0900

    Version 1.8.20

--- common/constants.php
+++ common/constants.php
@@ -3,7 +3,7 @@
 /**
  * RX_VERSION is the version number of the Rhymix CMS.
  */
-define('RX_VERSION', '1.8.19');
+define('RX_VERSION', '1.8.20');
 
 /**
  * RX_MICROTIME is the startup time of the current script, in microseconds since the Unix epoch.
