88920bc9feeb85db6a851e774c8f53cfa62e0baa
MinSoo Kim <misol.kr@gmail.com>
2016-09-04 03:11:11 +0900

    네모의 꿈 레이아웃 프로젝트
    라이믹스도 또 하나의 네모일까?
    라이믹스 기본 레이아웃, 스킨 프로젝트 입니다.
    코드 중에 MIT 라이선스를 가지는 햄버거 메뉴 버튼 CSS 소스가 사용되었습니다.
    구글 안드로이드 색상 가이드라인의 색상을 이용하였습니다.

---
+++ layouts/simple_world/layout.js
@@ -0,0 +1,37 @@
+$(function() {
+	$('.layout_menu>ul>li:first-child .layout_dropdown-content, .layout_menu>ul>li:first-child .layout_dropdown-content a').css('width', $('.layout_menu>ul>li:first-child').width()).css('min-width', $('.layout_menu>ul>li:first-child').width());
+
+	"use strict";
+
+	var toggles = document.querySelectorAll(".layout_mobile_menu");
+
+	for (var i = toggles.length - 1; i >= 0; i--) {
+		var toggle = toggles[i];
+		layout_toggleHandler(toggle);
+	};
+
+	function layout_toggleMenuOpener(obj) {
+		if(obj.classList.contains("is-active") === true){
+			var targetMenu = $(obj).attr('data-target');
+			$('#' + targetMenu).slideUp('300', function() {
+				$(this).css('display', '')
+			});
+
+			obj.classList.remove("is-active");
+		}
+		else {
+			var targetMenu = $(obj).attr('data-target');
+			$('#' + targetMenu).slideDown('300');
+
+			obj.classList.add("is-active");
+		}
+	}
+
+	function layout_toggleHandler(toggle) {
+		toggle.addEventListener( "click", function(e) {
+			e.preventDefault();
+			layout_toggleMenuOpener(this);
+		});
+	}
+
+});
