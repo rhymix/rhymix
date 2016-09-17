07d53f3e7e54e4f924a3f2632df54b667365813f
Kijin Sung <kijin@kijinsung.com>
2016-09-17 20:24:21 +0900

    Fix incorrect config name (DisplayHandler uses view.use_gzip)

--- modules/admin/admin.admin.controller.php
+++ modules/admin/admin.admin.controller.php
@@ -713,7 +713,7 @@ class adminAdminController extends admin
 		Rhymix\Framework\Config::set('view.minify_scripts', $vars->minify_scripts ?: 'common');
 		Rhymix\Framework\Config::set('view.concat_scripts', $vars->concat_scripts ?: 'none');
 		Rhymix\Framework\Config::set('view.server_push', $vars->use_server_push === 'Y');
-		Rhymix\Framework\Config::set('view.gzip', $vars->use_gzip === 'Y');
+		Rhymix\Framework\Config::set('view.use_gzip', $vars->use_gzip === 'Y');
 		
 		// Save
 		Rhymix\Framework\Config::save();
