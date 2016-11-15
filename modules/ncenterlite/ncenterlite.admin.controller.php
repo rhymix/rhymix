c8dae083e641c4fd440a263e3c20b73070f113d1
BJRambo <sosifam@070805.co.kr>
2016-11-15 18:30:09 +0900

    코딩 컨벤션 수정

--- modules/ncenterlite/ncenterlite.admin.controller.php
+++ modules/ncenterlite/ncenterlite.admin.controller.php
@@ -26,7 +26,7 @@ class ncenterliteAdminController extends ncenterlite
 			'document_read',
 			'layout_srl',
 			'mlayout_srl',
-			'use_sms'
+			'use_sms',
 		);
 		
 		foreach($config_vars as $val)
