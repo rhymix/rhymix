09b020471f714a98c19d4cef2d7f35f580204544
MinSoo Kim <misol.kr@gmail.com>
2017-02-19 00:04:45 +0900

    이미지 제목 간단한 내용 항목의 스타일 설정.

--- widgets/content/skins/simple_rectangle/js/content_widget.js
+++ widgets/content/skins/simple_rectangle/js/content_widget.js
@@ -14,14 +14,14 @@ function content_widget_plus(obj,list_per_page){
 	});
 	if(total_page === page+1)
 	{
-		if(obj.is('ul.misolContent_list'))
+		if(obj.is('ul.simple_content_list'))
 		{
-			obj.next('div.misolContentNavigator').css('display','none');
+			obj.next('div.simple_content_nav').css('display','none');
 		}
 	}
 	if(total_page <= page) return;
-	if(obj.is('ul.misolContent_list')) {
-		jQuery('button.soo_cont_more>span.page_info',obj.next('div.misolContentNavigator')).html(page+1 + '/' + total_page);
+	if(obj.is('ul.simple_content_list')) {
+		jQuery('button.soo_cont_more>span.page_info',obj.next('div.simple_content_nav')).html(page+1 + '/' + total_page);
 	}
 	list.each(function(i){
 		if(((page+1) * list_per_page) > i){
@@ -31,64 +31,7 @@ function content_widget_plus(obj,list_per_page){
 		}
 	});
 }
-/*
-function content_widget_next(obj,list_per_page){
-	var page = 1;
-	if(obj.is('table')) {
-		var list = jQuery('>tbody>tr',obj);
-	}else if(obj.is('ul')) {
-		var list = jQuery('>li',obj);
-	}
 
-	var total_page = parseInt((list.size()-1) / list_per_page,10)+1;
-	list.each(function(i){
-		if(jQuery(this).css('display') !='none'){
-			page = parseInt(i/list_per_page,10) + 1;
-			return false;
-		}
-	});
-	if(total_page <= page) return;
-	if(obj.is('ul.misolContent_list')) {
-		jQuery('>li.pageId',obj.next('ul.misolContentNavigator')).html(page+1 + '/' + total_page);
-	}
-	list.each(function(i){
-		if( (page* list_per_page) <= i && ((page+1) * list_per_page) > i){
-			jQuery(this).show();
-		}else{
-			jQuery(this).hide();
-		}
-	});
-}
-
-function content_widget_prev(obj,list_per_page){
-	var page = 1;
-	if(obj.is('table')){
-		var list = jQuery('>tbody>tr',obj);
-	}else if(obj.is('ul')){
-		var list = jQuery('>li',obj);
-	}
-
-	var total_page = parseInt((list.size()-1) / list_per_page,10)+1;
-	list.each(function(i){
-		if(jQuery(this).css('display') !='none'){
-			page = parseInt(i/list_per_page,10)+1;
-			return false;
-		}
-	});
-
-	if(page <= 1) return;
-	if(obj.is('ul.misolContent_list')) {
-		jQuery('>li.pageId',obj.next('ul.misolContentNavigator')).html(page-1 + '/' + total_page);
-	}
-	list.each(function(i){
-		if( ((page-2)* list_per_page)<= i && ((page-1) * list_per_page) > i){
-			jQuery(this).show();
-		}else{
-			jQuery(this).hide();
-		}
-	});
-}
-*/
 function content_widget_tab_show(tab,list,i){
 	tab.parents('ul.widgetTab').children('li.active').removeClass('active');
 	tab.parent('li').addClass('active');
