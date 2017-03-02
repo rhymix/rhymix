0d2db40e02eb17e8d0c027cfef72ab0f74483c3d
Kijin Sung <kijin@kijinsung.com>
2017-03-02 16:35:35 +0900

    Don't double-subtract points when deleting document or comment with attached files

--- modules/point/point.controller.php
+++ modules/point/point.controller.php
@@ -265,16 +265,6 @@ class pointController extends point
 			$cur_point -= $document_point;
 		}
 		
-		// Subtract points for attached files.
-		if ($obj->uploaded_count > 0)
-		{
-			$attached_files_point = $this->_getModulePointConfig($module_srl, 'upload_file');
-			if ($attached_files_point > 0)
-			{
-				$cur_point -= $attached_files_point * $obj->uploaded_count;
-			}
-		}
-		
 		// Increase the point.
 		$this->setPoint($member_srl, $cur_point);
 		return new Object();
@@ -357,16 +347,6 @@ class pointController extends point
 		// Add points for the comment.
 		$comment_point = $this->_getModulePointConfig($module_srl, 'insert_comment');
 		$cur_point -= $comment_point;
-		
-		// Subtract points for attached files.
-		if ($obj->uploaded_count > 0)
-		{
-			$attached_files_point = $this->_getModulePointConfig($module_srl, 'upload_file');
-			if ($attached_files_point > 0)
-			{
-				$cur_point -= $attached_files_point * $obj->uploaded_count;
-			}
-		}
 		
 		// Increase the point.
 		$this->setPoint($member_srl, $cur_point);
