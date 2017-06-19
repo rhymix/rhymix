89881d38ddf468cb23e63e74432696a0c79ba9e4
BJRambo <qw5414@naver.com>
2017-06-19 16:04:39 +0900

    읽지않은 쪽지함에서 쪽지를 읽을 수 없는 문제 고침

--- modules/communication/communication.view.php
+++ modules/communication/communication.view.php
@@ -179,6 +179,48 @@ class communicationView extends communication
 			return $this->stop('msg_not_logged');
 		}
 
+		$logged_info = Context::get('logged_info');
+
+		// Set the variables
+		$message_srl = Context::get('message_srl');
+
+		// extract contents if message_srl exists
+		if($message_srl)
+		{
+			$columnList = array('message_srl', 'sender_srl', 'receiver_srl', 'message_type', 'title', 'content', 'readed', 'regdate');
+			$message = $oCommunicationModel->getSelectedMessage($message_srl, $columnList);
+
+			switch($message->message_type)
+			{
+				case 'R':
+					if($message->receiver_srl != $logged_info->member_srl)
+					{
+						return $this->stop('msg_invalid_request');
+					}
+					break;
+
+				case 'S':
+					if($message->sender_srl != $logged_info->member_srl)
+					{
+						return $this->stop('msg_invalid_request');
+					}
+					break;
+
+				case 'T':
+					if($message->receiver_srl != $logged_info->member_srl && $message->sender_srl != $logged_info->member_srl)
+					{
+						return $this->stop('msg_invalid_request');
+					}
+					break;
+			}
+
+			if($message->message_srl == $message_srl && ($message->receiver_srl == $logged_info->member_srl || $message->sender_srl == $logged_info->member_srl))
+			{
+				stripEmbedTagForAdmin($message->content, $message->sender_srl);
+				Context::set('message', $message);
+			}
+		}
+
 		$columnList = array('message_srl', 'readed', 'title', 'member.member_srl', 'member.nick_name', 'message.regdate', 'readed_date');
 		$output = $oCommunicationModel->getReadedMessages('N', $columnList);
 
