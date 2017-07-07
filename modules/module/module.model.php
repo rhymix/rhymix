47433841a8f5d49459a14796397efe4b79fd2a03
BJRambo <qw5414@naver.com>
2017-07-07 19:07:42 +0900

    Create default object in ModuleModel::loadSkinInfo

--- modules/module/module.model.php
+++ modules/module/module.model.php
@@ -1268,8 +1268,7 @@ class moduleModel extends module
 
 					foreach($extra_vars as $var)
 					{
-						unset($obj);
-						unset($options);
+						$options = array();
 
 						$group = $group->title->body;
 						$name = $var->attrs->name;
@@ -1283,12 +1282,14 @@ class moduleModel extends module
 
 							for($i = 0; $i < $option_count; $i++)
 							{
+								$options[$i] = new stdClass();
 								$options[$i]->title = $var->default[$i]->body;
 								$options[$i]->value = $var->default[$i]->body;
 							}
 						}
 						else
 						{
+							$options[0] = new stdClass();
 							$options[0]->title = $var->default->body;
 							$options[0]->value = $var->default->body;
 						}
@@ -1296,7 +1297,7 @@ class moduleModel extends module
 						$width = $var->attrs->width;
 						$height = $var->attrs->height;
 
-						unset($obj);
+						$obj = new stdClass();
 						$obj->group = $group;
 						$obj->title = $title;
 						$obj->description = $description;
