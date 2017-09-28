7b1f0b4ee2b6b4ff8af7ab93e7e748ba6dc4c884
Kijin Sung <kijin@kijinsung.com>
2017-09-28 11:34:15 +0900

    Fix #916 show error earlier if attempting to add self as friend

--- modules/communication/communication.controller.php
+++ modules/communication/communication.controller.php
@@ -456,9 +456,13 @@ class communicationController extends communication
 		$logged_info = Context::get('logged_info');
 
 		$target_srl = (int) trim(Context::get('target_srl'));
-		if(!$target_srl || $target_srl == $logged_info->member_srl)
+		if(!$target_srl)
 		{
 			return new Object(-1, 'msg_invalid_request');
+		}
+		if($target_srl == $logged_info->member_srl)
+		{
+			return new Object(-1, 'msg_no_self_friend');
 		}
 		
 		// Check duplicate friend
