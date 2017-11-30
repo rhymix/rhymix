xpressengine/xe-core

fix #2177 XEVE-17-023 회원의 이메일을 회원 정보에서 노출하지 않도록 변경 - 모듈 업데이트를 통해 기존 설정이 변경됩니다 - 회원의 이메일 주소를 공개하도록 설정할 수 없도록 합니다 - 최고 관리자에게는 회원 정보보기에서 이메일 주소를 확인할 수 있습니다

modules/member/member.class.php
@@ -218,6 +218,14 @@ class member extends ModuleObject {
 			// check signup form ordering info
 			if(!$config->signupForm) return true;
 
+			foreach($config->signupForm as $form)
+			{
+				if($form->name === 'email_address' && $form->isPublic !== 'N')
+				{
+					return true;
+				}
+			}
+
 			// check agreement field exist
 			if($config->agreement) return true;
 
@@ -348,13 +356,24 @@ class member extends ModuleObject {
 			// check signup form ordering info
 			if(!$config->signupForm || !is_array($config->signupForm))
 			{
-				$identifier = 'user_id';
+				$identifier = 'email_address';
 
 				$config->signupForm = $oMemberAdminController->createSignupForm($identifier);
 				$config->identifier = $identifier;
 				unset($config->agreement);
-				$output = $oModuleController->updateModuleConfig('member', $config);
 			}
+
+			// 회원정보에서 이메일 노출 제거
+			// @see https://github.com/xpressengine/xe-core/issues/2177
+			foreach($config->signupForm as $form)
+			{
+				if($form->name === 'email_address')
+				{
+					$form->isPublic = 'N';
+					break;
+				}
+			}
+			$oModuleController->updateModuleConfig('member', $config);
 
 			if($config->skin)
 			{

rhymix/rhymix

modules/member/member.class.php
@@ -177,6 +177,7 @@ class member extends ModuleObject {
 	{
 		$oDB = &DB::getInstance();
 		$oModuleModel = getModel('module');
+
 		// check member directory (11/08/2007 added)
 		if(!is_dir("./files/member_extra_info")) return true;
 		// check member directory (22/10/2007 added)
@@ -216,7 +217,14 @@ class member extends ModuleObject {
 		if(!$config->signupForm || !is_array($config->signupForm)) return true;
 		foreach($config->signupForm as $signupItem)
 		{
-			if($signupItem->name === 'find_account_question') return true;
+			if($signupItem->name === 'find_account_question')
+			{
+				return true;
+			}
+			if($signupItem->name === 'email_address' && $signupItem->isPublic !== 'N')
+			{
+				return true;
+			}
 		}
 		if(!$config->agreements)
 		{
@@ -357,7 +365,11 @@ class member extends ModuleObject {
 				$config->identifier = $config->identifier ?: 'user_id';
 				$config->signupForm = $oMemberAdminController->createSignupForm($config->identifier);
 				$output = $oModuleController->updateModuleConfig('member', $config);
-				break;
+			}
+			if($signupItem->name === 'email_address' && $signupItem->isPublic !== 'N')
+			{
+				$signupItem->isPublic = 'N';
+				$output = $oModuleController->updateModuleConfig('member', $config);
 			}
 		}
 		if(!$config->agreements)