a0d688df9de1bec18475687222bbd96d4214bb3d
Kijin Sung <kijin@kijinsung.com>
2018-07-20 18:56:43 +0900

    unicode 문자를 이용한 닉네임 중복 우회 문제 고침
    xpressengine/xe-core#2025

--- modules/member/member.controller.php
+++ modules/member/member.controller.php
@@ -914,7 +914,7 @@ class memberController extends member
 		{
 			if(isset($args->{$val}))
 			{
-				$args->{$val} = preg_replace('/[\pZ\pC]+/u', '', $args->{$val});
+				$args->{$val} = preg_replace('/[\pZ\pC]+/u', '', html_entity_decode($args->{$val}));
 			}
 		}
 
