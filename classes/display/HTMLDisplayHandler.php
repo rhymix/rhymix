39e0c31de2b594f8cef60d0d34ee1df235daed91
Kijin Sung <kijin@kijinsung.com>
2020-10-04 16:51:35 +0900

    Revert "Remove unnecessary class="x" in mobile page edit screen #1416"
    This reverts commit 9cd715008b524bde5f2242bdaffb0a80cf01beac.

--- classes/display/HTMLDisplayHandler.php
+++ classes/display/HTMLDisplayHandler.php
@@ -99,12 +99,10 @@ class HTMLDisplayHandler
 		// add .x div for adminitration pages
 		if(Context::getResponseMethod() == 'HTML')
 		{
-			/*
 			if(Context::get('module') != 'admin' && strpos(Context::get('act'), 'Admin') > 0 && Context::get('act') != 'dispPageAdminContentModify' && Context::get('act') != 'dispPageAdminMobileContentModify')
 			{
 				$output = '<div class="x">' . $output . '</div>';
 			}
-			*/
 
 			if(Context::get('layout') != 'none')
 			{
