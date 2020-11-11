rhymix/rhymix

commit e9bec851f980ee65976d7ea959eff34b04942826

Fix forms with empty actions

classes/display/HTMLDisplayHandler.php
@@ -204,7 +204,7 @@ class HTMLDisplayHandler
 		// handles a relative path generated by using the rewrite module
 		if(Context::isAllowRewrite())
 		{
-			$pattern = '/(action|src|href)=(["\'])\.\/([^"\']*)(["\'])/s';
+			$pattern = '/(action|src|href)=(["\'])(?:\.\/([^"\']*))?(["\'])/s';
 			$output = preg_replace($pattern, '$1=$2' . \RX_BASEURL . '$3$4', $output);
 
 			$pattern = '/src=(["\'])((?:files\/(?:attach|cache|faceOff|member_extra_info|thumbnails)|addons|common|(?:m\.)?layouts|modules|widgets|widgetstyle)\/[^"\']+)(["\'])/s';

commit 2c1d380a0a49f3ff2a9db74f338782f39b26945f

Fix #1458 unnecessary slash in script concatenation (regression from e9bec85)

classes/display/HTMLDisplayHandler.php
@@ -223,7 +223,10 @@ class HTMLDisplayHandler
 		// handles a relative path generated by using the rewrite module
 		if(Context::isAllowRewrite())
 		{
-			$pattern = '/(action|src|href)=(["\'])(?:\.\/([^"\']*))?(["\'])/s';
+			$pattern = '/(action)=(["\'])(["\'])/s';
+			$output = preg_replace($pattern, '$1=$2' . \RX_BASEURL . '$3', $output);
+
+			$pattern = '/(action|src|href)=(["\'])\.\/([^"\']*)(["\'])/s';
 			$output = preg_replace($pattern, '$1=$2' . \RX_BASEURL . '$3$4', $output);
 
 			$pattern = '/src=(["\'])((?:files\/(?:attach|cache|faceOff|member_extra_info|thumbnails)|addons|common|(?:m\.)?layouts|modules|widgets|widgetstyle)\/[^"\']+)(["\'])/s';