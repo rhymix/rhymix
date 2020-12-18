f38e90b60ed7ac284f5fe748e944fa08dead6367
Kijin Sung <kijin@kijinsung.com>
2020-12-18 15:06:28 +0900

    Remove unnecessary condition for CUBRID DB

--- modules/admin/admin.admin.controller.php
+++ modules/admin/admin.admin.controller.php
@@ -147,14 +147,6 @@ class adminAdminController extends admin
 			}
 		}
 
-		// remove duplicate indexes (only for CUBRID)
-		$db_type = Context::getDBType();
-		if($db_type == 'cubrid')
-		{
-			$db = DB::getInstance();
-			$db->deleteDuplicateIndexes();
-		}
-
 		// check autoinstall packages
 		$oAutoinstallAdminController = getAdminController('autoinstall');
 		$oAutoinstallAdminController->checkInstalled();
