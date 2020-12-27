3ff91018cc5ad371d71368ecb7c5940e912d0d87
Min-Soo Kim <misol.kr@gmail.com>
2020-12-27 21:14:51 +0900

    Add unit test + 통합검색 키워드 글자 수 제한 변경

--- modules/integration_search/integration_search.view.php
+++ modules/integration_search/integration_search.view.php
@@ -115,9 +115,9 @@ class integration_searchView extends integration_search
 		// Set a variable for search keyword
 		$is_keyword = Context::get('is_keyword');
 		$is_keyword = escape(trim(utf8_normalize_spaces($is_keyword)));
-		if (mb_strlen($is_keyword, 'UTF-8') > 40)
+		if (mb_strlen($is_keyword, 'UTF-8') > 250)
 		{
-			$is_keyword = mb_substr($is_keyword, 0, 40);
+			$is_keyword = mb_substr($is_keyword, 0, 250);
 		}
 
 		// Set page variables
