4dba37b7aa524c4e67ac70578d33ca09695d0424
BJRambo <qw5414@naver.com>
2021-03-04 16:42:21 +0900

    중복될 수 있는 클래스명을 수정

--- modules/sociallogin/sociallogin.model.php
+++ modules/sociallogin/sociallogin.model.php
@@ -323,12 +323,12 @@ class SocialloginModel extends Sociallogin
 		$snsList = self::getUseSNSList();
 		
 		$buff = [];
-		$buff[] = '<ul class="sns_login">';
+		$buff[] = '<ul class="sociallogin_login">';
 		$signString = ($type === 'signup') ? 'Sign up' : 'Sign in';
 		foreach ($snsList as $key => $sns)
 		{
 			$ucfirstName = ucfirst($sns->name);
-			$buff[] = "<li><div class=\"sns_{$sns->name}\">";
+			$buff[] = "<li><div class=\"sociallogin_{$sns->name}\">";
 			$buff[] = "<a class=\"loginBtn\" href=\"{$sns->auth_url}\"><span class=\"icon\"></span>";
 			$buff[] = "<span class=\"buttonText\"> {$signString} with {$ucfirstName}</span>";
 			$buff[] = '</a></div></li>';
