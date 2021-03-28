23b967647f760429ebe41ec484963c1eb1f027b6
Kangjun Heo == P.Knowledge <knowledge@o.cnu.ac.kr>
2021-03-28 20:41:33 +0900

    PHP8에서 session.auto_start 체크가 안되는 경우
    intval로 변환하여 0이 나오도록한 뒤 0과 strict type check

--- modules/install/install.controller.php
+++ modules/install/install.controller.php
@@ -320,9 +320,9 @@ class installController extends install
 		$license_agreement_time = intval(trim(FileHandler::readFile($this->flagLicenseAgreement)));
 		if(isset($_SESSION['license_agreement']) && (!$license_agreement_time || ($license_agreement_time == $_SESSION['license_agreement'])))
 		{
-			$sess_autostart = ini_get('session.auto_start');
+			$sess_autostart = intval(ini_get('session.auto_start'));
 			
-			if($sess_autostart === 0 || $sess_autostart === "")
+			if($sess_autostart === 0)
 			{
 				$checklist['session'] = true;
 			}
