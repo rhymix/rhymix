39477109c772206b83e0cca3b1def5df77d510df
Kijin Sung <kijin@kijinsung.com>
2021-10-19 21:26:27 +0900

    Use composer/ca-bundle instead of shipping our own copy of cacert.pem

--- classes/context/Context.class.php
+++ classes/context/Context.class.php
@@ -286,8 +286,8 @@ class Context
 		}
 		
 		// Load certificate authorities for curl and openssl.
-		ini_set('curl.cainfo', RX_BASEDIR . 'common/libraries/cacert.pem');
-		ini_set('openssl.cafile', RX_BASEDIR . 'common/libraries/cacert.pem');
+		ini_set('curl.cainfo', RX_BASEDIR . 'vendor/composer/ca-bundle/res/cacert.pem');
+		ini_set('openssl.cafile', RX_BASEDIR . 'vendor/composer/ca-bundle/res/cacert.pem');
 		
 		// Load language support.
 		$enabled_langs = self::loadLangSelected();
