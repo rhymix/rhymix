0dd567bc00084a528db7ea5e22c24afd97a4485f
conory <conorycom@gmail.com>
2022-08-04 11:06:23 +0900

    Add an option to check the permissions of modules linked to documents or comments

--- modules/comment/comment.item.php
+++ modules/comment/comment.item.php
@@ -188,11 +188,20 @@ class commentItem extends BaseObject
 		}
 	}
 
-	function isAccessible()
+	function isAccessible($strict = false)
 	{
 		if(!$this->isExists())
 		{
 			return false;
+		}
+		
+		if ($strict)
+		{
+			$grant = ModuleModel::getGrant(ModuleModel::getModuleInfoByModuleSrl($this->get('module_srl')), Context::get('logged_info'));
+			if (isset($grant->list) && isset($grant->view) && ($grant->list !== true || $grant->view !== true))
+			{
+				return false;
+			}
 		}
 		
 		if (isset($_SESSION['accessible'][$this->comment_srl]) && $_SESSION['accessible'][$this->comment_srl] === $this->get('last_update'))
