7493fe14fcf5f57d688186a407a43d7412417c5f
Kijin Sung <kijin@kijinsung.com>
2022-10-30 17:08:57 +0900

    Preload datepicker lang file and always apply dateFormat defaults (fix #1985)
    영어를 예외처리하지 않도록 하여, 영어에서만 날짜 포맷이 다르게 지정되는 문제 수정

--- common/js/plugins/ui/rx_datepicker.js
+++ common/js/plugins/ui/rx_datepicker.js
@@ -2,12 +2,11 @@
  * @brief Load i18n file of datepicker
  * @author MinSoo Kim <misol.kr@gmail.com>
  **/
-if(typeof current_lang !== "undefined" && current_lang !== 'en') {
-	jQuery.getScript(request_uri + "common/js/plugins/ui/i18n/datepicker-"+ current_lang.replace("jp", "ja") +".js", function() {
-		var default_option = {
-			dateFormat:'yy-mm-dd'
-		};
-		jQuery.extend(jQuery.datepicker.regional[current_lang.replace("jp", "ja")],default_option);
-		jQuery.datepicker.setDefaults( jQuery.datepicker.regional[current_lang.replace("jp", "ja")] );
-	});
-}
+(function($) {
+	var datepicker_lang = (typeof current_lang !== 'undefined') ? current_lang.replace('jp', 'ja') : 'en';
+	var default_options = {
+		dateFormat: 'yy-mm-dd'
+	};
+	$.extend(jQuery.datepicker.regional[datepicker_lang], default_options);
+	$.datepicker.setDefaults(jQuery.datepicker.regional[datepicker_lang]);
+})(jQuery);
