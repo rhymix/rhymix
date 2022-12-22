1c1098111340d4eecd8b05229271fbb6a4ebb2bd
Kijin Sung <kijin@kijinsung.com>
2022-12-23 02:04:11 +0900

    Remove Object alias for BaseObject

--- classes/object/Object.class.php
+++ classes/object/Object.class.php
@@ -324,13 +324,5 @@ class BaseObject
 	}
 }
 
-/**
- * Alias to Object for backward compatibility.
- */
-if (version_compare(PHP_VERSION, '7.2', '<'))
-{
-	class_alias('BaseObject', 'Object');
-}
-
 /* End of file Object.class.php */
 /* Location: ./classes/object/Object.class.php */
