c8467fb588b5ac792263e3695bdccca81b343d87
Kijin Sung <kijin@kijinsung.com>
2023-08-30 23:23:34 +0900

    Migrate board module triggers to new format using module.xml

--- modules/board/board.class.php
+++ modules/board/board.class.php
@@ -33,38 +33,7 @@ class Board extends ModuleObject
 	 **/
 	function moduleInstall()
 	{
-		// use action forward(enabled in the admin model)
-		$oModuleController = getController('module');
-		$oModuleModel = getModel('module');
 
-		// 2007. 10. 17 insert member menu trigger
-		$oModuleController->insertTrigger('member.getMemberMenu', 'board', 'controller', 'triggerMemberMenu', 'after');
-
-		// install board module
-		$args = new stdClass;
-		$args->site_srl = 0;
-		$output = executeQuery('module.getSite', $args);
-		if(!$output->data->index_module_srl)
-		{
-			$args->mid = 'board';
-			$args->module = 'board';
-			$args->browser_title = 'Rhymix';
-			$args->skin = 'default';
-			$args->site_srl = 0;
-			$output = $oModuleController->insertModule($args);
-
-			if($output->toBool())
-			{
-				$module_srl = $output->get('module_srl');
-
-				$site_args = new stdClass;
-				$site_args->site_srl = 0;
-				$site_args->index_module_srl = $module_srl;
-
-				$oModuleController = getController('module');
-				$oModuleController->updateSite($site_args);
-			}
-		}
 	}
 
 	/**
@@ -72,14 +41,7 @@ class Board extends ModuleObject
 	 **/
 	function checkUpdate()
 	{
-		$oModuleModel = getModel('module');
 
-		// 2007. 10. 17 get the member menu trigger
-		if(!$oModuleModel->getTrigger('member.getMemberMenu', 'board', 'controller', 'triggerMemberMenu', 'after')) return true;
-
-		// 2011. 09. 20 when add new menu in sitemap, custom menu add
-		if(!$oModuleModel->getTrigger('menu.getModuleListInSitemap', 'board', 'model', 'triggerModuleListInSitemap', 'after')) return true;
-		return false;
 	}
 
 	/**
@@ -87,20 +49,7 @@ class Board extends ModuleObject
 	 **/
 	function moduleUpdate()
 	{
-		$oModuleModel = getModel('module');
-		$oModuleController = getController('module');
 
-		// 2007. 10. 17  check the member menu trigger, if it is not existed then insert
-		if(!$oModuleModel->getTrigger('member.getMemberMenu', 'board', 'controller', 'triggerMemberMenu', 'after'))
-		{
-			$oModuleController->insertTrigger('member.getMemberMenu', 'board', 'controller', 'triggerMemberMenu', 'after');
-		}
-
-		// 2011. 09. 20 when add new menu in sitemap, custom menu add
-		if(!$oModuleModel->getTrigger('menu.getModuleListInSitemap', 'board', 'model', 'triggerModuleListInSitemap', 'after'))
-		{
-			$oModuleController->insertTrigger('menu.getModuleListInSitemap', 'board', 'model', 'triggerModuleListInSitemap', 'after');
-		}
 	}
 
 	function moduleUninstall()
