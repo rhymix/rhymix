eb2883b8cbc2ca8f4103340a9d2cd59492a364f0
Kijin Sung <kijin@kijinsung.com>
2023-10-02 22:00:06 +0900

    Move communication module triggers to module.xml

--- modules/communication/communication.class.php
+++ modules/communication/communication.class.php
@@ -8,26 +8,12 @@
  */
 class Communication extends ModuleObject
 {
-	private $triggers = array(
-		array('moduleHandler.init', 'communication', 'controller', 'triggerModuleHandlerBefore', 'before'),
-		array('member.getMemberMenu', 'communication', 'controller', 'triggerMemberMenu', 'before')
-	);
-	private $delete_triggers = array(
-		array('moduleObject.proc', 'communication', 'controller', 'triggerModuleProcAfter', 'after')
-	);
 	/**
 	 * Implement if additional tasks are necessary when installing
 	 * @return Object
 	 */
 	function moduleInstall()
 	{
-		$oModuleController = getController('module');
-
-		foreach($this->triggers as $trigger)
-		{
-			$oModuleController->insertTrigger($trigger[0], $trigger[1], $trigger[2], $trigger[3], $trigger[4]);
-		}
-
 		// Create a temporary file storage for one new private message notification
 		FileHandler::makeDir('./files/member_extra_info/new_message_flags');
 	}
@@ -38,25 +24,6 @@ class Communication extends ModuleObject
 	 */
 	function checkUpdate()
 	{
-		$oModuleModel = getModel('module');
-
-		foreach($this->triggers as $trigger)
-		{
-			if(!$oModuleModel->getTrigger($trigger[0], $trigger[1], $trigger[2], $trigger[3], $trigger[4]))
-			{
-				return TRUE;
-			}
-		}
-
-		foreach($this->delete_triggers as $trigger)
-		{
-
-			if($oModuleModel->getTrigger($trigger[0], $trigger[1], $trigger[2], $trigger[3], $trigger[4]))
-			{
-				return TRUE;
-			}
-		}
-
 		if(!is_dir("./files/member_extra_info/new_message_flags"))
 		{
 			FileHandler::makeDir('./files/member_extra_info/new_message_flags');
@@ -75,40 +42,11 @@ class Communication extends ModuleObject
 	 */
 	function moduleUpdate()
 	{
-		$oModuleModel = getModel('module');
-		$oModuleController = getController('module');
-
-		foreach($this->triggers as $trigger)
-		{
-			if(!$oModuleModel->getTrigger($trigger[0], $trigger[1], $trigger[2], $trigger[3], $trigger[4]))
-			{
-				$oModuleController->insertTrigger($trigger[0], $trigger[1], $trigger[2], $trigger[3], $trigger[4]);
-			}
-		}
-
-		foreach($this->delete_triggers as $trigger)
-		{
-			if($oModuleModel->getTrigger($trigger[0], $trigger[1], $trigger[2], $trigger[3], $trigger[4]))
-			{
-				$oModuleController->deleteTrigger($trigger[0], $trigger[1], $trigger[2], $trigger[3], $trigger[4]);
-			}
-		}
-
 		if(!is_dir("./files/member_extra_info/new_message_flags"))
 		{
 			FileHandler::makeDir('./files/member_extra_info/new_message_flags');
 		}
 	}
-
-	/**
-	 * Re-generate the cache file
-	 * @return void
-	 */
-	function recompileCache()
-	{
-
-	}
-
 }
 /* End of file communication.class.php */
 /* Location: ./modules/comment/communication.class.php */
