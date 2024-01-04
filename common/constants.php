94e2d8e11a8a989034507a14c18aa197545baf07
Kijin Sung <kijin@kijinsung.com>
2024-01-04 10:47:52 +0900

    Version 2.1.11

--- common/constants.php
+++ common/constants.php
@@ -3,7 +3,7 @@
 /**
  * RX_VERSION is the version number of the Rhymix CMS.
  */
-define('RX_VERSION', '2.1.10');
+define('RX_VERSION', '2.1.11');
 
 /**
  * RX_MICROTIME is the startup time of the current script, in microseconds since the Unix epoch.
