0a459eb528fa2dd308f4b96bf614b86464bb87f8
Kijin Sung <kijin@kijinsung.com>
2024-05-04 20:52:51 +0900

    Mark default category in bold #2335

--- modules/document/tpl/js/document_category.js
+++ modules/document/tpl/js/document_category.js
@@ -6,10 +6,10 @@
 
 function Tree(category_module_srl) {
 
-    // clear tree;
-    $('#menu > ul > li > ul').remove();
+	// clear tree;
+	$('#menu > ul > li > ul').remove();
 
-    if($("ul.simpleTree > li > a").size() == 0){
+	if($("ul.simpleTree > li > a").size() == 0){
 		$('<a href="#__category_info" class="add modalAnchor"><img src="' + default_url + 'common/js/plugins/ui.tree/images/iconAdd.gif" /></a>')
 			.bind('before-open.mw', function(e){
 				addNode(0,e);
@@ -18,60 +18,60 @@ function Tree(category_module_srl) {
 			.xeModalWindow();
 	}
 
-    //ajax get data and transeform ul il
-    exec_json('document.getDocumentCategoryTree', { module_srl: category_module_srl }, function(data) {
-        var callback;
+	//ajax get data and transeform ul il
+	exec_json('document.getDocumentCategoryTree', { module_srl: category_module_srl }, function(data) {
+		var callback;
 		callback = function(item) {
-            var text = item.text;
-            var node_srl = item.node_srl;
-            var parent_srl = item.parent_srl;
-            var color = item.color;
-            var url = item.url;
+			var text = item.text;
+			var node_srl = item.node_srl;
+			var parent_srl = item.parent_srl;
+			var color = item.color;
+			var is_default = item.is_default;
+			var url = item.url;
 
-            // node
-            var node = '';
-            if(color && color !='transparent'){
-                node = $('<li id="tree_'+node_srl+'"></li>');
-				var $span = $('<span></span>').css('color', color).text(text);
-				node.append($span);
-            }else{
-                node = $('<li id="tree_'+node_srl+'"></li>');
-				var $span = $('<span></span>').text(text);
-				node.append($span);
-            }
+			// node
+			var node = $('<li id="tree_'+node_srl+'"></li>');
+			var title = $('<span></span>').text(text);
+			if (color && color !='transparent') {
+				title.css('color', color);
+			}
+			if (is_default == 'Y') {
+				title.css('font-weight', 'bold');
+			}
+			node.append(title);
 
-            // button
-            $('<a href="#__category_info" class="add modalAnchor"><img src="' + default_url + 'common/js/plugins/ui.tree/images/iconAdd.gif" /></a>').bind("click",function(e){
-                $("#tree_"+node_srl+" > span").click();
+			// button
+			$('<a href="#__category_info" class="add modalAnchor"><img src="' + default_url + 'common/js/plugins/ui.tree/images/iconAdd.gif" /></a>').bind("click",function(e){
+				$("#tree_"+node_srl+" > span").click();
 			})
 			.bind('before-open.mw', function(e){
-                addNode(node_srl,e);
+				addNode(node_srl,e);
 			})
-            .appendTo(node)
-			.xeModalWindow();
-
-            $('<a href="#__category_info" class="modify modalAnchor"><img src="' + default_url + 'common/js/plugins/ui.tree/images/iconModify.gif" /></a>').bind("click",function(e){
-                $("#tree_"+node_srl+" > span").click();
-			})
-			.bind('before-open.mw', function(e){
-                modifyNode(node_srl,e);
-            })
 			.appendTo(node)
 			.xeModalWindow();
 
-            $('<a href="#" class="delete"><img src="' + default_url + 'common/js/plugins/ui.tree/images/iconDel.gif" /></a>').bind("click",function(e){
-                deleteNode(node_srl);
-                return false;
-            }).appendTo(node);
+			$('<a href="#__category_info" class="modify modalAnchor"><img src="' + default_url + 'common/js/plugins/ui.tree/images/iconModify.gif" /></a>').bind("click",function(e){
+				$("#tree_"+node_srl+" > span").click();
+			})
+			.bind('before-open.mw', function(e){
+				modifyNode(node_srl,e);
+			})
+			.appendTo(node)
+			.xeModalWindow();
 
-            // insert parent child
-            if(parent_srl>0){
-                if($('#tree_'+parent_srl+'>ul').length==0) $('#tree_'+parent_srl).append($('<ul>'));
-                $('#tree_'+parent_srl+'> ul').append(node);
-            }else{
-                if($('#menu ul.simpleTree > li > ul').length==0) $("<ul>").appendTo('#menu ul.simpleTree > li');
-                $('#menu ul.simpleTree > li > ul').append(node);
-            }
+			$('<a href="#" class="delete"><img src="' + default_url + 'common/js/plugins/ui.tree/images/iconDel.gif" /></a>').bind("click",function(e){
+				deleteNode(node_srl);
+				return false;
+			}).appendTo(node);
+
+			// insert parent child
+			if(parent_srl>0){
+				if($('#tree_'+parent_srl+'>ul').length==0) $('#tree_'+parent_srl).append($('<ul>'));
+				$('#tree_'+parent_srl+'> ul').append(node);
+			}else{
+				if($('#menu ul.simpleTree > li > ul').length==0) $("<ul>").appendTo('#menu ul.simpleTree > li');
+				$('#menu ul.simpleTree > li > ul').append(node);
+			}
 
 			// look for children
 			if (item.list) {
@@ -85,68 +85,68 @@ function Tree(category_module_srl) {
 			callback(item);
 		});
 
-        //button show hide
-        $("#menu li").each(function(){
-            if($(this).parents('ul').size() > max_menu_depth) $("a.add",this).hide();
-            if($(">ul",this).size()>0) $(">a.delete",this).hide();
-        });
+		//button show hide
+		$("#menu li").each(function(){
+			if($(this).parents('ul').size() > max_menu_depth) $("a.add",this).hide();
+			if($(">ul",this).size()>0) $(">a.delete",this).hide();
+		});
 
 
-        // draw tree
-        simpleTreeCollection = $('.simpleTree').simpleTree({
-            autoclose: false,
-            afterClick:function(node){
-                $('#category_info').html("");
-                //alert("text-"+jQuery('span:first',node).text());
-            },
-            afterDblClick:function(node){
-                //alert("text-"+jQuery('span:first',node).text());
-            },
-            afterMove:function(destination, source, pos){
-                if(destination.size() == 0){
-                    Tree(category_module_srl);
-                    return;
-                }
-                var module_srl = $("#fo_category input[name=module_srl]").val();
-                var parent_srl = destination.attr('id').replace(/.*_/g,'');
-                var source_srl = source.attr('id').replace(/.*_/g,'');
+		// draw tree
+		simpleTreeCollection = $('.simpleTree').simpleTree({
+			autoclose: false,
+			afterClick:function(node){
+				$('#category_info').html("");
+				//alert("text-"+jQuery('span:first',node).text());
+			},
+			afterDblClick:function(node){
+				//alert("text-"+jQuery('span:first',node).text());
+			},
+			afterMove:function(destination, source, pos){
+				if(destination.size() == 0){
+					Tree(category_module_srl);
+					return;
+				}
+				var module_srl = $("#fo_category input[name=module_srl]").val();
+				var parent_srl = destination.attr('id').replace(/.*_/g,'');
+				var source_srl = source.attr('id').replace(/.*_/g,'');
 
-                var target = source.prevAll("li:not([class^=line])");
-                var target_srl = 0;
-                if(target.length >0){
-                    target_srl = source.prevAll("li:not([class^=line])").get(0).id.replace(/.*_/g,'');
-                    parent_srl = 0;
-                }
+				var target = source.prevAll("li:not([class^=line])");
+				var target_srl = 0;
+				if(target.length >0){
+					target_srl = source.prevAll("li:not([class^=line])").get(0).id.replace(/.*_/g,'');
+					parent_srl = 0;
+				}
 
-                $.exec_json("document.procDocumentMoveCategory",{ "module_srl":module_srl,"parent_srl":parent_srl,"target_srl":target_srl,"source_srl":source_srl},
-                function(data){
-                    $('#category_info').html('');
-                   if(data.error > 0) Tree(category_module_srl);
-                });
+				$.exec_json("document.procDocumentMoveCategory",{ "module_srl":module_srl,"parent_srl":parent_srl,"target_srl":target_srl,"source_srl":source_srl},
+				function(data){
+					$('#category_info').html('');
+				   if(data.error > 0) Tree(category_module_srl);
+				});
 
-            },
+			},
 
-            // i want you !! made by sol
-            beforeMovedToLine : function(destination, source, pos){
-                return ($(destination).parents('ul').size() + jQuery('ul',source).size() <= max_menu_depth);
-            },
+			// i want you !! made by sol
+			beforeMovedToLine : function(destination, source, pos){
+				return ($(destination).parents('ul').size() + jQuery('ul',source).size() <= max_menu_depth);
+			},
 
-            // i want you !! made by sol
-            beforeMovedToFolder : function(destination, source, pos){
-                return ($(destination).parents('ul').size() + jQuery('ul',source).size() <= max_menu_depth-1);
-            },
-            afterAjax:function()
-            {
-                //alert('Loaded');
-            },
-            animate:true
-            ,docToFolderConvert:true
-        });
+			// i want you !! made by sol
+			beforeMovedToFolder : function(destination, source, pos){
+				return ($(destination).parents('ul').size() + jQuery('ul',source).size() <= max_menu_depth-1);
+			},
+			afterAjax:function()
+			{
+				//alert('Loaded');
+			},
+			animate:true
+			,docToFolderConvert:true
+		});
 
-        // open all node
-        nodeToggleAll();
+		// open all node
+		nodeToggleAll();
 
-    });
+	});
 }
 
 function clearValue(){
@@ -220,37 +220,37 @@ function modifyNode(node,e){
 
 
 function nodeToggleAll(){
-    jQuery("[class*=close]", simpleTreeCollection[0]).each(function(){
-        simpleTreeCollection[0].nodeToggle(this);
-    });
+	jQuery("[class*=close]", simpleTreeCollection[0]).each(function(){
+		simpleTreeCollection[0].nodeToggle(this);
+	});
 }
 
 function deleteNode(node){
-    if(confirm(lang_confirm_delete)){
-        jQuery('#category_info').html("");
-        var params ={
-                "category_srl":node
-                ,"parent_srl":0
-                ,"module_srl":jQuery("#fo_category [name=module_srl]").val()
-                };
+	if(confirm(lang_confirm_delete)){
+		jQuery('#category_info').html("");
+		var params ={
+				"category_srl":node
+				,"parent_srl":0
+				,"module_srl":jQuery("#fo_category [name=module_srl]").val()
+				};
 
-        exec_json('document.procDocumentDeleteCategory', params, function(data){
-            if(data.error==0) Tree(category_module_srl);
-        });
-    }
+		exec_json('document.procDocumentDeleteCategory', params, function(data){
+			if(data.error==0) Tree(category_module_srl);
+		});
+	}
 }
 
 /* 카테고리 아이템 입력후 */
 function completeInsertCategory(ret_obj) {
-    jQuery('#category_info').html("");
-    Tree(category_module_srl);
+	jQuery('#category_info').html("");
+	Tree(category_module_srl);
 }
 
 function hideCategoryInfo() {
-    jQuery('#category_info').html("");
+	jQuery('#category_info').html("");
 }
 
 /* 카테고리 목록 갱신 */
 function doReloadTreeCategory(module_srl) {
-    exec_json('document.procDocumentMakeXmlFile', { module_srl: module_srl }, completeInsertCategory);
+	exec_json('document.procDocumentMakeXmlFile', { module_srl: module_srl }, completeInsertCategory);
 }
