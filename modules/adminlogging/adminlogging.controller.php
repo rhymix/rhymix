25564913b85beb0e5fae82805be8e1026514246c
Kijin Sung <kijin@kijinsung.com>
2024-11-16 22:36:37 +0900

    Record member_srl in admin_log table, and change request_vars format to JSON #2421

--- modules/adminlogging/adminlogging.controller.php
+++ modules/adminlogging/adminlogging.controller.php
@@ -31,23 +31,22 @@ class adminloggingController extends adminlogging
 	 * Insert log
 	 * @return void
 	 */
-	function insertLog($module, $act)
+	public function insertLog($module, $act)
 	{
-		if(!$module || !$act)
+		if (!$module || !$act)
 		{
 			return;
 		}
 
 		$args = new stdClass();
+		$args->member_srl = $this->user->member_srl;
 		$args->module = $module;
 		$args->act = $act;
-		$args->ipaddress = \RX_CLIENT_IP;
+		$args->request_vars = json_encode(Context::getRequestVars(), JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
 		$args->regdate = date('YmdHis');
-		$args->requestVars = print_r(Context::getRequestVars(), TRUE);
-
+		$args->ipaddress = \RX_CLIENT_IP;
 		$output = executeQuery('adminlogging.insertLog', $args);
 	}
-
 }
 /* End of file adminlogging.controller.php */
 /* Location: ./modules/adminlogging/adminlogging.controller.php */
