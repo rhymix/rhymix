0b0b8f58a36b6cad1fc2cea77ae1b27419a6a8bf
Kijin Sung <kijin@kijinsung.com>
2025-03-23 20:58:47 +0900

    Remove unnecessary method overload

--- modules/integration_search/integration_search.mobile.php
+++ modules/integration_search/integration_search.mobile.php
@@ -1,20 +1,6 @@
 <?php
-/* Copyright (C) Rhymix <https://rhymix.org> */
-/**
- * The view class of the integration_search module
- *
- * @author Rhymix Developers and Contributors <devops@rhymix.org>
- */
 
 class integration_searchMobile extends integration_searchView
 {
-	/**
-	 * Search Result
-	 *
-	 * @return Object
-	 */
-	function IS()
-	{
-		parent::IS();
-	}
+
 }
