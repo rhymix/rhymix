f29080bb62cfc4afa7166c6929b717fc5ef44ada
Kijin Sung <kijin@kijinsung.com>
2025-06-14 16:35:46 +0900

    Disable autocomplete and autocapitalize for password extravar input

--- modules/extravar/skins/default/form_types/password.blade.php
+++ modules/extravar/skins/default/form_types/password.blade.php
@@ -2,6 +2,7 @@
 	id="{{ $input_id }}"|if="$input_id" class="password rx_ev_password"
 	style="{{ $definition->style }}"|if="$definition->style"
 	value="{{ strval($value) !== '' ? $value : strval($default) }}"
+	autocomplete="off" autocapitalize="none"
 	@required(toBool($definition->is_required))
 	@disabled(toBool($definition->is_disabled))
 	@readonly(toBool($definition->is_readonly))
