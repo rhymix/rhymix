e586552b299876b859fc38e2a5a66bcf6a52a0d3
Kijin Sung <kijin@kijinsung.com>
2025-11-22 21:02:26 +0900

    Update PHP install script for unit test, now that PHP 8.5 is official

--- .github/workflows/setup-php.sh
+++ .github/workflows/setup-php.sh
@@ -4,47 +4,27 @@
 sudo add-apt-repository -y ppa:ondrej/php
 
 # Install all required packages
-if [[ "$1" == "8.5" ]]; then
-  sudo apt -y install \
-    php$1-bcmath \
-    php$1-cli \
-    php$1-common \
-    php$1-curl \
-    php$1-gd \
-    php$1-intl \
-    php$1-mbstring \
-    php$1-mysql \
-    php$1-readline \
-    php$1-sqlite3 \
-    php$1-xml \
-    php$1-zip
-else
-  sudo apt -y install \
-    php$1-apcu \
-    php$1-bcmath \
-    php$1-cli \
-    php$1-common \
-    php$1-curl \
-    php$1-gd \
-    php$1-intl \
-    php$1-mbstring \
-    php$1-mysql \
-    php$1-opcache \
-    php$1-readline \
-    php$1-sqlite3 \
-    php$1-xml \
-    php$1-zip
-fi
+sudo apt -y install \
+	php$1-apcu \
+	php$1-bcmath \
+	php$1-cli \
+	php$1-common \
+	php$1-curl \
+	php$1-gd \
+	php$1-intl \
+	php$1-mbstring \
+	php$1-mysql \
+	php$1-readline \
+	php$1-sqlite3 \
+	php$1-xml \
+	php$1-zip
 
 # Adjust php.ini settings
-if [[ "$1" == "8.5" ]]; then
-  sudo bash -c "echo 'register_argc_argv = On' >> /etc/php/$1/cli/php.ini"
-else
-  sudo bash -c "echo 'opcache.enable = 1' >> /etc/php/$1/cli/conf.d/10-opcache.ini"
-  sudo bash -c "echo 'opcache.enable_cli = 1' >> /etc/php/$1/cli/conf.d/10-opcache.ini"
-  sudo bash -c "echo 'opcache.jit = tracing' >> /etc/php/$1/cli/conf.d/10-opcache.ini"
-  sudo bash -c "echo 'opcache.jit_buffer_size = 128M' >> /etc/php/$1/cli/conf.d/10-opcache.ini"
-fi
+sudo bash -c "echo 'register_argc_argv = On' >> /etc/php/$1/cli/php.ini"
+sudo bash -c "echo 'opcache.enable = 1' >> /etc/php/$1/cli/conf.d/10-opcache.ini"
+sudo bash -c "echo 'opcache.enable_cli = 1' >> /etc/php/$1/cli/conf.d/10-opcache.ini"
+sudo bash -c "echo 'opcache.jit = tracing' >> /etc/php/$1/cli/conf.d/10-opcache.ini"
+sudo bash -c "echo 'opcache.jit_buffer_size = 128M' >> /etc/php/$1/cli/conf.d/10-opcache.ini"
 
 # Enable APCu
 if [ -f "/etc/php/$1/cli/conf.d/20-apcu.ini" ]; then
