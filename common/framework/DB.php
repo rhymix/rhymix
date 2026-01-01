d6b7cb52b88301c4298bcbf4bce8cb5c70951273
Kijin Sung <kijin@kijinsung.com>
2026-01-01 18:39:20 +0900

    Fix deprecations in PHP 8.5 #2639

--- common/framework/DB.php
+++ common/framework/DB.php
@@ -127,8 +127,17 @@ class DB
 			\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
 			\PDO::ATTR_EMULATE_PREPARES => false,
 			\PDO::ATTR_STATEMENT_CLASS => array('\Rhymix\Framework\Helpers\DBStmtHelper'),
-			\PDO::MYSQL_ATTR_USE_BUFFERED_QUERY => false,
 		);
+
+		// Use unbuffered queries to reduce memory usage.
+		if (\PHP_VERSION_ID >= 80400)
+		{
+			$options[\PDO\MySQL::ATTR_USE_BUFFERED_QUERY] = false;
+		}
+		else
+		{
+			$options[\PDO::MYSQL_ATTR_USE_BUFFERED_QUERY] = false;
+		}
 
 		// Preload the statement helper class.
 		class_exists('\Rhymix\Framework\Helpers\DBStmtHelper');
