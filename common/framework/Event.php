91306aa5960b21be16889e5070b25799f8370d0f
Kijin Sung <kijin@kijinsung.com>
2026-01-14 21:56:19 +0900

    Fix missing timestamp if event handler throws

--- common/framework/Event.php
+++ common/framework/Event.php
@@ -111,6 +111,7 @@ class Event implements EventDispatcherInterface, ListenerProviderInterface
 			catch (Exception $e)
 			{
 				$output = new BaseObject(-2, $e->getMessage());
+				$after_time = microtime(true);
 			}
 
 			if (Debug::isEnabledForCurrentUser())
