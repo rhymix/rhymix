90f1238b23d75817b4088c0ecfadd6f0384ffefd
Kijin Sung <kijin@kijinsung.com>
2026-03-31 22:28:39 +0900

    Use DocumentModel::getBlankDocument() to obtain a dummy document with module_srl in BoardView

--- modules/board/board.view.php
+++ modules/board/board.view.php
@@ -318,8 +318,7 @@ class BoardView extends Board
 				{
 					if (abs($oDocument->get('member_srl')) != $this->user->member_srl)
 					{
-						$oDocument = DocumentModel::getDocument(0);
-						$oDocument->add('module_srl', $this->module_srl);
+						$oDocument = DocumentModel::getBlankDocument($this->module_srl);
 						Context::set('document_srl', null, true);
 						$this->dispBoardMessage('msg_not_founded', 404);
 					}
@@ -328,8 +327,7 @@ class BoardView extends Board
 				// if the document is TEMP saved, pretend that it doesn't exist.
 				if($oDocument->getStatus() == 'TEMP')
 				{
-					$oDocument = DocumentModel::getDocument(0);
-					$oDocument->add('module_srl', $this->module_srl);
+					$oDocument = DocumentModel::getBlankDocument($this->module_srl);
 					Context::set('document_srl', null, true);
 					$this->dispBoardMessage('msg_not_founded', 404);
 				}
@@ -347,8 +345,7 @@ class BoardView extends Board
 		 */
 		else
 		{
-			$oDocument = DocumentModel::getDocument(0);
-			$oDocument->add('module_srl', $this->module_srl);
+			$oDocument = DocumentModel::getBlankDocument($this->module_srl);
 		}
 
 		/**
@@ -358,8 +355,7 @@ class BoardView extends Board
 		{
 			if(!$this->grant->view && !$oDocument->isGranted())
 			{
-				$oDocument = DocumentModel::getDocument(0);
-				$oDocument->add('module_srl', $this->module_srl);
+				$oDocument = DocumentModel::getBlankDocument($this->module_srl);
 				Context::set('document_srl', null, true);
 				$this->dispBoardMessage($this->user->isMember() ? 'msg_not_permitted' : 'msg_not_logged');
 			}
