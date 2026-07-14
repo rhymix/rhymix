ae32ba52af04b61b930f10edf926e6711b295332
Kijin Sung <kijin@kijinsung.com>
2026-07-15 00:16:18 +0900

    Use same condition as elsewhere to check for non-API request types

--- common/framework/AbstractController.php
+++ common/framework/AbstractController.php
@@ -357,7 +357,7 @@ abstract class AbstractController extends BaseObject
 			// Set layout and template paths if module configuration specifies them.
 			if (isset($this->module_info->skin) && $this->module_info->module === $this->module && strpos($this->act, 'Admin') === false)
 			{
-				if (!\RX_POST || Context::getRequestMethod() === 'HTML')
+				if (!in_array(Context::getRequestMethod(), ['JSON', 'XMLRPC', 'JS_CALLBACK']))
 				{
 					$use_default_skin = $this->module_info->{$is_mobile ? 'is_mskin_fix' : 'is_skin_fix'} === 'N';
 					if (!$this->getTemplatePath() || $use_default_skin)
