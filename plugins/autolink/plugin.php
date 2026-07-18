735a71198a4ec2d530f76e253a57353aee891539
Kijin Sung <kijin@kijinsung.com>
2026-07-18 17:25:26 +0900

    Remove unnecessary condition from autolink plugin

--- plugins/autolink/plugin.php
+++ plugins/autolink/plugin.php
@@ -3,15 +3,14 @@
 namespace Rhymix\Plugins\Autolink;
 
 use Rhymix\Framework\AbstractPlugin;
-use Rhymix\Framework\Responses\HTMLResponse;
 use Context;
 
 class Plugin extends AbstractPlugin
 {
 	public function __construct(object $config)
 	{
-		$this->after('moduleObject.proc', function($oModule) {
-			if (Context::getResponseMethod() === 'HTML' || (isset($oModule->response) && $oModule->response instanceof HTMLResponse))
+		$this->after('moduleObject.proc', function() {
+			if (Context::getResponseMethod() === 'HTML')
 			{
 				Context::loadFile(['./plugins/autolink/autolink.js', 'body']);
 			}
