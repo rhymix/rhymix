fd2fe9b481d40194763b50d12ede7e739d95277e
Kijin Sung <kijin@kijinsung.com>
2026-09-22 00:06:31 +0900

    Disable header and footer text editing by board managers who are not admins (backport from v2.2)

--- modules/admin/tpl/js/admin.js
+++ modules/admin/tpl/js/admin.js
@@ -409,6 +409,7 @@ jQuery(function($){
 			})
 			.click(function(){
 				var $this = $(this), $modal, $btnClose, disabled;
+				if($this.data('is_readonly')) return;
 
 				// get and initialize modal window
 				$modal = $( $this.attr('href') );
@@ -1962,6 +1963,17 @@ jQuery(function($){
 				$this.hide();
 				$setter.attr('href', '#g11n').xeModalWindow();
 
+				if ($this.prop('disabled')) {
+					$displayInput.prop('disabled', true);
+					$remover.data('is_readonly', true);
+					$setter.data('is_readonly', true);
+				}
+				if ($this.prop('readonly')) {
+					$displayInput.prop('readonly', true);
+					$remover.data('is_readonly', true);
+					$setter.data('is_readonly', true);
+				}
+
 				// bind selected
 				$displayInput.bind('selected.g11n', function(e, code, value){
 					$displayInput
@@ -1998,6 +2010,7 @@ jQuery(function($){
 					var $g11n_set_input = $('#lang_' + $this.data('lang-target'));
 
 					if(!$g11n_set_input.data('active')) return;
+					if($this.data('is_readonly')) return;
 
 					$g11n_set_input
 						.val('')
